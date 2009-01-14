239056c762d4ebd41041e6d6ebaabe9c09efae86
Balazs Scheidler <bazsi@balabit.hu>
2009-01-14 17:47:16 +0100

    [test_logqueue] added threaded testcase for logqueue
    	This is currently commented out as some synchronization would be needed
    	between the threads, but as the current problem is now solved,
    	and the synchronization is not trivial, I leave it commented out for
    	now.

--- tests/unit/test_logqueue.c
+++ tests/unit/test_logqueue.c
@@ -115,6 +115,91 @@ testcase_zero_diskbuf_alternating_send_acks()
   log_queue_free(q);
 }
 
+#if 0
+
+/* no synchronization between the feed/consume threads, therefore it does
+ * not succeed reliably. commented out for now, will fix at the next
+ * logqueue related threaded issue */
+
+GStaticMutex threaded_lock = G_STATIC_MUTEX_INIT;
+
+gpointer
+threaded_feed(gpointer st)
+{
+  LogQueue *q = (LogQueue *) st;
+  char *msg_str = "<155>2006-02-11T10:34:56+01:00 bzorp syslog-ng[23323]: árvíztűrőtükörfúrógép";
+  gint i;
+  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
+  LogMessage *msg;
+
+  for (i = 0; i < 100000; i++)
+    {
+      msg = log_msg_new(msg_str, strlen(msg_str), g_sockaddr_inet_new("10.10.10.10", 1010), 0, NULL, -1);
+      log_msg_add_ack(msg, &path_options);
+      msg->ack_func = test_ack;
+
+      g_static_mutex_lock(&threaded_lock);
+      if (!log_queue_push_tail(q, msg, &path_options))
+        {
+          fprintf(stderr, "Queue unable to consume enough messages: %d\n", fed_messages);
+          return GUINT_TO_POINTER(1);
+        }
+      g_static_mutex_unlock(&threaded_lock);
+    }
+  return NULL;
+}
+
+gpointer
+threaded_consume(gpointer st)
+{
+  LogQueue *q = (LogQueue *) st;
+  LogMessage *msg;
+  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
+  gboolean success;
+  gint i;
+
+  for (i = 0; i < 100000; i++)
+    {
+      g_static_mutex_lock(&threaded_lock);
+      msg = NULL;
+      success = log_queue_pop_head(q, &msg, &path_options, FALSE);
+      g_static_mutex_unlock(&threaded_lock);
+
+      g_assert(!success || (success && msg != NULL));
+      if (!success)
+        {
+          fprintf(stderr, "Queue didn't return enough messages: i=%d\n", i);
+          return GUINT_TO_POINTER(1);
+        }
+
+      log_msg_ack(msg, &path_options);
+      log_msg_unref(msg);
+    }
+
+  return NULL;
+}
+
+void
+testcase_with_threads()
+{
+  LogQueue *q;
+  GThread *thread_feed, *thread_consume;
+  gint i;
+
+  for (i = 0; i < 100; i++)
+    {
+      q = log_queue_new(100000, 0, 64);
+      thread_feed = g_thread_create(threaded_feed, q, TRUE, NULL);
+
+      thread_consume = g_thread_create(threaded_consume, q, TRUE, NULL);
+      g_thread_join(thread_feed);
+      g_thread_join(thread_consume);
+
+      log_queue_free(q);
+    }
+}
+#endif
+
 int 
 main()
 {
