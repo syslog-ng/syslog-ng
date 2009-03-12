b44bacd3fd121c8cd0b166c7794ba68fe5cea9ee
Balazs Scheidler <bazsi@balabit.hu>
2009-03-12 15:02:09 +0100

    [loggen] report the effective bandwidth used at the end of the run

--- tests/loggen/loggen.c
+++ tests/loggen/loggen.c
@@ -207,7 +207,7 @@ gen_messages(int sock)
   gettimeofday(&now, NULL);
   diff_usec = time_val_diff_in_usec(&now, &start);
   time_val_diff_in_timeval(&diff_tv, &now, &start);
-  fprintf(stderr, "average rate = %.2lf msg/sec, count=%ld, time=%ld.%03ld\n", (double) count * USEC_PER_SEC / diff_usec, count, diff_tv.tv_sec, diff_tv.tv_usec / 1000);
+  fprintf(stderr, "average rate = %.2lf msg/sec, count=%ld, time=%ld.%03ld, msg size=%d, bandwidth=%.2lf kB/sec\n", (double) count * USEC_PER_SEC / diff_usec, count, diff_tv.tv_sec, diff_tv.tv_usec / 1000, linelen, (double) count * linelen * (USEC_PER_SEC / 1024) / diff_usec);
 }
 
 void
