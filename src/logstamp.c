70d758fe40ad64f78e28e87b629c54fbd1fdc09e
Balazs Scheidler <bazsi@balabit.hu>
2009-04-01 10:00:35 +0200

    [LogStamp] make it possible to use frac_digits with non-ISO timestamps

--- src/logstamp.c
+++ src/logstamp.c
@@ -25,6 +25,26 @@
 #include "messages.h"
 #include "timeutils.h"
 
+static void
+log_stamp_append_frac_digits(LogStamp *stamp, GString *target, gint frac_digits)
+{
+  glong usecs;
+
+  usecs = stamp->time.tv_usec % 1000000;
+
+  if (frac_digits > 0)
+    {
+      gulong x;
+
+      g_string_append_c(target, '.');
+      for (x = 100000; frac_digits && x; x = x / 10)
+        {
+          g_string_append_c(target, (usecs / x) + '0');
+          usecs = usecs % x;
+          frac_digits--;
+        }
+    }
+}
 
 /** 
  * log_stamp_format:
@@ -43,7 +63,6 @@ log_stamp_append_format(LogStamp *stamp, GString *target, gint ts_format, glong 
   struct tm *tm, tm_storage;
   char buf[8];
   time_t t;
-  glong usecs;
   
   if (zone_offset != -1)
     target_zone_offset = zone_offset;
@@ -65,31 +84,21 @@ log_stamp_append_format(LogStamp *stamp, GString *target, gint ts_format, glong 
         {
         case TS_FMT_BSD:
           g_string_append_printf(target, "%s %2d %02d:%02d:%02d", month_names_abbrev[tm->tm_mon], tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+          log_stamp_append_frac_digits(stamp, target, frac_digits);
           break;
         case TS_FMT_ISO:
           g_string_append_printf(target, "%d-%02d-%02dT%02d:%02d:%02d", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
-          usecs = stamp->time.tv_usec % 1000000;
-          
-          if (frac_digits > 0)
-            {
-              gulong x;
-              
-              g_string_append_c(target, '.');
-              for (x = 100000; frac_digits && x; x = x / 10)
-                {
-                  g_string_append_c(target, (usecs / x) + '0'); 
-                  usecs = usecs % x;
-                  frac_digits--;
-                }
-            }
+          log_stamp_append_frac_digits(stamp, target, frac_digits);
           format_zone_info(buf, sizeof(buf), target_zone_offset);
           g_string_append(target, buf);
           break;
         case TS_FMT_FULL:
           g_string_append_printf(target, "%d %s %2d %02d:%02d:%02d", tm->tm_year + 1900, month_names_abbrev[tm->tm_mon], tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+          log_stamp_append_frac_digits(stamp, target, frac_digits);
           break;
         case TS_FMT_UNIX:
           g_string_append_printf(target, "%d", (int) stamp->time.tv_sec);
+          log_stamp_append_frac_digits(stamp, target, frac_digits);
           break;
         default:
           g_assert_not_reached();
