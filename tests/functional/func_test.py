b6e9ea617567b9da300adfc8b2462a6bf5efd892
Balazs Scheidler <bazsi@balabit.hu>
2009-04-30 22:41:23 +0200

    [func_test] added some basic performance testing
    Run loggen against a simple syslog-ng configuration. The baseline still
    has to be found, we currently require 1000 msg/sec everywhere, an 10000
    on my development box.
    
    I plan to specify more exact numbers for all of our build hosts.

--- tests/functional/func_test.py
+++ tests/functional/func_test.py
@@ -681,8 +681,40 @@ def test_wildcard_files():
         return False
     return True
 
+performance_conf = """@version: 3.0
+
+options { use_dns(no); };
+
+source s_int { internal(); };
+source s_tcp { tcp(port(%(port_number)d)); };
+
+destination d_messages { file("test-performance.log"); };
+
+log { source(s_tcp); destination(d_messages); };
+
+""" % locals()
+
+def test_performance():
+    expected_rate = {
+      'bzorp': 10000
+    }
+    print_user("Starting loggen for 10 seconds")
+    out = os.popen("../loggen/loggen --rate 1000000 --inet --stream --size 120 --interval 10 127.0.0.1 %d 2>&1 |tail -1" % port_number, 'r').read()
+
+    print_user("performane: %s" % out)
+    rate = float(re.sub('^.*rate = ([0-9.]+).*$', '\\1', out))
+
+    hostname = os.uname()[1]
+    if expected_rate.has_key(hostname):
+        return rate > expected_rate[hostname]
+
+    # we expect to be able to process at least 1000 msgs/sec even on our venerable HP-UX
+    return rate > 1000
 
 tests = (
+  (performance_conf, (
+     test_performance,
+  )),
   (drvtest_conf, (
      test_input_drivers,
      test_catchall,
