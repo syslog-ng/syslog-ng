9f8651bad14a38a73c6edd247a5b4053bb47575a
Balazs Scheidler <bazsi@balabit.hu>
2010-08-03 14:38:20 +0200

    main: fixed compilation problem if ENABLE_SUN_DOOR_MODULE is not defined

--- src/main.c
+++ src/main.c
@@ -339,7 +339,6 @@ version(void)
          "Enable-GProf: %s\n"
          "Enable-Memtrace: %s\n"
          "Enable-Sun-STREAMS: %s\n"
-         "Enable-Sun-Door: %s\n"
          "Enable-IPv6: %s\n"
          "Enable-Spoof-Source: %s\n"
          "Enable-TCP-Wrapper: %s\n"
@@ -354,7 +353,6 @@ version(void)
          ON_OFF_STR(ENABLE_GPROF),
          ON_OFF_STR(ENABLE_MEMTRACE),
          ON_OFF_STR(ENABLE_SUN_STREAMS_MODULE),
-         ON_OFF_STR(ENABLE_SUN_DOOR_MODULE),
          ON_OFF_STR(ENABLE_IPV6),
          ON_OFF_STR(ENABLE_SPOOF_SOURCE),
          ON_OFF_STR(ENABLE_TCP_WRAPPER),
