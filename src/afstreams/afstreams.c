105f3008066bbf1ed3256b77aff1e8309e219244
Balazs Scheidler <bazsi@balabit.hu>
2010-08-03 12:55:19 +0200

    afstreams: use LogProtoDGramServer instead of the old LogProtoPlainServer
    This patch follows up the refactoring of LogProto modularization which
    was missed last time as no Solaris compilation took place. But better late
    than never.

--- src/afstreams/afstreams.c
+++ src/afstreams/afstreams.c
@@ -185,7 +185,7 @@ afstreams_sd_init(LogPipe *s)
           return FALSE;
         }
       g_fd_set_nonblock(fd, TRUE);
-      self->reader = log_reader_new(log_proto_plain_new_server(log_transport_streams_new(fd), 0, self->reader_options.msg_size, LPPF_PKTTERM));
+      self->reader = log_reader_new(log_proto_dgram_server_new(log_transport_streams_new(fd), self->reader_options.msg_size, 0));
       log_reader_set_options(self->reader, s, &self->reader_options, 1, SCS_SUN_STREAMS, self->super.id, self->dev_filename->str);
       log_pipe_append(self->reader, s);
 
