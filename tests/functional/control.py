794bb6c046e048fbea32fc1b30b820631090c1bf
Balazs Scheidler <bazsi@balabit.hu>
2010-09-27 15:51:39 +0200

    functest: use the syslog-ng binary from the syslog-ng directory instead of src

--- tests/functional/control.py
+++ tests/functional/control.py
@@ -35,7 +35,7 @@ def start_syslogng(conf, keep_persist=False, verbose=False):
         for (root, dirs, files) in os.walk(os.path.abspath(os.path.join(os.environ['top_builddir'], 'modules'))):
             module_path = ':'.join(map(lambda x: root + '/' + x, dirs))
             break
-        rc = os.execl('../../src/syslog-ng', '../../src/syslog-ng', '-f', 'test.conf', '--fd-limit', '1024', '-F', verbose_opt, '-p', 'syslog-ng.pid', '-R', 'syslog-ng.persist', '--no-caps', '--enable-core', '--seed', '--module-path', module_path)
+        rc = os.execl('../../syslog-ng/syslog-ng', '../../syslog-ng/syslog-ng', '-f', 'test.conf', '--fd-limit', '1024', '-F', verbose_opt, '-p', 'syslog-ng.pid', '-R', 'syslog-ng.persist', '--no-caps', '--enable-core', '--seed', '--module-path', module_path)
         sys.exit(rc)
     time.sleep(5)
     print_user("Syslog-ng started")
