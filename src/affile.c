17531d911d544687fb9c5bd3b130dd5bf7903db0
Balazs Scheidler <bazsi@balabit.hu>
2011-01-01 20:56:23 +0100

    affile: fixed bogus permission bits on logfiles on platforms where mode_t is 16 bits only
    syslog-ng is creating files with bogus permissions on platforms where mode_t
    is 16 bits only. Such a beast is kFreeBSD.
    
    Credits to the Debian folks for noticing & tracking down the issue.
    The original reporter is in the "Reported-By" line, but also thanks for
    gcs@debian.org, algernon@debian.org and peter.czanik@balabit.hu for
    taking care of the issue.
    
    Reported-By: Steven Chamberlain <steven@pyro.eu.org>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- src/affile.c
+++ src/affile.c
@@ -605,7 +605,7 @@ affile_dd_set_file_gid(LogDriver *s, const gchar *file_gid)
 }
 
 void 
-affile_dd_set_file_perm(LogDriver *s, mode_t file_perm)
+affile_dd_set_file_perm(LogDriver *s, gint file_perm)
 {
   AFFileDestDriver *self = (AFFileDestDriver *) s;
   
@@ -641,7 +641,7 @@ affile_dd_set_dir_gid(LogDriver *s, const gchar *dir_gid)
 }
 
 void 
-affile_dd_set_dir_perm(LogDriver *s, mode_t dir_perm)
+affile_dd_set_dir_perm(LogDriver *s, gint dir_perm)
 {
   AFFileDestDriver *self = (AFFileDestDriver *) s;
   
@@ -971,9 +971,9 @@ affile_dd_new(gchar *filename, guint32 flags)
   self->filename_template = log_template_new(NULL, filename);
   self->flags = flags;
   self->file_uid = self->file_gid = -1;
-  self->file_perm = (mode_t) -1;
+  self->file_perm = -1;
   self->dir_uid = self->dir_gid = -1;
-  self->dir_perm = (mode_t) -1;
+  self->dir_perm = -1;
   log_writer_options_defaults(&self->writer_options);
   if (strchr(filename, '$') == NULL)
     {
--- src/afunix.h
+++ src/afunix.h
@@ -40,7 +40,7 @@ typedef struct _AFUnixSourceDriver
 
 void afunix_sd_set_uid(LogDriver *self, gchar *owner);
 void afunix_sd_set_gid(LogDriver *self, gchar *group);
-void afunix_sd_set_perm(LogDriver *self, mode_t perm);
+void afunix_sd_set_perm(LogDriver *self, gint perm);
 
 LogDriver *afunix_sd_new(gchar *filename, guint32 flags);
 
