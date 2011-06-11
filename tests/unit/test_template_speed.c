7f0eaa1bf2398a9e3c59a28be9d825cdee581bee
Balazs Scheidler <bazsi@balabit.hu>
2011-06-11 19:10:26 +0200

    test_template_speed: load the basicfuncs plugin
    This unit test used the "echo" template function, but that wasn't available,
    as it didn't get loaded, thus the results for the $(echo $MSG) test
    was bogus until now.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- tests/unit/test_template_speed.c
+++ tests/unit/test_template_speed.c
@@ -89,6 +89,7 @@ main(int argc G_GNUC_UNUSED, char *argv[] G_GNUC_UNUSED)
   tzset();
 
   plugin_load_module("syslogformat", configuration, NULL);
+  plugin_load_module("basicfuncs", configuration, NULL);
   msg_format_options_defaults(&parse_options);
   msg_format_options_init(&parse_options, configuration);
 
