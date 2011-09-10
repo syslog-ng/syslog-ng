7eaa1d4b2429447619e7458457f4deafd46a855c
Gergely Nagy <algernon@madhouse-project.org>
2011-09-10 13:30:13 +0200

    debian: Add a script to update to a new upstream version.
    The new script will update the control file, and libsyslog-ng's
    debhelper control files. It's usage is documented in
    debian/README.source.

---
+++ debian/tools/update-control.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+set -e
+
+if [ "$#" -lt 1 ]; then
+	cat <<EOF
+Usage: $0 UPSTREAM_VERSION
+EOF
+	exit 1
+fi
+
+UPSTREAM_VERSION="$1"
+UPSTREAM_VERSION_MAJOR="$(echo "$1" | cut -d "." -f 1,2)"
+
+# Update debian/control
+sed -e "s,@UPSTREAM_VERSION@,${UPSTREAM_VERSION},g" \
+    -e "s,@UPSTREAM_VERSION_MAJOR@,${UPSTREAM_VERSION_MAJOR},g" \
+    < debian/control.d/control.in \
+    > debian/control
+
+# Remove old libsyslog-ng-${VERSION}.* files
+rm -f debian/libsyslog-ng-[0-9].*
+
+# Update debian/libsyslog-ng-${UPSTREAM_VERSION}.install
+cp debian/control.d/libsyslog-ng.install \
+   debian/libsyslog-ng-${UPSTREAM_VERSION}.install
+
+# Update debian/libsyslog-ng-${UPSTREAM_VERSION}.lintian-overrides
+sed -e "s,@UPSTREAM_VERSION@,${UPSTREAM_VERSION},g" \
+    -e "s,@UPSTREAM_VERSION_MAJOR@,${UPSTREAM_VERSION_MAJOR},g" \
+    < debian/control.d/libsyslog-ng.lintian-overrides \
+    > debian/libsyslog-ng-${UPSTREAM_VERSION}.lintian-overrides
