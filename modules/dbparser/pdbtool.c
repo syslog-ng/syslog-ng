6d078782bccc3220804b5a8e36edd18af6460be6
Balazs Scheidler <bazsi@balabit.hu>
2011-09-17 13:25:16 +0200

    pdbtool: initialize the config with the proper version number
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- modules/dbparser/pdbtool.c
+++ modules/dbparser/pdbtool.c
@@ -1126,7 +1126,7 @@ main(int argc, char *argv[])
   log_tags_init();
   pattern_db_global_init();
 
-  configuration = cfg_new(0x0302);
+  configuration = cfg_new(CFG_CURRENT_VERSION);
 
   plugin_load_module("syslogformat", configuration, NULL);
   plugin_load_module("basicfuncs", configuration, NULL);
