9f06eca71cefdef423f7994c2e47b6a4b425bc44
Gergely Nagy <algernon@madhouse-project.org>
2011-10-07 11:07:34 +0200

    debian: help2man error propagation
    When help2man fails, the whole build should fail. However, the
    makefile snippet used pipes, and thus, hid errors.
    
    This has been corrected.

--- debian/tools/help2man.mk
+++ debian/tools/help2man.mk
@@ -8,15 +8,17 @@ clean:
 
 debian/man/syslog-ng.8: debian/man/syslog-ng.8.inc
 	help2man -N -s 8 -m "syslog-ng manual" -n "syslog-ng system logger application" \
-		 --version-string "${UPSTREAM_VERSION}" -h "--help-all" -l \
-		 -i $^ debian/build-tree/syslog-ng/syslog-ng | \
+		 --version-string "${UPSTREAM_VERSION}" -h "--help-all" \
+		 -i $^ debian/build-tree/syslog-ng/syslog-ng >$@.tmp
 	sed -e 's,^lt..syslog..ng \[OPTION...\] \(.*\),\1 [OPTIONS...],' \
-	    -e 's#, default=affile.*#.#' > $@
+	    -e 's#, default=affile.*#.#' <$@.tmp >$@
+	rm -f $@.tmp
 
 debian/man/loggen.1: debian/man/loggen.1.inc
 	help2man -N -s 1 -m "syslog-ng manual" \
-		 --version-string "${UPSTREAM_VERSION}" -h "--help-all" -l \
-		 -i $^ debian/build-tree/tests/loggen/loggen | \
-	sed -e "s,xlt-,," > $@
+		 --version-string "${UPSTREAM_VERSION}" -h "--help-all" \
+		 -i $^ debian/build-tree/tests/loggen/loggen >$@.tmp
+	sed -e "s,xlt-,," <$@.tmp >$@
+	rm -f $@.tmp
 
 .PHONY: all clean
