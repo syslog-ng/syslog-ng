fa675c3a5a51d4225723b7d76de6bb08529b12aa
Gergely Nagy <algernon@madhouse-project.org>
2011-10-27 23:42:49 +0200

    debian: Add the bootstrap script.

---
+++ debian/tools/bootstrap.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+set -e
+
+# Figure out the current version..
+UPSTREAM_VERSION="$(dpkg-parsechangelog | sed -n '/Version:/s/Version: \(.*\)\.dfsg.*/\1/p')"
+UPSTREAM_MAJOR="$(echo ${UPSTREAM_VERSION} | cut -d . -f 1,2)"
+
+git submodule --quiet update --init
+
+for branch in upstream/mirror upstream/dfsg patched debian; do
+	if ! git branch | grep -q "\s${branch}/${UPSTREAM_MAJOR}\$"; then
+		git branch --track ${branch}/${UPSTREAM_MAJOR} origin/${branch}/${UPSTREAM_MAJOR}
+	fi
+done
+
+debian/tools/update-control.sh ${UPSTREAM_VERSION}
