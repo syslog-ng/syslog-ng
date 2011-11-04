d2a58a06b27681219770d7c7a77f84958569dcb3
Tamas Pal <folti@balabit.hu>
2011-11-04 17:24:39 +0100

    core now builds pe-modules too.( fixes: TK-22351)

--- autogen.sh
+++ autogen.sh
@@ -5,17 +5,74 @@
 # This script is needed to setup build environment from checked out
 # source tree. 
 #
-SUBMODULES="lib/ivykis modules/afmongodb/libmongo-client"
+SUBMODULES="pe-modules"
+DUMMY_SUBMODULES="lib/ivykis modules/afmongodb/libmongo-client"
 
-for d in $SUBMODULES; do
-	[ -d $d ] || mkdir $d
+set -e
+autogen_submodules()
+{
+
+	origdir=`pwd`
+
+	for submod in $SUBMODULES; do
+		if [ -f .gitmodules ]; then
+			git submodule update --init --recursive -- $submod || git submodule update --init -- $submod
+		fi
+
+		echo "Running autogen in '$submod'..."
+		cd "$submod"
+		git checkout master
+		git pull
+		if [ -x autogen.sh ]; then
+			./autogen.sh
+		elif [ -f configure.in ] || [ -f configure.ac ]; then
+			autoreconf
+		else
+			echo "Don't know how to bootstrap submodule '$submod'" >&2
+			exit 1
+		fi
+		cd "$origdir"
+	done
+}
+if [ "$USER" = "testbot" ];then
+	skip_submodules=1
+fi
+
+for submod in $DUMMY_SUBMODULES; do
+	mkdir -p $submod
 done
 
+if [ -z "$skip_submodules" ] || [ "$skip_modules" = 0 ]; then
+	#autogen_submodules
+	:
+else
+	if [ -f .gitmodules ];then
+		for d in `grep 'path =' .gitmodules | sed -e 's/.*= *//'`; do
+			mkdir -p $d
+		done
+	fi
+fi
+
+(
+ pemodpath="pe-modules/modules/"
+ for pemod in license logstore diskq confighash snmp afsqlsource; do
+    if [ -d $pemodpath/$pemod ]; then
+        if [ -h $pemod ] || [ -d $pemod ]; then rm -rf $pemod; fi
+        cp -a $pemodpath/$pemod modules/$pemod
+    fi
+ done
+ petests_orig="pe-modules/tests"
+ petests="pe-tests"
+ if [ -d $petests_orig ]; then
+     if [ -h $petests ] || [ -d $petests ]; then rm -rf $petests; fi
+     cp -a $petests_orig $petests
+ fi
+)
 # bootstrap syslog-ng itself
 libtoolize --force
 aclocal -I m4 --install
 sed -i -e 's/PKG_PROG_PKG_CONFIG(\[0\.16\])/PKG_PROG_PKG_CONFIG([0.14])/g' aclocal.m4
 
 autoheader
-automake --foreign --add-missing
+automake --foreign --add-missing --copy
 autoconf
