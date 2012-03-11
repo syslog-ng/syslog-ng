3c14a264ab4c76b9372b1288d6808d19ec4d0aed
Balazs Scheidler <bazsi@balabit.hu>
2012-03-11 14:53:55 +0100

    logwriter: fixed a memory leak for messages that couldn't be flushed at once
    Sometimes a message is dequeued, and then can't be posted to the destination
    (probably because of an EAGAIN or something like that). In this case
    it is readded back to the queue. In this case a reference was lost, causing
    a memory leak, but not for all messages, but only for the affected ones.
    
    Reported-By: Evan Rempel <erempel@uvic.ca>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- lib/logwriter.c
+++ lib/logwriter.c
@@ -1016,7 +1016,7 @@ log_writer_flush(LogWriter *self, LogWriterFlushMode flush_mode)
         {
           /* push back to the queue */
           log_queue_push_head(self->queue, lm, &path_options);
-
+          log_msg_unref(lm);
           msg_set_context(NULL);
           log_msg_refcache_stop();
           break;
