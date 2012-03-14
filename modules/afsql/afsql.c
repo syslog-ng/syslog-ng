23542f90c476276000de1b2f145146583359fc7b
Mitzki András <micek@balabit.hu>
2012-03-14 11:52:44 +0100

    [afsql] do not free the string list of indexes if there wasn't any index column defined
    to avoid to free of the static empty string which is causing crash
    
    Signed-off-by: Juhász Viktor <jviktor@balabit.hu>

--- modules/afsql/afsql.c
+++ modules/afsql/afsql.c
@@ -1209,7 +1209,8 @@ afsql_dd_free(LogPipe *s)
   if (self->null_value)
     g_free(self->null_value);
   string_list_free(self->columns);
-  string_list_free(self->indexes);
+  if (self->enable_indexes)
+    string_list_free(self->indexes);
   string_list_free(self->values);
   log_template_unref(self->table);
   g_hash_table_destroy(self->validated_tables);
