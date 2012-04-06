d5d607c05251b38e821efe27bc46ac8db78dd722
Gergely Nagy <algernon@balabit.hu>
2012-04-06 11:35:42 +0200

    afinter: Make the parent class LogSrcDriver, instead of LogDriver.
    The parent of AFInterSourceDriver should be LogSrcDriver, because
    that's the struct we pass down to it anyway, and other parts of the
    code will cast it back to LogSrcDriver, too.
    
    But if its parent is LogDriver, as it was, we were corrupting the
    later parts of the original LogSrcDriver, with values from
    AFInterSourceDriver.
    
    Many thanks to Peter Eisenlohr <peter.eisenlohr@inform-software.com>
    for initial debugging, and for pinpointing mark messages as the
    triggers.
    
    Reported-by: Peter Czanik <czanik@balabit.hu>
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- lib/afinter.c
+++ lib/afinter.c
@@ -304,7 +304,7 @@ afinter_source_new(AFInterSourceDriver *owner, LogSourceOptions *options)
   AFInterSource *self = g_new0(AFInterSource, 1);
   
   log_source_init_instance(&self->super);
-  log_source_set_options(&self->super, options, 0, SCS_INTERNAL, owner->super.id, NULL, FALSE);
+  log_source_set_options(&self->super.super, options, 0, SCS_INTERNAL, owner->super.super.id, NULL, FALSE);
   afinter_source_init_watches(self);
   self->super.super.init = afinter_source_init;
   self->super.super.deinit = afinter_source_deinit;
@@ -328,7 +328,7 @@ afinter_sd_init(LogPipe *s)
       return FALSE;
     }
 
-  log_source_options_init(&self->source_options, cfg, self->super.group);
+  log_source_options_init(&self->source_options, cfg, self->super.super.group);
   self->source = afinter_source_new(self, &self->source_options);
   log_pipe_append(&self->source->super, s);
   log_pipe_init(&self->source->super, cfg);
@@ -370,9 +370,9 @@ afinter_sd_new(void)
   AFInterSourceDriver *self = g_new0(AFInterSourceDriver, 1);
 
   log_src_driver_init_instance((LogSrcDriver *)&self->super);
-  self->super.super.init = afinter_sd_init;
-  self->super.super.deinit = afinter_sd_deinit;
-  self->super.super.free_fn = afinter_sd_free;
+  self->super.super.super.init = afinter_sd_init;
+  self->super.super.super.deinit = afinter_sd_deinit;
+  self->super.super.super.free_fn = afinter_sd_free;
   log_source_options_defaults(&self->source_options);
   return (LogDriver *)&self->super.super;
 }
