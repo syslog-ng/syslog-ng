76c4319f56456e690498edf98f0755dd70552ec7
Gergely Nagy <algernon@balabit.hu>
2012-04-16 15:40:35 +0200

    debian/tools/bootstrap.sh: Fix UPSTREAM_VERSION discovery
    Make the UPSTREAM_VERSION discovery more robust, so that it recognises
    versions without any extra tags such as .dfsg or .original.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- debian/tools/bootstrap.sh
+++ debian/tools/bootstrap.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Figure out the current version..
-UPSTREAM_VERSION="$(dpkg-parsechangelog | sed -n '/Version:/s/Version: \(.*\)\.\(dfsg\|original\).*/\1/p' | sed -e 's,~\(.*\),,')"
+UPSTREAM_VERSION="$(dpkg-parsechangelog | grep '^Version:' | sed -e 's,^Version: ,,' -e 's,\.\(dfsg\|original\),,' -e 's,-[0-9\.]*~\(.*\),,')"
 UPSTREAM_MAJOR="$(echo ${UPSTREAM_VERSION} | cut -d . -f 1,2)"
 
 git submodule --quiet update --init
