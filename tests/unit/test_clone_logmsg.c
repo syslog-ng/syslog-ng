35a9060dd76e2394fbcfc5c23f05d6430a917002
Attila Magyar <athos@balabit.hu>
2012-05-04 18:08:48 +0200

    [test_clone_logmsg]: extracting reusable functions to a separate library (fixes: #B-06841)
    Signed-off-by: Attila Magyar <athos@balabit.hu>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- tests/unit/test_clone_logmsg.c
+++ tests/unit/test_clone_logmsg.c
@@ -15,22 +15,6 @@
 #include <stdio.h>
 
 void
-assert_log_message_has_tag(LogMessage *log_message, const gchar *tag_name)
-{
-  assert_true(log_msg_is_tag_by_name(log_message, tag_name), "Message has no '%s' tag", tag_name);
-}
-
-void
-assert_log_message_value(LogMessage *self, NVHandle handle, const gchar *expected_value)
-{
-  gssize key_name_length;
-  const gchar *key_name = log_msg_get_value_name(handle, &key_name_length);
-  const gchar *actual_value = log_msg_get_value(self, handle, NULL);
-
-  assert_string(actual_value, expected_value, "Value is not expected for key %s", key_name);
-}
-
-void
 assert_new_log_message_attributes(LogMessage *log_message)
 {
   assert_log_message_value(log_message, LM_V_HOST, "newhost");
@@ -54,63 +38,6 @@ set_new_log_message_attributes(LogMessage *log_message)
   log_msg_set_value(log_message, LM_V_MSGID, "newmsgid", -1);
   log_msg_set_value(log_message, LM_V_SOURCE, "newsource", -1);
   log_msg_set_value(log_message, log_msg_get_value_handle("newvalue"), "newvalue", -1);
-}
-
-void
-assert_log_messages_saddr(LogMessage *log_message_a, LogMessage *log_message_b)
-{
-  gchar address_a[256], address_b[256];
-
-  g_sockaddr_format(log_message_a->saddr, address_a, sizeof(address_a), GSA_FULL);
-  g_sockaddr_format(log_message_b->saddr, address_b, sizeof(address_b), GSA_FULL);
-  assert_string(address_a, address_b, "Socket address is not expected");
-}
-
-void
-assert_structured_data_of_messages(LogMessage *log_message_a, LogMessage *log_message_b)
-{
-  GString *structured_data_string_a = g_string_sized_new(0);
-  GString *structured_data_string_b = g_string_sized_new(0);
-
-  log_msg_format_sdata(log_message_a, structured_data_string_a, 0);
-  log_msg_format_sdata(log_message_b, structured_data_string_b, 0);
-  assert_string(structured_data_string_a->str, structured_data_string_b->str, "Structure data string are not the same");
-
-  g_string_free(structured_data_string_a, TRUE);
-  g_string_free(structured_data_string_b, TRUE);
-}
-
-void
-assert_log_message_values_equal(LogMessage *log_message_a, LogMessage *log_message_b, NVHandle handle)
-{
-  gssize key_name_length;
-  const gchar *key_name = log_msg_get_value_name(handle, &key_name_length);
-  const gchar *value_a = log_msg_get_value(log_message_a, handle, NULL);
-  const gchar *value_b = log_msg_get_value(log_message_b, handle, NULL);
-
-  assert_string(value_a, value_b, "Value is not expected for key %s", key_name);
-}
-
-void
-assert_log_messages_equal(LogMessage *log_message_a, LogMessage *log_message_b)
-{
-  assert_guint16(log_message_a->pri, log_message_b->pri, "Priorities are not the same");
-
-  assert_gint(log_message_a->timestamps[LM_TS_STAMP].tv_sec, log_message_b->timestamps[LM_TS_STAMP].tv_sec,
-      "Timestamps are not the same");
-  assert_guint32(log_message_a->timestamps[LM_TS_STAMP].tv_usec, log_message_b->timestamps[LM_TS_STAMP].tv_usec,
-      "Timestamps usec are not the same");
-  assert_guint32(log_message_a->timestamps[LM_TS_STAMP].zone_offset, log_message_b->timestamps[LM_TS_STAMP].zone_offset,
-      "Timestamp offset are not the same");
-
-  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_HOST);
-  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_PROGRAM);
-  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_MESSAGE);
-  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_PID);
-  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_MSGID);
-
-  assert_structured_data_of_messages(log_message_a, log_message_b);
-  assert_log_messages_saddr(log_message_a, log_message_b);
 }
 
 void
