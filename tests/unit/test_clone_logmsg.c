47c9c7cf17cf6c38d92209b64cdff3e465a8dbc6
Peter Gyorko <gyorkop@balabit.hu>
2012-05-04 15:42:39 +0200

    test_clone_logmsg: fixed minor things based on the review notes (fixes: #B-06841)
    Signed-off-by: Peter Gyorko <gyorkop@balabit.hu>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- tests/unit/test_clone_logmsg.c
+++ tests/unit/test_clone_logmsg.c
@@ -21,9 +21,10 @@ assert_log_message_has_tag(LogMessage *log_message, const gchar *tag_name)
 }
 
 void
-assert_value_of_log_message(LogMessage *self, NVHandle handle, const gchar *expected_value)
+assert_log_message_value(LogMessage *self, NVHandle handle, const gchar *expected_value)
 {
-  const gchar *key_name = nv_registry_get_handle_name(logmsg_registry, handle, 0);
+  gssize key_name_length;
+  const gchar *key_name = log_msg_get_value_name(handle, &key_name_length);
   const gchar *actual_value = log_msg_get_value(self, handle, NULL);
 
   assert_string(actual_value, expected_value, "Value is not expected for key %s", key_name);
@@ -32,14 +33,14 @@ assert_value_of_log_message(LogMessage *self, NVHandle handle, const gchar *expe
 void
 assert_new_log_message_attributes(LogMessage *log_message)
 {
-  assert_value_of_log_message(log_message, LM_V_HOST, "newhost");
-  assert_value_of_log_message(log_message, LM_V_HOST_FROM, "newhost");
-  assert_value_of_log_message(log_message, LM_V_MESSAGE, "newmsg");
-  assert_value_of_log_message(log_message, LM_V_PROGRAM, "newprogram");
-  assert_value_of_log_message(log_message, LM_V_PID, "newpid");
-  assert_value_of_log_message(log_message, LM_V_MSGID, "newmsgid");
-  assert_value_of_log_message(log_message, LM_V_SOURCE, "newsource");
-  assert_value_of_log_message(log_message, log_msg_get_value_handle("newvalue"), "newvalue");
+  assert_log_message_value(log_message, LM_V_HOST, "newhost");
+  assert_log_message_value(log_message, LM_V_HOST_FROM, "newhost");
+  assert_log_message_value(log_message, LM_V_MESSAGE, "newmsg");
+  assert_log_message_value(log_message, LM_V_PROGRAM, "newprogram");
+  assert_log_message_value(log_message, LM_V_PID, "newpid");
+  assert_log_message_value(log_message, LM_V_MSGID, "newmsgid");
+  assert_log_message_value(log_message, LM_V_SOURCE, "newsource");
+  assert_log_message_value(log_message, log_msg_get_value_handle("newvalue"), "newvalue");
 }
 
 void
@@ -74,17 +75,24 @@ assert_structured_data_of_messages(LogMessage *log_message_a, LogMessage *log_me
   log_msg_format_sdata(log_message_a, structured_data_string_a, 0);
   log_msg_format_sdata(log_message_b, structured_data_string_b, 0);
   assert_string(structured_data_string_a->str, structured_data_string_b->str, "Structure data string are not the same");
+
+  g_string_free(structured_data_string_a, TRUE);
+  g_string_free(structured_data_string_b, TRUE);
 }
 
 void
-assert_values_of_log_messages(LogMessage *log_message_a, LogMessage *log_message_b, NVHandle handle)
+assert_log_message_values_equal(LogMessage *log_message_a, LogMessage *log_message_b, NVHandle handle)
 {
+  gssize key_name_length;
+  const gchar *key_name = log_msg_get_value_name(handle, &key_name_length);
   const gchar *value_a = log_msg_get_value(log_message_a, handle, NULL);
-  assert_value_of_log_message(log_message_b, handle, value_a);
+  const gchar *value_b = log_msg_get_value(log_message_b, handle, NULL);
+
+  assert_string(value_a, value_b, "Value is not expected for key %s", key_name);
 }
 
 void
-assert_log_messages_are_the_same(LogMessage *log_message_a, LogMessage *log_message_b)
+assert_log_messages_equal(LogMessage *log_message_a, LogMessage *log_message_b)
 {
   assert_guint16(log_message_a->pri, log_message_b->pri, "Priorities are not the same");
 
@@ -95,11 +103,11 @@ assert_log_messages_are_the_same(LogMessage *log_message_a, LogMessage *log_mess
   assert_guint32(log_message_a->timestamps[LM_TS_STAMP].zone_offset, log_message_b->timestamps[LM_TS_STAMP].zone_offset,
       "Timestamp offset are not the same");
 
-  assert_values_of_log_messages(log_message_a, log_message_b, LM_V_HOST);
-  assert_values_of_log_messages(log_message_a, log_message_b, LM_V_PROGRAM);
-  assert_values_of_log_messages(log_message_a, log_message_b, LM_V_MESSAGE);
-  assert_values_of_log_messages(log_message_a, log_message_b, LM_V_PID);
-  assert_values_of_log_messages(log_message_a, log_message_b, LM_V_MSGID);
+  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_HOST);
+  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_PROGRAM);
+  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_MESSAGE);
+  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_PID);
+  assert_log_message_values_equal(log_message_a, log_message_b, LM_V_MSGID);
 
   assert_structured_data_of_messages(log_message_a, log_message_b);
   assert_log_messages_saddr(log_message_a, log_message_b);
@@ -112,7 +120,6 @@ test_cloning_with_log_message(gchar *msg)
   regex_t bad_hostname;
   GSockAddr *addr = g_sockaddr_inet_new("10.10.10.10", 1010);
   LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
-  GString *sd_str = g_string_sized_new(0);
 
   parse_options.flags = LP_SYSLOG_PROTOCOL;
   parse_options.bad_hostname = &bad_hostname;
@@ -124,18 +131,17 @@ test_cloning_with_log_message(gchar *msg)
   path_options.ack_needed = FALSE;
 
   cloned_log_message = log_msg_clone_cow(log_message, &path_options);
-  assert_log_messages_are_the_same(cloned_log_message, original_log_message);
+  assert_log_messages_equal(cloned_log_message, original_log_message);
 
   set_new_log_message_attributes(cloned_log_message);
 
-  assert_log_messages_are_the_same(log_message, original_log_message);
+  assert_log_messages_equal(log_message, original_log_message);
   assert_new_log_message_attributes(cloned_log_message);
   assert_log_message_has_tag(cloned_log_message, "newtag");
 
   log_msg_unref(cloned_log_message);
   log_msg_unref(log_message);
   log_msg_unref(original_log_message);
-  g_string_free(sd_str, TRUE);
 }
 
 int
