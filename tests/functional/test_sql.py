8c36bfb99a7bdbf69216ca6637b51b8276fdd626
Gergely Nagy <algernon@balabit.hu>
2012-06-01 10:02:31 +0200

    tests: Include /usr/lib64 in the dbd search path
    Certain systems put 64-bit libraries into /usr/lib64, so include that
    directory in the dbd search path for the tests. Thanks to Jose
    Oliveira <jpo@di.uminho.pt> for the patch!
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- tests/functional/test_sql.py
+++ tests/functional/test_sql.py
@@ -45,7 +45,7 @@ def check_env():
         soext='.sl'
 
     found = False
-    paths = (os.environ.get('dbd_dir', None), '/usr/local/lib/dbd', '/usr/lib/dbd', '/opt/syslog-ng/lib/dbd')
+    paths = (os.environ.get('dbd_dir', None), '/usr/local/lib/dbd', '/usr/lib/dbd', '/usr/lib64/dbd/', '/opt/syslog-ng/lib/dbd')
     for pth in paths:
         if pth and os.path.isfile('%s/libdbdsqlite3%s' % (pth, soext)):
             found = True
