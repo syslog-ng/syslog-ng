c0afec5b46eb3508fd3b1449e37b7e550f7d35e2
Gergely Nagy <algernon@balabit.hu>
2012-06-08 15:28:33 +0200

    dnscache: Fix a memory corruption when destroying the DNS cache
    The DNS cache gets destroyed every time a worker thread quits, which
    is fine, as most of the dns cache variables are thread local.
    
    However, dns_cache_hosts is not, it's a global static, and
    dns_cache_destroy() was freeing that aswell.
    
    The solution is to not free dns_cache_hosts when a worker stops, but
    do so when syslog-ng stops. This patch introduces dns_cache_deinit()
    which does just that, and removes the same thing from
    dns_cache_destroy(), which now only touches thread-local variables.
    
    Reported-by: EgonB <egon@local.ee>
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- lib/dnscache.c
+++ lib/dnscache.c
@@ -361,6 +361,12 @@ dns_cache_destroy(void)
   cache_last.prev = NULL;
   persist_first.next = NULL;
   persist_last.prev = NULL;
+}
+
+void
+dns_cache_deinit(void)
+{
   if (dns_cache_hosts)
     g_free(dns_cache_hosts);
+  dns_cache_hosts = NULL;
 }
