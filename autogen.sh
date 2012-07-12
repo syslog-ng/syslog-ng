fe8be050d8f8684eba4b1f60195d00a7443af18e
Juhasz Viktor <jviktor@balabit.hu>
2012-07-12 15:07:34 +0200

    [autogen.sh] pe version modified to 5.0
    Signed-off-by: Juhasz Viktor <jviktor@balabit.hu>

--- autogen.sh
+++ autogen.sh
@@ -37,25 +37,33 @@ if [ "$USER" = "testbot" ];then
 fi
 
 (
- pemodpath="$ZWA_ROOT/git/syslog-ng/syslog-ng-pe-modules--mainline--4.2/modules"
+ pemodpath="$ZWA_ROOT/git/syslog-ng/syslog-ng-pe-modules--mainline--5.0/modules"
  for pemod in license logstore diskq confighash snmp afsqlsource rltp-proto; do
     if [ -d $pemodpath/$pemod ]; then
         if [ -h modules/$pemod ] || [ -d modules/$pemod ]; then rm -rf modules/$pemod; fi
         ln -s $pemodpath/$pemod modules/$pemod
     fi
  done
- petests_orig="$ZWA_ROOT/git/syslog-ng/syslog-ng-pe-modules--mainline--4.2/tests"
+ petests_orig="$ZWA_ROOT/git/syslog-ng/syslog-ng-pe-modules--mainline--5.0/tests"
  petests="pe-tests"
  if [ -d $petests_orig ]; then
      if [ -h $petests ] || [ -d $petests ]; then rm -rf $petests; fi
      ln -s $petests_orig $petests
  fi
 )
+ACLOCALPATHS=
+for pth in /opt/libtool/share/aclocal /usr/local/share/aclocal; do
+	if [ -d $pth ];then
+		ACLOCAPATHS="$ACLOCALPATHS -I $pth"
+	fi
+done
 # bootstrap syslog-ng itself
-libtoolize --force
-aclocal -I m4 --install
+libtoolize --force --copy
+aclocal -I m4 $ACLOCALPATHS --install
 sed -i -e 's/PKG_PROG_PKG_CONFIG(\[0\.16\])/PKG_PROG_PKG_CONFIG([0.14])/g' aclocal.m4
 
 autoheader
 automake --foreign --add-missing --copy
 autoconf
+find -name libtool -o -name ltmain.sh | xargs sed -i -e "s,'file format pe-i386.*\?','file format \(pei\*-i386\(\.\*architecture: i386\)\?|pe-arm-wince|pe-x86-64\)',"
+sed -i -e "s, cmd //c, sh -c," ltmain.sh
