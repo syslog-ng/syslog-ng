1dfc28c85c6c7058578fd512ea98fb21ab2b6e03
Gergely Nagy <algernon@balabit.hu>
2012-08-06 15:58:51 +0200

    afinet: Disable KEEP_ALIVE when using spoof source
    Disabling KEEP_ALIVE when using spoof source will result in rebuilding
    the writer at reload, thus, also resolving the destination. This fixes
    the issue of never resolving the destination after a reload, and
    ending up sending packets to 0.0.0.0.
    
    Patch from Balazs Scheidler <bazsi@balabit.hu>, after debugging the
    problem in person, and going through all the different options.
    
    Reported-by: Marvin Nipper <marvin.nipper@stream.com>
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- modules/afsocket/afinet.c
+++ modules/afsocket/afinet.c
@@ -539,8 +539,11 @@ afinet_dd_setup_socket(AFSocketDestDriver *s, gint fd)
 static gboolean
 afinet_dd_init(LogPipe *s)
 {
-  AFInetDestDriver *self G_GNUC_UNUSED = (AFInetDestDriver *) s;
+  AFInetDestDriver *self = (AFInetDestDriver *) s;
   gboolean success;
+
+  if (self->spoof_source)
+    self->super.flags &= ~AFSOCKET_KEEP_ALIVE;
 
   success = afsocket_dd_init(s);
 #if ENABLE_SPOOF_SOURCE
