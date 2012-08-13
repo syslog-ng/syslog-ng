fdb537a904bcfcd104db418f7aa4e128d97aa08f
Attila Magyar <athos@balabit.hu>
2012-08-13 19:40:33 +0200

    unit: extracted init_and_load_syslogformat/deinit() to msg_parse_lib.h (fixes: #B-06841, #B-07483)
    Signed-off-by: Peter Gyorko <gyorkop@balabit.hu>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>
    Signed-off-by: Attila Magyar <athos@balabit.hu>
    
    (cherry picked from commit 475b3fb69af999ffd7bcddeb8e1ccf8c1aaaf9aa)

--- tests/unit/test_msgsdata.c
+++ tests/unit/test_msgsdata.c
@@ -2,29 +2,11 @@
 #include "apphook.h"
 #include "cfg.h"
 #include "plugin.h"
+#include "msg_parse_lib.h"
 #include "testutils.h"
 
 #include <stdlib.h>
 #include <string.h>
-
-MsgFormatOptions parse_options;
-
-void
-init_and_load_syslogformat_module()
-{
-  configuration = cfg_new(0x0302);
-  plugin_load_module("syslogformat", configuration, NULL);
-  msg_format_options_defaults(&parse_options);
-  msg_format_options_init(&parse_options, configuration);
-}
-
-void
-deinit_syslogformat_module()
-{
-  if (configuration)
-    cfg_free(configuration);
-  configuration = NULL;
-}
 
 void
 testcase_update_sdata(const gchar *msg, const gchar *expected_sd_str, gchar *elem_name1, ...)
