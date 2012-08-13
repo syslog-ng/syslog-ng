1769e67787e559d24b9f0bc3d425fa15aa76da8a
Attila Magyar <athos@balabit.hu>
2012-08-13 19:48:30 +0200

    [test_clone_logmsg]: extracting reusable functions to a separate library (fixes: #B-06841, #B-07483)
    Signed-off-by: Attila Magyar <athos@balabit.hu>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>
    Signed-off-by: Attila Magyar <athos@balabit.hu>
    
    (cherry picked from commit 35a9060dd76e2394fbcfc5c23f05d6430a917002)

--- tests/unit/msg_parse_lib.h
+++ tests/unit/msg_parse_lib.h
@@ -1,3 +1,5 @@
+#include "testutils.h"
+
 #include "cfg.h"
 #include "plugin.h"
 #include "logmsg.h"
@@ -7,21 +9,15 @@
 
 MsgFormatOptions parse_options;
 
-void
-init_and_load_syslogformat_module()
-{
-  configuration = cfg_new(0x0302);
-  plugin_load_module("syslogformat", configuration, NULL);
-  msg_format_options_defaults(&parse_options);
-  msg_format_options_init(&parse_options, configuration);
-}
+void init_and_load_syslogformat_module();
+void deinit_syslogformat_module();
 
-void
-deinit_syslogformat_module()
-{
-  if (configuration)
-    cfg_free(configuration);
-  configuration = NULL;
-}
+void assert_log_messages_equal(LogMessage *log_message_a, LogMessage *log_message_b);
+
+void assert_log_message_value(LogMessage *self, NVHandle handle, const gchar *expected_value);
+void assert_log_message_has_tag(LogMessage *log_message, const gchar *tag_name);
+void assert_log_messages_saddr(LogMessage *log_message_a, LogMessage *log_message_b);
+void assert_structured_data_of_messages(LogMessage *log_message_a, LogMessage *log_message_b);
+void assert_log_message_values_equal(LogMessage *log_message_a, LogMessage *log_message_b, NVHandle handle);
 
 #endif
