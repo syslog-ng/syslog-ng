36c00a985cd710db3377f40fef307ffdd89ea343
Balazs Scheidler <bazsi@balabit.hu>
2012-08-13 07:30:15 +0200

    test_template: remove cryptofuncs unit tests
    As they were moved to a separate test program in an earlier commit.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- tests/unit/test_template.c
+++ tests/unit/test_template.c
@@ -364,25 +364,6 @@ main(int argc G_GNUC_UNUSED, char *argv[] G_GNUC_UNUSED)
   testcase(msg, "$(% $FACILITY_NUM 0)", "NaN");
   testcase(msg, "$(+ foo bar)", "NaN");
 
-#if ENABLE_SSL
-  testcase(msg, "$(sha1 foo)", "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33");
-  testcase(msg, "$(sha1 bar)", "62cdb7020ff920e5aa642c3d4066950dd1f01f4d");
-  testcase(msg, "$(md5 foo)", "acbd18db4cc2f85cedef654fccc4a4d8");
-  testcase(msg, "$(hash foo)", "acbd18db4cc2f85cedef654fccc4a4d8");
-  testcase(msg, "$(md4 foo)", "0ac6700c491d70fb8650940b1ca1e4b2");
-  testcase(msg, "$(sha256 foo)", "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae");
-  testcase(msg, "$(sha512 foo)", "f7fbba6e0636f890e56fbbf3283e524c6fa3204ae298382d624741d0dc6638326e282c41be5e4254d8820772c5518a2c5a8c0c7f7eda19594a7eb539453e1ed7");
-  testcase_failure("$(sha1)", "$(hash) parsing failed, invalid number of arguments");
-  testcase(msg, "$(sha1 --length 5 foo)", "0beec");
-  testcase(msg, "$(sha1 -l 5 foo)", "0beec");
-  testcase_failure("$(sha1 --length 5)", "$(hash) parsing failed, invalid number of arguments");
-  testcase_failure("$(sha1 --length invalid_length_specification foo)", "Cannot parse integer value 'invalid_length_specification' for --length");
-  testcase(msg, "$(sha1 --length 99999 foo)", "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33");
-  testcase(msg, "$(sha1 foo bar)", "8843d7f92416211de9ebb963ff4ce28125932878");
-  testcase(msg, "$(sha1 \"foo bar\")", "3773dea65156909838fa6c22825cafe090ff8030");
-  testcase(msg, "$(md5 $(sha1 foo) bar)", "196894290a831b2d2755c8de22619a97");
-#endif
-
   /* message refs */
   testcase(msg, "$(echo ${HOST}@0 ${PID}@1)", "bzorp 23323");
   testcase(msg, "$(echo $HOST $PID)@0", "bzorp 23323");
