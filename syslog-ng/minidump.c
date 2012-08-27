cc8c6b5fb440511b8895c45fe2ae0a9bf8366d34
Juhasz Viktor <jviktor@balabit.hu>
2012-08-27 17:20:44 +0200

    [minidump] Drop the trace file creation routin, because its unnecessary.
    The dump file contains all information, and now we have tool to read backtrace from dump file

--- syslog-ng/minidump.c
+++ syslog-ng/minidump.c
@@ -51,115 +51,6 @@ SplitCurrentModulePath(char **dir, char **fileName)
   return TRUE;
 }
 
-BOOL
-GetLogicalAddress(VOID *addr, PTSTR moduleName, DWORD len, DWORD *section, DWORD *offset)
-{
-  MEMORY_BASIC_INFORMATION memoryBasicInformation;
-  DWORD allocationBase;
-  PIMAGE_DOS_HEADER peDosHeader;
-  PIMAGE_NT_HEADERS peNtHeader;
-  PIMAGE_SECTION_HEADER peSectionHeader;
-  DWORD rva;
-  unsigned i;
-
-  if (IsBadReadPtr(addr, sizeof(void *)))
-    return FALSE;
-
-  if (!VirtualQuery (addr, &memoryBasicInformation, sizeof (memoryBasicInformation)))
-    return FALSE;
-
-  allocationBase = (DWORD) memoryBasicInformation.AllocationBase;
-  if (allocationBase == 0)
-    return TRUE;
-
-  if (!GetModuleFileName ((HMODULE) allocationBase, moduleName, len))
-    return FALSE;
-
-  /* Point to the DOS header in memory */
-  peDosHeader = (PIMAGE_DOS_HEADER) allocationBase;
-
-  /* From the DOS header, find the NT (PE) header */
-  peNtHeader = (PIMAGE_NT_HEADERS) (allocationBase + peDosHeader->e_lfanew);
-
-  /* From the NT header, find the section header */
-  peSectionHeader = IMAGE_FIRST_SECTION (peNtHeader);
-
-  /* RVA is offset from module load address */
-  rva = (DWORD) addr - allocationBase;
-
-  /* Iterate through the section table,
-     looking for the one that encompasses
-   * the linear address. */
-  for (i = 0; i < peNtHeader->FileHeader.NumberOfSections; i++, peSectionHeader++)
-    {
-      DWORD section_start = peSectionHeader->VirtualAddress;
-      DWORD section_end = section_start + max(peSectionHeader->SizeOfRawData, peSectionHeader->Misc.VirtualSize);
-
-      // Is the address in this section???
-      if ((rva >= section_start) && (rva <= section_end))
-        {
-          /*
-           * Yes, address is in the section.  Calculate section and offset,
-           * and store in the "section" & "offset" params, which were
-           * passed by reference.
-           */
-          *section = i + 1;
-          *offset = rva - section_start;
-          return TRUE;
-        }
-    }
-  return FALSE;
-}
-
-
-void
-IntelStackWalk (PCONTEXT context, FILE *outfile)
-{
-  void *framePointer = NULL;
-  void *eip = NULL;
-  void *prevFrame = NULL;
-  char *moduleName = malloc(MAX_FILE_NAME_LENGTH);
-#ifdef _X86_
-  eip = (void *)context->Eip;
-  framePointer = (void *)context->Ebp;
-#elif defined(_x86_64_)
-  eip = (void *)context->Rip;
-  framePointer = (void *)context->Rbp;
-#endif
-  fprintf(outfile, "Call stack:\n");
-  fprintf(outfile, "Address   Frame     Logical addr  Module\n");
-  do
-    {
-      moduleName[0] = '\0';
-      DWORD section = 0, offset = 0;
-      if (!IsBadReadPtr(eip, sizeof(eip)))
-        {
-          if (!GetLogicalAddress((VOID*) eip, moduleName, MAX_FILE_NAME_LENGTH, &section, &offset))
-            {
-              break;
-            }
-        }
-      fprintf(outfile, "%p  %p  %04lx:%08lx %s\n", eip, framePointer, section, offset, moduleName);
-      eip = ((void **)framePointer)[1];
-      prevFrame = framePointer;
-      // precede to next higher frame on stack
-      framePointer = ((void **) framePointer)[0];
-      if (framePointer == NULL)
-        {
-          break;
-        }
-      if (framePointer <= prevFrame)
-        {
-          break;
-        }
-      if (IsBadReadPtr (framePointer, sizeof (framePointer) * 2))
-        {
-          break;
-        }
-    }
-  while (TRUE);
-}
-
 LONG WINAPI
 DropMinidump(struct _EXCEPTION_POINTERS *pExceptionInfo, char *dumpFileName, char *dbgHelpPath)
 {
@@ -226,8 +117,6 @@ MinidumpWriter(struct _EXCEPTION_POINTERS *pExceptionInfo)
   char *fileName = NULL;
   char *dbgHelpPath = malloc(MAX_FILE_NAME_LENGTH + 1);
   char *dumpFileName = malloc(MAX_FILE_NAME_LENGTH + 1);
-  char *traceFileName = malloc(MAX_FILE_NAME_LENGTH + 1);
-  FILE *traceFile = NULL;
   if (!SplitCurrentModulePath(&directory, &fileName))
     {
       goto exit;
@@ -235,27 +124,11 @@ MinidumpWriter(struct _EXCEPTION_POINTERS *pExceptionInfo)
 
   snprintf(dbgHelpPath, MAX_FILE_NAME_LENGTH, "%s\\DBGHELP.DLL", directory);
   snprintf(dumpFileName, MAX_FILE_NAME_LENGTH, "%s\\%s.dmp", directory, fileName);
-  snprintf(traceFileName, MAX_FILE_NAME_LENGTH, "%s\\%s.trace", directory, fileName);
-
-  traceFile = fopen(traceFileName, "w");
-  if (traceFile)
-    {
-      setvbuf(traceFile, NULL, _IONBF, 0);
-      fprintf(traceFile, "Start stack walk\n");
-      IntelStackWalk(pExceptionInfo->ContextRecord, traceFile);
-      fprintf(traceFile, "End of stack walk\n");
-      fclose(traceFile);
-    }
-  else
-    {
-      fprintf(stderr, "Can't create trace file!\n");
-    }
 
   retval = DropMinidump(pExceptionInfo, dumpFileName, dbgHelpPath);
 exit:
   free(dbgHelpPath);
   free(dumpFileName);
-  free(traceFileName);
   if (directory)
     free(directory);
   if (fileName)
