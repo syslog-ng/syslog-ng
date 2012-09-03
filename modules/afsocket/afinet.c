399aa1f725267ae03e60a7fb8375f44ed3d1412e
Gergely Nagy <algernon@balabit.hu>
2012-09-03 11:41:36 +0200

    afsocket: Fix compilation without spoof-source
    AFInetDestDriver->spoof_source is only available when compiling with
    spoof-source enabled, so guard it with an #if.
    
    Reported and patched by Dave Reisner <dreisner@archlinux.org>.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- modules/afsocket/afinet.c
+++ modules/afsocket/afinet.c
@@ -636,11 +636,13 @@ afinet_dd_setup_socket(AFSocketDestDriver *s, gint fd)
 static gboolean
 afinet_dd_init(LogPipe *s)
 {
-  AFInetDestDriver *self = (AFInetDestDriver *) s;
+  AFInetDestDriver *self G_GNUC_UNUSED = (AFInetDestDriver *) s;
   gboolean success;
 
+#if ENABLE_SPOOF_SOURCE
   if (self->spoof_source)
     self->super.flags &= ~AFSOCKET_KEEP_ALIVE;
+#endif
 
   success = afsocket_dd_init(s);
 #if ENABLE_SPOOF_SOURCE
