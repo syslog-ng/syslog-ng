5c87b82fab9c53d51b2ea79d32d8ca26bb76ff71
Gergely Nagy <algernon@balabit.hu>
2012-10-14 12:58:21 +0200

    tools/bootstrap: sync submodules in syslog-ng-$MAJOR
    Since submodule locations may change between versions, sync the
    submodules in syslog-ng-${UPSTREAM_MAJOR} before doing a recursive
    update.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- debian/tools/bootstrap.sh
+++ debian/tools/bootstrap.sh
@@ -6,6 +6,8 @@ set -e
 UPSTREAM_VERSION="$(dpkg-parsechangelog | grep '^Version:' | sed -e 's,^Version: ,,' -e 's,\.\(dfsg\|original\),,' -e 's,-[0-9\.]*[~\+]\(.*\),,')"
 UPSTREAM_MAJOR="$(echo ${UPSTREAM_VERSION} | cut -d . -f 1,2)"
 
+git submodule --quiet update --init
+(cd syslog-ng-${UPSTREAM_MAJOR} && git submodule --quiet sync)
 git submodule --quiet update --recursive --init
 
 debian/tools/update-control.sh ${UPSTREAM_VERSION}
