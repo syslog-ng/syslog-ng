2005c13811775bb651fd60c84c72c15c1a05953a
Gergely Nagy <algernon@balabit.hu>
2012-10-16 07:54:37 +0200

    debian/tools/bootstrap.sh: Make it work in the exported dir too.
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- debian/tools/bootstrap.sh
+++ debian/tools/bootstrap.sh
@@ -7,7 +7,11 @@ UPSTREAM_VERSION="$(dpkg-parsechangelog | grep '^Version:' | sed -e 's,^Version:
 UPSTREAM_MAJOR="$(echo ${UPSTREAM_VERSION} | cut -d . -f 1,2)"
 
 git submodule --quiet update --init
-(cd syslog-ng-${UPSTREAM_MAJOR} && git submodule --quiet sync)
+if [ -d syslog-ng-${UPSTREAM_MAJOR} ]; then
+        (cd syslog-ng-${UPSTREAM_MAJOR} && git submodule --quiet sync)
+else
+        git submodule --quiet sync
+fi
 git submodule --quiet update --recursive --init
 
 debian/tools/update-control.sh ${UPSTREAM_VERSION}
