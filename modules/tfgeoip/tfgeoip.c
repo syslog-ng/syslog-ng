82b09cc75d8d844668eb7ca8e8215c4276bcb54a
Gergely Nagy <algernon@balabit.hu>
2012-10-19 17:47:40 +0200

    tfgeoip: $(geoip) template function
    This implements a $(geoip) template function using libgeoip, based on
    a patch from Csaba Major. The function can be used to look up the
    2-letter country code of any IPv4 address or host passed to the function.
    
    Currently only the 2-letter codes are supported, and only from the
    default database, and only for IPv4 addresses (or hosts). Adding
    further functionality can be done on demand, however.
    
    Signed-off-by: Csaba Major <csaba.major@balabit.com>
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

---
+++ modules/tfgeoip/tfgeoip.c
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2012 BalaBit IT Ltd, Budapest, Hungary
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "plugin.h"
+#include "templates.h"
+#include "messages.h"
+#include "cfg.h"
+#include "tls-support.h"
+
+#include "config.h"
+
+#include <GeoIP.h>
+
+typedef struct _TFGeoIPState
+{
+  GeoIP *gi;
+} TFGeoIPState;
+
+TLS_BLOCK_START
+{
+  TFGeoIPState *geoip_state;
+}
+TLS_BLOCK_END;
+
+#define local_state __tls_deref(geoip_state)
+
+static void
+tf_geoip_init(void)
+{
+  if (!local_state)
+    {
+      local_state = g_new0(TFGeoIPState, 1);
+      local_state->gi = GeoIP_new(GEOIP_MMAP_CACHE);
+    }
+}
+
+static gboolean
+tf_geoip(LogMessage *msg, gint argc, GString *argv[], GString *result)
+{
+  const char *country;
+
+  if (argc != 1)
+    {
+      msg_debug("tfgeoip takes only one argument",
+                evt_tag_int("count", argc),
+                NULL);
+      return FALSE;
+    }
+
+  country = GeoIP_country_code_by_name(local_state->gi, argv[0]->str);
+  g_string_append(result, country);
+
+  return TRUE;
+}
+TEMPLATE_FUNCTION_SIMPLE(tf_geoip);
+
+static Plugin tfgeoip_plugins[] =
+  {
+    TEMPLATE_FUNCTION_PLUGIN(tf_geoip, "geoip"),
+  };
+
+gboolean
+tfgeoip_module_init(GlobalConfig *cfg, CfgArgs *args)
+{
+  tf_geoip_init();
+  plugin_register(cfg, tfgeoip_plugins, G_N_ELEMENTS(tfgeoip_plugins));
+  return TRUE;
+}
+
+const ModuleInfo module_info =
+{
+  .canonical_name = "tfgeoip",
+  .version = VERSION,
+  .description = "The tfgeoip module provides a template function to get GeoIP info from an IPv4 address.",
+  .core_revision = SOURCE_REVISION,
+  .plugins = tfgeoip_plugins,
+  .plugins_len = G_N_ELEMENTS(tfgeoip_plugins),
+};
