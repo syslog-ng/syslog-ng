473f1fac79ef7e28138af14311562d9865e31cb5
Gergely Nagy <algernon@madhouse-project.org>
2013-02-22 12:19:29 +0100

    afsocket: Require libsystemd-daemon for socket activation
    Instead of carrying a convenience copy of sd-daemon.c and sd-daemon.h,
    require libsystemd-daemon for socket activation. If the library is not
    found, systemd support will be disabled.
    
    This is so other parts of syslog-ng can easily use systemd things
    aswell, without having to move sd-daemon.* to lib/ or some similar
    place.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- modules/afsocket/afunix-source.c
+++ modules/afsocket/afunix-source.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2012 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
  * Copyright (c) 1998-2012 Balázs Scheidler
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -26,10 +26,8 @@
 #include "messages.h"
 #include "gprocess.h"
 
-#if WITH_LIBSYSTEMD
+#if ENABLE_SYSTEMD
 #include <systemd/sd-daemon.h>
-#else
-#include "sd-daemon.h"
 #endif
 
 #include <string.h>
