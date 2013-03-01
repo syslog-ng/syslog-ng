4df1e7ec68fcb2b6e231fbf013a04202eb40ebbc
Balazs Scheidler <bazsi@balabit.hu>
2013-03-01 13:55:31 +0100

    test_logproto: factor out LogProtoTextServer testcases

--- lib/logproto/tests/test_logproto.h
+++ lib/logproto/tests/test_logproto.h
@@ -4,6 +4,6 @@
 void test_log_proto_indented_multiline_server(void);
 void test_log_proto_server_options(void);
 void test_log_proto_record_server(void);
-
+void test_log_proto_text_server(void);
 
 #endif
