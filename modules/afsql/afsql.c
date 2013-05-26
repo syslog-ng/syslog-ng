22d8c5ad88ac59e96b29d38f7a559fd0a4963239
Balazs Scheidler <bazsi@balabit.hu>
2013-05-26 19:15:39 +0200

    afsql: allocate locks/condvars at startup
    Instead of doing this for all start_thread/stop_thread pairs, allocate
    them once to avoid churn.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- modules/afsql/afsql.c
+++ modules/afsql/afsql.c
@@ -999,8 +999,6 @@ afsql_dd_database_thread(gpointer arg)
 static void
 afsql_dd_start_thread(AFSqlDestDriver *self)
 {
-  self->db_thread_wakeup_cond = g_cond_new();
-  self->db_thread_mutex = g_mutex_new();
   self->db_thread = create_worker_thread(afsql_dd_database_thread, self, TRUE, NULL);
 }
 
@@ -1012,8 +1010,6 @@ afsql_dd_stop_thread(AFSqlDestDriver *self)
   g_cond_signal(self->db_thread_wakeup_cond);
   g_mutex_unlock(self->db_thread_mutex);
   g_thread_join(self->db_thread);
-  g_mutex_free(self->db_thread_mutex);
-  g_cond_free(self->db_thread_wakeup_cond);
 }
 
 static gchar *
@@ -1265,8 +1261,10 @@ afsql_dd_free(LogPipe *s)
   g_hash_table_destroy(self->validated_tables);
   g_hash_table_destroy(self->dbd_options);
   g_hash_table_destroy(self->dbd_options_numeric);
-  if(self->session_statements)
+  if (self->session_statements)
     string_list_free(self->session_statements);
+  g_mutex_free(self->db_thread_mutex);
+  g_cond_free(self->db_thread_wakeup_cond);
   log_dest_driver_free(s);
 }
 
@@ -1305,6 +1303,9 @@ afsql_dd_new(void)
 
   log_template_options_defaults(&self->template_options);
   init_sequence_number(&self->seq_num);
+
+  self->db_thread_wakeup_cond = g_cond_new();
+  self->db_thread_mutex = g_mutex_new();
   return &self->super.super;
 }
 
