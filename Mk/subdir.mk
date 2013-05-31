751a6d5ec5ad0b99b481507186e081372332c170
Gergely Nagy <algernon@balabit.hu>
2013-05-31 13:50:56 +0200

    Mk/subdir.mk: Support partial installation
    When using the wrapper Makefile (Mk/subdir.mk), allow make install to
    work from within a subdir. The solution is a bit of a hack, and contains
    some hard-coded assumptions, and only works for lib/ and stuff under
    modules/, but that's where 99% of the work is done anyway.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- Mk/subdir.mk
+++ Mk/subdir.mk
@@ -3,12 +3,47 @@ top_builddir	= $(shell ${top_srcdir}/Mk/find-top-builddir.sh)
 
 self		= $(subst ${top_builddir}/,,${CURDIR})
 self_sub	= $(subst -,_,$(subst /,_,${self}))
+basedir		= $(firstword $(subst /, ,${self}))
 
 all:
 	${AM_v_at}${MAKE} -C ${top_builddir} ${self}/
 
 check:
 	${AM_v_at}${MAKE} -C ${top_builddir} local-check check_subdir=${self_sub}
+
+INSTALL_ARGS	= bin_SCRIPTS= \
+		bin_PROGRAMS= \
+		sbin_PROGRAMS= \
+		libexec_PROGRAMS= \
+		man_MANS= \
+		tools_DATA= \
+		xsd_DATA= \
+		INSTALL_EXEC_HOOKS=
+
+ifeq (${basedir},lib)
+INSTALL_ARGS	+= module_LTLIBRARIES=
+else
+ ifeq (${basedir},modules)
+INSTALL_ARGS	+= lib_LTLIBRARIES= pkginclude_HEADERS= pkgconfig_DATA=
+  ifneq (${self_sub},modules)
+   ifeq (${self_sub},modules_afsocket)
+INSTALL_ARGS	+= module_LTLIBRARIES="modules/afsocket/libafsocket-notls.la modules/afsocket/libafsocket-tls.la"
+INSTALL_ARGS	+= INSTALL_EXEC_HOOKS=afsocket-install-exec-hook
+   else
+INSTALL_ARGS	+= module_LTLIBRARIES=${self}/lib$(word 2,$(subst /, ,${self})).la
+   endif
+  endif
+ else
+INSTALL_ARGS	=
+ endif
+endif
+
+install:
+ifeq (${INSTALL_ARGS},)
+	@echo "Installing from this directory is not supported."
+else
+	${AM_v_at}${MAKE} -C ${top_builddir} install ${INSTALL_ARGS}
+endif
 
 clean dist distcheck:
 	${AM_v_at}${MAKE} -C ${top_builddir} $@
