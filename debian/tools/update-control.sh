58bb9c92662c816c7291bd2fcdc4b2c3174ac126
Gergely Nagy <algernon@balabit.hu>
2013-07-02 12:58:02 +0200

    debian: Drop the libsyslog-ng-* packages
    Move the main shared library into syslog-ng-core, it is a private
    library anyway. Also rename the libsyslog-ng-dev package to
    syslog-ng-dev accordingly.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- debian/tools/update-control.sh
+++ debian/tools/update-control.sh
@@ -26,20 +26,3 @@ sed -e "s,@UPSTREAM_VERSION@,${UPSTREAM_VERSION},g" \
     -e "/@IF_ENABLED/d" \
     < debian/control.d/control.in \
     > debian/control
-
-# Remove old libsyslog-ng-${VERSION}.* files
-rm -f debian/libsyslog-ng-[0-9].*.install \
-      debian/libsyslog-ng-dev.install
-
-# Update libsyslog-ng-*
-for file in libsyslog-ng.install libsyslog-ng-dev.install; do
-        for feature in . $@; do
-                if [ -e debian/control.d/${feature}/${file} ]; then
-                        cp debian/control.d/${feature}/${file} debian/${file}
-                fi
-        done
-done
-
-# Update debian/libsyslog-ng-${UPSTREAM_VERSION}.install
-mv debian/libsyslog-ng.install \
-   debian/libsyslog-ng-${UPSTREAM_VERSION}.install
