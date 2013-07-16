e8251758e68eaa09f91d6416d8cf743a9251bb43
Juhasz Viktor <jviktor@balabit.hu>
2013-07-16 16:50:46 +0200

    LogTransportMemory: rewritten to be able to handle read and write methods as well
    Signed-off-by: Daniel Gados <gdani@balabit.hu>

--- tests/unit/libtest_memreader.c
+++ tests/unit/libtest_memreader.c
@@ -2,6 +2,8 @@
 #include "persist-state.h"
 #include "plugin.h"
 
+#include "logreader.c"
+
 LogProto *
 create_logproto(gchar *name,LogTransport *transport,LogProtoOptions *options)
 {
@@ -18,28 +20,246 @@ create_logproto(gchar *name,LogTransport *transport,LogProtoOptions *options)
   return proto;
 }
 
+LogTransportMemoryBuffer *
+log_transport_memory_buffer_new(gchar *data, guint32 length)
+{
+  LogTransportMemoryBuffer *buffer = g_new0(LogTransportMemoryBuffer, 1);
+  buffer->data = data;
+  buffer->length = length;
+  buffer->pos = 0;
+  buffer->count = 0;
+
+  return buffer;
+}
+
+void
+log_transport_memory_buffer_free(LogTransportMemoryBuffer *buffer, gboolean free_raw_data)
+{
+  if (free_raw_data)
+    g_free(buffer->data);
+
+  g_free(buffer);
+}
+
+gssize
+log_transport_memory_buffer_read(LogTransportMemoryBuffer *buffer, gpointer buf, gsize count)
+{
+  gsize count_transferred = MIN(buffer->count, count);
+  gsize count_transferred_step1 = 0;
+  gsize count_transferred_step2 = 0;
+
+  g_assert(buffer->pos < buffer->length);
+  g_assert(buffer->count <= buffer->length);
+
+  if (buffer->pos + count_transferred >= buffer->length)
+    {
+      count_transferred_step1 = MIN(buffer->length - buffer->pos, count_transferred);
+      count_transferred_step2 = count_transferred - count_transferred_step1;
+    }
+  else
+    {
+      count_transferred_step1 = count_transferred;
+      count_transferred_step2 = 0;
+    }
+
+  if (count_transferred_step1 > 0)
+    {
+      memcpy(buf, buffer->data + buffer->pos, count_transferred_step1);
+      buffer->pos = (buffer->pos + count_transferred_step1) % buffer->length;
+      buffer->count -= count_transferred_step1;
+
+      g_assert(buffer->count >= 0);
+    }
+
+  if (count_transferred_step2 > 0)
+    {
+      g_assert(buffer->pos == 0);
+
+      memcpy(buf + count_transferred_step1, buffer->data + buffer->pos, count_transferred_step2);
+      buffer->pos = (buffer->pos + count_transferred_step2) % buffer->length;
+      buffer->count -= count_transferred_step2;
+
+      g_assert(buffer->count >= 0);
+    }
+
+  return count_transferred;
+}
+
+gssize
+log_transport_memory_buffer_write(LogTransportMemoryBuffer *buffer, gpointer buf, gsize count)
+{
+  gsize count_transferred = MIN(buffer->length - buffer->count, count);
+  gsize count_transferred_step1 = 0;
+  gsize count_transferred_step2 = 0;
+  gsize startpos = (buffer->pos + buffer->count) % buffer->length;
+
+  g_assert(buffer->pos < buffer->length);
+  g_assert(buffer->count <= buffer->length);
+
+  if (startpos + count_transferred >= buffer->length)
+    {
+      count_transferred_step1 = MIN(buffer->length - startpos, count_transferred);
+      count_transferred_step2 = count_transferred - count_transferred_step1;
+    }
+  else
+    {
+      count_transferred_step1 = count_transferred;
+      count_transferred_step2 = 0;
+    }
+
+  if (count_transferred_step1 > 0)
+    {
+      memcpy(buffer->data + startpos, buf, count_transferred_step1);
+      startpos = (startpos + count_transferred_step1) % buffer->length;
+      buffer->count += count_transferred_step1;
+
+      g_assert(buffer->count <= buffer->length);
+    }
+
+  if (count_transferred_step2 > 0)
+    {
+      g_assert(startpos == 0);
+
+      memcpy(buffer->data + startpos, buf + count_transferred_step1, count_transferred_step2);
+      startpos = (startpos + count_transferred_step2) % buffer->length;
+      buffer->count += count_transferred_step2;
+
+      g_assert(buffer->count <= buffer->length);
+    }
+
+  return count_transferred;
+}
+
+void
+log_transport_memory_buffer_test_write(LogTransportMemoryBuffer *buffer, gchar *buf)
+{
+  char buf20[20];
+  snprintf((char *)buf20, sizeof(buf20), "%s", buf);
+  fprintf(stderr, "WRITE : %s\n", buf20);
+  log_transport_memory_buffer_write(buffer, buf20, MIN(sizeof(buf20), strlen(buf20)) );
+}
+
+gboolean
+log_transport_memory_buffer_test_read(LogTransportMemoryBuffer *buffer, gchar *buf, guint max_read)
+{
+  char buf20[20];
+  memset(buf20, 0x0, sizeof(buf20));
+
+  log_transport_memory_buffer_read(buffer, buf20, MIN(sizeof(buf20), max_read));
+  fprintf(stderr, "READ  : %s\n", buf20);
+  fprintf(stderr, "EXPECT: %s\n", buf);
+  fprintf(stderr, "-------\n");
+  return (strcmp(buf20, buf) == 0);
+}
+
+gboolean
+log_transport_memory_buffer_test()
+{
+  gchar data[10];
+  gboolean res = TRUE;
+
+  LogTransportMemoryBuffer *buffer = log_transport_memory_buffer_new(data, sizeof(data));
+  log_transport_memory_buffer_test_write(buffer, "AAA");
+  res &= log_transport_memory_buffer_test_read(buffer, "AAA", 20);
+  log_transport_memory_buffer_test_write(buffer, "BBBBB");
+  res &= log_transport_memory_buffer_test_read(buffer, "BBBBB", 20);
+  log_transport_memory_buffer_test_write(buffer, "CCCCC");
+  res &= log_transport_memory_buffer_test_read(buffer, "CCCCC", 20);
+  log_transport_memory_buffer_test_write(buffer, "DDDDD");
+  res &= log_transport_memory_buffer_test_read(buffer, "DDDDD", 20);
+  log_transport_memory_buffer_test_write(buffer, "EE");
+  res &= log_transport_memory_buffer_test_read(buffer, "EE", 20);
+  log_transport_memory_buffer_test_write(buffer, "FFFFFFFFFF");
+  res &= log_transport_memory_buffer_test_read(buffer, "FFFFFFFFFF", 20);
+  log_transport_memory_buffer_test_write(buffer, "GGG");
+  log_transport_memory_buffer_test_write(buffer, "GGG");
+  log_transport_memory_buffer_test_write(buffer, "H");
+  res &= log_transport_memory_buffer_test_read(buffer, "GGGGGGH", 20);
+  log_transport_memory_buffer_test_write(buffer, "II");
+  log_transport_memory_buffer_test_write(buffer, "II");
+  res &= log_transport_memory_buffer_test_read(buffer, "IIII", 20);
+  log_transport_memory_buffer_test_write(buffer, "FFFFFFFFFF");
+  res &= log_transport_memory_buffer_test_read(buffer, "FFFFFFFFFF", 20);
+  log_transport_memory_buffer_test_write(buffer, "FFFFFFFFFFF");
+  res &= log_transport_memory_buffer_test_read(buffer, "FFFFFFFFFF", 20);
+  log_transport_memory_buffer_test_write(buffer, "FFFFFF");
+  log_transport_memory_buffer_test_write(buffer, "FFFFF");
+  res &= log_transport_memory_buffer_test_read(buffer, "FFFFFFFFFF", 20);
+
+  log_transport_memory_buffer_free(buffer, FALSE);
+  return res;
+}
+
 gssize
 log_transport_memory_read_method(LogTransport *s, gpointer buf, gsize count, GSockAddr **sa)
 {
   LogTransportMemory *self = (LogTransportMemory *) s;
-  gssize result = 0;
   if (self->read_buffer)
     {
-      result = count < strlen((self->read_buffer + self->read_buffer_pos)) ? count : strlen((self->read_buffer + self->read_buffer_pos));
-      memcpy(buf, self->read_buffer + self->read_buffer_pos, result);
-      self->read_buffer_pos += result;
+      gsize bytes = 0;
+
+      if (self->func_try_read)
+        self->func_try_read(s);
+
+      bytes = log_transport_memory_buffer_read(self->read_buffer, buf, MIN(count, self->max_read));
+
+      if (bytes > 0)
+        {
+          if (self->func_read)
+            self->func_read(s);
+          return bytes;
+        }
+      else
+        {
+          errno = EAGAIN;
+          return -1;
+        }
     }
-  return result;
+
+  return 0;
 }
 
 gssize
 log_transport_memory_write_method(LogTransport *s, const gpointer buf, gsize count)
 {
-  //LogTransportMemory *self = (LogTransportMemory *) s;
+  LogTransportMemory *self = (LogTransportMemory *) s;
+  if (self->write_buffer)
+    {
+      gsize bytes = 0;
+
+      if (self->func_try_write)
+        self->func_try_write(s);
+
+      bytes = log_transport_memory_buffer_write(self->write_buffer, buf, MIN(count, self->max_write));
+
+      if (bytes > 0)
+        {
+          if( self->func_write)
+            self->func_write(s);
+          return bytes;
+        }
+      else
+        {
+          errno = EAGAIN;
+          return -1;
+        }
+    }
+
   return 0;
 }
 
-LogTransport *log_transport_memory_new(gchar *read_buffer, guint32 read_buffer_length, gchar *write_buffer, guint32 write_buffer_length, guint flags)
+void
+log_transport_memory_free_method(LogTransport *s)
+{
+  LogTransportMemory *self = (LogTransportMemory *)s;
+  if (self->read_buffer)
+    log_transport_memory_buffer_free(self->read_buffer, TRUE);
+  if (self->write_buffer)
+    log_transport_memory_buffer_free(self->write_buffer, TRUE);
+}
+
+LogTransport *
+log_transport_memory_new(LogTransportMemoryBuffer *read_buffer, LogTransportMemoryBuffer *write_buffer, guint flags)
 {
   LogTransportMemory *self = g_new0(LogTransportMemory, 1);
 
@@ -48,23 +268,22 @@ LogTransport *log_transport_memory_new(gchar *read_buffer, guint32 read_buffer_l
   self->super.flags = flags;
   self->super.read = log_transport_memory_read_method;
   self->super.write = log_transport_memory_write_method;
-  self->super.free_fn = log_transport_free_method;
-
+  self->super.free_fn = log_transport_memory_free_method;
   self->read_buffer = read_buffer;
-  self->read_buffer_length = read_buffer_length;
-  self->read_buffer_pos = 0;
-
   self->write_buffer = write_buffer;
-  self->write_buffer_length = write_buffer_length;
-  self->write_buffer_pos = 0;
+  self->max_read = (read_buffer ? read_buffer->length : 0);
+  self->max_write = (write_buffer ? write_buffer->length : 0);
+  self->name = "TEST";
   return &self->super;
 }
 
-LogReader *log_reader_new_memory_source(LogReaderOptions *options, guint32 read_buffer_length, LogReaderNotifyMethod notif, LogReaderQueueMethod queue, LogTransport **new_transport, const gchar *prefix, const gchar *garbage, GlobalConfig *cfg)
+LogReader *
+log_reader_new_memory_source(LogReaderOptions *options, guint32 read_buffer_length, LogReaderNotifyMethod notif, LogReaderQueueMethod queue, LogTransport **new_transport, const gchar *prefix, const gchar *garbage, GlobalConfig *cfg)
 {
-  gchar *read_buffer = g_malloc0(read_buffer_length);
+  gchar *buf = g_malloc0(read_buffer_length);
+  LogTransportMemoryBuffer *read_buffer = log_transport_memory_buffer_new(buf, read_buffer_length);
   LogProto *proto = NULL;
-  *new_transport = log_transport_memory_new(read_buffer, read_buffer_length, NULL, 0, 0);
+  *new_transport = log_transport_memory_new(read_buffer, NULL, 0);
   LogProtoServerOptions *proto_options = g_malloc0(sizeof(LogProtoServerOptions));
 
   if (prefix)
@@ -100,11 +319,13 @@ LogReader *log_reader_new_memory_source(LogReaderOptions *options, guint32 read_
   return reader;
 }
 
-LogReader *log_reader_new_file_source(LogReaderOptions *options, guint32 read_buffer_length, LogReaderNotifyMethod notif, LogReaderQueueMethod queue, LogTransport **new_transport, GlobalConfig *cfg)
+LogReader *
+log_reader_new_file_source(LogReaderOptions *options, guint32 read_buffer_length, LogReaderNotifyMethod notif, LogReaderQueueMethod queue, LogTransport **new_transport, GlobalConfig *cfg)
 {
   LogProtoOptions *proto_options = g_malloc0(sizeof(LogProtoOptions));
-  gchar *read_buffer = g_malloc0(read_buffer_length);
-  *new_transport = log_transport_memory_new(read_buffer, read_buffer_length, NULL, 0, 0);
+  gchar *buf = g_malloc0(read_buffer_length);
+  LogTransportMemoryBuffer *read_buffer = log_transport_memory_buffer_new(buf, read_buffer_length);
+  *new_transport = log_transport_memory_new(read_buffer, NULL, 0);
   memset(proto_options,0,sizeof(LogProtoOptions));
   proto_options->super.size = 8192;
   proto_options->super.flags = LPBS_NOMREAD;
@@ -121,8 +342,9 @@ LogReader *log_reader_new_file_source(LogReaderOptions *options, guint32 read_bu
   return reader;
 }
 
-void log_test_reader_add_message(LogTransport *tr, const gchar *msg, guint32 msg_size)
+void
+log_test_reader_add_message(LogTransport *tr, const gchar *msg, guint32 msg_size)
 {
   LogTransportMemory *transport = (LogTransportMemory *)tr;
-  memcpy(transport->read_buffer + strlen(transport->read_buffer), msg, msg_size);
+  log_transport_memory_buffer_write(transport->read_buffer, msg, msg_size);
 }
--- tests/unit/libtest_memreader.h
+++ tests/unit/libtest_memreader.h
@@ -19,59 +19,40 @@
 #include <iv_event.h>
 
 typedef struct {
+  gchar *data;
+  guint32 length;
+  guint32 pos;
+  guint32 count;
+} LogTransportMemoryBuffer;
+
+typedef struct {
   LogTransport super;
-  gchar *read_buffer;
-  guint32 read_buffer_length;
-  guint32 read_buffer_pos;
-  gchar *write_buffer;
-  guint32 write_buffer_length;
-  guint32 write_buffer_pos;
+  LogTransportMemoryBuffer *read_buffer;
+  LogTransportMemoryBuffer *write_buffer;
+  guchar *name;
+  gsize max_read;
+  gsize max_write;
+  void (*func_read)(void *);
+  void (*func_write)(void *);
+  void (*func_try_read)(void *);
+  void (*func_try_write)(void *);
 } LogTransportMemory;
-
-struct _LogReader
-{
-  LogSource super;
-  LogProto *proto;
-  gboolean immediate_check;
-  gboolean waiting_for_preemption;
-  LogPipe *control;
-  LogReaderOptions *options;
-  GSockAddr *peer_addr;
-  gchar *follow_filename;
-  ino_t inode;
-  gint64 size;
-
-  struct iv_fd fd_watch;
-  struct iv_timer follow_timer;
-  struct iv_task restart_task;
-  struct iv_task immediate_check_task;
-  struct iv_event schedule_wakeup;
-  MainLoopIOWorkerJob io_job;
-  gboolean suspended:1;
-  gint pollable_state;
-  gint notify_code;
-  regex_t *prefix_matcher;
-  regex_t *garbage_matcher;
-  /* Because of multiline processing logreader has to store the last read line which should start with the prefix */
-  gchar *partial_message;
-  /* store if we have to wait for a prefix, because last event was a garbage found */
-  gboolean wait_for_prefix;
-  gboolean flush;
-  time_t last_msg_received;
-
-  gboolean pending_proto_present;
-  GCond *pending_proto_cond;
-  GStaticMutex pending_proto_lock;
-  LogProto *pending_proto;
-};
 
 typedef void (*LogReaderNotifyMethod)(LogPipe *s, LogPipe *sender, gint notify_code, gpointer user_data);
 typedef void (*LogReaderQueueMethod)(LogPipe *s, LogMessage *msg, const LogPathOptions *path_options, gpointer user_data);
 
-LogTransport *log_transport_memory_new(gchar *read_buffer, guint32 read_buffer_length, gchar *write_buffer, guint32 write_buffer_length, guint flags);
+LogTransportMemoryBuffer *log_transport_memory_buffer_new(gchar *data, guint32 length);
+void log_transport_memory_buffer_free(LogTransportMemoryBuffer *buffer, gboolean free_raw_data);
+gssize log_transport_memory_buffer_read(LogTransportMemoryBuffer *buffer, gpointer buf, gsize count);
+gssize log_transport_memory_buffer_write(LogTransportMemoryBuffer *buffer, gpointer buf, gsize count);
+
+gboolean log_transport_memory_buffer_test();
+
+LogTransport *log_transport_memory_new(LogTransportMemoryBuffer *read_buffer, LogTransportMemoryBuffer *write_buffer, guint flags);
+
 LogReader *log_reader_new_memory_source(LogReaderOptions *options, guint32 read_buffer_length, LogReaderNotifyMethod notif, LogReaderQueueMethod queue, LogTransport **new_transport, const gchar *prefix, const gchar *garbage, GlobalConfig *cfg);
 LogReader *log_reader_new_file_source(LogReaderOptions *options, guint32 read_buffer_length, LogReaderNotifyMethod notif, LogReaderQueueMethod queue, LogTransport **new_transport, GlobalConfig *cfg);
+
 void log_test_reader_add_message(LogTransport *transport, const gchar *msg, guint32 msg_size);
 
-void log_reader_read_log(LogReader *self);
 #endif
