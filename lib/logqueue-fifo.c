dfea44d4a72909d2ffa31889717a1ca3244668c1
Juhasz Viktor <jviktor@balabit.hu>
2013-08-01 10:08:18 +0200

    queue: print out which destination's queue become full
    Signed-off-by: Juhasz Viktor <jviktor@balabit.hu>

--- lib/logqueue-fifo.c
+++ lib/logqueue-fifo.c
@@ -178,6 +178,7 @@ log_queue_fifo_move_input_unlocked(LogQueueFifo *self, gint thread_id)
                 evt_tag_int("queue_len", queue_len),
                 evt_tag_int("log_fifo_size", self->qoverflow_size),
                 evt_tag_int("count", n),
+                evt_tag_str("persist_name", self->super.persist_name),
                 NULL);
     }
   stats_counter_add(self->super.stored_messages, self->qoverflow_input[thread_id].len);
@@ -288,6 +289,7 @@ log_queue_fifo_push_tail(LogQueue *s, LogMessage *msg, const LogPathOptions *pat
       msg_debug("Destination queue full, dropping message",
                 evt_tag_int("queue_len", log_queue_fifo_get_length(&self->super)),
                 evt_tag_int("log_fifo_size", self->qoverflow_size),
+                evt_tag_str("persist_name", self->super.persist_name),
                 NULL);
     }
   return;
