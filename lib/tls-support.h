8b61d9963fda4dea6e0447ea89364b30ee0b2e38
Juhasz Viktor <jviktor@balabit.hu>
2013-09-05 12:11:03 +0200

    thread local storage: surround the struct with cookies
    So TLS can be found while analyzing a core file in debugger, regardless of
    which TLS implementation is in effect.
    
    In case of the pthread_*() implementation, it's a little hairy to access
    the internals of the __tls_variables struct during initialization, since
    when the function is being compiled, the definition of the struct is
    not created yet. Actually, that's why the size argument is required.
    Since putting the cookies into the struct requires it to be defined when
    the function doing the work is compiled, I needed to defer compilation of
    the helper functions until the struct definition is generated by generating
    the function from a macro. With this, the size argument became useless.
    
    Conflicts:
    
    	lib/tls-support.h

--- lib/tls-support.h
+++ lib/tls-support.h
@@ -26,6 +26,26 @@
 
 /* this header is about Thread Local Storage and not about Transport Layer Security */
 
+/* Some platforms and/or build-specific constellations can make
+ * finding TLS variables in a debugger extremely hard, especially
+ * in case of core files without a running process. To make it
+ * easier, all implementations contain a cookie. To localize the
+ * thread local storage in the memory, look for the string
+ * "TLSBEG*\x00" and "TLSEND*\x00" where * can be one of 'P', 'W'
+ * or 'T'. Between these two 8 character long strings, is the
+ * struct that holds thread local variables. P, W and T means
+ * pthread_*, Windows and __thread implementation respectively.
+ * The exact format of the struct is as follows:
+ *
+ *     char beginning_cookie[8]             -- TLSBEG*\x00
+ *     char *source                         -- Pointer to the name of the source file
+ *     struct { user defined variables };
+ *     char end_cookie[8]                   -- TLSEND*\x00
+ *
+ * Note that in case of POSIX threads, the cookies will only be
+ * put in place when the first TLS variable is used by the program.
+ */
+
 #include "syslog-ng.h"
 
 /* We don't have thread-local-storage support on Win32 and the code that
@@ -36,56 +56,80 @@
 
 #include <stdlib.h>
 #include <pthread.h>
+#include <string.h>
 
-static struct __tls_variables *
-__tls_init_thread(pthread_key_t key, size_t size)
-{
-  struct __tls_variables *ptr;
 
-  ptr = calloc(1, size);
-  if (!ptr)
-    abort();
+#define TLS_BLOCK_START                                                     \
+  static pthread_key_t __tls_key;                                           \
+  static void __attribute__((constructor)) __tls_init_key(void)             \
+  {                                                                         \
+    pthread_key_create(&__tls_key, free);                                   \
+  }                                                                         \
+                                                                            \
+  struct __tls_variables                                                    \
+  {                                                                         \
+    gchar beginning_cookie[8];                                              \
+    gchar *source;                                                          \
+    struct
 
-  pthread_setspecific(key, ptr);
-  return ptr;
-}
+#define TLS_BLOCK_END                                                           \
+    variables;                                                                  \
+    gchar end_cookie[8];                                                        \
+  };                                                                            \
+                                                                                \
+  static struct __tls_variables *                                               \
+  __tls_init_thread(pthread_key_t key, const gchar *source)                     \
+  {                                                                             \
+    struct __tls_variables *ptr;                                                \
+                                                                                \
+    ptr = calloc(1, sizeof(struct __tls_variables));                            \
+    if (!ptr)                                                                   \
+      abort();                                                                  \
+                                                                                \
+    strncpy(ptr->beginning_cookie, "TLSBEGP", sizeof(ptr->beginning_cookie));   \
+    ptr->source = source;                                                       \
+    strncpy(ptr->end_cookie, "TLSENDP", sizeof(ptr->end_cookie));               \
+                                                                                \
+    pthread_setspecific(key, ptr);                                              \
+    return ptr;                                                                 \
+  }                                                                             \
+                                                                                \
+  static inline struct __tls_variables *                                        \
+  __tls_deref_helper(pthread_key_t key, const gchar *source)                    \
+  {                                                                             \
+    struct __tls_variables *ptr;                                                \
+                                                                                \
+    ptr = pthread_getspecific(key);                                             \
+    if (!ptr)                                                                   \
+      ptr = __tls_init_thread(key, source);                                     \
+                                                                                \
+    return ptr;                                                                 \
+  }
 
-static inline struct __tls_variables *__tls_deref_helper(pthread_key_t key, size_t size)
-{
-  struct __tls_variables *ptr;
-
-  ptr = pthread_getspecific(key);
-  if (!ptr)
-    ptr = __tls_init_thread(key, size);
-  return ptr;
-}
-
-#define TLS_BLOCK_START                                         \
-  static pthread_key_t __tls_key;                               \
-  static void __attribute__((constructor)) __tls_init_key(void) \
-  {                                                             \
-    pthread_key_create(&__tls_key, free);                       \
-  }                                                             \
-                                                                \
-  struct __tls_variables
-
-#define TLS_BLOCK_END
-
-#define __tls_deref(var)   (*({ struct __tls_variables *__ptr = __tls_deref_helper(__tls_key, sizeof(struct __tls_variables)); &__ptr->var; }))
+#define __tls_deref(var)   (*({ struct __tls_variables *__ptr = __tls_deref_helper(__tls_key, __BASE_FILE__); &__ptr->variables.var; }))
 
 #define __thread #
 
 #else  /* HAVE_TLS */
 
-#define TLS_BLOCK_START                         \
-  struct __tls_variables
+#define TLS_BLOCK_START                                                     \
+  struct __tls_variables                                                    \
+  {                                                                         \
+    gchar beginning_cookie[8];                                              \
+    gchar *source;                                                          \
+    struct
 
-#define TLS_BLOCK_END                           \
-  ;                                             \
-  static __thread struct __tls_variables __tls
+#define TLS_BLOCK_END                                                       \
+    variables;                                                              \
+    gchar end_cookie[8];                                                    \
+  };                                                                        \
+  static __thread struct __tls_variables __tls = {                          \
+    .beginning_cookie   = { 'T', 'L', 'S', 'B', 'E', 'G', 'T', '\x00' },    \
+    .source             = __BASE_FILE__,                                    \
+    .end_cookie         = { 'T', 'L', 'S', 'E', 'N', 'D', 'T', '\x00' },    \
+  }
 
-
-#define __tls_deref(var)   (__tls.var)
+#define __tls_deref(var)   (__tls.variables.var)
 
 #endif
 
