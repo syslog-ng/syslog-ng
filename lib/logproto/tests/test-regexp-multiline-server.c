02ba94e84972223c62fba37c4533e371295e5755
Balazs Scheidler <bazsi@balabit.hu>
2013-10-23 16:55:44 +0200

    logproto: remove dead code from test-regexp-multiline-server
    As it seems I have left this unit test half-way finished, clean the
    remnants of the indented-multiline tests that I used as skeleton.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- lib/logproto/tests/test-regexp-multiline-server.c
+++ lib/logproto/tests/test-regexp-multiline-server.c
@@ -26,7 +26,7 @@ test_lines_separated_with_prefix(gboolean input_is_stream)
             /* 32 bytes max line length, which means that the complete
              * multi-line block plus one additional line must fit into 32
              * bytes. */
-            (0 && input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
+            (input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
               "Foo First Line\n"
               "Foo Second Line\n"
               "Foo Third Line\n"
@@ -55,7 +55,7 @@ test_lines_separated_with_prefix_and_garbage(gboolean input_is_stream)
             /* 32 bytes max line length, which means that the complete
              * multi-line block plus one additional line must fit into 32
              * bytes. */
-            (0 && input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
+            (input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
               "Foo First Line Bar\n"
               "Foo Second Line Bar\n"
               "Foo Third Line Bar\n"
@@ -84,7 +84,7 @@ test_lines_separated_with_garbage(gboolean input_is_stream)
             /* 32 bytes max line length, which means that the complete
              * multi-line block plus one additional line must fit into 32
              * bytes. */
-            (0 && input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
+            (input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
               "Foo First Line Bar\n"
               "Foo Second Line Bar\n"
               "Foo Third Line Bar\n"
@@ -113,7 +113,7 @@ test_first_line_without_prefix(gboolean input_is_stream)
             /* 32 bytes max line length, which means that the complete
              * multi-line block plus one additional line must fit into 32
              * bytes. */
-            (0 && input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
+            (input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)(
               "First Line\n"
               "Foo Second Line\n"
               "Foo Third Line\n"
@@ -133,59 +133,6 @@ test_first_line_without_prefix(gboolean input_is_stream)
   log_proto_server_free(proto);
 }
 
-
-#if 0
-
-static void
-test_input_starts_with_continuation(gboolean input_is_stream)
-{
-  LogProtoServer *proto;
-
-  proto_server_options.max_msg_size = 32;
-
-  proto = log_proto_regexp_multiline_server_new(
-            /* 32 bytes max line length */
-            (input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)
-            (
-             " 01234567\n", -1,
-             "01234567\n", -1,
-             "newline\n", -1,
-             LTM_PADDING,
-             LTM_EOF),
-            get_inited_proto_server_options());
-
-  assert_proto_server_fetch(proto, " 01234567", -1);
-  assert_proto_server_fetch(proto, "01234567", -1);
-
-  log_proto_server_free(proto);
-}
-
-static void
-test_multiline_at_eof(gboolean input_is_stream)
-{
-  LogProtoServer *proto;
-
-  proto_server_options.max_msg_size = 32;
-
-  proto = log_proto_regexp_multiline_server_new(
-            /* 32 bytes max line length */
-            (input_is_stream ? log_transport_mock_stream_new : log_transport_mock_records_new)
-            (
-             "01234567\n", -1,
-             " 01234567\n", -1,
-             " end\n", -1,
-             LTM_EOF),
-            get_inited_proto_server_options());
-
-  assert_proto_server_fetch(proto, "01234567\n"
-                                   " 01234567\n"
-                                   " end", -1);
-  assert_proto_server_fetch_failure(proto, LPS_EOF, NULL);
-
-  log_proto_server_free(proto);
-}
-#endif
-
 void
 test_log_proto_regexp_multiline_server(void)
 {
@@ -197,9 +144,4 @@ test_log_proto_regexp_multiline_server(void)
   PROTO_TESTCASE(test_lines_separated_with_garbage, TRUE);
   PROTO_TESTCASE(test_first_line_without_prefix, FALSE);
   PROTO_TESTCASE(test_first_line_without_prefix, TRUE);
-
-  //PROTO_TESTCASE(test_line_without_continuation, FALSE);
-  //PROTO_TESTCASE(test_input_starts_with_continuation, TRUE);
-  //PROTO_TESTCASE(test_multiline_at_eof, FALSE);
-  //PROTO_TESTCASE(test_multiline_at_eof, TRUE);
 }
