861489c4b3a895ca88f9cdbb98b351f1c5a330cf
Balazs Scheidler <bazsi@balabit.hu>
2013-11-02 20:19:12 +0100

    LogProtoServer: fixed a use-after-free after reload
    self->options is a borrowed memory area, managed by the owning driver
    that constructed the given LogProtoServer instance. During reload however,
    the driver object itself is freed, while the LogProtoServer instance
    is kept alive, thereby causing the options pointer to point into the
    void.
    
    The solution is to update the options pointer whenever the owner is changed,
    chaining into a similar solution in the LogReader code which does exactly
    that.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

--- lib/logreader.c
+++ lib/logreader.c
@@ -697,6 +697,8 @@ log_reader_set_options(LogPipe *s, LogPipe *control, LogReaderOptions *options, 
   self->control = control;
 
   self->options = options;
+  if (self->proto)
+    log_proto_server_set_options(self->proto, &self->options->proto_options.super);
 }
 
 /* run in the main thread in reaction to a log_reader_reopen to change
