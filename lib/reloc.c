b0b1605dfc14840b446ab7ac8a95047fd021211a
Balazs Scheidler <bazsi@balabit.hu>
2013-11-07 17:13:14 +0100

    reloc: new module that implements autoconf path expansion
    In autoconf the installation layout is controlled by shell variables that
    are expanded recursively.
    
    For instance, we have a prefix variable and then this gets referenced in
    sysconfdir by having the value '${prefix}/etc'
    
    Earlier, the configure script did the expansion prior to putting these
    values in config.h, now this is being pushed to the syslog-ng binary
    so it can be relocated after compilation.
    
    The new reloc module implements the shell-like substitution and
    a PathResolver class which can be used with a Cache instance to resolve
    autoconf-style directory layouts.
    
    Signed-off-by: Viktor Juhasz <jviktor@balabit.hu>
    Signed-off-by: Tamas Pal <folti@balabit.hu>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

---
+++ lib/reloc.c
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "reloc.h"
+#include "cache.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Cache *path_cache;
+
+typedef struct _PathResolver
+{
+  CacheResolver super;
+  GHashTable *configure_variables;
+} PathResolver;
+
+
+void
+path_resolver_add_configure_variable(CacheResolver *s, const gchar *name, const gchar *value)
+{
+  PathResolver *self = (PathResolver *) s;
+
+  g_hash_table_insert(self->configure_variables, g_strdup(name), g_strdup(value));
+}
+
+static void
+path_resolver_populate_configure_variables(PathResolver *self, const gchar *sysprefix)
+{
+  path_resolver_add_configure_variable(&self->super, "${prefix}", sysprefix);
+  path_resolver_add_configure_variable(&self->super, "${exec_prefix}", PATH_EXECPREFIX);
+  path_resolver_add_configure_variable(&self->super, "${libexecdir}", PATH_LIBEXECDIR);
+  path_resolver_add_configure_variable(&self->super, "${datarootdir}", PATH_DATAROOTDIR);
+  path_resolver_add_configure_variable(&self->super, "${datadir}", PATH_DATADIR);
+  path_resolver_add_configure_variable(&self->super, "${localstatedir}", PATH_LOCALSTATEDIR);
+}
+
+
+
+/* NOTE: we really don't have an easy way to handle errors, as installation
+ * paths are resolved quite early while syslog-ng starts up.  The best is to
+ * abort, as an error is either a problem in the configure variables OR a
+ * coding mistake. For now, we desperately try to write an error message to
+ * stderr and then abort the process. */
+
+#define handle_fatal_error(fmt, ...) \
+    do {						\
+      fprintf(stderr, fmt, __VA_ARGS__);		\
+      g_assert_not_reached();				\
+    } while (0)
+
+gpointer
+path_resolver_resolve(CacheResolver *s, const gchar *orig)
+{
+  PathResolver *self = (PathResolver *) s;
+  gchar *subst_start = NULL;
+  gchar *subst_end = NULL;
+  gchar *new_value, *prefix, *suffix, *replacement, *value;
+  gchar *confvar;
+  gint prefix_len, confvar_len;
+
+
+  /* Find variable enclosed within ${...}, replace with the value in
+   * configure_variables.  Repeat until no more substitutions can be made.
+   * */
+
+  value = g_strdup(orig);
+  subst_start = strstr(value, "${");
+  while (subst_start != NULL)
+    {
+      prefix = NULL;
+      prefix_len = 0;
+      suffix = NULL;
+
+      subst_end = strchr(subst_start, '}');
+
+      if (subst_end == NULL)
+        handle_fatal_error("Relocation resolution error: missing '}' in string '%s'. Please re-compile syslog-ng with proper path variables.\n", value);
+
+      /* extract confvar */
+
+      confvar_len = (subst_end + 1) - subst_start;
+      confvar = g_strndup(subst_start, confvar_len);
+
+      /* lookup replacement */
+
+      replacement = g_hash_table_lookup(self->configure_variables, confvar);
+
+      if (replacement == NULL)
+        handle_fatal_error("Relocation resolution error: Unknown configure variable: '%s' in string '%s'.\nPlease re-compile syslog-ng with proper path variables.\n", confvar, value);
+
+      g_free(confvar);
+
+      /* replace confvar with replacement */
+
+      suffix = (subst_end + 1);
+
+      prefix_len = subst_start - value;
+      prefix = g_strndup(value, prefix_len);
+
+      /* construct the new value */
+      new_value = g_strconcat(prefix, replacement, suffix, NULL);
+      g_free(prefix);
+
+      /* commit the new value */
+      g_free(value);
+      value = new_value;
+
+      subst_start = strstr(value, "${");
+    }
+  return value;
+}
+
+static void
+path_resolver_free(CacheResolver *s)
+{
+  PathResolver *self = (PathResolver *) s;
+
+  g_hash_table_unref(self->configure_variables);
+}
+
+CacheResolver *
+path_resolver_new(const gchar *sysprefix)
+{
+  PathResolver *self = g_new0(PathResolver, 1);
+
+  self->super.resolve_elem = path_resolver_resolve;
+  self->super.free_elem = g_free;
+  self->super.free_fn = path_resolver_free;
+  self->configure_variables = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_free);
+
+  path_resolver_populate_configure_variables(self, sysprefix);
+
+  return &self->super;
+}
+
+static gchar *
+lookup_sysprefix(void)
+{
+  gchar *v;
+
+  v = getenv("SYSLOGNG_PREFIX");
+  if (v)
+    return v;
+  return PATH_PREFIX;
+}
+
+const gchar *
+get_installation_path_for(const gchar *template)
+{
+  if (!path_cache)
+    path_cache = cache_new(path_resolver_new(lookup_sysprefix()));
+  return cache_lookup(path_cache, template);
+}
---
+++ lib/tests/test_reloc.c
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+#include "testutils.h"
+#include "reloc.h"
+
+CacheResolver *path_resolver;
+Cache *path_cache;
+
+static void
+assert_install_path(const gchar *template, const gchar *expected)
+{
+  const gchar *expanded_path;
+
+  expanded_path = cache_lookup(path_cache, template);
+  assert_string(expanded_path, expected, "Expanded install path doesn't match expected value");
+}
+
+static void
+test_absolute_dirs_remain_unchanged(void)
+{
+  assert_install_path("/opt/syslog-ng", "/opt/syslog-ng");
+}
+
+static void
+test_configure_variables_are_resolved(void)
+{
+  assert_install_path("${prefix}/bin", "/test/bin");
+}
+
+static void
+test_configure_variables_are_resolved_recursively(void)
+{
+  path_resolver_add_configure_variable(path_resolver, "${foo}", "/foo");
+  path_resolver_add_configure_variable(path_resolver, "${bar}", "${foo}");
+
+  assert_install_path("${foo}/bin", "/foo/bin");
+  assert_install_path("${bar}/bin", "/foo/bin");
+}
+
+static void
+test_confiure_variables_are_resolved_in_the_middle(void)
+{
+  assert_install_path("/foo/${prefix}/bar", "/foo//test/bar");
+}
+
+static void
+init_path_cache(void)
+{
+  path_resolver = path_resolver_new("/test");
+  path_cache = cache_new(path_resolver);
+}
+
+static void
+free_path_cache(void)
+{
+  cache_free(path_cache);
+}
+
+int
+main(int argc, char *argv[])
+{
+  init_path_cache();
+  test_absolute_dirs_remain_unchanged();
+  test_configure_variables_are_resolved();
+  test_configure_variables_are_resolved_recursively();
+  test_confiure_variables_are_resolved_in_the_middle();
+  free_path_cache();
+  return 0;
+}
