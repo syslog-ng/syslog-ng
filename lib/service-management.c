a7f6989815dbb358343f31c126db885254e5f38a
Balazs Scheidler <bazsi@balabit.hu>
2013-11-11 22:10:21 +0100

    mainloop: factor out systemd communication into a service-management module
    Some communication takes place between syslog-ng and systemd after
    the former has started. We indicate when initialization at startup
    finished and also some textual messages to indicate our state.
    
    Earlier it was right in the middle of the mainloop code, now factor it
    out to a separate module, perhaps this interface can be useful
    for other similar implementations.
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

---
+++ lib/service-management.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 1998-2013 Balázs Scheidler
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+#include "service-management.h"
+
+#if ENABLE_SYSTEMD
+#include <systemd/sd-daemon.h>
+
+void
+service_management_publish_status(const gchar *status)
+{
+  gchar *status_buffer;
+  time_t now = time(NULL);
+
+  status_buffer = g_strdup_printf("STATUS=%s (%s)", status, ctime(&now));
+  sd_notify(0, status_buffer);
+  g_free(status_buffer);
+}
+
+void
+service_management_clear_status(void)
+{
+  sd_notify(0, "STATUS=");
+}
+
+void
+service_management_indicate_readiness(void)
+{
+  sd_notify(0, "READY=1");
+}
+
+#endif
