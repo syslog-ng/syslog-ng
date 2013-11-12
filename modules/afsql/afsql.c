4616550f7e6f4d1293e908d9f288ae5bf224df6c
Daniel Gados <gdani@balabit.hu>
2013-11-12 16:04:25 +0100

    afsql: change column hash calculation for oracle from md5 to sha1
    Signed-off-by: Daniel Gados <gdani@balabit.hu>

--- modules/afsql/afsql.c
+++ modules/afsql/afsql.c
@@ -35,7 +35,6 @@
 
 #include <dbi/dbi.h>
 #include <string.h>
-#include <openssl/md5.h>
 
 /* field flags */
 enum
@@ -394,19 +393,26 @@ afsql_dd_create_index(AFSqlDestDriver *self, gchar *table, gchar *column)
   if (strcmp(self->type, s_oracle) == 0)
     {
       /* NOTE: oracle index indentifier length is max 30 characters
-       * so we use the first 30 characters of the table_column md5 hash */
+       * so we use the first 30 characters of the table_column sha1 hash */
       if ((strlen(table) + strlen(column)) > 25)
         {
 
 #if ENABLE_SSL
-          guchar hash[MD5_DIGEST_LENGTH];
+          EVP_MD_CTX sha;
+          gint hashlen;
+          guchar hash[EVP_MAX_MD_SIZE];
           gchar hash_str[31];
           gchar *cat = g_strjoin("_", table, column, NULL);
 
-          MD5((guchar *)cat, strlen(cat), hash);
+          EVP_MD_CTX_init(&sha);
+          EVP_DigestInit_ex(&sha, EVP_sha1(), NULL);
+          EVP_DigestUpdate(&sha, cat, strlen(cat));
+          EVP_DigestFinal_ex(&sha, hash, &hashlen);
+          EVP_MD_CTX_cleanup(&sha);
+
           g_free(cat);
 
-          format_hex_string(hash, sizeof(hash), hash_str, sizeof(hash_str));
+          format_hex_string(hash, hashlen, hash_str, sizeof(hash_str));
           hash_str[0] = 'i';
           g_string_printf(query_string, "CREATE INDEX %s ON %s (%s)",
               hash_str, table, column);
