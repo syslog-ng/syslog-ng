8820c4011785624d6c7331220fafb9d345f2a841
Viktor Tusa <tusa@balabit.hu>
2013-11-22 14:07:45 +0100

    coverage: Removed running of firefox in coverage collector script
    Because it prevents coverage generation on a non-desktop machine...

--- tests/collect-cov.sh
+++ tests/collect-cov.sh
@@ -7,4 +7,3 @@ lcov --extract coverage.info "${top_srcdir}/*" --output-file coverage.info
 lcov --remove coverage.info "${top_srcdir}/modules/afmongodb/libmongo-client/*" --output-file coverage.info
 lcov --remove coverage.info "${top_srcdir}/modules/afamqp/rabbitmq-c/*" --output-file coverage.info
 genhtml coverage.info --output-directory coverage.html
-firefox  coverage.html/index.html
