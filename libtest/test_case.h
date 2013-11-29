6c973c6620046f3fb49ea298eba16414268ab3ef
Juhász Viktor <jviktor@balabit.hu>
2013-11-29 10:36:51 +0100

    test_rewrite: add unit test to detect the rewrite condition reference corruption.
    Signed-off-by: Juhász Viktor <jviktor@balabit.hu>

---
+++ libtest/test_case.h
@@ -0,0 +1,37 @@
+/*
+ * test_case.h
+ *
+ *  Created on: Nov 27, 2013
+ *      Author: jviktor
+ */
+
+#ifndef TEST_CASE_H_
+#define TEST_CASE_H_
+
+typedef struct _TestCase TestCase;
+
+struct _TestCase {
+  void (*setup)(TestCase *self);
+  void (*run)(TestCase *self);
+  void (*teardown)(TestCase *self);
+};
+
+
+static void
+run_test_case(TestCase *tc)
+{
+  if (tc->setup) tc->setup(tc);
+  tc->run(tc);
+  if (tc->teardown) tc->teardown(tc);
+}
+
+#define RUN_TESTCASE(class_name, test_case_prefix, test_function) { \
+  class_name *tc = g_new0(class_name, 1); \
+  tc->super.setup = test_case_prefix ## _setup; \
+  tc->super.teardown = test_case_prefix ## _teardown; \
+  tc->super.run = test_function;\
+  run_test_case(&tc->super); \
+  g_free(tc); \
+}
+
+#endif /* TEST_CASE_H_ */
---
+++ tests/unit/test_rewrite.c
@@ -0,0 +1,71 @@
+#include "apphook.h"
+#include "plugin.h"
+#include "testutils.h"
+#include "config_parse_lib.h"
+#include "logrewrite.h"
+#include "test_case.h"
+
+typedef struct _RewriteTestCase {
+  TestCase super;
+  LogProcessRule *log_process_rule;
+  LogRewrite *test_rewrite;
+  LogMessage *msg;
+  NVHandle device_id;
+  LogPathOptions po;
+} RewriteTestCase;
+
+void
+rewrite_testcase_setup(TestCase *s)
+{
+  RewriteTestCase *self = (RewriteTestCase *)s;
+  configuration = cfg_new(0x500);
+  assert_true(parse_config("rewrite s_test{set(\"00100\", value(\"device_id\") condition(program(\"ARCGIS\")));};", LL_CONTEXT_ROOT, NULL, NULL), ASSERTION_ERROR("Parsing the given configuration failed"));
+  self->log_process_rule = g_hash_table_lookup(configuration->rewriters, "s_test");
+  assert_not_null(self->log_process_rule, ASSERTION_ERROR("Can't find parsed rewrite rule"));
+  self->device_id = log_msg_get_value_handle("device_id");
+  self->test_rewrite = (LogRewrite *)self->log_process_rule->head->data;
+  self->msg = log_msg_new_empty();
+  log_msg_set_value(self->msg, LM_V_PROGRAM, "ARCGIS", -1);
+  self->po.ack_needed = FALSE;
+  self->po.flow_control_requested = FALSE;
+  self->po.matched = NULL;
+}
+
+void
+rewrite_testcase_teardown(TestCase *s)
+{
+  RewriteTestCase *self = (RewriteTestCase *)s;
+  log_msg_unref(self->msg);
+  cfg_free(configuration);
+}
+
+void
+test_condition_success(TestCase *s)
+{
+  RewriteTestCase *self = (RewriteTestCase *) s;
+  log_pipe_queue((LogPipe *) self->test_rewrite, log_msg_ref(self->msg), &self->po);
+  assert_string(log_msg_get_value(self->msg, self->device_id, NULL), "00100", ASSERTION_ERROR("Bad device_id"));
+}
+
+void
+test_reference_on_condition_cloned(TestCase *s)
+{
+  RewriteTestCase *self = (RewriteTestCase *) s;
+  LogPipe *cloned_rule = log_process_pipe_clone(self->test_rewrite);
+  assert_guint32(self->test_rewrite->condition->ref_cnt, 2, ASSERTION_ERROR("Bad reference number of condition"));
+  log_pipe_unref(cloned_rule);
+  assert_guint32(self->test_rewrite->condition->ref_cnt, 1, ASSERTION_ERROR("Bad reference number of condition"));
+}
+
+int
+main(int argc, char **argv)
+{
+	app_startup();
+	putenv("TZ=MET-1METDST");
+	tzset();
+
+	start_grabbing_messages();
+	RUN_TESTCASE(RewriteTestCase, rewrite_testcase, test_condition_success);
+	RUN_TESTCASE(RewriteTestCase, rewrite_testcase, test_reference_on_condition_cloned);
+	stop_grabbing_messages();
+}
