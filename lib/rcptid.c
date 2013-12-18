da82f40d973c8f7a10a00939b0e6c6b9805bbdcd
Balazs Scheidler <bazsi@balabit.hu>
2013-12-18 19:18:02 +0100

    rcptid: Add unique receipient id framework
    This patch adds an rcptid subsytem that can be used to generate per-message
    identifiers unique on a given host.
    
    This id is persistent across restarts, and is globally unique for the messages
    generated/received on this host/instance.  It is somewhat faster than uuid
    template function, but is only a non-zero 48 bit counter.
    
    Signed-off-by: Csaba Karsai <cskarsai@balabit.hu>
    Signed-off-by: Viktor Juhasz <jviktor@balabit.hu>
    Signed-off-by: Viktor Tusa <tusa@balabit.hu>
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

---
+++ lib/rcptid.c
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2013 Viktor Juhasz
+ * Copyright (c) 2013 Viktor Tusa
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+#include "rcptid.h"
+#include "messages.h"
+#include "str-format.h"
+
+static struct _RcptidService
+{
+  PersistState *persist_state;
+  PersistEntryHandle persist_handle;
+  GStaticMutex lock;
+} rcptid_service = 
+{
+  .lock = G_STATIC_MUTEX_INIT
+};
+
+/* NOTE: RcptIdInstance is a singleton, so we don't pass self around as an argument */
+
+#define self (&rcpt_instance)
+
+static RcptidState *
+rcptid_map_state(void)
+{
+  return (RcptidState *) persist_state_map_entry(rcptid_service.persist_state, rcptid_service.persist_handle);
+}
+
+static void
+rcptid_unmap_state(void)
+{
+  persist_state_unmap_entry(rcptid_service.persist_state, rcptid_service.persist_handle);
+}
+
+static gboolean
+rcptid_is_initialized(void)
+{
+  return rcptid_service.persist_state != NULL;
+}
+
+static gboolean
+rcptid_restore_entry(void)
+{
+  RcptidState *data = rcptid_map_state();
+
+  if (data->header.version > 0)
+    {
+      msg_error("Internal error restoring log reader state, stored data is too new",
+                evt_tag_int("version", data->header.version),
+                NULL);
+      return FALSE;
+    }
+
+  if ((data->header.big_endian && G_BYTE_ORDER == G_LITTLE_ENDIAN) ||
+      (!data->header.big_endian && G_BYTE_ORDER == G_BIG_ENDIAN))
+    {
+      data->header.big_endian = !data->header.big_endian;
+      data->g_rcptid = GUINT64_SWAP_LE_BE(data->g_rcptid);
+    }
+
+  rcptid_unmap_state();
+  return TRUE;
+}
+
+static gboolean
+rcptid_create_new_entry(void)
+{
+  RcptidState *data;
+
+  rcptid_service.persist_handle = persist_state_alloc_entry(rcptid_service.persist_state, "next.rcptid", sizeof(RcptidState));
+  if (!rcptid_service.persist_handle)
+    {
+      msg_error("Error allocating RCPTID structure in persist-state",
+                NULL);
+      return FALSE;
+    }
+
+  data = rcptid_map_state();
+  data->header.version = 0;
+  data->header.big_endian = (G_BYTE_ORDER == G_BIG_ENDIAN);
+  data->g_rcptid = 1;
+  rcptid_unmap_state();
+  return TRUE;
+}
+
+void
+rcptid_append_formatted_id(GString *result, guint64 rcptid)
+{
+  if (rcptid)
+    {
+      format_uint64_padded(result, 0, 0, 10, rcptid);
+    }
+}
+
+void
+rcptid_set_id(guint64 id)
+{
+  RcptidState *data;
+
+  if (!rcptid_is_initialized())
+    return;
+
+  g_static_mutex_lock(&rcptid_service.lock);
+
+  data = rcptid_map_state();
+  data->g_rcptid = id;
+
+  rcptid_unmap_state();
+
+  g_static_mutex_unlock(&rcptid_service.lock);
+}
+
+guint64
+rcptid_generate_id(void)
+{
+  RcptidState *data;
+  guint64 new_id;
+
+  if (!rcptid_is_initialized())
+    return 0;
+
+  g_static_mutex_lock(&rcptid_service.lock);
+
+  data = rcptid_map_state();
+
+  new_id = data->g_rcptid++;
+  if (!(data->g_rcptid &= 0xFFFFFFFFFFFF))
+    ++data->g_rcptid;
+
+  rcptid_unmap_state();
+
+  g_static_mutex_unlock(&rcptid_service.lock);
+
+  return new_id;
+}
+
+/*restore RCTPID from persist file, if possible, else
+  create new enrty point with "next.rcptid" name*/
+gboolean
+rcptid_init(PersistState *state, gboolean use_rcptid)
+{
+  gsize size;
+  guint8 version;
+
+  g_assert(rcptid_service.persist_state == NULL);
+  if (!use_rcptid)
+    return TRUE;
+
+  rcptid_service.persist_state = state;
+  rcptid_service.persist_handle = persist_state_lookup_entry(state, "next.rcptid", &size, &version);
+
+  if (rcptid_service.persist_handle)
+    return rcptid_restore_entry();
+  else
+    return rcptid_create_new_entry();
+}
+
+void
+rcptid_deinit(void)
+{
+  rcptid_service.persist_state = NULL;
+}
---
+++ lib/tests/test_rcptid.c
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2013 Viktor Juhasz
+ * Copyright (c) 2013 Viktor Tusa
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+#include "testutils.h"
+#include "syslog-ng.h"
+#include "apphook.h"
+#include "rcptid.h"
+
+#include <unistd.h>
+
+gboolean verbose = FALSE;
+PersistState *state;
+
+void
+setup_persist_id_test()
+{
+  unlink("test_values.persist");
+  state = persist_state_new("test_values.persist");
+
+  if (!persist_state_start(state))
+    {
+      fprintf(stderr, "Error starting persist_state object\n");
+      exit(1);
+    }
+  rcptid_init(state, TRUE);
+}
+
+void
+teardown_persist_id_test()
+{
+  persist_state_commit(state);
+  persist_state_free(state);
+  rcptid_deinit();
+}
+
+static void
+test_rcptid_is_persistent_across_persist_backend_reinits(void)
+{
+  guint64 rcptid;
+
+  setup_persist_id_test();
+
+  rcptid_set_id(0x0000FFFFFFFFFFFE);
+  rcptid = rcptid_generate_id();
+
+  assert_guint64(rcptid, 0x0000FFFFFFFFFFFE, "Rcptid initialization to specific value failed!");
+  
+  persist_state_commit(state);
+  persist_state_free(state);
+
+  state = persist_state_new("test_values.persist");
+
+  if (!persist_state_start(state))
+    {
+      fprintf(stderr, "Error starting persist_state object\n");
+      exit(1);
+    }
+
+  rcptid_deinit();
+  rcptid_init(state, TRUE);
+
+  rcptid = rcptid_generate_id();
+
+  assert_guint64(rcptid, 0x0000FFFFFFFFFFFF, "Rcptid did not persisted across persist backend reinit!");
+
+  teardown_persist_id_test();
+}
+
+static void
+test_rcptid_overflows_at_48bits_and_is_reset_to_one(void)
+{
+  guint64 rcptid;
+  
+  setup_persist_id_test();
+
+  rcptid_set_id(0x0000FFFFFFFFFFFF);
+  rcptid = rcptid_generate_id();
+  rcptid = rcptid_generate_id();
+
+  assert_guint64(rcptid, (guint64) 1, "Rcptid counter overflow handling did not work!");
+  teardown_persist_id_test();
+}
+
+static void
+test_rcptid_is_formatted_as_a_number_when_nonzero(void)
+{
+  GString *formatted_rcptid = g_string_sized_new(0);
+
+  setup_persist_id_test();
+
+  rcptid_append_formatted_id(formatted_rcptid, 1);
+  assert_string(formatted_rcptid->str, "1", "RCPTID macro formatting for non-zero number failed!");
+
+  teardown_persist_id_test();
+  g_string_free(formatted_rcptid, TRUE);
+}
+
+static void
+test_rcptid_is_an_empty_string_when_zero(void)
+{
+  GString *formatted_rcptid = g_string_sized_new(0);
+
+  setup_persist_id_test();
+
+  rcptid_append_formatted_id(formatted_rcptid, 0);
+  assert_string(formatted_rcptid->str, "", "RCPTID macro formatting for zero value failed!");
+
+  teardown_persist_id_test();
+  g_string_free(formatted_rcptid, TRUE);
+}
+
+static void
+rcptid_test_case()
+{
+  test_rcptid_is_persistent_across_persist_backend_reinits();
+  test_rcptid_overflows_at_48bits_and_is_reset_to_one();
+  test_rcptid_is_formatted_as_a_number_when_nonzero();
+  test_rcptid_is_an_empty_string_when_zero();
+}
+
+int
+main(int argc G_GNUC_UNUSED, char *argv[] G_GNUC_UNUSED)
+{
+  if (argc > 1)
+    verbose = TRUE;
+  app_startup();
+
+  rcptid_test_case();
+  return 0;
+}
