be381a4e3f3eca0695f5976678ea26c1ec0a80e4
Balazs Scheidler <bazsi@balabit.hu>
2013-12-30 19:04:18 +0100

    pseudofile: new destination driver to deliver messages to pseudo files
    This is a very simple destination driver that can be used to deliver
    messages to special files, like the ones in /proc or /dev.
    
    It is much simpler than the file driver:
      - opens and closes the file for each message instead of keeping it opened
      - doesn't support templates in the name of the file
      - works in the context of the input driver, doesn't have an actual queue
    
    Signed-off-by: Balazs Scheidler <bazsi@balabit.hu>

---
+++ modules/pseudofile/pseudofile-parser.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 1998-2013 Balázs Scheidler
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "pseudofile.h"
+#include "cfg-parser.h"
+#include "pseudofile-grammar.h"
+
+extern int pseudofile_debug;
+
+int pseudofile_parse(CfgLexer *lexer, LogDriver **instance, gpointer arg);
+
+static CfgLexerKeyword pseudofile_keywords[] = {
+  { "pseudofile",               KW_PSEUDOFILE },
+
+  { NULL }
+};
+
+CfgParser pseudofile_parser =
+{
+#if ENABLE_DEBUG
+  .debug_flag = &pseudofile_debug,
+#endif
+  .name = "pseudofile",
+  .keywords = pseudofile_keywords,
+  .parse = (gint (*)(CfgLexer *, gpointer *, gpointer)) pseudofile_parse,
+  .cleanup = (void (*)(gpointer)) log_pipe_unref,
+};
+
+CFG_PARSER_IMPLEMENT_LEXER_BINDING(pseudofile_, LogDriver **)
