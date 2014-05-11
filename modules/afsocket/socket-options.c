40ee68ad674356294ed7f2fd2f75a7b7d46baf33
Viktor Tusa <tusavik@gmail.com>
2014-05-11 00:26:42 +0000

    socket: Various leak fixes in afsocket module.
    TransportMapper and SocketOptions structures are not embedded ones, so
    they should be freed up, when freeing up drivers.
    
    Reported-by: Xufeng Zhang <xufeng.zhang@windriver.com>
    Signed-off-by: Viktor Tusa <tusavik@gmail.com>
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- modules/afsocket/socket-options.c
+++ modules/afsocket/socket-options.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2002-2014 BalaBit IT Ltd, Budapest, Hungary
  * Copyright (c) 1998-2013 Balázs Scheidler
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -82,6 +82,7 @@ void
 socket_options_init_instance(SocketOptions *self)
 {
   self->setup_socket = socket_options_setup_socket_method;
+  self->free = g_free;
 }
 
 SocketOptions *
