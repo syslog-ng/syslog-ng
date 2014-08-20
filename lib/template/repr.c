8951499d4ef2ea98d96fa81a669526b3686396c8
Balazs Scheidler <bazsi@balabit.hu>
2014-08-20 19:02:03 +0200

    templates: split off compiled template representation into a separate module
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

---
+++ lib/template/repr.c
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2002-2013 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 1998-2013 Balázs Scheidler
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "template/repr.h"
+
+void
+log_template_elem_free(LogTemplateElem *e)
+{
+  switch (e->type)
+    {
+    case LTE_FUNC:
+      if (e->func.state)
+        {
+          e->func.ops->free_state(e->func.state);
+          g_free(e->func.state);
+        }
+      if (e->func.ops && e->func.ops->free_fn)
+        e->func.ops->free_fn(e->func.ops);
+      break;
+    }
+  if (e->default_value)
+    g_free(e->default_value);
+  if (e->text)
+    g_free(e->text);
+  g_free(e);
+}
+
+void
+log_template_elem_free_list(GList *el)
+{
+  for (; el; el = el->next)
+    {
+      log_template_elem_free((LogTemplateElem *) el->data);
+    }
+  g_list_free(el);
+}
