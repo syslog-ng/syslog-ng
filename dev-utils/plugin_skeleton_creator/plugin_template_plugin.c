89361d4d1817560ce8c906209355f1b737a28010
Juhász Viktor <viktor.juhasz@balabit.com>
2014-09-24 12:06:10 +0200

    plugin-skeleton-creator: Creating skeleton for a new plugin is very mechanic and demotivating
    This shell script create a plugin skeleton, to help focus on the functionality of the plugin,
    instead of lot of energy goes to create the skeleton and make it "make-able".
    
    This script help to create new plugin with creating skeleton
    Parameters:
    	-n Name of the plugin (required)
    	-k The keyword in config file (default: name of the plugin)
    	-t Type of the plugin (default: LL_CONTEXT_DESTINATION)
    	-d Plugin root dir (default: ${top_srcdir}/modules)
    	-h Print this help message
    
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

---
+++ dev-utils/plugin_skeleton_creator/plugin_template_plugin.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2002-2012 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 1998-2012 Balázs Scheidler
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "cfg-parser.h"
+#include "plugin.h"
+#include "plugin-types.h"
+
+extern CfgParser @PLUGIN_NAME_US@_parser;
+
+static Plugin @PLUGIN_NAME_US@_plugins[] =
+{
+  {
+    .type = @PLUGIN_TYPE@,
+    .name = "@PLUGIN_KEY@",
+    .parser = &@PLUGIN_NAME_US@_parser,
+  },
+};
+
+gboolean
+@PLUGIN_NAME_US@_module_init(GlobalConfig *cfg, CfgArgs *args)
+{
+  plugin_register(cfg, @PLUGIN_NAME_US@_plugins, G_N_ELEMENTS(@PLUGIN_NAME_US@_plugins));
+  return TRUE;
+}
+
+const ModuleInfo module_info =
+{
+  .canonical_name = "@PLUGIN_NAME_US@",
+  .version = VERSION,
+  .description = "Please fill this description",
+  .core_revision = SOURCE_REVISION,
+  .plugins = @PLUGIN_NAME_US@_plugins,
+  .plugins_len = G_N_ELEMENTS(@PLUGIN_NAME_US@_plugins),
+};
+
