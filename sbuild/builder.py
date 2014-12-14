b1d7da38a0e6b07e74681c52d69ed1756774c09d
Viktor Juhász <jviktor@balabit.com>
2014-12-14 13:28:31 +0100

    builder: set the SOURCE_REVISION environment in configuriation time
    Signed-off-by: Viktor Juhász <jviktor@balabit.com>

--- sbuild/builder.py
+++ sbuild/builder.py
@@ -1,12 +1,26 @@
 from AutotoolsBuilder import AutotoolsBuilder
 import os
+import Utils
+import subprocess
 
 class SyslogNGCoreBuilder(AutotoolsBuilder):
+    def get_source_revision(self):
+        branch_cmd = "git branch | grep '^\* ' | cut -c3-"
+        branch = subprocess.check_output(branch_cmd, shell=True, close_fds=False, cwd=self.source_dir).strip()
+        remote_cmd = "git rev-parse --abbrev-ref --symbolic-full-name @{u} | cut -f1 -d/"
+        remote = subprocess.check_output(remote_cmd, shell=True, close_fds=False, cwd=self.source_dir).strip()
+        remote_url_cmd = "git remote -v | grep \"^%s\" | head -1 | awk '{print $2}'"%remote
+        remote_url = subprocess.check_output(remote_url_cmd, shell=True, close_fds=False, cwd=self.source_dir).strip()
+        sha_cmd = "git rev-list --max-count=1 HEAD"
+        sha = subprocess.check_output(sha_cmd, shell=True, close_fds=False, cwd=self.source_dir).strip()
+        return "%s#%s#%s"%(remote_url, branch, sha)
 
     def environment_override(self):
+        revision = self.get_source_revision()
         self._add_extra_env(
          {"PKG_CONFIG_PATH" : "%s/lib/pkgconfig"%(self.install_dir),
-          "PATH": os.environ['PATH'] + ":%s/bin"%(self.install_dir)
+          "PATH": os.environ['PATH'] + ":%s/bin"%(self.install_dir),
+          "SOURCE_REVISION" : revision,
          })
 
 def get_builder():
