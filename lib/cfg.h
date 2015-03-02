a722e12299b6f9d7dc2e139739731c4164fd2993
Benke Tibor <tibor.benke@balabit.com>
2015-03-02 11:14:55 +0100

    version: bump default config version to 5.3
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- lib/cfg.h
+++ lib/cfg.h
@@ -44,8 +44,8 @@ struct _LogConnection;
 struct _LogCenter;
 struct _LogTemplate;
 
-#define CFG_CURRENT_VERSION 0x0502
-#define CFG_CURRENT_VERSION_STRING "5.2"
+#define CFG_CURRENT_VERSION 0x0503
+#define CFG_CURRENT_VERSION_STRING "5.3"
 
 /* configuration data kept between configuration reloads */
 typedef struct _PersistConfig PersistConfig;
