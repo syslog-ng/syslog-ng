a5bca8628f43157ace2371931be22acd1fd37023
Balazs Scheidler <balazs.scheidler@balabit.com>
2015-03-05 17:10:37 +0100

    debuggercli: add lexertoken module
    This module contains a datastruct called LexerToken that is being used to
    communicate tokens between lexers and grammars.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

---
+++ modules/python/pylib/syslogng/debuggercli/lexertoken.py
@@ -0,0 +1,11 @@
+from __future__ import print_function, absolute_import
+import ply.lex
+
+
+class LexerToken(ply.lex.LexToken):
+    def __init__(self, type, value=None, partial=False, lexpos=-1):
+        self.type = type
+        self.value = value
+        self.partial = partial
+        self.lineno = 0
+        self.lexpos = lexpos
