aa2305550fc29948eaf490612c8277f671366908
Gregory Nagy <grigori.grant@gmail.com>
2015-04-05 18:57:10 +0200

    bootstrap: requirements file added for installing python dependencies easily in TravisCI or other environments. `pip install -r requirements.txt` installs the required modules specified in the file.
    Signed-off-by: Gergő Nagy <gergo.nagy@balabit.com>

--- .bootstrap/travis_install_ubuntu.sh
+++ .bootstrap/travis_install_ubuntu.sh
@@ -5,6 +5,5 @@ sudo apt-get install -qq -y curl
 curl --silent http://packages.madhouse-project.org/debian/add-release.sh | sudo sh
 sudo apt-get update -qq -y
 sudo apt-get install -qq -y pkg-config flex bison xsltproc docbook-xsl libevtlog-dev libnet1-dev libglib2.0-dev libdbi0-dev libssl-dev libjson0-dev libwrap0-dev libpcre3-dev libcap-dev libesmtp-dev libgeoip-dev libhiredis-dev sqlite3 libdbd-sqlite3 libriemann-client-dev
-sudo pip install nose ply pep8 pylint
-
+sudo pip install -r requirements.txt
 
