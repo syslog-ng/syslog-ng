96856bb0dfac62f8b2aee3c111d19d9dc2c03671
PÁSZTOR György <gyorgy.pasztor@balabit.com>
2015-04-09 17:07:25 +0200

    lib/tlscontext.c:
    * option change back to l, since that is an iterater. See bazsi's comment
    * remove the "none" option. if we want an empty set instead of no-sslv2, then we can write ssl_options() which also means an empty set. So, based on this, we do not need the "none" keyword. however, recent ssl libraries does not allow sslv2, even if we do not set the SSL_OP_NO_SSLv2 option.
    
    Signed-off-by: PÁSZTOR György <gyorgy.pasztor@balabit.com>

--- lib/tlscontext.c
+++ lib/tlscontext.c
@@ -432,26 +432,24 @@ TLSSslOptions
 tls_lookup_options(GList *options)
 {
   TLSSslOptions ret=TSO_NONE;
-  GList *option;
-  for(option=options; option != NULL ; option=option->next)
+  GList *l;
+  for(l=options; l != NULL ; l=l->next)
     {
-      msg_debug("ssl-option",evt_tag_str("opt",option->data),NULL);
-      if(strcasecmp(option->data,"none") == 0)
-        ret|=TSO_NONE;
-      else if (strcasecmp(option->data,"no-sslv2") == 0 || strcasecmp(option->data,"no_sslv2") == 0)
+      msg_debug("ssl-option",evt_tag_str("opt",l->data),NULL);
+      if (strcasecmp(l->data,"no-sslv2") == 0 || strcasecmp(l->data,"no_sslv2") == 0)
         ret|=TSO_NOSSLv2;
-      else if (strcasecmp(option->data,"no-sslv3") == 0 || strcasecmp(option->data,"no_sslv3") == 0)
+      else if (strcasecmp(l->data,"no-sslv3") == 0 || strcasecmp(l->data,"no_sslv3") == 0)
         ret|=TSO_NOSSLv3;
-      else if (strcasecmp(option->data,"no-tlsv1") == 0 || strcasecmp(option->data,"no_tlsv1") == 0)
+      else if (strcasecmp(l->data,"no-tlsv1") == 0 || strcasecmp(l->data,"no_tlsv1") == 0)
         ret|=TSO_NOTLSv1;
 #ifdef SSL_OP_NO_TLSv1_2
-      else if (strcasecmp(option->data,"no-tlsv11") == 0 || strcasecmp(option->data,"no_tlsv11") == 0)
+      else if (strcasecmp(l->data,"no-tlsv11") == 0 || strcasecmp(l->data,"no_tlsv11") == 0)
         ret|=TSO_NOTLSv11;
-      else if (strcasecmp(option->data,"no-tlsv12") == 0 || strcasecmp(option->data,"no_tlsv12") == 0)
+      else if (strcasecmp(l->data,"no-tlsv12") == 0 || strcasecmp(l->data,"no_tlsv12") == 0)
         ret|=TSO_NOTLSv12;
 #endif
       else
-        msg_error("Unknown ssl-option",evt_tag_str("option",option->data),NULL);
+        msg_error("Unknown ssl-option",evt_tag_str("option",l->data),NULL);
     }
   msg_debug("ssl-options parsed",evt_tag_printf("parsed value","%d" ,ret),NULL);
   return ret;
