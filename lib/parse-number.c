6801d8c491ef1aaa29a81bb281bf7514b27e4000
Juhász Viktor <viktor.juhasz@balabit.com>
2015-06-23 10:13:34 +0200

    parse-number: the base used is determined by the format in the sequence.
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

--- lib/parse-number.c
+++ lib/parse-number.c
@@ -125,7 +125,7 @@ _parse_number(const gchar *s, gchar **endptr, gint64 *d)
   gint64 val;
 
   errno = 0;
-  val = strtoll(s, endptr, 10);
+  val = strtoll(s, endptr, 0);
 
   if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
       || (errno != 0 && val == 0))
