79cb9db3f216fa1ef1d4d452dae831dc85d76d23
htrap <htrapdev@gmail.com>
2015-07-21 21:57:26 +0530

    fixing bulk mode iterations

--- modules/riak/libriack/lib/riack/setop.c
+++ modules/riak/libriack/lib/riack/setop.c
@@ -67,7 +67,7 @@ riack_setop_set (riack_setop_t *setop, ...)
             if(idx == 0)
               setop->adds = malloc(sizeof(ProtobufCBinaryData));
             else
-              setop->adds = realloc(setop->adds, sizeof(ProtobufCBinaryData) *(idx+1));
+              setop->adds = realloc(setop->adds, sizeof(ProtobufCBinaryData) * (idx+1));
             setop->n_adds = idx  + 1;
             adds = (char *)va_arg(args, char *);
             
