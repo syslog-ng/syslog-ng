b2c3320e52411d7b2de34efb72c4d6e831a7ba91
Gergő Nagy <gergo.nagy@balabit.com>
2015-08-03 12:53:23 +0200

    sngexample: functions reordered
    Functions of `LogDestination` have been reordered
    to follow the call sequence.
    
    Signed-off-by: Gergő Nagy <gergo.nagy@balabit.com>

--- modules/python/sngexample.py
+++ modules/python/sngexample.py
@@ -1,24 +1,16 @@
 class LogDestination(object):
 
+    def init(self):
+        """This method is called at initialization time"""
+        return True
+
     def open(self):
         """Open a connection to the target service"""
         return True
 
-    def close(self):
-        """Close the connection to the target service"""
-        pass
-
     def is_opened(self):
         """Check if the connection to the target is able to receive messages"""
         return True
-
-    def init(self, args):
-        """This method is called at initialization time"""
-        return True
-
-    def deinit(self):
-        """This method is called at deinitialization time"""
-        pass
 
     def send(self, msg):
         """Send a message to the target service
@@ -27,6 +19,13 @@ class LogDestination(object):
         destination for a period specified by the time-reopen() option."""
         pass
 
+    def close(self):
+        """Close the connection to the target service"""
+        pass
+
+    def deinit(self):
+        """This method is called at deinitialization time"""
+        pass
 
 class DummyPythonDest(LogDestination):
 
