f2ed3d85f5f6f5d1c28e3f0f1649403fb43fe24b
Balazs Scheidler <balazs.scheidler@balabit.com>
2015-10-12 18:43:55 +0200

    dbparser: fixed up long lines in test_timer_wheel
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- modules/dbparser/tests/test_timer_wheel.c
+++ modules/dbparser/tests/test_timer_wheel.c
@@ -18,14 +18,16 @@ timer_callback(TimerWheel *self, guint64 now, gpointer user_data)
 
   if (now != expires)
     {
-      fprintf(stderr, "Expected time is not matching current time in callback, now=%" G_GUINT64_FORMAT ", expires=%" G_GUINT64_FORMAT "\n",
-              now, expires);
+      fprintf(stderr, "Expected time is not matching current time in callback, "
+                      "now=%" G_GUINT64_FORMAT ", expires=%" G_GUINT64_FORMAT "\n",
+                      now, expires);
       exit(1);
     }
   if (prev_now > now)
     {
-      fprintf(stderr, "Callback current time is not monotonically increasing, prev_now=%" G_GUINT64_FORMAT ", now=%" G_GUINT64_FORMAT "\n",
-              prev_now, now);
+      fprintf(stderr, "Callback current time is not monotonically increasing, "
+                      "prev_now=%" G_GUINT64_FORMAT ", now=%" G_GUINT64_FORMAT "\n",
+                      prev_now, now);
     }
   prev_now = now;
   num_callbacks++;
@@ -39,7 +41,8 @@ _test_assoc_data(TimerWheel *wheel)
   timer_wheel_set_associated_data(wheel, g_strdup(ASSOC_DATA_STRING), (GDestroyNotify) g_free);
   if (strcmp(timer_wheel_get_associated_data(wheel), ASSOC_DATA_STRING) != 0)
     {
-      fprintf(stderr, "Associated data mismatch, found=%s, expected=%s", (gchar *) timer_wheel_get_associated_data(wheel), ASSOC_DATA_STRING);
+      fprintf(stderr, "Associated data mismatch, found=%s, expected=%s",
+                      (gchar *) timer_wheel_get_associated_data(wheel), ASSOC_DATA_STRING);
       exit(1);
     }
 }
@@ -101,7 +104,9 @@ test_wheel(gint seed)
   timer_wheel_set_time(wheel, latest + 1);
   if (num_callbacks != expected_callbacks)
     {
-      fprintf(stderr, "Error: not enough callbacks received, num_callbacks=%d, expected=%d\n", num_callbacks, expected_callbacks);
+      fprintf(stderr, "Error: not enough callbacks received, "
+                      "num_callbacks=%d, expected=%d\n",
+                      num_callbacks, expected_callbacks);
       exit(1);
     }
   timer_wheel_free(wheel);
