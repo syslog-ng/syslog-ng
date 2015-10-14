c3df6fd5a8981e77c9b1e2c185cd1ad37df13254
u2yg <u2yg@users.noreply.github.com>
2015-10-14 19:46:07 +0200

    afmongodb: updated copyrights
    Signed-off-by: u2yg <u2yg@users.noreply.github.com>

--- modules/afmongodb/afmongodb.c
+++ modules/afmongodb/afmongodb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2015 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2010-2015 BalaBit
  * Copyright (c) 2010-2014 Gergely Nagy <algernon@balabit.hu>
  *
  * This program is free software; you can redistribute it and/or modify it
