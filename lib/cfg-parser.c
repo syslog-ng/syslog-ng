2c77c5127ba80531d0137c5958daad81b08ca1e6
MÓZES Ádám István <adam.mozes@balabit.com>
2015-11-19 13:33:50 +0100

    Initial grammar changes
    Signed-off-by: MÓZES Ádám István <adam.mozes@balabit.com>

--- lib/cfg-parser.c
+++ lib/cfg-parser.c
@@ -148,6 +148,7 @@ static CfgLexerKeyword main_keywords[] = {
   { "dns_cache_expire",   KW_DNS_CACHE_EXPIRE },
   { "dns_cache_expire_failed", KW_DNS_CACHE_EXPIRE_FAILED },
   { "pass_unix_credentials", KW_PASS_UNIX_CREDENTIALS },
+  { "persist_id",            KW_PERSIST_NAME, 0x0308 },
 
   { "retries",            KW_RETRIES, 0x0303 },
 
