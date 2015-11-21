f937920ef06f02d4942ca8bda5f6d79e7e10296a
Balazs Scheidler <balazs.scheidler@balabit.com>
2015-11-21 06:59:59 +0100

    date: refactor test program and add time-stamp() coverage
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- modules/date/tests/test_date.c
+++ modules/date/tests/test_date.c
@@ -6,48 +6,85 @@
 #include <locale.h>
 #include <stdlib.h>
 
-static void
-testcase(gchar *msg, gchar *timezone, gchar *format, gchar *expected)
+static LogParser *
+_construct_parser(gchar *timezone, gchar *format, gint time_stamp)
 {
-  LogTemplate *templ;
-  LogMessage *logmsg;
   LogParser *parser;
-  gboolean success;
-  GString *res = g_string_sized_new(128);
 
   parser = date_parser_new (configuration);
-  if (format != NULL) date_parser_set_format(parser, format);
-  if (timezone != NULL) date_parser_set_timezone(parser, timezone);
+  if (format != NULL)
+    date_parser_set_format(parser, format);
+  if (timezone != NULL)
+    date_parser_set_timezone(parser, timezone);
+  date_parser_set_time_stamp(parser, time_stamp);
 
   log_pipe_init(&parser->super);
+  return parser;
+}
+
+static LogMessage *
+_construct_logmsg(const gchar *msg)
+{
+  LogMessage *logmsg;
 
   logmsg = log_msg_new_empty();
   logmsg->timestamps[LM_TS_RECVD].tv_sec = 1451473200; /* Dec  30 2015 */
-  log_msg_set_value(logmsg, log_msg_get_value_handle("MESSAGE"), msg, -1);
+  log_msg_set_value(logmsg, LM_V_MESSAGE, msg, -1);
+  return logmsg;
+}
+
+static void
+assert_parsed_date_equals_with_stamp(gchar *msg, gchar *timezone, gchar *format, gint time_stamp, gchar *expected)
+{
+  LogMessage *logmsg;
+  LogParser *parser = _construct_parser(timezone, format, time_stamp);
+  gboolean success;
+  GString *res = g_string_sized_new(128);
+
+  logmsg = _construct_logmsg(msg);
   success = log_parser_process(parser, &logmsg, NULL, log_msg_get_value(logmsg, LM_V_MESSAGE, NULL), -1);
 
-  if (!success && expected)
+  if (!success)
     {
       fprintf(stderr, "unable to parse format=%s msg=%s\n", format, msg);
       exit(1);
     }
-  else if (success && !expected)
+  else
     {
-      fprintf(stderr, "successfully parsed but expected failure, format=%s msg=%s\n", format, msg);
-      exit(1);
-    }
-  else if (expected)
-    {
-      /* Convert to ISODATE */
-      templ = compile_template("${ISODATE}", FALSE);
-      log_template_format(templ, logmsg, NULL, LTZ_LOCAL, -1, NULL, res);
+      log_stamp_append_format(&logmsg->timestamps[time_stamp], res, TS_FMT_ISO, -1, 0);
       assert_nstring(res->str, res->len, expected, strlen(expected),
                      "incorrect date parsed msg=%s format=%s",
                      msg, format);
-      log_template_unref(templ);
     }
 
   g_string_free(res, TRUE);
+  log_pipe_unref(&parser->super);
+  log_msg_unref(logmsg);
+  return;
+}
+
+static void
+assert_parsed_date_equals(gchar *msg, gchar *timezone, gchar *format, gchar *expected)
+{
+  assert_parsed_date_equals_with_stamp(msg, timezone, format, LM_TS_STAMP, expected);
+}
+
+static void
+assert_parsing_fails(gchar *msg)
+{
+  LogMessage *logmsg;
+  LogParser *parser = _construct_parser(NULL, NULL, LM_TS_STAMP);
+  gboolean success;
+
+  logmsg = _construct_logmsg(msg);
+  success = log_parser_process(parser, &logmsg, NULL, log_msg_get_value(logmsg, LM_V_MESSAGE, NULL), -1);
+
+  if (success)
+    {
+      fprintf(stderr, "successfully parsed but expected failure, msg=%s\n", msg);
+      exit(1);
+    }
+
   log_pipe_unref(&parser->super);
   log_msg_unref(logmsg);
   return;
@@ -65,44 +102,47 @@ int main()
   configuration = cfg_new(0x0302);
 
   /* Various ISO8601 formats */
-  testcase("2015-01-26T16:14:49+0300", NULL, NULL, "2015-01-26T16:14:49+03:00");
-  testcase("2015-01-26T16:14:49+0330", NULL, NULL, "2015-01-26T16:14:49+03:30");
-  testcase("2015-01-26T16:14:49+0200", NULL, NULL, "2015-01-26T16:14:49+02:00");
-  testcase("2015-01-26T16:14:49+03:00", NULL, NULL, "2015-01-26T16:14:49+03:00");
-  testcase("2015-01-26T16:14:49+03:30", NULL, NULL, "2015-01-26T16:14:49+03:30");
-  testcase("2015-01-26T16:14:49+02:00", NULL, NULL, "2015-01-26T16:14:49+02:00");
-  testcase("2015-01-26T16:14:49Z", NULL, NULL, "2015-01-26T16:14:49+00:00");
-  testcase("2015-01-26T16:14:49A", NULL, NULL, "2015-01-26T16:14:49-01:00");
-  testcase("2015-01-26T16:14:49B", NULL, NULL, "2015-01-26T16:14:49-02:00");
-  testcase("2015-01-26T16:14:49N", NULL, NULL, "2015-01-26T16:14:49+01:00");
-  testcase("2015-01-26T16:14:49O", NULL, NULL, "2015-01-26T16:14:49+02:00");
-  testcase("2015-01-26T16:14:49GMT", NULL, NULL, "2015-01-26T16:14:49+00:00");
-  testcase("2015-01-26T16:14:49PDT", NULL, NULL, "2015-01-26T16:14:49-07:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49+0300", NULL, NULL, "2015-01-26T16:14:49+03:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49+0330", NULL, NULL, "2015-01-26T16:14:49+03:30");
+  assert_parsed_date_equals("2015-01-26T16:14:49+0200", NULL, NULL, "2015-01-26T16:14:49+02:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49+03:00", NULL, NULL, "2015-01-26T16:14:49+03:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49+03:30", NULL, NULL, "2015-01-26T16:14:49+03:30");
+  assert_parsed_date_equals("2015-01-26T16:14:49+02:00", NULL, NULL, "2015-01-26T16:14:49+02:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49Z", NULL, NULL, "2015-01-26T16:14:49+00:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49A", NULL, NULL, "2015-01-26T16:14:49-01:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49B", NULL, NULL, "2015-01-26T16:14:49-02:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49N", NULL, NULL, "2015-01-26T16:14:49+01:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49O", NULL, NULL, "2015-01-26T16:14:49+02:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49GMT", NULL, NULL, "2015-01-26T16:14:49+00:00");
+  assert_parsed_date_equals("2015-01-26T16:14:49PDT", NULL, NULL, "2015-01-26T16:14:49-07:00");
 
   /* RFC 2822 */
-  testcase("Tue, 27 Jan 2015 11:48:46 +0200", NULL, "%a, %d %b %Y %T %z", "2015-01-27T11:48:46+02:00");
+  assert_parsed_date_equals("Tue, 27 Jan 2015 11:48:46 +0200", NULL, "%a, %d %b %Y %T %z", "2015-01-27T11:48:46+02:00");
 
   /* Apache-like */
-  testcase("21/Jan/2015:14:40:07 +0500", NULL, "%d/%b/%Y:%T %z", "2015-01-21T14:40:07+05:00");
+  assert_parsed_date_equals("21/Jan/2015:14:40:07 +0500", NULL, "%d/%b/%Y:%T %z", "2015-01-21T14:40:07+05:00");
 
   /* Try with additional text at the end, should fail */
-  testcase("2015-01-26T16:14:49+0300 Disappointing log file", NULL, NULL, NULL);
+  assert_parsing_fails("2015-01-26T16:14:49+0300 Disappointing log file");
 
   /* Dates without timezones. America/Phoenix has no DST */
-  testcase("Tue, 27 Jan 2015 11:48:46", NULL, "%a, %d %b %Y %T", "2015-01-27T11:48:46+01:00");
-  testcase("Tue, 27 Jan 2015 11:48:46", "America/Phoenix", "%a, %d %b %Y %T", "2015-01-27T11:48:46-07:00");
-  testcase("Tue, 27 Jan 2015 11:48:46", "+05:00", "%a, %d %b %Y %T", "2015-01-27T11:48:46+05:00");
+  assert_parsed_date_equals("Tue, 27 Jan 2015 11:48:46", NULL, "%a, %d %b %Y %T", "2015-01-27T11:48:46+01:00");
+  assert_parsed_date_equals("Tue, 27 Jan 2015 11:48:46", "America/Phoenix", "%a, %d %b %Y %T", "2015-01-27T11:48:46-07:00");
+  assert_parsed_date_equals("Tue, 27 Jan 2015 11:48:46", "+05:00", "%a, %d %b %Y %T", "2015-01-27T11:48:46+05:00");
 
   /* Try without the year. */
-  testcase("01/Jan:00:40:07 +0500", NULL, "%d/%b:%T %z", "2016-01-01T00:40:07+05:00");
-  testcase("01/Aug:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-08-01T00:40:07+05:00");
-  testcase("01/Sep:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-09-01T00:40:07+05:00");
-  testcase("01/Oct:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-10-01T00:40:07+05:00");
-  testcase("01/Nov:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-11-01T00:40:07+05:00");
+  assert_parsed_date_equals("01/Jan:00:40:07 +0500", NULL, "%d/%b:%T %z", "2016-01-01T00:40:07+05:00");
+  assert_parsed_date_equals("01/Aug:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-08-01T00:40:07+05:00");
+  assert_parsed_date_equals("01/Sep:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-09-01T00:40:07+05:00");
+  assert_parsed_date_equals("01/Oct:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-10-01T00:40:07+05:00");
+  assert_parsed_date_equals("01/Nov:00:40:07 +0500", NULL, "%d/%b:%T %z", "2015-11-01T00:40:07+05:00");
 
 
-  testcase("1446128356 +01:00", NULL, "%s %z", "2015-10-29T15:19:16+01:00");
-  testcase("1446128356", "Europe/Budapest", "%s", "2015-10-29T15:19:16+01:00");
+  assert_parsed_date_equals("1446128356 +01:00", NULL, "%s %z", "2015-10-29T15:19:16+01:00");
+  assert_parsed_date_equals("1446128356", "Europe/Budapest", "%s", "2015-10-29T15:19:16+01:00");
+
+
+  assert_parsed_date_equals_with_stamp("2015-01-26T16:14:49+03:00", NULL, NULL, LM_TS_RECVD, "2015-01-26T16:14:49+03:00");
 
   app_shutdown();
   return 0;
