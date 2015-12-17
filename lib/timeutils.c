46b6bf3420633190582dcb4c7fc7eeb523c5781d
Gergő Nagy <grigori.grant@gmail.com>
2015-12-17 12:56:05 +0100

    timeutils: unnecesarry spaces removed
    Signed-off-by: Gergő Nagy <grigori.grant@gmail.com>

--- lib/timeutils.c
+++ lib/timeutils.c
@@ -58,8 +58,8 @@ const char *weekday_names[] =
 #define TZ_MAGIC "TZif"
 
 const gint64 LOWEST_TIME32    = (gint64)((gint32)0x80000000);
-static const gchar *time_zone_path_list[] = 
-{ 
+static const gchar *time_zone_path_list[] =
+{
 #ifdef PATH_TIMEZONEDIR
   PATH_TIMEZONEDIR,               /* search the user specified dir */
 #endif
@@ -237,7 +237,7 @@ cached_gmtime(time_t *when, struct tm *tm)
 /**
  * get_local_timezone_ofs:
  * @when: time in UTC
- * 
+ *
  * Return the zone offset (measured in seconds) of @when expressed in local
  * time. The function also takes care about daylight saving.
  **/
@@ -255,19 +255,19 @@ get_local_timezone_ofs(time_t when)
   struct tm gtm;
   struct tm ltm;
   long tzoff;
-  
+
   cached_localtime(&when, &ltm);
   cached_gmtime(&when, &gtm);
 
   tzoff = (ltm.tm_hour - gtm.tm_hour) * 3600;
   tzoff += (ltm.tm_min - gtm.tm_min) * 60;
   tzoff += ltm.tm_sec - gtm.tm_sec;
-  
+
   if (tzoff > 0 && (ltm.tm_year < gtm.tm_year || ltm.tm_mon < gtm.tm_mon || ltm.tm_mday < gtm.tm_mday))
     tzoff -= 86400;
   else if (tzoff < 0 && (ltm.tm_year > gtm.tm_year || ltm.tm_mon > gtm.tm_mon || ltm.tm_mday > gtm.tm_mday))
     tzoff += 86400;
-  
+
   return tzoff;
 #endif /* SYSLOG_NG_HAVE_STRUCT_TM_TM_GMTOFF */
 }
@@ -386,17 +386,17 @@ determine_year_for_month(gint month, const struct tm *now)
 /*
  ** TZ file header **
  *
- *  struct tzhead 
+ *  struct tzhead
  *    {
  *       char    tzh_magic[4];            TZ_MAGIC "TZif"
- *       char    tzh_version[1];          '\0' or '2' as of 2005 
- *       char    tzh_reserved[15];        reserved--must be zero 
- *       char    tzh_ttisgmtcnt[4];       coded number of trans. time flags 
- *       char    tzh_ttisstdcnt[4];       coded number of trans. time flags 
- *       char    tzh_leapcnt[4];          coded number of leap seconds 
- *       char    tzh_timecnt[4];          coded number of transition times 
- *       char    tzh_typecnt[4];          coded number of local time types 
- *       char    tzh_charcnt[4];          coded number of abbr. chars 
+ *       char    tzh_version[1];          '\0' or '2' as of 2005
+ *       char    tzh_reserved[15];        reserved--must be zero
+ *       char    tzh_ttisgmtcnt[4];       coded number of trans. time flags
+ *       char    tzh_ttisstdcnt[4];       coded number of trans. time flags
+ *       char    tzh_leapcnt[4];          coded number of leap seconds
+ *       char    tzh_timecnt[4];          coded number of transition times
+ *       char    tzh_typecnt[4];          coded number of local time types
+ *       char    tzh_charcnt[4];          coded number of abbr. chars
  *    };
  *
  ** TZ file body part **
@@ -411,9 +411,9 @@ determine_year_for_month(gint month, const struct tm *now)
  *      tzh_leapcnt repetitions of
  *              one (char [4])          coded leap second transition times
  *              one (char [4])          total correction after above
- *      tzh_ttisstdcnt (char)s          indexed by type; if TRUE, transition time is standard time, if FALSE, transition time is wall clock time if absent, 
- *                                      transition times are assumed to be wall clock time 
- *      tzh_ttisgmtcnt (char)s          indexed by type; if TRUE, transition time is UTC, if FALSE, transition time is local time if absent, 
+ *      tzh_ttisstdcnt (char)s          indexed by type; if TRUE, transition time is standard time, if FALSE, transition time is wall clock time if absent,
+ *                                      transition times are assumed to be wall clock time
+ *      tzh_ttisgmtcnt (char)s          indexed by type; if TRUE, transition time is UTC, if FALSE, transition time is local time if absent,
  *                                      transition times are assumed to be local time
  */
 
@@ -437,7 +437,7 @@ typedef struct _Transition
 struct _ZoneInfo
 {
   Transition *transitions;
-  gint64 timecnt; 
+  gint64 timecnt;
   gint32 last_transitions_index;
 };
 
@@ -465,10 +465,10 @@ readcoded32(unsigned char **input, gint64 minv, gint64 maxv)
 
   if (val < minv || val > maxv)
     {
-      msg_error("Error while processing the time zone file", 
-                evt_tag_str("message", "oded value out-of-range"), 
+      msg_error("Error while processing the time zone file",
+                evt_tag_str("message", "oded value out-of-range"),
                 evt_tag_int("value", val),
-                evt_tag_printf("expected", "[%"G_GINT64_FORMAT", %"G_GINT64_FORMAT"]", minv, maxv), 
+                evt_tag_printf("expected", "[%"G_GINT64_FORMAT", %"G_GINT64_FORMAT"]", minv, maxv),
                 NULL);
       g_assert_not_reached();
     }
@@ -476,7 +476,7 @@ readcoded32(unsigned char **input, gint64 minv, gint64 maxv)
 }
 
 /* Read zic-coded 64-bit integer from file */
-static gint64 
+static gint64
 readcoded64(unsigned char **input, gint64 minv, gint64 maxv)
 {
   unsigned char buf[8]; /* must be UNSIGNED */
@@ -493,9 +493,9 @@ readcoded64(unsigned char **input, gint64 minv, gint64 maxv)
   if (val < minv || val > maxv)
     {
       msg_error("Error while processing the time zone file",
-                evt_tag_str("message", "Coded value out-of-range"), 
-                evt_tag_int("value", val), 
-                evt_tag_printf("expected", "[%"G_GINT64_FORMAT", %"G_GINT64_FORMAT"]", minv, maxv), 
+                evt_tag_str("message", "Coded value out-of-range"),
+                evt_tag_int("value", val),
+                evt_tag_printf("expected", "[%"G_GINT64_FORMAT", %"G_GINT64_FORMAT"]", minv, maxv),
                 NULL);
       g_assert_not_reached();
     }
@@ -503,7 +503,7 @@ readcoded64(unsigned char **input, gint64 minv, gint64 maxv)
 }
 
 /* Read a booleanean value */
-static gboolean 
+static gboolean
 readbool(unsigned char **input)
 {
   gchar c;
@@ -514,8 +514,8 @@ readbool(unsigned char **input)
   if (c!=0 && c!=1)
     {
       msg_error("Error while processing the time zone file",
-                evt_tag_str("message", "Boolean value out-of-range"), 
-                evt_tag_int("value", c), 
+                evt_tag_str("message", "Boolean value out-of-range"),
+                evt_tag_int("value", c),
                 NULL);
       g_assert_not_reached();
     }
@@ -523,7 +523,7 @@ readbool(unsigned char **input)
 }
 
 /* Read a character value */
-static gchar 
+static gchar
 readchar(unsigned char **input)
 {
   unsigned char c;
@@ -575,12 +575,12 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
 
   if (strncmp((gchar*)buf, TZ_MAGIC, 4) != 0)
     {
-      msg_error("Error while processing the time zone file", 
-                evt_tag_str("message", TZ_MAGIC" signature is missing"), 
+      msg_error("Error while processing the time zone file",
+                evt_tag_str("message", TZ_MAGIC" signature is missing"),
                 NULL);
       goto error;
     }
-  
+
   /* read the version byte */
   buf = *input;
   *input += 1;
@@ -591,12 +591,12 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
    */
   if (buf[0] != 0 && buf[0] != '2' && buf[0] != '3')
     {
-      msg_error("Error in the time zone file", 
-                evt_tag_str("message", "Bad Olson version info"), 
+      msg_error("Error in the time zone file",
+                evt_tag_str("message", "Bad Olson version info"),
                 NULL);
       goto error;
     }
-  else 
+  else
     {
       if (buf[0] != 0)
         *version = buf[0] - '0';
@@ -615,21 +615,21 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
   typecnt  = readcoded32(input, 0, G_MAXINT64);
   charcnt  = readcoded32(input, 0, G_MAXINT64);
 
-  /* 
+  /*
    * Confirm sizes that we assume to be equal.  These assumptions
    * are drawn from a reading of the zic source (2003a), so they
-   * should hold unless the zic source changes. 
+   * should hold unless the zic source changes.
    */
 
-  if (isgmtcnt != typecnt || 
-      isdstcnt != typecnt) 
+  if (isgmtcnt != typecnt ||
+      isdstcnt != typecnt)
     {
-      msg_warning("Error in the time zone file", 
-                   evt_tag_str("message", "Count mismatch between tzh_ttisgmtcnt, tzh_ttisdstcnt, tth_typecnt"), 
+      msg_warning("Error in the time zone file",
+                   evt_tag_str("message", "Count mismatch between tzh_ttisgmtcnt, tzh_ttisdstcnt, tth_typecnt"),
                    NULL);
     }
 
-  /* 
+  /*
    * Used temporarily to store transition times and types.  We need
    * to do this because the times and types are stored in two
    * separate arrays.
@@ -641,7 +641,7 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
   /* Read transition times */
   for (i = 0; i < timecnt; ++i)
     {
-      if (is64bitData) 
+      if (is64bitData)
         {
           transition_times[i] = readcoded64(input, G_MININT64, G_MAXINT64);
         }
@@ -650,17 +650,17 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
           transition_times[i] = readcoded32(input, G_MININT64, G_MAXINT64);
         }
     }
-  
+
   /* Read transition types */
   for (i = 0; i < timecnt; ++i)
     {
       guint8 t = (guint8)readchar(input);
       if (t >= typecnt)
         {
-          msg_warning("Error in the time zone file", 
-                      evt_tag_str("message", "Illegal type number"), 
-                      evt_tag_printf("val", "%ld", (long) t), 
-                      evt_tag_printf("expected", "[0, %" G_GINT64_FORMAT "]", typecnt-1), 
+          msg_warning("Error in the time zone file",
+                      evt_tag_str("message", "Illegal type number"),
+                      evt_tag_printf("val", "%ld", (long) t),
+                      evt_tag_printf("expected", "[0, %" G_GINT64_FORMAT "]", typecnt-1),
                       NULL);
           goto error;
         }
@@ -678,14 +678,14 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
       gmt_offsets[i] = readcoded32(input, G_MININT64, G_MAXINT64);
       if (gmt_offsets[i] > offs * 60 * 60 || gmt_offsets[i] < -1 * offs * 60 * 60)
         {
-          msg_warning("Error in the time zone file", 
-                      evt_tag_str("message", "Illegal gmtoffset number"), 
-                      evt_tag_int("val", gmt_offsets[i]), 
-                      evt_tag_printf("expected", "[%d, %d]", -1 * offs * 60 * 60, offs * 60 * 60), 
+          msg_warning("Error in the time zone file",
+                      evt_tag_str("message", "Illegal gmtoffset number"),
+                      evt_tag_int("val", gmt_offsets[i]),
+                      evt_tag_printf("expected", "[%d, %d]", -1 * offs * 60 * 60, offs * 60 * 60),
                       NULL);
           goto error;
         }
-      /* ignore isdst flag */ 
+      /* ignore isdst flag */
       readbool(input);
       /* ignore abbr index */
       readchar(input);
@@ -703,7 +703,7 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
     {
       info = zone_info_new(timecnt);
     }
- 
+
   /* Build transitions vector out of corresponding times and types. */
   insertInitial = FALSE;
   if (is64bitData)
@@ -732,7 +732,7 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
 
           if (minidx != -1)
             {
-              /* 
+              /*
                * If there are any transitions before the 32bit minimum time,
                * put the type information with the 32bit minimum time
                */
@@ -752,7 +752,7 @@ zone_info_parser(unsigned char **input, gboolean is64bitData, gint *version)
     {
       for (i = 0; i < timecnt; ++i)
         {
-          info->transitions[i].time = transition_times[i];        
+          info->transitions[i].time = transition_times[i];
           info->transitions[i].gmtoffset = gmt_offsets[transition_types[i]];
         }
     }
@@ -813,9 +813,9 @@ zone_info_get_offset(ZoneInfo *self, gint64 timestamp)
   if (self->transitions == NULL)
     return 0;
 
-  if (self->last_transitions_index != -1 && 
-     self->last_transitions_index < (self->timecnt - 1) && 
-     self->transitions[self->last_transitions_index].time < timestamp &&  
+  if (self->last_transitions_index != -1 &&
+     self->last_transitions_index < (self->timecnt - 1) &&
+     self->transitions[self->last_transitions_index].time < timestamp &&
      self->transitions[self->last_transitions_index + 1].time > timestamp)
     {
       return  self->transitions[ self->last_transitions_index ].gmtoffset;
@@ -823,13 +823,13 @@ zone_info_get_offset(ZoneInfo *self, gint64 timestamp)
   else
     {
       for (i = 0; i < (self->timecnt - 1); i++)
-        if (self->transitions[i].time < timestamp && 
+        if (self->transitions[i].time < timestamp &&
             self->transitions[i+1].time > timestamp)
           break;
- 
+
       self->last_transitions_index  = i;
     }
- 
+
   return self->transitions[self->last_transitions_index].gmtoffset;
 }
 
@@ -837,7 +837,7 @@ static gboolean
 zone_info_read(const gchar *zonename, ZoneInfo **zone, ZoneInfo **zone64)
 {
   unsigned char *buff = NULL;
-  gchar *filename = NULL; 
+  gchar *filename = NULL;
   int byte_read = 0;
   int version;
   GError *error = NULL;
@@ -904,21 +904,21 @@ time_zone_info_new(const gchar *tz)
 {
   TimeZoneInfo *self = g_new0(TimeZoneInfo,1);
   self->zone_offset = -1;
- 
-  /* if no time zone was specified return with an empty TimeZoneInfo pointer */  
+
+  /* if no time zone was specified return with an empty TimeZoneInfo pointer */
   if (!tz)
     return self;
 
-  if ((*tz == '+' || *tz == '-') && strlen(tz) == 6 && 
-      isdigit((int) *(tz+1)) && isdigit((int) *(tz+2)) && 
-      (*(tz+3) == ':') && isdigit((int) *(tz+4)) && 
+  if ((*tz == '+' || *tz == '-') && strlen(tz) == 6 &&
+      isdigit((int) *(tz+1)) && isdigit((int) *(tz+2)) &&
+      (*(tz+3) == ':') && isdigit((int) *(tz+4)) &&
       isdigit((int) *(tz+5)))
     {
       /* timezone offset */
       gint sign = *tz == '-' ? -1 : 1;
       gint hours, mins;
       tz++;
-      
+
       hours = (*tz - '0') * 10 + *(tz+1) - '0';
       mins = (*(tz+3) - '0') * 10 + *(tz+4) - '0';
       if ((hours < 24 && mins <= 60) || (hours == 24 && mins == 0))
@@ -941,7 +941,7 @@ time_zone_info_new(const gchar *tz)
   return NULL;
 }
 
-void 
+void
 time_zone_info_free(TimeZoneInfo *self)
 {
   g_assert(self);
