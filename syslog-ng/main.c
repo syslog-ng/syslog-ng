0432629e91d4912eb9e9cdc0da8f361945ca1388
Balazs Scheidler <balazs.scheidler@balabit.com>
2015-12-29 13:35:14 +0100

    main: don't split the printf() invocation with an ifdef
    As it makes the code pretty fragile and the output is generated by multiple
    printf() statements anyway.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- syslog-ng/main.c
+++ syslog-ng/main.c
@@ -129,13 +129,13 @@ version(void)
     }
   printf(SYSLOG_NG_PACKAGE " " SYSLOG_NG_VERSION "\n"
          "Installer-Version: %s\n"
-         "Revision: " SYSLOG_NG_SOURCE_REVISION "\n"
-#if WITH_COMPILE_DATE
-         "Compile-Date: " __DATE__ " " __TIME__ "\n"
-#endif
-         "Available-Modules: ",
+         "Revision: " SYSLOG_NG_SOURCE_REVISION "\n",
          installer_version);
 
+#if WITH_COMPILE_DATE
+  printf("Compile-Date: " __DATE__ " " __TIME__ "\n");
+#endif
+  printf("Available-Modules: ");
   plugin_list_modules(stdout, FALSE);
 
   printf("Enable-Debug: %s\n"
