b7d86d981ea45ae076144b33debfea998c99ea87
bkil-syslogng <tamas.nagy@balabit.com>
2016-01-05 13:35:27 +0100

    lib/utf8utils.c: bugfix for invalid UTF-8 input
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- libtest/template_lib.c
+++ libtest/template_lib.c
@@ -113,6 +113,7 @@ create_sample_message(void)
   log_msg_set_value_by_name(msg, ".json.sub.value1", "subvalue1", -1);
   log_msg_set_value_by_name(msg, ".json.sub.value2", "subvalue2", -1);
   log_msg_set_value_by_name(msg, "escaping", "binary stuff follows \"\xad árvíztűrőtükörfúrógép", -1);
+  log_msg_set_value_by_name(msg, "escaping2", "\xc3", -1);
 
   return msg;
 }
