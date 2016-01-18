4c605489ab49fe3656ab85d037533e6e891bd109
Juhász Viktor <viktor.juhasz@balabit.com>
2016-01-18 10:14:26 +0100

    test_logqueue: the queue specific functions are factored out.
    These functions are ready to use them with another type of queues

--- tests/unit/test_logqueue.c
+++ tests/unit/test_logqueue.c
@@ -28,6 +28,7 @@
 #include "mainloop.h"
 #include "tls-support.h"
 #include "mainloop-io-worker.h"
+#include "libtest/queue_utils_lib.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -35,68 +36,9 @@
 #include <iv_list.h>
 #include <iv_thread.h>
 
-int acked_messages = 0;
-int fed_messages = 0;
 MsgFormatOptions parse_options;
 
 #define OVERFLOW_SIZE 10000
-
-void
-test_ack(LogMessage *msg, AckType ack_type)
-{
-  acked_messages++;
-}
-
-void
-feed_some_messages(LogQueue **q, int n)
-{
-  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
-  LogMessage *msg;
-  gint i;
-
-  path_options.ack_needed = (*q)->use_backlog;
-  for (i = 0; i < n; i++)
-    {
-      char *msg_str = "<155>2006-02-11T10:34:56+01:00 bzorp syslog-ng[23323]: árvíztűrőtükörfúrógép";
-      GSockAddr *sa;
-
-      sa = g_sockaddr_inet_new("10.10.10.10", 1010);
-      msg = log_msg_new(msg_str, strlen(msg_str), sa, &parse_options);
-      g_sockaddr_unref(sa);
-      log_msg_add_ack(msg, &path_options);
-      msg->ack_func = test_ack;
-      log_queue_push_tail((*q), msg, &path_options);
-      fed_messages++;
-    }
-
-}
-
-void
-send_some_messages(LogQueue *q, gint n)
-{
-  gint i;
-  LogMessage *msg;
-  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
-
-  for (i = 0; i < n; i++)
-    {
-      msg = log_queue_pop_head(q, &path_options);
-      log_msg_ack(msg, &path_options, AT_PROCESSED);
-      log_msg_unref(msg);
-    }
-}
-
-void
-app_ack_some_messages(LogQueue *q, gint n)
-{
-  log_queue_ack_backlog(q, n);
-}
-
-void
-rewind_messages(LogQueue *q)
-{
-  log_queue_rewind_backlog_all(q);
-}
 
 void
 testcase_zero_diskbuf_and_normal_acks()
@@ -110,7 +52,7 @@ testcase_zero_diskbuf_and_normal_acks()
   fed_messages = 0;
   acked_messages = 0;
   for (i = 0; i < 10; i++)
-    feed_some_messages(&q, 10);
+    feed_some_messages(q, 10, &parse_options);
 
   send_some_messages(q, fed_messages);
   app_ack_some_messages(q, fed_messages);
@@ -136,7 +78,7 @@ testcase_zero_diskbuf_alternating_send_acks()
   acked_messages = 0;
   for (i = 0; i < 10; i++)
     {
-      feed_some_messages(&q, 10);
+      feed_some_messages(q, 10, &parse_options);
       send_some_messages(q, 10);
       app_ack_some_messages(q, 10);
     }
