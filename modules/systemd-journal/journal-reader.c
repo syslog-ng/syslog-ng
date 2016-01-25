3c8411c72253023b56d92990f448b2e430c27d07
Tibor Benke <tibor.benke@balabit.com>
2016-01-25 09:46:38 +0100

    JournalReaderOption: add default value to prefix
    The default value is '.journald.' and it is effective at least from
    syslog-ng 3.8.
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- modules/systemd-journal/journal-reader.c
+++ modules/systemd-journal/journal-reader.c
@@ -599,6 +599,16 @@ journal_reader_options_init(JournalReaderOptions *options, GlobalConfig *cfg, co
   if (options->recv_time_zone_info == NULL)
     options->recv_time_zone_info = time_zone_info_new(options->recv_time_zone);
 
+  if (options->prefix == NULL && !cfg_is_config_version_older(cfg, 0x0308))
+    {
+      gchar *value = ".journald.";
+      msg_warning("Default value changed for the prefix() option of systemd-journal source in " VERSION_3_8,
+                  evt_tag_str("old_value", ""),
+                  evt_tag_str("new_value", value),
+                  NULL);
+      options->prefix = g_strdup(value);
+    }
+
   options->initialized = TRUE;
 }
 
