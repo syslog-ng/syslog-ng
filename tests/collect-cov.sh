8f01e2c88f8186da0a6226ad08122d33caf20bc7
bkil-syslogng <tamas.nagy@balabit.com>
2016-01-27 15:44:58 +0100

    configure,autogen.sh,afmongodb/Makefile.am: rename libmongo-client
    Rename libmongo-client to mongo-c-driver in build scripts
    
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- tests/collect-cov.sh
+++ tests/collect-cov.sh
@@ -4,6 +4,6 @@ set -e
 
 lcov --capture --directory `pwd` --compat-libtool --output-file coverage.info --base-directory "${top_srcdir}"
 lcov --extract coverage.info "${top_srcdir}/*" --output-file coverage.info
-lcov --remove coverage.info "${top_srcdir}/modules/afmongodb/libmongo-client/*" --output-file coverage.info
+lcov --remove coverage.info "${top_srcdir}/modules/afmongodb/mongo-c-driver/*" --output-file coverage.info
 lcov --remove coverage.info "${top_srcdir}/modules/afamqp/rabbitmq-c/*" --output-file coverage.info
 genhtml coverage.info --output-directory coverage.html
