03ebb37879bd914e3bdfc231ada9f35d2bb548b0
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-07 09:22:29 +0100

    modules/afmongodb: extract object struct to afmongodb-private.h
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/afmongodb/afmongodb.c
+++ modules/afmongodb/afmongodb.c
@@ -25,43 +25,14 @@
 #include "afmongodb.h"
 #include "afmongodb-parser.h"
 #include "messages.h"
-#include "string-list.h"
 #include "stats/stats-registry.h"
 #include "logmsg/nvtable.h"
 #include "logqueue.h"
-#include "value-pairs/value-pairs.h"
 #include "value-pairs/evttag.h"
 #include "plugin.h"
 #include "plugin-types.h"
-#include "logthrdestdrv.h"
 
-#include "mongoc.h"
 #include <time.h>
-
-typedef struct
-{
-  LogThrDestDriver super;
-
-  /* Shared between main/writer; only read by the writer, never
-     written */
-  gchar *coll;
-  gchar *uri;
-
-  LogTemplateOptions template_options;
-
-  time_t last_msg_stamp;
-
-  ValuePairs *vp;
-
-  /* Writer-only stuff */
-  const gchar *db;
-  mongoc_uri_t *uri_obj;
-  mongoc_client_t *client;
-  mongoc_collection_t *coll_obj;
-
-  GString *current_value;
-  bson_t *bson;
-} MongoDBDestDriver;
 
 /*
  * Configuration
