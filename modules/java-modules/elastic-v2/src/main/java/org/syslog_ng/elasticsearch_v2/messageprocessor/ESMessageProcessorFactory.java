69abc6d92fba7a66c3ad25b95f75b3be145f932d
bkil-syslogng <tamas.nagy@balabit.com>
2016-03-10 15:40:31 +0100

    modules/java-modules: copyright declaration fixes
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/java-modules/elastic-v2/src/main/java/org/syslog_ng/elasticsearch_v2/messageprocessor/ESMessageProcessorFactory.java
+++ modules/java-modules/elastic-v2/src/main/java/org/syslog_ng/elasticsearch_v2/messageprocessor/ESMessageProcessorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 BalaBit IT Ltd, Budapest, Hungary
+ * Copyright (c) 2016 Balabit
  * Copyright (c) 2016 Viktor Juhasz <viktor.juhasz@balabit.com>
  *
  * This program is free software; you can redistribute it and/or modify it
