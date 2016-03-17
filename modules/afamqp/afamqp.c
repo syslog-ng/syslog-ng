3d8cec9e14520d26dda58ea9b3b1d366b9296f3d
MÓZES Ádám István <adam.mozes@balabit.com>
2016-03-17 16:36:46 +0100

    afamqp: register the generate_persist_name method
    This commit registers the new _format_persist_name
    as the LogPipe::generate_persist_name method.
    
    Signed-off-by: MÓZES Ádám István <adam.mozes@balabit.com>

--- modules/afamqp/afamqp.c
+++ modules/afamqp/afamqp.c
@@ -585,6 +585,7 @@ afamqp_dd_new(GlobalConfig *cfg)
 
   self->super.super.super.super.init = afamqp_dd_init;
   self->super.super.super.super.free_fn = afamqp_dd_free;
+  self->super.super.super.super.generate_persist_name = afamqp_dd_format_persist_name;
 
   self->super.worker.thread_init = afamqp_worker_thread_init;
   self->super.worker.disconnect = afamqp_dd_disconnect;
