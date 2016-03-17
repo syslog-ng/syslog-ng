c8ea52e5c3d7f294978321067d3b1e1fbd954000
MÓZES Ádám István <adam.mozes@balabit.com>
2016-03-17 16:36:48 +0100

    afsocket-dest: register the generate_persist_name method
    This commit registers the new _format_persist_name
    as the LogPipe::generate_persist_name method.
    
    Signed-off-by: MÓZES Ádám István <adam.mozes@balabit.com>

--- modules/afsocket/afsocket-dest.c
+++ modules/afsocket/afsocket-dest.c
@@ -552,6 +552,7 @@ afsocket_dd_init_instance(AFSocketDestDriver *self,
   self->super.super.super.queue = NULL;
   self->super.super.super.free_fn = afsocket_dd_free;
   self->super.super.super.notify = afsocket_dd_notify;
+  self->super.super.super.generate_persist_name = afsocket_dd_format_name;
   self->setup_addresses = afsocket_dd_setup_addresses;
   self->construct_writer = afsocket_dd_construct_writer_method;
   self->transport_mapper = transport_mapper;
