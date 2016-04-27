35260afa7b751bcdf549bfb81ec83226b4d61694
László Várady <laszlo.varady@balabit.com>
2016-04-27 17:00:23 +0200

    libtest: add assert_template_format_with_context_msgs()
    Signed-off-by: László Várady <laszlo.varady@balabit.com>

--- libtest/template_lib.h
+++ libtest/template_lib.h
@@ -36,6 +36,7 @@ void assert_template_format_with_escaping(const gchar *template, gboolean escapi
 void assert_template_format_with_escaping_msg(const gchar *template, gboolean escaping,
                                      const gchar *expected, LogMessage *msg);
 void assert_template_format_with_context(const gchar *template, const gchar *expected);
+void assert_template_format_with_context_msgs(const gchar *template, const gchar *expected, LogMessage **msgs, gint num_messages);
 void assert_template_failure(const gchar *template, const gchar *expected_failure);
 
 LogMessage *create_empty_message(void);
