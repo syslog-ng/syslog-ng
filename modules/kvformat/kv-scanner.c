e64fce22e3677096c8de1fdafb59907b21db05f2
bkil-syslogng <tamas.nagy@balabit.com>
2016-06-08 19:07:48 +0200

    kvformat/kv-scanner.c: refactor to enhance style
    Signed-off-by: bkil-syslogng <tamas.nagy@balabit.com>

--- modules/kvformat/kv-scanner.c
+++ modules/kvformat/kv-scanner.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Balabit
+ * Copyright (c) 2015-2016 Balabit
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
@@ -84,25 +84,63 @@ _kv_scanner_extract_key(KVScanner *self)
   return TRUE;
 }
 
+static void
+_decode_backslash_escape(KVScanner *self, gchar ch)
+{
+  gchar control;
+  switch (ch)
+    {
+    case 'b':
+      control = '\b';
+      break;
+    case 'f':
+      control = '\f';
+      break;
+    case 'n':
+      control = '\n';
+      break;
+    case 'r':
+      control = '\r';
+      break;
+    case 't':
+      control = '\t';
+      break;
+    case '\\':
+      control = '\\';
+      break;
+    default:
+      if (self->quote_char != ch)
+        g_string_append_c(self->value, '\\');
+      control = ch;
+      break;
+    }
+  g_string_append_c(self->value, control);
+}
+
+static gboolean
+_is_delimiter(const gchar *cur)
+{
+  return (*cur == ' ') || (strncmp(cur, ", ", 2) == 0);
+}
+
 static gboolean
 _kv_scanner_extract_value(KVScanner *self)
 {
   const gchar *cur;
-  gchar control;
 
   g_string_truncate(self->value, 0);
-  self->quote_state = KV_QUOTE_INITIAL;
   self->value_was_quoted = FALSE;
   cur = &self->input[self->input_pos];
+
+  self->quote_state = KV_QUOTE_INITIAL;
   while (*cur && self->quote_state != KV_QUOTE_FINISH)
     {
       switch (self->quote_state)
         {
         case KV_QUOTE_INITIAL:
-          if (*cur == ' ' || strncmp(cur, ", ", 2) == 0)
+          if (_is_delimiter(cur))
             {
               self->quote_state = KV_QUOTE_FINISH;
-              break;
             }
           else if (*cur == '\"' || *cur == '\'')
             {
@@ -110,51 +148,22 @@ _kv_scanner_extract_value(KVScanner *self)
               self->quote_char = *cur;
               if (self->value->len == 0)
                 self->value_was_quoted = TRUE;
-              break;
             }
-          g_string_append_c(self->value, *cur);
+          else
+            {
+              g_string_append_c(self->value, *cur);
+            }
           break;
         case KV_QUOTE_STRING:
           if (*cur == self->quote_char)
-            {
-              self->quote_state = KV_QUOTE_INITIAL;
-              break;
-            }
+            self->quote_state = KV_QUOTE_INITIAL;
           else if (*cur == '\\')
-            {
-              self->quote_state = KV_QUOTE_BACKSLASH;
-              break;
-            }
-          g_string_append_c(self->value, *cur);
+            self->quote_state = KV_QUOTE_BACKSLASH;
+          else
+            g_string_append_c(self->value, *cur);
           break;
         case KV_QUOTE_BACKSLASH:
-          switch (*cur)
-            {
-            case 'b':
-              control = '\b';
-              break;
-            case 'f':
-              control = '\f';
-              break;
-            case 'n':
-              control = '\n';
-              break;
-            case 'r':
-              control = '\r';
-              break;
-            case 't':
-              control = '\t';
-              break;
-            case '\\':
-              control = '\\';
-              break;
-            default:
-              if (self->quote_char != *cur)
-                g_string_append_c(self->value, '\\');
-              control = *cur;
-              break;
-            }
-          g_string_append_c(self->value, control);
+          _decode_backslash_escape(self, *cur);
           self->quote_state = KV_QUOTE_STRING;
           break;
         }
