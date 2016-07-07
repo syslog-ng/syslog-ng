021497c59aedccb23014b246c169708b2788429e
Laszlo Budai <stentor.bgyk@gmail.com>
2016-07-07 12:28:54 +0200

    kvtagger: copyright cleanup
    Signed-off-by: Laszlo Budai <stentor.bgyk@gmail.com>

--- modules/kvtagger/kvtagger.h
+++ modules/kvtagger/kvtagger.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2002-2015 Balabit
- * Copyright (c) 1998-2015 Balázs Scheidler
+ * Copyright (c) 2016 Balabit
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
