f58739c12903c04e6ec3ea748620018163b48c2b
Noémi Ványi <sitbackandwait@gmail.com>
2016-07-15 10:41:17 +0200

    plugin-skeleton-creator: remove BalaBit from copyright

--- dev-utils/plugin_skeleton_creator/plugin_template_plugin.c
+++ dev-utils/plugin_skeleton_creator/plugin_template_plugin.c
@@ -1,6 +1,4 @@
 /*
- * Copyright (c) 2002-2014 BalaBit S.a.r.l.
- *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
  * by the Free Software Foundation, or (at your option) any later version.
