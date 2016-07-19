5195ae3b805e6f6c57e68a30998bb8c0c05b9b50
Laszlo Budai <Laszlo.Budai@balabit.com>
2016-07-19 12:39:13 +0200

    debian: cleanup-upstream-debian.sh: cleaned up
    Signed-off-by: Laszlo Budai <Laszlo.Budai@balabit.com>

--- debian/tools/cleanup-upstream-debian.sh
+++ debian/tools/cleanup-upstream-debian.sh
@@ -3,21 +3,3 @@ set -e
 
 rm -f README.markdown
 
-cd debian
-
-rm -f \
-   Makefile \
-   README.Debian \
-   changelog.in \
-   syslog-ng.conf.example \
-   syslog-ng.default \
-   syslog-ng.files \
-   syslog-ng.init \
-   syslog-ng.logrotate \
-   syslog-ng.logrotate.example \
-   syslog-ng.postinst \
-   syslog-ng.postrm \
-   syslog-ng.preinst
-
-touch -r ../Makefile.am Makefile.am
-touch -r ../Makefile.in Makefile.in
