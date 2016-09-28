fbc06a496027526ff55097606866d21300359acf
Noémi Ványi <sitbackandwait@gmail.com>
2016-09-28 13:58:19 +0200

    lib/test/test_cache: switch to Criterion

--- lib/tests/test_cache.c
+++ lib/tests/test_cache.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2013 Balabit
+ * Copyright (c) 2002-2016 Balabit
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -21,7 +21,7 @@
  *
  */
 #include "cache.h"
-#include "testutils.h"
+#include <criterion/criterion.h>
 
 gint fetch_count;
 gint free_fn_count;
@@ -58,7 +58,8 @@ assert_cache_lookup(Cache *c, const gchar *key)
   gchar *expected_value = g_strdup_printf("almafa_%s", key);
 
   value = cache_lookup(c, key);
-  assert_string(value, expected_value, "Value error for \"%s\" key", key);
+  cr_assert_str_eq(value, expected_value, "Lookup key: %s, Expected value: %s, Actual value: %s",
+                   key, value, expected_value);
 
   g_free(expected_value);
 }
@@ -68,8 +69,8 @@ assert_cache_lookup_uncached(Cache *c, const gchar *key)
 {
   fetch_count = 0;
   assert_cache_lookup(c, key);
-  assert_gint(fetch_count, 1, "Cache lookup expected when looking up uncached elements, but one didn't arrive key=\"%s\"",
-              key);
+  cr_assert_eq(fetch_count, 1,
+               "Cache lookup expected when looking up uncached elements, but one didn't arrive key=\"%s\"", key);
 }
 
 static void
@@ -77,12 +78,11 @@ assert_cache_lookup_cached(Cache *c, const gchar *key)
 {
   fetch_count = 0;
   assert_cache_lookup(c, key);
-  assert_gint(fetch_count, 0, "Cache lookup unexpected when looking up cached elements, but one did arrive key=\"%s\"",
-              key);
+  cr_assert_eq(fetch_count, 0, "Cache lookup unexpected when looking up cached elements, but one did arrive key=\"%s\"",
+               key);
 }
 
-void
-test_cache_write_and_read(void)
+Test(cache, test_write_and_read)
 {
   Cache *c;
 
@@ -99,21 +99,12 @@ test_cache_write_and_read(void)
   cache_free(c);
 }
 
-void
-test_cache_free_calls_resolver_free_fn(void)
+Test(cache, test_free_calls_resolver_free_fn)
 {
   Cache *c;
 
   free_fn_count = 0;
   c = cache_new(dummy_cache_resolver());
   cache_free(c);
-  assert_gint(free_fn_count, 1, "cache_free call the free_fn %d times", free_fn_count);
-}
-
-int
-main(int argc G_GNUC_UNUSED, char *argv[] G_GNUC_UNUSED)
-{
-  test_cache_write_and_read();
-  test_cache_free_calls_resolver_free_fn();
-  return 0;
+  cr_assert_eq(free_fn_count, 1, "cache_free call the free_fn %d times", free_fn_count);
 }
