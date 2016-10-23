6ad99478165c42a083a74d1c7c376fff58203052
Balazs Scheidler <balazs.scheidler@balabit.com>
2016-10-23 17:08:00 +0200

    basicfuncs: add list related template functions
    A list is defined as a comma separated list of values. The single element
    list is an element without a comma at all.
    
    The list-concat template function concatenates these lists
    into a single list.
    
    $(list-concat $HOST $PROGRAM $PID)
    
    You can use the comma-notation as well:
    $(list-concat $HOST,$PROGRAM $PID)
    
    both will result in "host,program,pid".
    
    $(list-head list ...)          returns the first element (unquoted)
    $(list-nth NDX list ...)       returns the specific element (unquoted)
    $(list-concat list1 list2 ...) returns a list containing the concatenated list
    $(list-append list elem1 elem2 ...) returns a list, appending elem1, elem2 ...
    $(list-tail list ...)          returns a list containing everything except for the first element
    $(list-slice FROM:TO list ...) returns a list containing the slice [FROM:TO), Python style slice
                                   boundaries are supported (e.g. negative)
    $(list-count ...)              returns the number of elements in list
    
    Functions above, either return a well formed list (e.g. all elements
    are properly encoded/quoted) or return a single element, in which
    case all quotation is decoded and the value contains the literal value.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- modules/basicfuncs/basic-funcs.c
+++ modules/basicfuncs/basic-funcs.c
@@ -43,6 +43,7 @@
 #include "cond-funcs.c"
 #include "ip-funcs.c"
 #include "misc-funcs.c"
+#include "list-funcs.c"
 #include "tf-template.c"
 
 static Plugin basicfuncs_plugins[] =
@@ -62,6 +63,15 @@ static Plugin basicfuncs_plugins[] =
   TEMPLATE_FUNCTION_PLUGIN(tf_uppercase, "uppercase"),
   TEMPLATE_FUNCTION_PLUGIN(tf_replace_delimiter, "replace-delimiter"),
   TEMPLATE_FUNCTION_PLUGIN(tf_string_padding, "padding"),
+
+  /* list-funcs */
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_concat, "list-concat"),
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_head, "list-head"),
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_nth, "list-nth"),
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_tail, "list-tail"),
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_slice, "list-slice"),
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_count, "list-count"),
+  TEMPLATE_FUNCTION_PLUGIN(tf_list_append, "list-append"),
 
   /* numeric-funcs */
   TEMPLATE_FUNCTION_PLUGIN(tf_num_plus, "+"),
