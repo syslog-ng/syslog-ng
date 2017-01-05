ecd998d26924cdbdea369af30b4789b294748c45
Andras <andras.mitzki@balabit.com>
2017-01-05 11:28:21 +0100

    PythonModule: Handle deletion of _syslogng module
      * We should only delete if the "_syslogng" module
    exists
      * Without this patch syslog-ng crashes at start
    in the following conditions:
        * OS: Ubuntu 16.04
        * config contains: python{};
    
    Signed-off-by: Andras <andras.mitzki@balabit.com>

--- modules/python/python-main.c
+++ modules/python/python-main.c
@@ -53,7 +53,8 @@ _py_construct_main_module(void)
   PyObject *modules = PyImport_GetModuleDict();
 
   /* make sure the module registry doesn't contain our module */
-  PyDict_DelItemString(modules, "_syslogng");
+  if (PyDict_Contains(modules, PyBytes_FromString("_syslogng")))
+    PyDict_DelItemString(modules, "_syslogng");
 
   /* create a new module */
   module = PyImport_AddModule("_syslogng");
