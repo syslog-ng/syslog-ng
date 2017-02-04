3af1f2e6a6b7cac50ae62f22980c7eaf3952d8d6
Juhász Viktor <juhasz.viktor81@gmail.com>
2017-02-04 16:37:45 +0100

    FileReader: extract the file reader from the SourceDriver

--- modules/affile/affile-source.c
+++ modules/affile/affile-source.c
@@ -130,21 +130,29 @@ affile_is_device_node(const gchar *filename)
 }
 
 gboolean
-_sd_open_file(AFFileSourceDriver *self, gchar *name, gint *fd)
+_sd_open_file(FileReader *self, gchar *name, gint *fd)
 {
   return affile_open_file(name,
-                          &self->file_reader_options.file_open_options,
-                          &self->file_reader_options.file_perm_options, fd);
+                          &self->file_reader_options->file_open_options,
+                          &self->file_reader_options->file_perm_options, fd);
 }
 
 static inline const gchar *
 affile_sd_format_persist_name(const LogPipe *s)
 {
-  const AFFileSourceDriver *self = (const AFFileSourceDriver *)s;
+  AFFileSourceDriver *self = (AFFileSourceDriver *)s;
+  return log_pipe_get_persist_name(&self->file_reader->super);
+}
+
+
+static inline const gchar *
+file_reader_format_persist_name(const LogPipe *s)
+{
+  const FileReader *self = (const FileReader *)s;
   static gchar persist_name[1024];
 
-  if (s->persist_name)
-    g_snprintf(persist_name, sizeof(persist_name), "affile_sd.%s.curpos", s->persist_name);
+  if (self->owner->super.super.persist_name)
+    g_snprintf(persist_name, sizeof(persist_name), "affile_sd.%s.curpos", self->owner->super.super.persist_name);
   else
     g_snprintf(persist_name, sizeof(persist_name), "affile_sd_curpos(%s)", self->filename->str);
 
@@ -152,14 +160,14 @@ affile_sd_format_persist_name(const LogPipe *s)
 }
 
 static void
-affile_sd_recover_state(LogPipe *s, GlobalConfig *cfg, LogProtoServer *proto)
+file_reader_recover_state(LogPipe *s, GlobalConfig *cfg, LogProtoServer *proto)
 {
-  AFFileSourceDriver *self = (AFFileSourceDriver *) s;
+  FileReader *self = (FileReader *) s;
 
-  if (self->file_reader_options.file_open_options.is_pipe || self->file_reader_options.follow_freq <= 0)
+  if (self->file_reader_options->file_open_options.is_pipe || self->file_reader_options->follow_freq <= 0)
     return;
 
-  if (!log_proto_server_restart_with_state(proto, cfg->state, affile_sd_format_persist_name(s)))
+  if (!log_proto_server_restart_with_state(proto, cfg->state, file_reader_format_persist_name(s)))
     {
       msg_error("Error converting persistent state from on-disk format, losing file position information",
                 evt_tag_str("filename", self->filename->str));
@@ -184,10 +192,10 @@ _is_fd_pollable(gint fd)
 }
 
 static PollEvents *
-affile_sd_construct_poll_events(AFFileSourceDriver *self, gint fd)
+file_reader_construct_poll_events(FileReader *self, gint fd)
 {
-  if (self->file_reader_options.follow_freq > 0)
-    return poll_file_changes_new(fd, self->filename->str, self->file_reader_options.follow_freq, &self->super.super.super);
+  if (self->file_reader_options->follow_freq > 0)
+    return poll_file_changes_new(fd, self->filename->str, self->file_reader_options->follow_freq, &self->super);
   else if (fd >= 0 && _is_fd_pollable(fd))
     return poll_fd_events_new(fd);
   else
@@ -202,11 +210,11 @@ affile_sd_construct_poll_events(AFFileSourceDriver *self, gint fd)
 }
 
 static LogTransport *
-affile_sd_construct_transport(AFFileSourceDriver *self, gint fd)
+file_reader_construct_transport(FileReader *self, gint fd)
 {
-  if (self->file_reader_options.file_open_options.is_pipe)
+  if (self->file_reader_options->file_open_options.is_pipe)
     return log_transport_pipe_new(fd);
-  else if (self->file_reader_options.follow_freq > 0)
+  else if (self->file_reader_options->follow_freq > 0)
     return log_transport_file_new(fd);
   else if (affile_is_linux_proc_kmsg(self->filename->str))
     return log_transport_device_new(fd, 10);
@@ -224,14 +232,14 @@ affile_sd_construct_transport(AFFileSourceDriver *self, gint fd)
 }
 
 static LogProtoServer *
-affile_sd_construct_proto(AFFileSourceDriver *self, gint fd)
+file_reader_construct_proto(FileReader *self, gint fd)
 {
-  LogReaderOptions *reader_options = &self->file_reader_options.reader_options;
+  LogReaderOptions *reader_options = &self->file_reader_options->reader_options;
   LogProtoServerOptions *proto_options = &reader_options->proto_options.super;
   LogTransport *transport;
   MsgFormatHandler *format_handler;
 
-  transport = affile_sd_construct_transport(self, fd);
+  transport = file_reader_construct_transport(self, fd);
 
   format_handler = reader_options->parse_options.format_handler;
   if ((format_handler && format_handler->construct_proto))
@@ -240,10 +248,10 @@ affile_sd_construct_proto(AFFileSourceDriver *self, gint fd)
       return format_handler->construct_proto(&reader_options->parse_options, transport, proto_options);
     }
 
-  if (self->file_reader_options.pad_size)
+  if (self->file_reader_options->pad_size)
     {
       proto_options->position_tracking_enabled = TRUE;
-      return log_proto_padded_record_server_new(transport, proto_options, self->file_reader_options.pad_size);
+      return log_proto_padded_record_server_new(transport, proto_options, self->file_reader_options->pad_size);
     }
   else if (affile_is_linux_proc_kmsg(self->filename->str))
     return log_proto_linux_proc_kmsg_reader_new(transport, proto_options);
@@ -252,18 +260,18 @@ affile_sd_construct_proto(AFFileSourceDriver *self, gint fd)
   else
     {
       proto_options->position_tracking_enabled = TRUE;
-      switch (self->file_reader_options.multi_line_mode)
+      switch (self->file_reader_options->multi_line_mode)
         {
         case MLM_INDENTED:
           return log_proto_indented_multiline_server_new(transport, proto_options);
         case MLM_PREFIX_GARBAGE:
           return log_proto_prefix_garbage_multiline_server_new(transport, proto_options,
-                 self->file_reader_options.multi_line_prefix,
-                 self->file_reader_options.multi_line_garbage);
+                 self->file_reader_options->multi_line_prefix,
+                 self->file_reader_options->multi_line_garbage);
         case MLM_PREFIX_SUFFIX:
           return log_proto_prefix_suffix_multiline_server_new(transport, proto_options,
-                 self->file_reader_options.multi_line_prefix,
-                 self->file_reader_options.multi_line_garbage);
+                 self->file_reader_options->multi_line_prefix,
+                 self->file_reader_options->multi_line_garbage);
         default:
           return log_proto_text_server_new(transport, proto_options);
         }
@@ -271,7 +279,7 @@ affile_sd_construct_proto(AFFileSourceDriver *self, gint fd)
 }
 
 static void
-_deinit_sd_logreader(AFFileSourceDriver *self)
+_deinit_sd_logreader(FileReader *self)
 {
   log_pipe_deinit((LogPipe *) self->reader);
   log_pipe_unref((LogPipe *) self->reader);
@@ -281,16 +289,16 @@ _deinit_sd_logreader(AFFileSourceDriver *self)
 static void
 _setup_logreader(LogPipe *s, PollEvents *poll_events, LogProtoServer *proto, gboolean check_immediately)
 {
-  AFFileSourceDriver *self = (AFFileSourceDriver *) s;
-  self->reader = log_reader_new(self->super.super.super.cfg);
+  FileReader *self = (FileReader *) s;
+  self->reader = log_reader_new(log_pipe_get_config(s));
   log_reader_reopen(self->reader, proto, poll_events);
 
   log_reader_set_options(self->reader,
                          s,
-                         &self->file_reader_options.reader_options,
+                         &self->file_reader_options->reader_options,
                          STATS_LEVEL1,
                          SCS_FILE,
-                         self->super.super.id,
+                         self->owner->super.id,
                          self->filename->str);
   if (check_immediately)
     log_reader_set_immediate_check(self->reader);
@@ -312,15 +320,15 @@ _is_immediate_check_needed(gboolean file_opened, gboolean open_deferred)
 }
 
 static gboolean
-affile_sd_open_file(LogPipe *s, gboolean recover_state)
+file_reader_open_file(LogPipe *s, gboolean recover_state)
 {
-  AFFileSourceDriver *self = (AFFileSourceDriver *) s;
+  FileReader *self = (FileReader *) s;
   GlobalConfig *cfg = log_pipe_get_config(s);
   gint fd;
   gboolean file_opened, open_deferred = FALSE;
 
   file_opened = _sd_open_file(self, self->filename->str, &fd);
-  if (!file_opened && self->file_reader_options.follow_freq > 0)
+  if (!file_opened && self->file_reader_options->follow_freq > 0)
     {
       msg_info("Follow-mode file source not found, deferring open", evt_tag_str("filename", self->filename->str));
       open_deferred = TRUE;
@@ -333,13 +341,13 @@ affile_sd_open_file(LogPipe *s, gboolean recover_state)
       PollEvents *poll_events;
       gboolean check_immediately;
 
-      poll_events = affile_sd_construct_poll_events(self, fd);
+      poll_events = file_reader_construct_poll_events(self, fd);
       if (!poll_events)
         {
           close(fd);
           return FALSE;
         }
-      proto = affile_sd_construct_proto(self, fd);
+      proto = file_reader_construct_proto(self, fd);
 
       check_immediately = _is_immediate_check_needed(file_opened, open_deferred);
       _setup_logreader(s, poll_events, proto, check_immediately);
@@ -352,33 +360,33 @@ affile_sd_open_file(LogPipe *s, gboolean recover_state)
           return FALSE;
         }
       if (recover_state)
-        affile_sd_recover_state(s, cfg, proto);
+        file_reader_recover_state(s, cfg, proto);
     }
   else
     {
       msg_error("Error opening file for reading",
                 evt_tag_str("filename", self->filename->str),
                 evt_tag_errno(EVT_TAG_OSERROR, errno));
-      return self->super.super.optional;
+      return self->owner->super.optional;
     }
   return TRUE;
 
 }
 
 static void
-affile_sd_reopen_on_notify(LogPipe *s, gboolean recover_state)
+file_reader_reopen_on_notify(LogPipe *s, gboolean recover_state)
 {
-  AFFileSourceDriver *self = (AFFileSourceDriver *) s;
+  FileReader *self = (FileReader *) s;
 
   _deinit_sd_logreader(self);
-  affile_sd_open_file(s, recover_state);
+  file_reader_open_file(s, recover_state);
 }
 
 /* NOTE: runs in the main thread */
 static void
-affile_sd_notify(LogPipe *s, gint notify_code, gpointer user_data)
+file_reader_notify(LogPipe *s, gint notify_code, gpointer user_data)
 {
-  AFFileSourceDriver *self = (AFFileSourceDriver *) s;
+  FileReader *self = (FileReader *) s;
 
   switch (notify_code)
     {
@@ -386,14 +394,14 @@ affile_sd_notify(LogPipe *s, gint notify_code, gpointer user_data)
     {
       msg_verbose("Follow-mode file source moved, tracking of the new file is started",
                   evt_tag_str("filename", self->filename->str));
-      affile_sd_reopen_on_notify(s, TRUE);
+      file_reader_reopen_on_notify(s, TRUE);
       break;
     }
     case NC_READ_ERROR:
     {
       msg_verbose("Error while following source file, reopening in the hope it would work",
                   evt_tag_str("filename", self->filename->str));
-      affile_sd_reopen_on_notify(s, FALSE);
+      file_reader_reopen_on_notify(s, FALSE);
       break;
     }
     default:
@@ -404,15 +412,20 @@ affile_sd_notify(LogPipe *s, gint notify_code, gpointer user_data)
 static void
 affile_sd_queue(LogPipe *s, LogMessage *msg, const LogPathOptions *path_options, gpointer user_data)
 {
-  AFFileSourceDriver *self = (AFFileSourceDriver *) s;
+  log_src_driver_queue_method(s, msg, path_options, user_data);
+}
+
+static void
+file_reader_queue(LogPipe *s, LogMessage *msg, const LogPathOptions *path_options, gpointer user_data)
+{
+  FileReader *self = (FileReader *)s;
   static NVHandle filename_handle = 0;
 
   if (!filename_handle)
     filename_handle = log_msg_get_value_handle("FILE_NAME");
 
   log_msg_set_value(msg, filename_handle, self->filename->str, self->filename->len);
-
-  log_src_driver_queue_method(s, msg, path_options, user_data);
+  log_pipe_forward_msg(s, msg, path_options);
 }
 
 static gboolean
@@ -423,6 +436,12 @@ _are_multi_line_settings_invalid(AFFileSourceDriver *self)
 
   return (!is_garbage_mode && !is_suffix_mode) && (self->file_reader_options.multi_line_prefix
          || self->file_reader_options.multi_line_garbage);
+}
+
+static gboolean
+file_reader_init(LogPipe *s)
+{
+  return file_reader_open_file(s, TRUE);
 }
 
 static gboolean
@@ -443,7 +462,17 @@ affile_sd_init(LogPipe *s)
       return FALSE;
     }
 
-  return affile_sd_open_file(s, TRUE);
+  log_pipe_append(&self->file_reader->super, &self->super.super.super);
+  return log_pipe_init(&self->file_reader->super);
+}
+
+static gboolean
+file_reader_deinit(LogPipe *s)
+{
+  FileReader *self = (FileReader *)s;
+  if (self->reader)
+    _deinit_sd_logreader(self);
+  return TRUE;
 }
 
 static gboolean
@@ -451,9 +480,7 @@ affile_sd_deinit(LogPipe *s)
 {
   AFFileSourceDriver *self = (AFFileSourceDriver *) s;
 
-  if (self->reader)
-    _deinit_sd_logreader(self);
-
+  log_pipe_deinit(&self->file_reader->super);
   if (!log_src_driver_deinit_method(s))
     return FALSE;
 
@@ -461,19 +488,41 @@ affile_sd_deinit(LogPipe *s)
 }
 
 static void
+file_reader_free(LogPipe *s)
+{
+  FileReader *self = (FileReader *)s;
+  g_string_free(self->filename, TRUE);
+  g_assert(!self->reader);
+}
+static void
 affile_sd_free(LogPipe *s)
 {
   AFFileSourceDriver *self = (AFFileSourceDriver *) s;
 
+  log_pipe_unref(&self->file_reader->super);
   g_string_free(self->filename, TRUE);
-  g_assert(!self->reader);
-
   log_reader_options_destroy(&self->file_reader_options.reader_options);
 
   multi_line_regexp_free(self->file_reader_options.multi_line_prefix);
   multi_line_regexp_free(self->file_reader_options.multi_line_garbage);
 
   log_src_driver_free(s);
+}
+
+static FileReader *
+file_reader_new(gchar *filename, LogSrcDriver *owner, GlobalConfig *cfg)
+{
+  FileReader *self = g_new0(FileReader, 1);
+  log_pipe_init_instance(&self->super, cfg);
+  self->owner = owner;
+  self->filename = g_string_new(filename);
+  self->super.init = file_reader_init;
+  self->super.queue = file_reader_queue;
+  self->super.deinit = file_reader_deinit;
+  self->super.notify = file_reader_notify;
+  self->super.free_fn = file_reader_free;
+  self->super.generate_persist_name = file_reader_format_persist_name;
+  return self;
 }
 
 static AFFileSourceDriver *
@@ -483,10 +532,11 @@ affile_sd_new_instance(gchar *filename, GlobalConfig *cfg)
 
   log_src_driver_init_instance(&self->super, cfg);
   self->filename = g_string_new(filename);
+  self->file_reader = file_reader_new(filename, &self->super, cfg);
+  self->file_reader->file_reader_options = &self->file_reader_options;
   self->super.super.super.init = affile_sd_init;
   self->super.super.super.queue = affile_sd_queue;
   self->super.super.super.deinit = affile_sd_deinit;
-  self->super.super.super.notify = affile_sd_notify;
   self->super.super.super.free_fn = affile_sd_free;
   self->super.super.super.generate_persist_name = affile_sd_format_persist_name;
   log_reader_options_defaults(&self->file_reader_options.reader_options);
