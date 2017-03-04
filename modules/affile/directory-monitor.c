6d76d5da275726eb00487f903f413c6d7105a545
Juhász Viktor <juhasz.viktor81@gmail.com>
2017-03-04 15:05:31 +0100

    directory_monitor: object monitors the directory.
    At first step it has no monitor it dynamically,
    but it can read all files from a directory.
    Callback function will be called for every directory entry
    in the given directory.
    
    Signed-off-by: Juhász Viktor <juhasz.viktor81@gmail.com>

---
+++ modules/affile/directory-monitor.c
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2017 Balabit
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "directory-monitor.h"
+
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <messages.h>
+
+static gchar *
+build_filename(const gchar *basedir, const gchar *path)
+{
+  gchar *result;
+
+  if (!path)
+    return NULL;
+
+  if (basedir)
+    {
+      result = (gchar *)g_malloc(strlen(basedir) + strlen(path) + 2);
+      sprintf(result, "%s/%s", basedir, path);
+    }
+  else
+    {
+      result = g_strdup(path);
+    }
+
+  return result;
+}
+
+static inline long
+get_path_max()
+{
+  long path_max;
+#ifdef PATH_MAX
+  path_max = PATH_MAX;
+#else
+  path_max = pathconf(path, _PC_PATH_MAX);
+  if (path_max <= 0)
+    path_max = 4096;
+#endif
+  return path_max;
+}
+
+/*
+ Resolve . and ..
+ Resolve symlinks
+ Resolve tricki symlinks like a -> ../a/../a/./b
+*/
+gchar *
+resolve_to_absolute_path(const gchar *path, const gchar *basedir)
+{
+  long path_max = get_path_max();
+  gchar *res;
+  gchar *w_name;
+
+  w_name = build_filename(basedir, path);
+  res = (char *)g_malloc(path_max);
+
+  if (!realpath(w_name, res))
+    {
+      g_free(res);
+      if (errno == ENOENT)
+        {
+          res = g_strdup(path);
+        }
+      else
+        {
+          msg_error("Can't resolve to absolute path", evt_tag_str("path", path), evt_tag_errno("error", errno), NULL);
+          res = NULL;
+        }
+    }
+  g_free(w_name);
+  return res;
+}
+
+gchar *
+directory_monitor_get_real_path(DirectoryMonitor *self)
+{
+  gchar *dir_real_path = NULL;
+  if (!g_path_is_absolute(self->dir))
+    {
+      gchar *wd = g_get_current_dir();
+      dir_real_path = resolve_to_absolute_path(self->dir, wd);
+      g_free(wd);
+    }
+  else
+    {
+      dir_real_path = resolve_to_absolute_path(self->dir, NULL);
+    }
+  return dir_real_path;
+}
+
+void
+directory_monitor_collect_all_files(DirectoryMonitor *self, COLLECT_FILES_CALLBACK callback, gpointer user_data)
+{
+  GError *error = NULL;
+  gchar *abs_path = directory_monitor_get_real_path(self);
+  GDir *directory = g_dir_open(abs_path, 0, &error);
+  if (!directory)
+    {
+      msg_error("Can not open directory",
+                evt_tag_str("base_dir", abs_path),
+                evt_tag_str("error", error->message));
+      g_error_free(error);
+      g_free(abs_path);
+      return;
+    }
+  const gchar *filename = g_dir_read_name(directory);
+  while(filename)
+    {
+      gchar *filename_real_path = resolve_to_absolute_path(filename, abs_path);
+      gchar *filename_full_path = build_filename(abs_path, filename);
+      FileType file_type = g_file_test(filename_real_path, G_FILE_TEST_IS_DIR) ? FILE_IS_DIRECTORY : FILE_IS_REGULAR;
+      callback(filename, filename_full_path, file_type, user_data);
+      g_free(filename_real_path);
+      g_free(filename_full_path);
+      filename = g_dir_read_name(directory);
+    }
+  g_free(abs_path);
+  g_dir_close(directory);
+}
+
+DirectoryMonitor *
+directory_monitor_new(const gchar *dir)
+{
+  DirectoryMonitor *self = g_new0(DirectoryMonitor, 1);
+  self->dir = g_strdup(dir);
+  return self;
+}
+
+void
+directory_monitor_free(DirectoryMonitor *self)
+{
+  if (self)
+    {
+      g_free(self->dir);
+      g_free(self);
+    }
+}
---
+++ modules/affile/directory-monitor.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2017 Balabit
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+#ifndef MODULES_AFFILE_DIRECTORY_MONITOR_H_
+#define MODULES_AFFILE_DIRECTORY_MONITOR_H_
+
+#include <syslog-ng.h>
+
+typedef enum {
+  FILE_IS_DIRECTORY,
+  FILE_IS_REGULAR
+} FileType;
+
+typedef  void (*COLLECT_FILES_CALLBACK)(const gchar *name,
+                                        const gchar *full_path,
+                                        FileType file_type,
+                                        gpointer user_data);
+
+typedef struct _DirectoryMonitor {
+  gchar *dir;
+} DirectoryMonitor;
+
+DirectoryMonitor* directory_monitor_new(const gchar *dir);
+void directory_monitor_free(DirectoryMonitor *self);
+
+gchar *directory_monitor_get_real_path(DirectoryMonitor *self);
+void directory_monitor_collect_all_files(DirectoryMonitor *self, COLLECT_FILES_CALLBACK, gpointer user_data);
+
+#endif /* MODULES_AFFILE_DIRECTORY_MONITOR_H_ */
