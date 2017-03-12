4a0bd3665e392cecc355f9a67cf1acefa7618f92
Juhász Viktor <juhasz.viktor81@gmail.com>
2017-03-12 09:49:48 +0100

    directory-monitor-factory: the creating of monitor is moved inside a factory.
    Because not every platform has inotify functionality,
    and later it more type of directory monitor(s) will be supported.
    
    Signed-off-by: Juhász Viktor <juhasz.viktor81@gmail.com>

---
+++ modules/affile/directory-monitor-factory.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2017 Balabit
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "directory-monitor-factory.h"
+
+#if HAVE_INOTIFY
+#include "directory-monitor-inotify.h"
+#endif
+
+DirectoryMonitor *
+create_directory_monitor(DirectoryMonitorOptions *options)
+{
+  DirectoryMonitor *monitor = NULL;
+#if HAVE_INOTIFY
+  monitor = directory_monitor_inotify_new(options->dir, options->follow_freq);
+#else
+  monitor = directory_monitor_new(options->dir, options->follow_freq);
+#endif
+  return monitor;
+}
