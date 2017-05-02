b7eafe80cbe4f96ddb399b0cf2c8fc0ce60de11d
Laszlo Budai <laszlo.budai@balabit.com>
2017-05-02 16:22:15 +0200

    affile: fix stats source for pipes
    Fixes: #1452
    
    Signed-off-by: Laszlo Budai <laszlo.budai@balabit.com>

--- modules/affile/file-reader.c
+++ modules/affile/file-reader.c
@@ -210,6 +210,12 @@ _deinit_sd_logreader(FileReader *self)
   self->reader = NULL;
 }
 
+static gint
+_get_stats_source(FileReader *self)
+{
+  return self->file_reader_options->file_open_options.is_pipe ? SCS_PIPE : SCS_FILE;
+}
+
 static void
 _setup_logreader(LogPipe *s, PollEvents *poll_events, LogProtoServer *proto, gboolean check_immediately)
 {
@@ -221,7 +227,7 @@ _setup_logreader(LogPipe *s, PollEvents *poll_events, LogProtoServer *proto, gbo
                          s,
                          &self->file_reader_options->reader_options,
                          STATS_LEVEL1,
-                         SCS_FILE,
+                         _get_stats_source(self),
                          self->owner->super.id,
                          self->filename->str);
   if (check_immediately)
