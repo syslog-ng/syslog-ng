c5c841a29e8e9431d0028412441286e96a5eef51
Noémi Ványi <sitbackandwait@gmail.com>
2017-05-02 15:45:21 +0200

    stats-query: implement indexing
    Previously, the full name of a counter was unknown.
    However, it is required to know it in order to support
    fully querying counters using globbing.
    Full name is query_key of its StatsCluster + name of its type.
    E.g center.received.processed. Constructing this name is pricey,
    so it is only done for each queried counter only once.
    
    This made it possible to get rid of NamedStatsCounterItem which was
    an ugly workaround and the callback have became more simple. Furthermore,
    the wizardry with the query expressions are removed, too. From now on,
    it is possible to call pattern_match on the filter expression specified
    by the user.
    
    The function _update_index optimizes for the lookup in the hashtable.
    It makes sure that only those counters are in the index which is alive
    and was queried in the past.
    
    Signed-off-by: Noémi Ványi <sitbackandwait@gmail.com>

--- lib/stats/stats-query.c
+++ lib/stats/stats-query.c
@@ -28,125 +28,124 @@
 
 #include <string.h>
 
-typedef struct _NamedStatsCounterItem
-{
-  StatsCluster *cluster;
-  gint index;
-} NamedStatsCounterItem;
 
 static GHashTable *counter_index;
-typedef gboolean (*StatsClusterCounterCb)(StatsCluster *sc, NamedStatsCounterItem *named_counter,
-                                          StatsFormatCb format_cb, gpointer user_data);
 
-
-static const gchar *
-_get_counter_name_by_index(gint type_index)
+static void
+_setup_filter_expression(const gchar *expr, gchar **key_str)
 {
-  const gchar *_counter_type[SC_TYPE_MAX+1] =
-  {
-    "dropped",
-    "processed",
-    "stored",
-    "suppressed",
-    "stamp",
-    "MAX"
-  };
-
-  if (type_index > SC_TYPE_MAX)
-    return "";
-
-  return _counter_type[type_index];
+  if (!expr || g_str_equal(expr, ""))
+    {
+      *key_str = g_strdup("*");
+    }
+  else
+    {
+      *key_str = g_strdup(expr);
+    }
 }
 
-static const gchar *
-_get_name_of_counter_item(NamedStatsCounterItem *item)
+static gchar *
+_construct_counter_item_name(StatsCluster *sc, gint type)
 {
-  return _get_counter_name_by_index(item->index);
+  GString *name = g_string_new("");
+
+  g_string_append(name, sc->query_key);
+  g_string_append(name, ".");
+  g_string_append(name, stats_cluster_get_type_name(sc, type));
+
+  return g_string_free(name, FALSE);
 }
 
 static void
-_append_live_counters(StatsCluster *sc, gchar *ctr_str, GList **counters)
+_add_counter_to_index(StatsCluster *sc, gint type, StatsCounterItem *counter)
 {
-  gint i;
-  gboolean is_wildcard = FALSE;
-  if (ctr_str[0] == '*')
-    is_wildcard = TRUE;
+  gchar *counter_full_name = NULL;
 
-  for (i = 0; i < SC_TYPE_MAX; i++)
+  counter_full_name = stats_counter_get_name(counter);
+  if (counter_full_name == NULL)
+    counter_full_name = _construct_counter_item_name(sc, type);
+
+  sc->counter_group.counters[type].name = counter_full_name;
+  g_hash_table_insert(counter_index, counter_full_name, &sc->counter_group.counters[type]);
+  sc->indexed_mask |= type;
+}
+
+static void
+_remove_counter_from_index(StatsCluster *sc, gint type)
+{
+  gchar *key = stats_counter_get_name(&sc->counter_group.counters[type]);
+  g_hash_table_remove(counter_index, key);
+  sc->indexed_mask |= type;
+}
+
+static void
+_index_counter(StatsCluster *sc, gint type, StatsCounterItem *counter, gpointer user_data)
+{
+  if (!stats_cluster_is_indexed(sc, type) && stats_cluster_is_alive(sc, type))
     {
-      if (stats_cluster_is_alive(sc, i))
-        {
-          if (is_wildcard || !strcmp(_get_counter_name_by_index(i), ctr_str))
-            {
-              NamedStatsCounterItem *c = g_new0(NamedStatsCounterItem, 1);
-              c->cluster = sc;
-              c->index = i;
-              *counters = g_list_append(*counters, c);
-            }
-        }
+      _add_counter_to_index(sc, type, counter);
+    }
+  else if (stats_cluster_is_indexed(sc, type) && !stats_cluster_is_alive(sc, type))
+    {
+      _remove_counter_from_index(sc, type);
     }
 }
 
-void
-_split_expr(const gchar *expr, gchar **key, gchar **ctr)
+static void
+_update_indexes_of_cluster_if_needed(gpointer key, gpointer value)
 {
-  const gchar *last_delim_pos = strrchr(expr, '.');
-  size_t expr_len = strlen(expr);
-  if (last_delim_pos)
-    {
-      size_t key_len = last_delim_pos - expr ;
-      *key = g_strndup(expr, key_len);
-      *ctr = g_strndup(expr + key_len + 1, expr_len - key_len - 1);
-    }
-  else
-    {
-      *key = g_strdup(expr);
-      *ctr = g_strdup("*");
-    }
+  StatsCluster *sc = (StatsCluster *)key;
+  stats_cluster_foreach_counter(sc, _index_counter, NULL);
 }
 
-void
-_setup_filter_expression(const gchar *expr, gchar **key_str, gchar **ctr_str)
+static void
+_update_index(void)
 {
-  if (!expr)
+  GHashTable *counter_container = stats_registry_get_container();
+  gpointer key, value;
+  GHashTableIter iter;
+
+  g_hash_table_iter_init(&iter, counter_container);
+  while (g_hash_table_iter_next(&iter, &key, &value))
     {
-      *key_str = g_strdup("*");
-      *ctr_str = g_strdup("*");
-    }
-  else
-    {
-      _split_expr(expr, key_str, ctr_str);
+      _update_indexes_of_cluster_if_needed(key, value);
     }
 }
 
 static gboolean
-_find_key_by_pattern(gpointer key, gpointer value, gpointer user_data)
+_is_pattern_matches_key(GPatternSpec *pattern, gpointer key)
 {
-  StatsCluster *sc = (StatsCluster *)key;
-  GPatternSpec *pattern = (GPatternSpec *)user_data;
+  gchar *counter_name = (gchar *) key;
+  return g_pattern_match_string(pattern, counter_name);
+}
 
-  return g_pattern_match_string(pattern, sc->query_key);
+static gboolean
+_is_single_match(gchar *key_str)
+{
+  gboolean is_wildcard = strchr(key_str, '*') ? TRUE : FALSE;
+  gboolean is_joker = strchr(key_str, '?') ? TRUE : FALSE;
+
+  return !is_wildcard && !is_joker;
 }
 
 static GList *
-_query_counter_hash(gchar *key_str, gchar *ctr_str)
+_query_counter_hash(gchar *key_str)
 {
-  GHashTable *counter_hash = stats_registry_get_container();
   GPatternSpec *pattern = g_pattern_spec_new(key_str);
-  StatsCluster *sc = NULL;
   GList *counters = NULL;
   gpointer key, value;
   GHashTableIter iter;
+  gboolean single_match;
 
-  g_hash_table_iter_init (&iter, counter_hash);
-
-  gboolean single_match = strchr(key_str, '*') ? FALSE : TRUE;
+  _update_index();
+  single_match = _is_single_match(key_str);
+  g_hash_table_iter_init(&iter, counter_index);
   while (g_hash_table_iter_next(&iter, &key, &value))
     {
-      if (_find_key_by_pattern(key, value, (gpointer)pattern))
+      if (_is_pattern_matches_key(pattern, key))
         {
-          sc = (StatsCluster *)key;
-          _append_live_counters(sc, ctr_str, &counters);
+          StatsCounterItem *counter = (StatsCounterItem *) value;
+          counters = g_list_append(counters, counter);
 
           if (single_match)
             break;
@@ -161,8 +160,8 @@ _format_selected_counters(GList *counters, StatsFormatCb format_cb, gpointer res
 {
   for (GList *counter = counters; counter; counter = counter->next)
     {
-      NamedStatsCounterItem *c = counter->data;
-      format_cb(c->cluster, &c->cluster->counter_group.counters[c->index], _get_name_of_counter_item(c), result);
+      StatsCounterItem *c = counter->data;
+      format_cb(c, result);
     }
 }
 
@@ -172,9 +171,8 @@ _reset_selected_counters(GList *counters)
   GList *c;
   for (c = counters; c; c = c->next)
     {
-      NamedStatsCounterItem *counter = c->data;
-      StatsCounterItem *item = &counter->cluster->counter_group.counters[counter->index];
-      stats_counter_set(item, 0);
+      StatsCounterItem *counter = c->data;
+      stats_counter_set(counter, 0);
     }
 }
 
@@ -185,12 +183,11 @@ _stats_query_get(const gchar *expr, StatsFormatCb format_cb, gpointer result, gb
     return FALSE;
 
   gchar *key_str = NULL;
-  gchar *ctr_str = NULL;
   GList *counters = NULL;
   gboolean found_match = FALSE;
 
-  _split_expr(expr, &key_str, &ctr_str);
-  counters = _query_counter_hash(key_str, ctr_str);
+  _setup_filter_expression(expr, &key_str);
+  counters = _query_counter_hash(key_str);
   _format_selected_counters(counters, format_cb, result);
 
   if (must_reset)
@@ -199,9 +196,7 @@ _stats_query_get(const gchar *expr, StatsFormatCb format_cb, gpointer result, gb
   if (g_list_length(counters) > 0)
     found_match = TRUE;
 
-  g_list_free_full(counters, g_free);
   g_free(key_str);
-  g_free(ctr_str);
 
   return found_match;
 }
@@ -226,23 +221,25 @@ _sum_selected_counters(GList *counters, gpointer user_data)
   gint64 *sum = (gint64 *) args[1];
   for (c = counters; c; c = c->next)
     {
-      NamedStatsCounterItem *counter = c->data;
-      *sum += counter->cluster->counter_group.counters[counter->index].value;
+      StatsCounterItem *counter = c->data;
+      *sum += stats_counter_get(counter);
     }
 }
 
 gboolean
 _stats_query_get_sum(const gchar *expr, StatsFormatCb format_cb, gpointer result, gboolean must_reset)
 {
+  if (!expr)
+    return FALSE;
+
   gchar *key_str = NULL;
-  gchar *ctr_str = NULL;
   GList *counters = NULL;
   gboolean found_match = FALSE;
   gint64 sum = 0;
   gpointer args[] = {result, &sum};
 
-  _setup_filter_expression(expr, &key_str, &ctr_str);
-  counters = _query_counter_hash(key_str, ctr_str);
+  _setup_filter_expression(expr, &key_str);
+  counters = _query_counter_hash(key_str);
   _sum_selected_counters(counters, (gpointer)args);
   _format_selected_counters(counters, format_cb, (gpointer)args);
 
@@ -252,9 +249,7 @@ _stats_query_get_sum(const gchar *expr, StatsFormatCb format_cb, gpointer result
   if (g_list_length(counters) > 0)
     found_match = TRUE;
 
-  g_list_free_full(counters, g_free);
   g_free(key_str);
-  g_free(ctr_str);
 
   return found_match;
 }
@@ -275,12 +270,11 @@ gboolean
 _stats_query_list(const gchar *expr, StatsFormatCb format_cb, gpointer result, gboolean must_reset)
 {
   gchar *key_str = NULL;
-  gchar *ctr_str = NULL;
   GList *counters = NULL;
   gboolean found_match = FALSE;
 
-  _setup_filter_expression(expr, &key_str, &ctr_str);
-  counters = _query_counter_hash(key_str, ctr_str);
+  _setup_filter_expression(expr, &key_str);
+  counters = _query_counter_hash(key_str);
   _format_selected_counters(counters, format_cb, result);
 
   if (must_reset)
@@ -289,9 +283,7 @@ _stats_query_list(const gchar *expr, StatsFormatCb format_cb, gpointer result, g
   if (g_list_length(counters) > 0)
     found_match = TRUE;
 
-  g_list_free_full(counters, g_free);
   g_free(key_str);
-  g_free(ctr_str);
 
   return found_match;
 }
--- lib/stats/stats-query.h
+++ lib/stats/stats-query.h
@@ -27,7 +27,7 @@
 #include "stats-cluster.h"
 #include "syslog-ng.h"
 
-typedef gboolean (*StatsFormatCb)(StatsCluster *sc, StatsCounterItem *ctr, const gchar *ctr_name, gpointer user_data);
+typedef gboolean (*StatsFormatCb)(StatsCounterItem *ctr, gpointer user_data);
 
 gboolean stats_query_list(const gchar *expr, StatsFormatCb format_cb, gpointer result);
 gboolean stats_query_list_and_reset_counters(const gchar *expr, StatsFormatCb format_cb, gpointer result);
