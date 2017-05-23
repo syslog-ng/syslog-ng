3a114e1c2fd8eef1ff6644de5dc113a9e5a6587e
SZALAY Attila <sasa@ubainba.hu>
2017-05-23 21:48:29 +0200

    Added eventlog files
    The goal is to merge the libevtlog into the syslog-ng. As a first step this
    patch add all the files from the libeventlog project to the /lib/eventlog
    directory as-is.

---
+++ lib/eventlog/src/evtmaps.h
@@ -0,0 +1,54 @@
+/*
+ * Event Logging API
+ * Copyright (c) 2003 BalaBit IT Ltd.
+ * All rights reserved.
+ * Author: Balazs Scheidler
+ *
+ * $Id: evtmaps.h,v 1.3 2003/01/10 09:24:51 bazsi Exp $
+ *
+ * Some of the ideas are based on the discussions on the log-analysis
+ * mailing list (http://www.loganalysis.org/).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of BalaBit nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY BALABIT AND CONTRIBUTORS S IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#ifndef __EVTMAPS_H_INCLUDED
+#define __EVTMAPS_H_INCLUDED
+
+/* base event map, with no namespace */
+
+#define EVT_TAG_PID		"pid"
+#define EVT_TAG_PROG		"prog"
+#define EVT_TAG_ISOSTAMP	"isostamp"
+#define EVT_TAG_UTCSTAMP	"utcstamp"
+#define EVT_TAG_TIMEZONE	"tz"
+#define EVT_TAG_MSGID		"msgid"
+
+#define EVT_TAG_FD		"fd"
+#define EVT_TAG_OSERROR		"error"
+#define EVT_TAG_FILENAME	"filename"
+
+#endif
