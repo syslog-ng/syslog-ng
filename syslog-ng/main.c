4a5182580e254f96195194b8e0486d527fd39d4c
Gabor Nagy <gabor.nagy@balabit.com>
2017-06-02 15:51:39 +0200

    main: move cfg dependent startup function calls after cfg has been initialized
    Counter registration is configuration dependent (it's directly depending on StatsOptions object).
    This object is not initialized in app_startup phase, but after main_loop_read_and_init_config.
    New phase created which can safely use StatsOptions.
    
    Move stats initialization from log_msg_global_init into
    to log_msg_stats_global_init function.
    It is needed as counter registration is configuration dependent
    when stats level is used.
    By default stats level 0 is working without a cfg, because
    the stat level check function allows it.
    
    Signed-off-by: Gabor Nagy <gabor.nagy@balabit.com>

--- syslog-ng/main.c
+++ syslog-ng/main.c
@@ -270,6 +270,7 @@ main(int argc, char *argv[])
   app_startup();
   main_loop_init(main_loop, &main_loop_options);
   rc = main_loop_read_and_init_config(main_loop);
+  app_finish_app_startup_after_cfg_init();
 
   if (rc)
     {
