436a4b87a938db52480d6124e3674a92cfd5a7b1
Laszlo Budai <laszlo.budai@balabit.com>
2017-07-19 12:47:43 +0200

    Revert "Merge pull request #1571 from faithlesstomas/issue1570"
    This reverts commit 194b85ce8ab70e41d1a3c2efa2069334ae4fc3df, reversing
    changes made to 67876774d268d7ecc2d2e22533dc1c10b2289c80.
    
    Fixes: #1587

--- modules/systemd-journal/journald-subsystem.c
+++ modules/systemd-journal/journald-subsystem.c
@@ -76,8 +76,6 @@ typedef int
 typedef int
 (*SD_JOURNAL_SEEK_CURSOR)(sd_journal *j, const char *cursor);
 typedef int
-(*SD_JOURNAL_TEST_CURSOR)(sd_journal *j, const char *cursor);
-typedef int
 (*SD_JOURNAL_GET_FD)(sd_journal *j);
 typedef int
 (*SD_JOURNAL_PROCESS)(sd_journal *j);
@@ -93,7 +91,6 @@ static SD_JOURNAL_NEXT sd_journal_next;
 static SD_JOURNAL_RESTART_DATA sd_journal_restart_data;
 static SD_JOURNAL_ENUMERATE_DATA sd_journal_enumerate_data;
 static SD_JOURNAL_SEEK_CURSOR sd_journal_seek_cursor;
-static SD_JOURNAL_TEST_CURSOR sd_journal_test_cursor;
 static SD_JOURNAL_GET_FD sd_journal_get_fd;
 static SD_JOURNAL_PROCESS sd_journal_process;
 static SD_JOURNAL_GET_REALTIME_USEC sd_journal_get_realtime_usec;
@@ -225,12 +222,6 @@ int
 journald_seek_cursor(Journald *self, const gchar *cursor)
 {
   return sd_journal_seek_cursor(self->journal, cursor);
-}
-
-int
-journald_test_cursor(Journald *self, const gchar *cursor)
-{
-  return sd_journal_test_cursor(self->journal, cursor);
 }
 
 int
