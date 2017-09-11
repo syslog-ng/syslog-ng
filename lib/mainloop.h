33c999d3254544bf31136e5677d47e0b53952e11
Antal Nemes <antal.nemes@balabit.com>
2017-09-11 15:48:37 +0200

    mainloop: block mainloop-exit until workers exit
    Prior this patch: during exit, mainloop did not wait worker threads to
    exit, which resulted racing of app_shutdown and app_thread_stop. This
    patch path block mainloop exit until the number of threads alive drop
    to zero.
    
    Signed-off-by: Antal Nemes <antal.nemes@balabit.com>

--- lib/mainloop.h
+++ lib/mainloop.h
@@ -27,6 +27,8 @@
 #include "syslog-ng.h"
 #include "thread-utils.h"
 
+volatile gint main_loop_workers_running;
+
 typedef struct _MainLoop MainLoop;
 
 typedef struct _MainLoopOptions
@@ -38,6 +40,8 @@ typedef struct _MainLoopOptions
 } MainLoopOptions;
 
 extern ThreadId main_thread_handle;
+extern GCond thread_halt_cond;
+extern GMutex workers_running_lock;
 
 typedef gpointer (*MainLoopTaskFunc)(gpointer user_data);
 
