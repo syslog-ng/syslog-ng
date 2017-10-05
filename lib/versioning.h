6ec42e550bf0780a488a154efba0adfe393d8807
Balazs Scheidler <balazs.scheidler@balabit.com>
2017-10-05 21:30:29 +0200

    versioning: add syslog-ng 3.13 as version number
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- lib/versioning.h
+++ lib/versioning.h
@@ -104,6 +104,7 @@
 #define VERSION_3_10 "syslog-ng 3.10"
 #define VERSION_3_11 "syslog-ng 3.11"
 #define VERSION_3_12 "syslog-ng 3.12"
+#define VERSION_3_13 "syslog-ng 3.13"
 
 #define VERSION_VALUE_3_0  0x0300
 #define VERSION_VALUE_3_1  0x0301
@@ -118,11 +119,12 @@
 #define VERSION_VALUE_3_10 0x030a
 #define VERSION_VALUE_3_11 0x030b
 #define VERSION_VALUE_3_12 0x030c
+#define VERSION_VALUE_3_13 0x030d
 
 /* config version code, in the same format as GlobalConfig->version */
-#define VERSION_VALUE   0x030c
-#define VERSION_CURRENT VERSION_3_12
-#define VERSION_CURRENT_VER_ONLY "3.12"
+#define VERSION_VALUE   0x030d
+#define VERSION_CURRENT VERSION_3_13
+#define VERSION_CURRENT_VER_ONLY "3.13"
 
 #define version_convert_from_user(v)  (v)
 
