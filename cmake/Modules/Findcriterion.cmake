8d483219b0897cb67553af5cd4e9335d9771d63d
Antal Nemes <antal.nemes@balabit.com>
2017-12-07 10:53:08 +0100

    cmake: running unit tests
    Signed-off-by: Antal Nemes <antal.nemes@balabit.com>

--- cmake/Modules/Findcriterion.cmake
+++ cmake/Modules/Findcriterion.cmake
@@ -27,3 +27,7 @@ libfind_pkg_detect(CRITERION criterion FIND_PATH criterion/criterion.h FIND_LIBR
 set(CRITERION_PROCESS_INCLUDES CRITERION_INCLUDE_DIR)
 set(CRITERION_PROCESS_LIBS CRITERION_LIBRARY)
 libfind_process(CRITERION)
+
+if (NOT CRITERION_FOUND)
+    MESSAGE(WARNING "Criterion not found: some unit tests will not be compiled")
+endif()
---
+++ cmake/add_tests.cmake
@@ -0,0 +1,50 @@
+#############################################################################
+# Copyright (c) 2017 Balabit
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+
+
+include(CMakeParseArguments)
+
+function (add_unit_test)
+  cmake_parse_arguments(ADD_UNIT_TEST "CRITERION;LIBTEST" "TARGET" "SOURCES;DEPENDS;INCLUDES" ${ARGN})
+
+  if (NOT ADD_UNIT_TEST_SOURCES)
+    set(ADD_UNIT_TEST_SOURCES "${ADD_UNIT_TEST_TARGET}.c")
+  endif()
+
+  add_executable(${ADD_UNIT_TEST_TARGET} ${ADD_UNIT_TEST_SOURCES})
+  target_link_libraries(${ADD_UNIT_TEST_TARGET} ${ADD_UNIT_TEST_DEPENDS} syslog-ng)
+  target_include_directories(${ADD_UNIT_TEST_TARGET} PUBLIC ${ADD_UNIT_TEST_INCLUDES})
+  set_property(TARGET ${ADD_UNIT_TEST_TARGET} APPEND PROPERTY LINK_FLAGS "-Wl,--no-as-needed")
+
+  if (${ADD_UNIT_TEST_CRITERION})
+    target_link_libraries(${ADD_UNIT_TEST_TARGET} criterion)
+    set_property(TARGET ${ADD_UNIT_TEST_TARGET} PROPERTY POSITION_INDEPENDENT_CODE FALSE)
+  endif()
+
+  if (${ADD_UNIT_TEST_LIBTEST})
+    target_link_libraries(${ADD_UNIT_TEST_TARGET} libtest)
+    target_include_directories(${ADD_UNIT_TEST_TARGET} PUBLIC "${CMAKE_SOURCE_DIR}/libtest")
+  endif()
+
+  add_test (${ADD_UNIT_TEST_TARGET} ${ADD_UNIT_TEST_TARGET})
+endfunction ()
