aeb906162825a7587c2fdd03847947ca0b01096a
Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>
2017-12-13 15:00:04 +0800

    mainloop: masking worker_id for undefined behavior on huge shifting exponent
    Signed-off-by: Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>

--- lib/mainloop-worker.c
+++ lib/mainloop-worker.c
@@ -103,7 +103,8 @@ _release_thread_id(void)
   g_static_mutex_lock(&main_loop_workers_idmap_lock);
   if (main_loop_worker_id)
     {
-      main_loop_workers_idmap[main_loop_worker_type] &= ~(1 << (main_loop_worker_id - 1));
+      const gint id = main_loop_worker_id & (sizeof(guint64) * CHAR_BIT - 1);
+      main_loop_workers_idmap[main_loop_worker_type] &= ~(1 << (id - 1));
       main_loop_worker_id = 0;
     }
   g_static_mutex_unlock(&main_loop_workers_idmap_lock);
