c0c34619fb277a5ccf19436114711df50acc1ff8
László Várady <laszlo.varady@balabit.com>
2018-02-07 16:33:28 +0100

    affile: simplify time-reap()
    The removed condition is a legacy from an old version of affile, where only
    one time-reap() timer was used per destination.
    
    Since the current implementation creates a timer for every destination
    writer, the logic can be simplified.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>
    Signed-off-by: László Várady <laszlo.varady@balabit.com>

--- modules/affile/affile-dest.c
+++ modules/affile/affile-dest.c
@@ -119,7 +119,7 @@ affile_dw_arm_reaper(AFFileDestWriter *self)
   /* not yet reaped, set up the next callback */
   iv_validate_now();
   self->reap_timer.expires = iv_now;
-  timespec_add_msec(&self->reap_timer.expires, self->owner->time_reap * 1000 / 2);
+  timespec_add_msec(&self->reap_timer.expires, self->owner->time_reap * 1000);
   iv_timer_register(&self->reap_timer);
 }
 
@@ -131,9 +131,7 @@ affile_dw_reap(gpointer s)
   main_loop_assert_main_thread();
 
   g_static_mutex_lock(&self->lock);
-  if (!log_writer_has_pending_writes(self->writer) &&
-      !self->queue_pending &&
-      (cached_g_current_time_sec() - self->last_msg_stamp) >= self->owner->time_reap)
+  if (!log_writer_has_pending_writes(self->writer) && !self->queue_pending)
     {
       g_static_mutex_unlock(&self->lock);
       msg_verbose("Destination timed out, reaping",
