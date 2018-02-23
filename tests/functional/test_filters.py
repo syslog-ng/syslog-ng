4b3b14fc6fa97dc3cc5ac33db24ef0370aab286f
Gabor Nagy <gabor.nagy@balabit.com>
2018-02-23 12:34:28 +0100

    VERSION: 3.14
    Signed-off-by: Gabor Nagy <gabor.nagy@balabit.com>

--- tests/functional/test_filters.py
+++ tests/functional/test_filters.py
@@ -25,7 +25,7 @@ from log import *
 from messagegen import *
 from messagecheck import *
 
-config = """@version: 3.13
+config = """@version: 3.14
 
 options { ts_format(iso); chain_hostnames(no); keep_hostname(yes); threaded(yes); };
 
