3472ddf64004929f92fd7e912af523a2b1916c4d
Antal Nemes <antal.nemes@balabit.com>
2018-04-20 11:47:45 +0200

    messages: temporary api : evt_tag_capture_errno
    Signed-off-by: Antal Nemes <antal.nemes@balabit.com>

--- lib/tests/test_messages.c
+++ lib/tests/test_messages.c
@@ -26,6 +26,7 @@
 #include "logmsg/logmsg.h"
 
 #include <criterion/criterion.h>
+#include <errno.h>
 
 TestSuite(test_messages, .init = app_startup, .fini = app_shutdown);
 
@@ -39,4 +40,36 @@ Test(test_messages, simple_test)
 {
   msg_set_post_func(simple_test_asserter);
   msg_error("simple test");
+}
+
+void test_errno_asserter(LogMessage *msg)
+{
+  gchar expected_message[100];
+  g_snprintf(expected_message, sizeof(expected_message), "test errno; error='%s (%d)'", strerror(5), 5);
+  cr_assert_str_eq(log_msg_get_value_by_name(msg, "MESSAGE", NULL), expected_message);
+  log_msg_unref(msg);
+}
+
+Test(test_messages, test_errno)
+{
+  errno = 5;
+  msg_set_post_func(test_errno_asserter);
+  msg_error("test errno",
+            evt_tag_errno("error", errno));
+}
+
+void test_errno_capture_asserter(LogMessage *msg)
+{
+  gchar pattern[100];
+  g_snprintf(pattern, sizeof(pattern), "error='%s (%d)'", strerror(9), 9);
+  const gchar *msg_content = log_msg_get_value_by_name(msg, "MESSAGE", NULL);
+  cr_assert(g_strstr_len(msg_content, -1, pattern), "searching: >>%s<< in >>%s<<", pattern, msg_content);
+  log_msg_unref(msg);
+}
+
+Test(test_messages, test_errno_capture)
+{
+  errno = 9;
+  msg_set_post_func(test_errno_capture_asserter);
+  msg_error("test errno capture", (errno=10,evt_tag_capture_errno("error")));
 }
