53dc57bdd3d2e51e8e4d41a9a3e0546fd1686c4b
László Várady <laszlo.varady@balabit.com>
2018-05-24 13:17:51 +0200

    ack-tracker: increment window size for all ack_types
    Even if the message is dropped by the destination and its ac_type is
    AT_SUSPENDED, the window size should be incremented.
    
    Signed-off-by: László Várady <laszlo.varady@balabit.com>

--- lib/early_ack_tracker.c
+++ lib/early_ack_tracker.c
@@ -62,7 +62,10 @@ early_ack_tracker_manage_msg_ack(AckTracker *s, LogMessage *msg, AckType ack_typ
   EarlyAckTracker *self = (EarlyAckTracker *)s;
 
   if (ack_type == AT_SUSPENDED)
-    log_source_flow_control_suspend(self->super.source);
+    {
+      log_source_flow_control_adjust(self->super.source, 1);
+      log_source_flow_control_suspend(self->super.source);
+    }
   else
     log_source_flow_control_adjust(self->super.source, 1);
 
