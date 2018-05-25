628f6ad80d2fc777f345844d7ff39dc1e2308207
norberttakacs <norbert.takacs@balabit.com>
2018-05-25 10:42:40 +0200

    Rewrite test_rcptid based on criterion
    Signed-off-by: norberttakacs <norbert.takacs@balabit.com>

--- lib/tests/test_rcptid.c
+++ lib/tests/test_rcptid.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002-2013 Balabit
+ * Copyright (c) 2002-2018 Balabit
  * Copyright (c) 2013 Viktor Juhasz
  * Copyright (c) 2013 Viktor Tusa
  *
@@ -22,7 +22,7 @@
  * COPYING for details.
  *
  */
-#include "testutils.h"
+#include <criterion/criterion.h>
 #include "syslog-ng.h"
 #include "apphook.h"
 #include "rcptid.h"
@@ -30,106 +30,81 @@
 
 #include <unistd.h>
 
-gboolean verbose = FALSE;
-PersistState *state;
-
-void
-setup_persist_id_test(void)
+static PersistState *
+setup_persist_id_test(const gchar *persist_file)
 {
-  state = clean_and_create_persist_state_for_test("test_values.persist");
+  PersistState *state;
+
+  state = clean_and_create_persist_state_for_test(persist_file);
   rcptid_init(state, TRUE);
+  return state;
 }
 
-void
-teardown_persist_id_test(void)
+static void
+teardown_persist_id_test(PersistState *state)
 {
   commit_and_destroy_persist_state(state);
   rcptid_deinit();
 }
 
 static void
-test_rcptid_is_persistent_across_persist_backend_reinits(void)
+setup(void)
+{
+  app_startup();
+}
+
+static void
+teardown(void)
+{
+  app_shutdown();
+}
+
+TestSuite(rcptid, .init = setup, .fini = teardown);
+
+Test(rcptid, test_rcptid_is_persistent_across_persist_backend_reinits)
 {
   guint64 rcptid;
 
-  setup_persist_id_test();
-
+  PersistState *state = setup_persist_id_test("test_values.backend_reinits");
   rcptid_set_id(0xFFFFFFFFFFFFFFFE);
   rcptid = rcptid_generate_id();
-
-  assert_guint64(rcptid, 0xFFFFFFFFFFFFFFFE, "Rcptid initialization to specific value failed!");
+  cr_assert_eq(rcptid, 0xFFFFFFFFFFFFFFFE, "Rcptid initialization to specific value failed!");
 
   state = restart_persist_state(state);
 
-  rcptid_deinit();
-  rcptid_init(state, TRUE);
-
   rcptid = rcptid_generate_id();
-
-  assert_guint64(rcptid, 0xFFFFFFFFFFFFFFFF, "Rcptid did not persisted across persist backend reinit!");
-
-  teardown_persist_id_test();
+  cr_assert_eq(rcptid, 0xFFFFFFFFFFFFFFFF, "Rcptid did not persisted across persist backend reinit!");
+  teardown_persist_id_test(state);
 }
 
-static void
-test_rcptid_overflows_at_64bits_and_is_reset_to_one(void)
+Test(rcptid, test_rcptid_overflows_at_64bits_and_is_reset_to_one)
 {
   guint64 rcptid;
 
-  setup_persist_id_test();
-
+  PersistState *state = setup_persist_id_test("test_values.reset_to_one");
   rcptid_set_id(0xFFFFFFFFFFFFFFFF);
   rcptid = rcptid_generate_id();
   rcptid = rcptid_generate_id();
-
-  assert_guint64(rcptid, (guint64) 1, "Rcptid counter overflow handling did not work!");
-  teardown_persist_id_test();
+  cr_assert_eq(rcptid, (guint64) 1, "Rcptid counter overflow handling did not work!");
+  teardown_persist_id_test(state);
 }
 
-static void
-test_rcptid_is_formatted_as_a_number_when_nonzero(void)
+Test(rcptid, test_rcptid_is_formatted_as_a_number_when_nonzero)
 {
+  PersistState *state = setup_persist_id_test("test_values.nonzero");
   GString *formatted_rcptid = g_string_sized_new(0);
-
-  setup_persist_id_test();
-
   rcptid_append_formatted_id(formatted_rcptid, 1);
-  assert_string(formatted_rcptid->str, "1", "RCPTID macro formatting for non-zero number failed!");
-
-  teardown_persist_id_test();
+  cr_assert_str_eq(formatted_rcptid->str, "1", "RCPTID macro formatting for non-zero number failed!");
   g_string_free(formatted_rcptid, TRUE);
+  teardown_persist_id_test(state);
 }
 
-static void
-test_rcptid_is_an_empty_string_when_zero(void)
+Test(rcptid, test_rcptid_is_an_empty_string_when_zero)
 {
+  PersistState *state = setup_persist_id_test("test_values.zero");
   GString *formatted_rcptid = g_string_sized_new(0);
-
-  setup_persist_id_test();
-
   rcptid_append_formatted_id(formatted_rcptid, 0);
-  assert_string(formatted_rcptid->str, "", "RCPTID macro formatting for zero value failed!");
-
-  teardown_persist_id_test();
+  cr_assert_str_eq(formatted_rcptid->str, "", "RCPTID macro formatting for zero value failed!");
   g_string_free(formatted_rcptid, TRUE);
-}
-
-static void
-rcptid_test_case(void)
-{
-  test_rcptid_is_persistent_across_persist_backend_reinits();
-  test_rcptid_overflows_at_64bits_and_is_reset_to_one();
-  test_rcptid_is_formatted_as_a_number_when_nonzero();
-  test_rcptid_is_an_empty_string_when_zero();
-}
-
-int
-main(int argc G_GNUC_UNUSED, char *argv[] G_GNUC_UNUSED)
-{
-  if (argc > 1)
-    verbose = TRUE;
-  app_startup();
-
-  rcptid_test_case();
-  return 0;
+  teardown_persist_id_test(state);
 }
