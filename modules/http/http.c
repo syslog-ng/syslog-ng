7b6fa138672d0c51bb75f83384d8fc68a7b0c858
Balazs Scheidler <balazs.scheidler@balabit.com>
2018-06-02 03:49:04 +0200

    http: move setters to the front of the file
    By convention, setters are in the front of the file, and implementation
    logic towards the end, move them, so it is easier to navigate within
    the actual logic.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

--- modules/http/http.c
+++ modules/http/http.c
@@ -24,6 +24,205 @@
 #include "syslog-names.h"
 #include "scratch-buffers.h"
 
+void
+http_dd_set_url(LogDriver *d, const gchar *url)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->url);
+  self->url = g_strdup(url);
+}
+
+void
+http_dd_set_user(LogDriver *d, const gchar *user)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->user);
+  self->user = g_strdup(user);
+}
+
+void
+http_dd_set_password(LogDriver *d, const gchar *password)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->password);
+  self->password = g_strdup(password);
+}
+
+void
+http_dd_set_user_agent(LogDriver *d, const gchar *user_agent)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->user_agent);
+  self->user_agent = g_strdup(user_agent);
+}
+
+void
+http_dd_set_headers(LogDriver *d, GList *headers)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_list_free_full(self->headers, g_free);
+  self->headers = g_list_copy_deep(headers, ((GCopyFunc)g_strdup), NULL);
+}
+
+void
+http_dd_set_method(LogDriver *d, const gchar *method)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  if (g_ascii_strcasecmp(method, "POST") == 0)
+    self->method_type = METHOD_TYPE_POST;
+  else if (g_ascii_strcasecmp(method, "PUT") == 0)
+    self->method_type = METHOD_TYPE_PUT;
+  else
+    {
+      msg_warning("Unsupported method is set(Only POST and PUT are supported), default method POST will be used",
+                  evt_tag_str("method", method));
+      self->method_type = METHOD_TYPE_POST;
+    }
+}
+
+void
+http_dd_set_body(LogDriver *d, LogTemplate *body)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  log_template_unref(self->body_template);
+  self->body_template = log_template_ref(body);
+}
+
+LogTemplateOptions *
+http_dd_get_template_options(LogDriver *d)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  return &self->template_options;
+}
+
+void
+http_dd_set_ca_dir(LogDriver *d, const gchar *ca_dir)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->ca_dir);
+  self->ca_dir = g_strdup(ca_dir);
+}
+
+void
+http_dd_set_ca_file(LogDriver *d, const gchar *ca_file)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->ca_file);
+  self->ca_file = g_strdup(ca_file);
+}
+
+void
+http_dd_set_cert_file(LogDriver *d, const gchar *cert_file)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->cert_file);
+  self->cert_file = g_strdup(cert_file);
+}
+
+void
+http_dd_set_key_file(LogDriver *d, const gchar *key_file)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->key_file);
+  self->key_file = g_strdup(key_file);
+}
+
+void
+http_dd_set_cipher_suite(LogDriver *d, const gchar *ciphers)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  g_free(self->ciphers);
+  self->ciphers = g_strdup(ciphers);
+}
+
+void
+http_dd_set_ssl_version(LogDriver *d, const gchar *value)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  if (strcmp(value, "default") == 0)
+    {
+      /*
+       * Negotiate the version based on what the remote server supports.
+       * SSLv2 is disabled by default as of libcurl 7.18.1.
+       * SSLv3 is disabled by default as of libcurl 7.39.0.
+       */
+      self->ssl_version = CURL_SSLVERSION_DEFAULT;
+
+    }
+  else if (strcmp(value, "tlsv1") == 0)
+    {
+      /* TLS 1.x */
+      self->ssl_version = CURL_SSLVERSION_TLSv1;
+    }
+  else if (strcmp(value, "sslv2") == 0)
+    {
+      /* SSL 2 only */
+      self->ssl_version = CURL_SSLVERSION_SSLv2;
+
+    }
+  else if (strcmp(value, "sslv3") == 0)
+    {
+      /* SSL 3 only */
+      self->ssl_version = CURL_SSLVERSION_SSLv3;
+    }
+#ifdef CURL_SSLVERSION_TLSv1_0
+  else if (strcmp(value, "tlsv1_0") == 0)
+    {
+      /* TLS 1.0 only */
+      self->ssl_version = CURL_SSLVERSION_TLSv1_0;
+    }
+#endif
+#ifdef CURL_SSLVERSION_TLSv1_1
+  else if (strcmp(value, "tlsv1_1") == 0)
+    {
+      /* TLS 1.1 only */
+      self->ssl_version = CURL_SSLVERSION_TLSv1_1;
+    }
+#endif
+#ifdef CURL_SSLVERSION_TLSv1_2
+  else if (strcmp(value, "tlsv1_2") == 0)
+    {
+      /* TLS 1.2 only */
+      self->ssl_version = CURL_SSLVERSION_TLSv1_2;
+    }
+#endif
+  else
+    {
+      msg_warning("curl: unsupported SSL version",
+                  evt_tag_str("ssl_version", value));
+    }
+}
+
+void
+http_dd_set_peer_verify(LogDriver *d, gboolean verify)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  self->peer_verify = verify;
+}
+
+void
+http_dd_set_timeout(LogDriver *d, glong timeout)
+{
+  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
+
+  self->timeout = timeout;
+}
+
 static const gchar *
 _format_persist_name(const LogPipe *s)
 {
@@ -255,204 +454,6 @@ _insert(LogThreadedDestDriver *s, LogMessage *msg)
   return retval;
 }
 
-void
-http_dd_set_url(LogDriver *d, const gchar *url)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->url);
-  self->url = g_strdup(url);
-}
-
-void
-http_dd_set_user(LogDriver *d, const gchar *user)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->user);
-  self->user = g_strdup(user);
-}
-
-void
-http_dd_set_password(LogDriver *d, const gchar *password)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->password);
-  self->password = g_strdup(password);
-}
-
-void
-http_dd_set_user_agent(LogDriver *d, const gchar *user_agent)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->user_agent);
-  self->user_agent = g_strdup(user_agent);
-}
-
-void
-http_dd_set_headers(LogDriver *d, GList *headers)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_list_free_full(self->headers, g_free);
-  self->headers = g_list_copy_deep(headers, ((GCopyFunc)g_strdup), NULL);
-}
-
-void
-http_dd_set_method(LogDriver *d, const gchar *method)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  if (g_ascii_strcasecmp(method, "POST") == 0)
-    self->method_type = METHOD_TYPE_POST;
-  else if (g_ascii_strcasecmp(method, "PUT") == 0)
-    self->method_type = METHOD_TYPE_PUT;
-  else
-    {
-      msg_warning("Unsupported method is set(Only POST and PUT are supported), default method POST will be used",
-                  evt_tag_str("method", method));
-      self->method_type = METHOD_TYPE_POST;
-    }
-}
-
-void
-http_dd_set_body(LogDriver *d, LogTemplate *body)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  log_template_unref(self->body_template);
-  self->body_template = log_template_ref(body);
-}
-
-LogTemplateOptions *
-http_dd_get_template_options(LogDriver *d)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  return &self->template_options;
-}
-
-void
-http_dd_set_ca_dir(LogDriver *d, const gchar *ca_dir)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->ca_dir);
-  self->ca_dir = g_strdup(ca_dir);
-}
-
-void
-http_dd_set_ca_file(LogDriver *d, const gchar *ca_file)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->ca_file);
-  self->ca_file = g_strdup(ca_file);
-}
-
-void
-http_dd_set_cert_file(LogDriver *d, const gchar *cert_file)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->cert_file);
-  self->cert_file = g_strdup(cert_file);
-}
-
-void
-http_dd_set_key_file(LogDriver *d, const gchar *key_file)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->key_file);
-  self->key_file = g_strdup(key_file);
-}
-
-void
-http_dd_set_cipher_suite(LogDriver *d, const gchar *ciphers)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  g_free(self->ciphers);
-  self->ciphers = g_strdup(ciphers);
-}
-
-void
-http_dd_set_ssl_version(LogDriver *d, const gchar *value)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  if (strcmp(value, "default") == 0)
-    {
-      /*
-       * Negotiate the version based on what the remote server supports.
-       * SSLv2 is disabled by default as of libcurl 7.18.1.
-       * SSLv3 is disabled by default as of libcurl 7.39.0.
-       */
-      self->ssl_version = CURL_SSLVERSION_DEFAULT;
-
-    }
-  else if (strcmp(value, "tlsv1") == 0)
-    {
-      /* TLS 1.x */
-      self->ssl_version = CURL_SSLVERSION_TLSv1;
-    }
-  else if (strcmp(value, "sslv2") == 0)
-    {
-      /* SSL 2 only */
-      self->ssl_version = CURL_SSLVERSION_SSLv2;
-
-    }
-  else if (strcmp(value, "sslv3") == 0)
-    {
-      /* SSL 3 only */
-      self->ssl_version = CURL_SSLVERSION_SSLv3;
-    }
-#ifdef CURL_SSLVERSION_TLSv1_0
-  else if (strcmp(value, "tlsv1_0") == 0)
-    {
-      /* TLS 1.0 only */
-      self->ssl_version = CURL_SSLVERSION_TLSv1_0;
-    }
-#endif
-#ifdef CURL_SSLVERSION_TLSv1_1
-  else if (strcmp(value, "tlsv1_1") == 0)
-    {
-      /* TLS 1.1 only */
-      self->ssl_version = CURL_SSLVERSION_TLSv1_1;
-    }
-#endif
-#ifdef CURL_SSLVERSION_TLSv1_2
-  else if (strcmp(value, "tlsv1_2") == 0)
-    {
-      /* TLS 1.2 only */
-      self->ssl_version = CURL_SSLVERSION_TLSv1_2;
-    }
-#endif
-  else
-    {
-      msg_warning("curl: unsupported SSL version",
-                  evt_tag_str("ssl_version", value));
-    }
-}
-
-void
-http_dd_set_peer_verify(LogDriver *d, gboolean verify)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  self->peer_verify = verify;
-}
-
-void
-http_dd_set_timeout(LogDriver *d, glong timeout)
-{
-  HTTPDestinationDriver *self = (HTTPDestinationDriver *) d;
-
-  self->timeout = timeout;
-}
 
 gboolean
 http_dd_init(LogPipe *s)
