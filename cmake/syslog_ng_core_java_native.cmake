92cd4df480c007d6c35ec605a587d16baf06046f
Kokan <kokaipeter@gmail.com>
2018-07-11 07:22:09 +0200

    java: cmake create interface lib for generated headers
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- cmake/syslog_ng_core_java_native.cmake
+++ cmake/syslog_ng_core_java_native.cmake
@@ -13,7 +13,9 @@ if ("${Java_VERSION}" VERSION_LESS 1.8)
     get_property(SYSLOG_NG_CLASSPATH TARGET syslog-ng-core PROPERTY CLASSDIR)
   endif()
 
-  create_javah(GENERATED_FILES BUILT_HEADERS
+  create_javah(
+        TARGET syslog-ng-core-java-headers
+        GENERATED_FILES BUILT_HEADERS
         CLASSES    org.syslog_ng.LogMessage org.syslog_ng.InternalMessageSender org.syslog_ng.LogDestination org.syslog_ng.LogTemplate org.syslog_ng.LogPipe
         CLASSPATH ${SYSLOG_NG_CLASSPATH}
         OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}
@@ -21,6 +23,10 @@ if ("${Java_VERSION}" VERSION_LESS 1.8)
   )
 
   set_source_files_properties(${BUILT_HEADERS} PROPERTIES GENERATED TRUE)
+
+  add_library(syslog-ng-core-java-native INTERFACE)
+  target_include_directories(syslog-ng-core-java-native INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
+  add_dependencies(syslog-ng-core-java-native syslog-ng-core syslog-ng-core-java-headers)
 else()
   #The GENERATE_NATIVE_HEADERS variable introduced only in cmake 3.11.1
   #This is just a dirty workaround for that:
@@ -29,11 +35,14 @@ else()
   add_jar(syslog-ng-core
       SOURCES ${JAVA_SOURCES}
       OUTPUT_DIR ${CMAKE_CURRENT_BINARY_DIR}
-      GENERATE_NATIVE_HEADERS syslog-ng-core-headers
+      GENERATE_NATIVE_HEADERS syslog-ng-core-java-native
   )
 
-  set(BUILT_HEADERS "${CMAKE_CURRENT_BINARY_DIR}/syslog-ng-core.jar")
-  set_source_files_properties(${BUILT_HEADERS} PROPERTIES GENERATED TRUE)
+  #The GENERATE_NATIVE_HEADERS has been introduced in cmake 3.11
+  if(NOT TARGET syslog-ng-core-java-native)
+    add_library(syslog-ng-core-java-native INTERFACE)
+    target_include_directories(syslog-ng-core-java-native INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
+    add_dependencies(syslog-ng-core-java-native syslog-ng-core)
+  endif()
 endif()
-
 endmacro(create_syslog_ng_core_java_native)
