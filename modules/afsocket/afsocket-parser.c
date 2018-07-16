110481ce3578fd52d5b434c4b947f7464367a0f2
Laszlo Szemere <laszlo.szemere@balabit.com>
2018-07-16 09:37:54 +0200

    afsocket-grammar: new configuration syntax for the failover function. failover-servers() option deprecated. replaced by failover(), and the list of the failover servers, got an own (mandatory) option inside it.
    Signed-off-by: Laszlo Szemere <laszlo.szemere@balabit.com>

--- modules/afsocket/afsocket-parser.c
+++ modules/afsocket/afsocket-parser.c
@@ -82,10 +82,12 @@ static CfgLexerKeyword afsocket_keywords[] =
   { "listen_backlog",     KW_LISTEN_BACKLOG },
   { "keep_alive",         KW_KEEP_ALIVE },
   { "systemd_syslog",     KW_SYSTEMD_SYSLOG  },
-  { "failover_servers",   KW_FAILOVER_SERVERS },
-  { "failback",           KW_FAILOVER_FAILBACK },
-  { "tcp_probe_interval", KW_FAILOVER_TCP_PROBE_INTERVAL },
-  { "successful_probes_required", KW_FAILOVER_SUCCESSFUL_PROBES_REQUIRED },
+  { "failover_servers",   KW_FAILOVER, KWS_OBSOLETE, "failover-servers has been deprecated, try failover() and use servers() option inside it." },
+  { "failover",           KW_FAILOVER },
+  { "failback",           KW_FAILBACK },
+  { "servers",            KW_SERVERS },
+  { "tcp_probe_interval", KW_TCP_PROBE_INTERVAL },
+  { "successful_probes_required", KW_SUCCESSFUL_PROBES_REQUIRED },
   { NULL }
 };
 
