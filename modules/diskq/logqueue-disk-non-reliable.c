4af45909f7bd3e9fe9d927426112d83fd484c78e
Laszlo Budai <stentor.bgyk@gmail.com>
2018-09-21 14:09:36 +0200

    diskq: nonreliable: update memory_usage directly when started
    It has to be added to the memory_usage;
    
    -> and make it possible to remove
     * memory_usage_qout_initial_value and
     * memory_usage_overflow_initial_value
    from LoqQueue
    
    Signed-off-by: Laszlo Budai <stentor.bgyk@gmail.com>

--- modules/diskq/logqueue-disk-non-reliable.c
+++ modules/diskq/logqueue-disk-non-reliable.c
@@ -32,7 +32,7 @@ typedef struct
 {
   guint index_in_queue;
   guint item_number_per_message;
-  gssize *value_accumulator;
+  LogQueue *queue;
 } DiskqMemusageLoaderState;
 
 static gboolean
@@ -49,7 +49,7 @@ _update_memory_usage_during_load(gpointer data, gpointer s)
   if (_object_is_message_in_position(state->index_in_queue, state->item_number_per_message))
     {
       LogMessage *msg = (LogMessage *)data;
-      *state->value_accumulator += log_msg_get_size(msg);
+      log_queue_memory_usage_add(state->queue, log_msg_get_size(msg));
     }
   state->index_in_queue++;
 }
@@ -63,12 +63,12 @@ _start(LogQueueDisk *s, const gchar *filename)
 
   DiskqMemusageLoaderState qout_sum = { .index_in_queue = 0,
                                         .item_number_per_message = ITEM_NUMBER_PER_MESSAGE,
-                                        .value_accumulator = &self->super.super.memory_usage_qout_initial_value
+                                        .queue = &self->super.super
                                       };
 
   DiskqMemusageLoaderState overflow_sum = { .index_in_queue = 0,
                                             .item_number_per_message = ITEM_NUMBER_PER_MESSAGE,
-                                            .value_accumulator = &self->super.super.memory_usage_overflow_initial_value
+                                            .queue = &self->super.super
                                           };
 
   g_queue_foreach(self->qout, _update_memory_usage_during_load, &qout_sum);
