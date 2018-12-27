b4d439db078692843c2f46442e30118a6ee1103e
Balazs Scheidler <balazs.scheidler@oneidentity.com>
2018-12-27 18:07:07 +0100

    timeutils: add WallClockTime class and implementation
    This patch adds a simple wrapper over "struct tm" with fields that are
    not portable across all platforms. For instance, tm_gmtoff is not
    present on a number of platforms but would be very useful internally,
    so we _always_ have the capability to represent a broken-down time with
    all the properties needed for proper timezone reference.
    
    The design of this class is so that we don't need to copy the contents
    of "struct tm" around, rather we contain one instance and
    provide wrapper macros so that fields that are present in struct tm are
    used from there, those that aren't will be part of the wrapper structure.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@oneidentity.com>

---
+++ lib/timeutils/wallclocktime.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2002-2018 Balabit
+ * Copyright (c) 1998-2018 Balázs Scheidler
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "timeutils/wallclocktime.h"
+
+void
+wall_clock_time_unset(WallClockTime *self)
+{
+  WallClockTime val = WALL_CLOCK_TIME_INIT;
+  *self = val;
+}
