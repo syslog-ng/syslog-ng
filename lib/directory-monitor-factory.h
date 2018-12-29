ab66f685e76f988fd95660da4506539302042f67
Mahmoud Salama <mahmoud_salama8086@yahoo.com>
2018-12-29 11:40:59 -0500

    directory-monitor: move functionality from affile module to libsyslog-ng

--- lib/directory-monitor-factory.h
+++ lib/directory-monitor-factory.h
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2017 Balabit
+ * Copyright (c) 2002-2013 Balabit
+ * Copyright (c) 1998-2013 Balázs Scheidler
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 as published
- * by the Free Software Foundation, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  * As an additional exemption you are allowed to compile & link against the
@@ -19,6 +21,7 @@
  * COPYING for details.
  *
  */
+
 #ifndef MODULES_AFFILE_DIRECTORY_MONITOR_FACTORY_H_
 #define MODULES_AFFILE_DIRECTORY_MONITOR_FACTORY_H_
 
---
+++ lib/directory-monitor-poll.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2002-2013 Balabit
+ * Copyright (c) 1998-2013 Balázs Scheidler
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef MODULES_AFFILE_DIRECTORY_MONITOR_POLL_H_
+#define MODULES_AFFILE_DIRECTORY_MONITOR_POLL_H_
+
+#include "directory-monitor.h"
+
+DirectoryMonitor *directory_monitor_poll_new(const gchar *dir, guint recheck_time);
+
+#endif /* MODULES_AFFILE_DIRECTORY_MONITOR_POLL_H_ */
