aa9fe7414dfd43487a5de791770db864a2fc71a5
Attila Szakacs <attila.szakacs@balabit.com>
2019-02-04 11:04:46 +0100

    redis: consume ref at set_command
    Signed-off-by: Attila Szakacs <attila.szakacs@balabit.com>

--- modules/redis/redis.h
+++ modules/redis/redis.h
@@ -31,9 +31,9 @@ LogDriver *redis_dd_new(GlobalConfig *cfg);
 void redis_dd_set_host(LogDriver *d, const gchar *host);
 void redis_dd_set_port(LogDriver *d, gint port);
 void redis_dd_set_auth(LogDriver *d, const gchar *auth);
-void redis_dd_set_command(LogDriver *d, const gchar *command,
-                          LogTemplate *key,
-                          LogTemplate *param1, LogTemplate *param2);
+void redis_dd_set_command_ref(LogDriver *d, const gchar *command,
+                              LogTemplate *key,
+                              LogTemplate *param1, LogTemplate *param2);
 LogTemplateOptions *redis_dd_get_template_options(LogDriver *d);
 
 #endif
