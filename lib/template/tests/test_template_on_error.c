49b297c39d5b67318897774d75c9306466089f2f
Mehul Prajapati <mehulprajapati2802@gmail.com>
2019-02-28 04:11:10 -0800

    test_template_on_error: rewrite test cases in criterion
    Signed-off-by: Mehul Prajapati <mehul.encs@gmail.com>

--- lib/template/tests/test_template_on_error.c
+++ lib/template/tests/test_template_on_error.c
@@ -25,59 +25,46 @@
 #include <libgen.h>
 
 #include "template/templates.h"
-#include "testutils.h"
+
+#include <criterion/criterion.h>
+#include <criterion/parameterized.h>
 #include "apphook.h"
 
-#define assert_on_error_parse(on_error,expected)                        \
-  do                                                                    \
-    {                                                                   \
-      gint r;                                                           \
-                                                                        \
-      assert_true(log_template_on_error_parse(on_error, &r),            \
-                  "Parsing '%s' works", on_error);                      \
-      assert_gint32(r, expected, "'%s' parses down to '%s'",            \
-                    on_error, #expected);                               \
-    } while(0)
-
-#define assert_on_error_parse_fails(on_error)                           \
-  do                                                                    \
-    {                                                                   \
-      gint r;                                                           \
-                                                                        \
-      assert_false(log_template_on_error_parse(on_error, &r),            \
-                   "Parsing '%s' works", on_error);                      \
-    } while(0)
-
-static void
-test_template_on_error(void)
+typedef struct _TemplateTestCase
 {
-  testcase_begin("Testing LogTemplate on-error parsing");
+  const gchar *actual;
+  gint expected;
+} TemplateTestCase;
 
-  assert_on_error_parse("drop-message", ON_ERROR_DROP_MESSAGE);
-  assert_on_error_parse("silently-drop-message",
-                        ON_ERROR_DROP_MESSAGE | ON_ERROR_SILENT);
+ParameterizedTestParameters(template_on_err, test_success)
+{
+  static TemplateTestCase test_cases[] =
+  {
+    {"drop-message", ON_ERROR_DROP_MESSAGE},
+    {"silently-drop-message", ON_ERROR_DROP_MESSAGE | ON_ERROR_SILENT},
+    {"drop-property", ON_ERROR_DROP_PROPERTY},
+    {"silently-drop-property", ON_ERROR_DROP_PROPERTY | ON_ERROR_SILENT},
+    {"fallback-to-string", ON_ERROR_FALLBACK_TO_STRING},
+    {"silently-fallback-to-string", ON_ERROR_FALLBACK_TO_STRING | ON_ERROR_SILENT}
+  };
 
-  assert_on_error_parse("drop-property", ON_ERROR_DROP_PROPERTY);
-  assert_on_error_parse("silently-drop-property",
-                        ON_ERROR_DROP_PROPERTY | ON_ERROR_SILENT);
-
-  assert_on_error_parse("fallback-to-string", ON_ERROR_FALLBACK_TO_STRING);
-  assert_on_error_parse("silently-fallback-to-string",
-                        ON_ERROR_FALLBACK_TO_STRING | ON_ERROR_SILENT);
-
-  assert_on_error_parse_fails("do-what-i-mean");
-
-  testcase_end();
+  return cr_make_param_array(TemplateTestCase, test_cases, sizeof(test_cases) / sizeof(test_cases[0]));
 }
 
-int
-main (void)
+ParameterizedTest(TemplateTestCase *test_cases, template_on_err, test_success)
 {
-  app_startup();
+  gint r;
 
-  test_template_on_error();
-
-  app_shutdown();
-
-  return 0;
+  cr_assert(log_template_on_error_parse(test_cases->actual, &r), "Parsing '%s' works", test_cases->actual);
+  cr_assert_eq(r, test_cases->expected, "'%s' parses down to '%d'", test_cases->actual, test_cases->expected);
 }
+
+Test(template_on_error, test_fail)
+{
+  gint r;
+  gchar *pattern = "do-what-i-mean";
+
+  cr_assert_not(log_template_on_error_parse(pattern, &r), "Parsing '%s' works", pattern);
+}
+
+TestSuite(template_on_error, .init = app_startup, .fini = app_shutdown);
