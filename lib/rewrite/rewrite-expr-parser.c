9386a6adc946211223d4ccc58430e0c4271429ea
Kokan <kokaipeter@gmail.com>
2019-03-07 09:41:31 +0100

    rewrite: set-level()
    Changes the message level based on its numeric value:
    ```
     rewrite {
       set-level("${user_macro}");
     };
    ```
    The option is template that expects to be resolved into a numeric representation of a valid level.
    
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- lib/rewrite/rewrite-expr-parser.c
+++ lib/rewrite/rewrite-expr-parser.c
@@ -36,6 +36,7 @@ static CfgLexerKeyword rewrite_expr_keywords[] =
   { "subst",              KW_SUBST },
   { "set_tag",            KW_SET_TAG },
   { "clear_tag",          KW_CLEAR_TAG },
+  { "set_level",          KW_SET_LEVEL },
 
   { "groupset",           KW_GROUP_SET },
   { "groupunset",         KW_GROUP_UNSET },
