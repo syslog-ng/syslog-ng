059314d6cc0946cdf39b9610980d9f92b2266061
Mehul Prajapati <mehulprajapati2802@gmail.com>
2019-03-09 00:03:21 -0800

    test_decode: rewrite test cases in criterion
    Signed-off-by: Mehul Prajapati <mehul.encs@gmail.com>

--- lib/str-repr/tests/test_decode.c
+++ lib/str-repr/tests/test_decode.c
@@ -22,29 +22,7 @@
  *
  */
 #include "str-repr/decode.h"
-#include "testutils.h"
-
-#define str_repr_decode_testcase_begin(func, args)             \
-  do                                                            \
-    {                                                           \
-      testcase_begin("%s(%s)", func, args);                     \
-    }                                                           \
-  while (0)
-
-#define str_repr_decode_testcase_end()                           \
-  do                                                            \
-    {                                                           \
-      testcase_end();                                           \
-    }                                                           \
-  while (0)
-
-#define STR_REPR_DECODE_TESTCASE(x, ...) \
-  do {                                                          \
-      str_repr_decode_testcase_begin(#x, #__VA_ARGS__);     \
-      x(__VA_ARGS__);                                           \
-      str_repr_decode_testcase_end();                                \
-  } while(0)
-
+#include <criterion/criterion.h>
 
 GString *value;
 
@@ -54,8 +32,8 @@ assert_decode_equals(const gchar *input, const gchar *expected)
   GString *str = g_string_new("");
   const gchar *end;
 
-  assert_true(str_repr_decode(str, input, &end), "Decode operation failed while success was expected, input=%s", input);
-  assert_string(str->str, expected, "Decoded value does not match expected");
+  cr_assert(str_repr_decode(str, input, &end), "Decode operation failed while success was expected, input=%s", input);
+  cr_assert_str_eq(str->str, expected, "Decoded value does not match expected");
   g_string_free(str, TRUE);
 }
 
@@ -65,9 +43,9 @@ assert_decode_equals_and_fails(const gchar *input, const gchar *expected)
   GString *str = g_string_new("");
   const gchar *end;
 
-  assert_false(str_repr_decode(str, input, &end), "Decode operation succeeded while failure was expected, input=%s",
-               input);
-  assert_string(str->str, expected, "Decoded value does not match expected");
+  cr_assert_not(str_repr_decode(str, input, &end), "Decode operation succeeded while failure was expected, input=%s",
+                input);
+  cr_assert_str_eq(str->str, expected, "Decoded value does not match expected");
   g_string_free(str, TRUE);
 }
 
@@ -90,20 +68,18 @@ assert_decode_with_three_tabs_as_delimiter_equals(const gchar *input, const gcha
   const gchar *end;
 
   str_repr_decode_with_options(str, input, &end, &options);
-  assert_string(str->str, expected, "Decoded value does not match expected");
+  cr_assert_str_eq(str->str, expected, "Decoded value does not match expected");
   g_string_free(str, TRUE);
 }
 
-static void
-test_decode_unquoted_strings(void)
+Test(decode, test_decode_unquoted_strings)
 {
   assert_decode_equals("a", "a");
   assert_decode_equals("alma", "alma");
   assert_decode_equals("al ma", "al");
 }
 
-static void
-test_decode_double_quoted_strings(void)
+Test(decode, test_decode_double_quoted_strings)
 {
   assert_decode_equals("\"al ma\"", "al ma");
   /* embedded quote */
@@ -114,8 +90,7 @@ test_decode_double_quoted_strings(void)
   assert_decode_equals("\"\\p\"", "\\p");
 }
 
-static void
-test_decode_apostrophe_quoted_strings(void)
+Test(decode, test_decode_apostrophe_quoted_strings)
 {
   assert_decode_equals("'al ma'", "al ma");
   /* embedded quote */
@@ -127,8 +102,7 @@ test_decode_apostrophe_quoted_strings(void)
   assert_decode_equals("'\\p\'", "\\p");
 }
 
-static void
-test_decode_malformed_strings(void)
+Test(decode, test_decode_malformed_strings)
 {
   assert_decode_equals_and_fails("'alma", "'alma");
   assert_decode_equals_and_fails("\"alma", "\"alma");
@@ -139,8 +113,7 @@ test_decode_malformed_strings(void)
   assert_decode_equals_and_fails("'alma'@korte", "'alma'@korte");
 }
 
-static void
-test_decode_delimited_strings(void)
+Test(decode, test_decode_delimited_strings)
 {
   assert_decode_with_three_tabs_as_delimiter_equals("alma\t\t\tkorte", "alma");
 
@@ -150,16 +123,4 @@ test_decode_delimited_strings(void)
   assert_decode_with_three_tabs_as_delimiter_equals("alma\t\t", "alma\t\t");
 
   assert_decode_with_three_tabs_as_delimiter_equals("\t\t\tfoobar", "");
-
-}
-
-int
-main(int argc, char *argv[])
-{
-  STR_REPR_DECODE_TESTCASE(test_decode_unquoted_strings);
-  STR_REPR_DECODE_TESTCASE(test_decode_double_quoted_strings);
-  STR_REPR_DECODE_TESTCASE(test_decode_apostrophe_quoted_strings);
-  STR_REPR_DECODE_TESTCASE(test_decode_malformed_strings);
-  STR_REPR_DECODE_TESTCASE(test_decode_delimited_strings);
-  return 0;
 }
