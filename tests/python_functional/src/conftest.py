0f2b0c9294d9e38fcc7afb9402dd133e1f3c0868
Andras Mitzki <andras.mitzki@balabit.com>
2019-03-10 07:32:18 +0100

    light-unittest: Create fixtures for unittests
    - create separate conftest for unittests
    - this patch prepares to disable old tc_unittest fixture
    - also fixes a bug when unittests and functional_tests was
    run in a same command in this order:
    `python3 -m pytest --installdir=[install-path] src/ functional_tests/`
    - because installdir and reportdir are mocked for unittests,
    we should save and re-set it
    
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

---
+++ tests/python_functional/src/conftest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#############################################################################
+# Copyright (c) 2015-2019 Balabit
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+
+import pytest
+from src.setup.testcase_parameters import TestcaseParameters
+
+
+@pytest.fixture
+def test_message():
+    return "test message - öüóőúéáű\n"
+
+
+@pytest.fixture
+def fake_testcase_parameters(request, tmpdir):
+    orig_installdir = request.config.option.installdir
+    orig_reportdir = request.config.option.reports
+    request.config.option.installdir = tmpdir.join("installdir")
+    request.config.option.reports = tmpdir.join("reports")
+    request.config.option.runwithvalgrind = False
+    yield TestcaseParameters(request)
+    request.config.option.installdir = orig_installdir
+    request.config.option.reports = orig_reportdir
+
+
+@pytest.fixture
+def temp_file(tmpdir):
+    return tmpdir.join("test_file.log")
