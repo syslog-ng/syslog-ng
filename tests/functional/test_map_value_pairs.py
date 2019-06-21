4d3f30b4bf85c0700031b84f9b8615d0b3d88853
Laszlo Budai <laszlo.budai@balabit.com>
2019-06-21 09:30:31 +0200

    tests/functional: version:  3.22
    Signed-off-by: Laszlo Budai <laszlo.budai@balabit.com>

--- tests/functional/test_map_value_pairs.py
+++ tests/functional/test_map_value_pairs.py
@@ -27,7 +27,7 @@ from messagecheck import *
 from control import flush_files, stop_syslogng
 import os
 
-config = """@version: 3.21
+config = """@version: 3.22
 
 options { keep-hostname(yes); };
 
