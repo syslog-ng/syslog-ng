5929b5eed65d2706b6914c5f3bebb8df5fdba3e6
Kokan <kokaipeter@gmail.com>
2019-07-11 14:32:07 +0200

    filter-opt: test for different handle
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- lib/filter/optimizer/tests/test_optimizer.c
+++ lib/filter/optimizer/tests/test_optimizer.c
@@ -313,6 +313,18 @@ Test(filter_optimizer, same_match_filter_via_set_handler)
   app_shutdown();
 }
 
+Test(filter_optimizer, different_match_filter_via_set_handler)
+{
+  app_startup();
+  FilterExprNode *expr = _compile_standalone_filter("match('f1' value('1')) or match('f2' value('2'));");
+
+  FilterExprNode *result = filter_expr_optimizer_run(expr,  &concatenate_or_filters);
+
+  cr_assert_eq(expr, result);
+
+  app_shutdown();
+}
+
 Test(filter_optimizer, same_filter_expr_with_or)
 {
   app_startup();
