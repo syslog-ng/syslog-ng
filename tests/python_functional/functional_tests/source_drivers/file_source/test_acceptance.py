95e91a8957f8995bb0daea49f423c6b0899919d3
László Várady <laszlo.varady@balabit.com>
2019-07-25 21:40:08 +0200

    light: file-src: use static hostname with keep-hostname enabled
    Signed-off-by: László Várady <laszlo.varady@balabit.com>

--- tests/python_functional/functional_tests/source_drivers/file_source/test_acceptance.py
+++ tests/python_functional/functional_tests/source_drivers/file_source/test_acceptance.py
@@ -22,10 +22,8 @@
 #############################################################################
 import pytest
 
-from src.common.network_operations import get_short_hostname
-
-input_log = "<38>Feb 11 21:27:22 {} testprogram[9999]: test message\n".format(get_short_hostname())
-expected_log = "Feb 11 21:27:22 {} testprogram[9999]: test message\n".format(get_short_hostname())
+input_log = "<38>Feb 11 21:27:22 testhost testprogram[9999]: test message\n"
+expected_log = "Feb 11 21:27:22 testhost testprogram[9999]: test message\n"
 
 
 @pytest.mark.parametrize(
@@ -38,6 +36,7 @@ def test_acceptance(config, syslog_ng, input_log, expected_log, counter):
     file_source = config.create_file_source(file_name="input.log")
     file_destination = config.create_file_destination(file_name="output.log")
     config.create_logpath(statements=[file_source, file_destination])
+    config.create_global_options(keep_hostname="yes")
 
     file_source.write_log(input_log, counter)
     syslog_ng.start(config)
