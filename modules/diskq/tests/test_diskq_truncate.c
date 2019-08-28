1ce1b9aac5fc19a4d6464ac949b3e770c5576385
Kokan <kokaipeter@gmail.com>
2019-08-28 12:46:32 +0200

    diskq: refactor test_diskq_truncate helpers
    Signed-off-by: Gabor Nagy <gabor.nagy@balabit.com>
    Signed-off-by: Kokan <kokaipeter@gmail.com>

--- modules/diskq/tests/test_diskq_truncate.c
+++ modules/diskq/tests/test_diskq_truncate.c
@@ -65,11 +65,20 @@ _save_diskqueue(LogQueue *q)
   log_queue_unref(q);
 }
 
-static gsize
-_calculate_expected_file_size(gint number_of_msgs)
+static gint64
+_get_file_size(LogQueue *q)
+{
+  struct stat file_stats;
+  const gchar *filename = log_queue_disk_get_filename(q);
+  assert_gint(stat(filename, &file_stats), 0, "Stat call failed, errno:%d", errno);
+  return (gint64)file_stats.st_size;
+}
+
+static gint64
+_calculate_expected_file_size(gint64 number_of_msgs)
 {
   guint diskq_record_len_size = 4;
-  gsize one_msg_size = get_one_message_serialized_size();
+  gint64 one_msg_size = get_one_message_serialized_size();
   return ((one_msg_size + diskq_record_len_size) * number_of_msgs) + QDISK_RESERVED_SPACE;
 }
 
@@ -77,12 +86,10 @@ static void
 _assert_diskq_actual_file_size_with_stored(LogQueue *q)
 {
   QDisk *qdisk = ((LogQueueDisk *)q)->qdisk;
-  gchar *filename = qdisk->filename;
 
-  struct stat diskq_file_stat;
-  cr_assert_eq(stat(filename, &diskq_file_stat), 0, "Stat call failed, errno:%d", errno);
-  cr_assert_eq(qdisk->file_size, diskq_file_stat.st_size,
-               "File size does not match with stored size; Actual file size: %ld, Expected file size: %ld\n", diskq_file_stat.st_size,
+  gint64 actual_file_size = _get_file_size(q);
+  cr_assert_eq(qdisk->file_size, actual_file_size,
+               "File size does not match with stored size; Actual file size: %ld, Expected file size: %ld\n", actual_file_size,
                qdisk->file_size);
 }
 
@@ -90,21 +97,15 @@ static void
 _assert_diskq_actual_file_size_with_expected(LogQueue *q, gboolean should_file_be_empty,
                                              gint64 number_of_messages_on_disk)
 {
-  QDisk *qdisk = ((LogQueueDisk *)q)->qdisk;
-  gchar *filename = qdisk->filename;
-
-  struct stat diskq_file_stat;
-  cr_assert_eq(stat(filename, &diskq_file_stat), 0, "Stat call failed, errno:%d", errno);
-
   if (should_file_be_empty)
     {
-      cr_assert_eq(diskq_file_stat.st_size, QDISK_RESERVED_SPACE, "Truncate after load failed: file is not empty");
+      cr_assert_eq(_get_file_size(q), QDISK_RESERVED_SPACE, "Truncate after load failed: file is not empty");
     }
   else
     {
       // diskq file is truncated on read when it becomes empty,
       // thus we have to assert to the original file size (after push finished), i.e. the max recorded size
-      cr_assert_eq(diskq_file_stat.st_size, _calculate_expected_file_size(number_of_messages_on_disk),
+      cr_assert_eq(_get_file_size(q), _calculate_expected_file_size(number_of_messages_on_disk),
                    "Truncate after load failed: expected size differs");
     }
 }
