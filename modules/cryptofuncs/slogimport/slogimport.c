e4b80c6b461613ef65822ce7c7c283ac7c285350
Airbus Commercial Aircraft <secure-logging@airbus.com>
2020-02-07 15:39:31 +0100

    slogimport: Copyright policy updated
    Signed-off-by: Airbus Commercial Aircraft <secure-logging@airbus.com>

--- modules/cryptofuncs/slogimport/slogimport.c
+++ modules/cryptofuncs/slogimport/slogimport.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019-2020 Airbus Commercial Aircraft <secure-logging@airbus.com>
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
