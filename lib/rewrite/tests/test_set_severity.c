353ea3c6cf55c04b05af7483a61228501206e24f
Romain Tartière <romain@blogreen.org>
2020-02-23 13:09:53 -1000

    set-severity: Avoid reading an unallocated message
    Signed-off-by: Romain Tartière <romain@blogreen.org>

--- lib/rewrite/tests/test_set_severity.c
+++ lib/rewrite/tests/test_set_severity.c
@@ -91,11 +91,13 @@ Test(set_severity, large_number)
   LogRewrite *set_severity = log_rewrite_set_severity_new(_create_template("8"), cfg);
 
   log_pipe_init(&set_severity->super);
+  log_msg_ref(msg);
   log_pipe_queue(&set_severity->super, msg, &path_options);
 
   cr_assert_eq(msg->pri & LOG_PRIMASK, default_severity);
   assert_grabbed_log_contains("invalid severity to set");
 
+  log_msg_unref(msg);
   log_pipe_deinit(&set_severity->super);
   log_pipe_unref(&set_severity->super);
 }
