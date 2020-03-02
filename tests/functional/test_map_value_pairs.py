9ae2c944b95a856d4845759fc7a1a6dfffb4fb4d
Laszlo Budai <laszlo.budai@outlook.com>
2020-03-02 15:32:53 +0100

    tests/functional: 3.26
    Signed-off-by: Laszlo Budai <laszlo.budai@outlook.com>

--- tests/functional/test_map_value_pairs.py
+++ tests/functional/test_map_value_pairs.py
@@ -27,7 +27,7 @@ from messagecheck import *
 from control import flush_files, stop_syslogng
 import os
 
-config = """@version: 3.25
+config = """@version: 3.26
 
 options { keep-hostname(yes); };
 
