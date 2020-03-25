cd72b39a8b5c1626df674d77e68a5c47fd629711
Antal Nemes <antal.nemes.hu@gmail.com>
2020-03-25 15:16:56 +0100

    light: turn render functions into pure function
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

--- tests/python_functional/src/syslog_ng_config/renderer.py
+++ tests/python_functional/src/syslog_ng_config/renderer.py
@@ -22,6 +22,96 @@
 #############################################################################
 
 
+def render_version(version):
+    return "@version: {}\n".format(version)
+
+
+def render_includes(includes):
+    include_lines = ['@include "{}"'.format(include) for include in includes]
+    return "\n".join(include_lines)
+
+
+def render_global_options(global_options):
+    globals_options_header = "options {\n"
+    globals_options_footer = "};\n"
+
+    config_snippet = globals_options_header
+    for option_name, option_value in global_options.items():
+        config_snippet += "    {}({});\n".format(option_name, option_value)
+    config_snippet += globals_options_footer
+
+    return config_snippet
+
+
+def render_positional_options(positional_parameters):
+    config_snippet = ""
+    for parameter in positional_parameters:
+        config_snippet += "        {}\n".format(str(parameter))
+    return config_snippet
+
+
+def render_driver_options(driver_options):
+    config_snippet = ""
+
+    for option_name, option_value in driver_options.items():
+        if isinstance(option_value, dict):
+            config_snippet += "        {}(\n".format(option_name)
+            config_snippet += render_driver_options(option_value)
+            config_snippet += "        )\n"
+        elif (isinstance(option_value, tuple) or isinstance(option_value, list)):
+            for element in option_value:
+                config_snippet += "        {}({})\n".format(option_name, element)
+        else:
+            config_snippet += "        {}({})\n".format(option_name, option_value)
+
+    return config_snippet
+
+
+def render_statement_groups(statement_groups):
+    config_snippet = ""
+
+    for statement_group in statement_groups:
+        # statement header
+        config_snippet += "\n{} {} {{\n".format(
+            statement_group.group_type, statement_group.group_id,
+        )
+
+        for statement in statement_group:
+            # driver header
+            config_snippet += "    {} (\n".format(statement.driver_name)
+
+            # driver options
+            config_snippet += render_positional_options(statement.positional_parameters)
+            config_snippet += render_driver_options(statement.options)
+
+            # driver footer
+            config_snippet += "    );\n"
+
+            # statement footer
+        config_snippet += "};\n"
+
+    return config_snippet
+
+
+def render_logpath_groups(logpath_groups):
+    config_snippet = ""
+
+    for logpath_group in logpath_groups:
+        config_snippet += "\nlog {\n"
+        for statement_group in logpath_group.logpath:
+            if statement_group.group_type == "log":
+                config_snippet += render_logpath_groups(logpath_groups=[statement_group])
+            else:
+                config_snippet += "    {}({});\n".format(
+                    statement_group.group_type, statement_group.group_id,
+                )
+        if logpath_group.flags:
+            config_snippet += "    flags({});\n".format("".join(logpath_group.flags))
+        config_snippet += "};\n"
+
+    return config_snippet
+
+
 class ConfigRenderer(object):
     def __init__(self, syslog_ng_config):
         self.__syslog_ng_config = syslog_ng_config
@@ -42,101 +132,17 @@ class ConfigRenderer(object):
         config = ""
 
         if version:
-            config += self.__render_version(version)
+            config += render_version(version)
         if includes:
-            config += self.__render_includes(includes)
+            config += render_includes(includes)
         if global_options:
-            config += self.__render_global_options(global_options)
+            config += render_global_options(global_options)
         if statement_groups:
-            config += self.__render_statement_groups(statement_groups)
+            config += render_statement_groups(statement_groups)
         if logpath_groups:
-            config += self.__render_logpath_groups(logpath_groups)
+            config += render_logpath_groups(logpath_groups)
 
         if re_create_config:
             self.__syslog_ng_config_content = config
         else:
             self.__syslog_ng_config_content += config
-
-    def __render_version(self, version):
-        return "@version: {}\n".format(version)
-
-    def __render_includes(self, includes):
-        include_lines = ['@include "{}"'.format(include) for include in includes]
-        return "\n".join(include_lines)
-
-    def __render_global_options(self, global_options):
-        globals_options_header = "options {\n"
-        globals_options_footer = "};\n"
-
-        config_snippet = globals_options_header
-        for option_name, option_value in global_options.items():
-            config_snippet += "    {}({});\n".format(option_name, option_value)
-        config_snippet += globals_options_footer
-
-        return config_snippet
-
-    def __render_positional_options(self, positional_parameters):
-        config_snippet = ""
-        for parameter in positional_parameters:
-            config_snippet += "        {}\n".format(str(parameter))
-        return config_snippet
-
-    def __render_driver_options(self, driver_options):
-
-        config_snippet = ""
-
-        for option_name, option_value in driver_options.items():
-            if isinstance(option_value, dict):
-                config_snippet += "        {}(\n".format(option_name)
-                config_snippet += self.__render_driver_options(option_value)
-                config_snippet += "        )\n"
-            elif (isinstance(option_value, tuple) or isinstance(option_value, list)):
-                for element in option_value:
-                    config_snippet += "        {}({})\n".format(option_name, element)
-            else:
-                config_snippet += "        {}({})\n".format(option_name, option_value)
-
-        return config_snippet
-
-    def __render_statement_groups(self, statement_groups):
-        config_snippet = ""
-
-        for statement_group in statement_groups:
-            # statement header
-            config_snippet += "\n{} {} {{\n".format(
-                statement_group.group_type, statement_group.group_id,
-            )
-
-            for statement in statement_group:
-                # driver header
-                config_snippet += "    {} (\n".format(statement.driver_name)
-
-                # driver options
-                config_snippet += self.__render_positional_options(statement.positional_parameters)
-                config_snippet += self.__render_driver_options(statement.options)
-
-                # driver footer
-                config_snippet += "    );\n"
-
-            # statement footer
-            config_snippet += "};\n"
-
-        return config_snippet
-
-    def __render_logpath_groups(self, logpath_groups):
-        config_snippet = ""
-
-        for logpath_group in logpath_groups:
-            config_snippet += "\nlog {\n"
-            for statement_group in logpath_group.logpath:
-                if statement_group.group_type == "log":
-                    config_snippet += self.__render_logpath_groups(logpath_groups=[statement_group])
-                else:
-                    config_snippet += "    {}({});\n".format(
-                        statement_group.group_type, statement_group.group_id,
-                    )
-            if logpath_group.flags:
-                config_snippet += "    flags({});\n".format("".join(logpath_group.flags))
-            config_snippet += "};\n"
-
-        return config_snippet
