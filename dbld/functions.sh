e0fac5a4f77bd0653d0fd69b29d2afc445d429c0
Balazs Scheidler <bazsi77@gmail.com>
2020-04-03 17:54:36 +0200

    dbld: add framework for controlling build options via featureflags
    syslog-ng is build using different options on various platforms.  Some of
    the platforms have dependencies for a specific feature whereas others don't.
    
    Previously this was controlled using a combination of package lists
    (packages.manifest that controlled what gets installed in a docker image),
    configure options (in debian/rules and syslog-ng.spec) and conditionals
    scattered in our rpm/deb packaging scripts.
    
    This patch is intended to introduce a higher level concept "featureflags",
    which is specified in a single location (dbld/build.manifest).  This then
    controls:
    
      1) the installation of build dependencies (via Build-Depends and
         BuildRequires lines)
      2) the configure arguments
      3) the set of packages built
    
    This patch changes the format of build.manifest, read the description of the
    format at the beginning of that file as a comment block.
    
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- dbld/functions.sh
+++ dbld/functions.sh
@@ -5,8 +5,21 @@ function get_version() {
 	[ -d /source/scripts ] && cd /source && scripts/version.sh || echo "unknown-version"
 }
 
-function run_build_command() {
+function _map_cmdline_null()
+{
+	echo -n ""
+}
+
+function _map_env_null()
+{
+	echo "_=_"
+}
+
+function run_build_command_with_build_manifest_parameters() {
 	OS=$(echo $OS_PLATFORM |  cut -d '-' -f 1)
+	map_environment=$1
+	map_cmdline=$2
+	shift 2
 
 	# we sort longer strings in front, to make more specific items
 	# first.
@@ -14,16 +27,87 @@ function run_build_command() {
 	TMPDIR=$(mktemp -d)
 
 	IFS=$'\t'
-	egrep -e "^${OS}([^-]|$)" -e "^${OS_PLATFORM}" /dbld/build.manifest | sort -r | head -1 | while read os env cmdline; do
+	egrep -e "^${OS}([^-]|$)" -e "^${OS_PLATFORM}" /dbld/build.manifest | sort -r | head -1 | while read os featureflags env cmdline; do
 		unset IFS
+		echo $os $featureflags $env $cmdline
 		declare -a env_values
 		if [ "${env}" != "-" -a "${env}" != "" ]; then
+			# xargs processes removes quote characters just like the shell
 			echo "${env}" | tr ',' '\n' | xargs -n1 echo > $TMPDIR/env.list
 		        readarray -t env_values < $TMPDIR/env.list
 		fi
-		echo "Running build as: " env "${env_values[@]}" "$@" $cmdline
-		env "${env_values[@]}" "$@" $cmdline
+
+		if [ "`${map_cmdline} ${featureflags}`" = "" ]; then
+
+			# no extra command line values (e.g.  deb), execute
+			# it without them on the env command line.  In this
+			# case we supplied an extra, zero-length argument to
+			# dpkg-buildpackage that it errored out on (understandably).
+			#
+			# I couldn't find a way to expand a variable in shell that
+			#   1) was able to handle spaces in arguments
+			#   2) could also represent no extra arguments (not even an empty one)
+			#
+			# that's why we are using this ugly if statement above and the
+			# double evaluation of ${map_cmdline}.
+			#
+
+			echo "Runnign build as: " env "${env_values[@]}" "`${map_environment} ${featureflags}`" "$@" $cmdline
+			env "${env_values[@]}" "`${map_environment} ${featureflags}`" "$@" $cmdline
+		else
+			${map_cmdline} ${featureflags} | xargs -n1 echo > $TMPDIR/cmdline.list
+			readarray -t cmdline_values < $TMPDIR/cmdline.list
+			echo "Running build as: " env "${env_values[@]}" "`${map_environment} ${featureflags}`" "$@" $cmdline "${cmdline_values[@]}"
+			env "${env_values[@]}" "`${map_environment} ${featureflags}`" "$@" $cmdline "${cmdline_values[@]}"
+		fi
 	done
+}
+
+function _map_feature_flags_to_deb_build_profiles()
+{
+    echo -n DEB_BUILD_PROFILES=
+    IFS=,
+    for feature in $1; do
+        case "$feature" in
+          nojava|nopython)
+            # these are standard Debian build profiles, keep them intact
+            echo -n "$feature "
+            ;;
+          *)
+            # everything else is prefixed with "sng-"
+            echo -n "sng-$feature "
+            ;;
+        esac
+    done
+    echo
+}
+
+function deb_run_build_command()
+{
+    run_build_command_with_build_manifest_parameters _map_feature_flags_to_deb_build_profiles _map_cmdline_null "$@"
+}
+
+function _map_feature_flags_to_rpmbuild_with_and_without_options()
+{
+    IFS=,
+    echo -n "--define='_dbld 1' "
+    for feature in $1; do
+        case "$feature" in
+            no*)
+                feature_without_no=`echo $feature | sed -e 's/^no//'`
+                echo -n "--define='_without_${feature_without_no} --without-${feature_without_no}' "
+                ;;
+            *)
+                echo -n "--define='_with_${feature} --with-${feature}' "
+                ;;
+        esac
+    done
+}
+
+
+function rpm_run_build_command()
+{
+    run_build_command_with_build_manifest_parameters _map_env_null _map_feature_flags_to_rpmbuild_with_and_without_options "$@"
 }
 
 function validate_man_binary() {
