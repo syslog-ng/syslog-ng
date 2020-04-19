07a32390094282b09cae34cb21576cf322990c9d
Laszlo Szemere <laszlo.szemere@oneidentity.com>
2020-04-19 23:23:11 +0200

    test_msgparse: modify to test longer than 32 SD-ID's
    Signed-off-by: Laszlo Szemere <laszlo.szemere@oneidentity.com>

--- tests/unit/test_msgparse.c
+++ tests/unit/test_msgparse.c
@@ -204,22 +204,30 @@ run_parameterized_test(struct msgparse_params *params)
     test_log_messages_can_be_parsed(param);
 }
 
-Test(msgparse, test_failed_to_parse_too_long_sd_id)
+Test(msgparse, test_parse_longer_than_32_sd_id)
 {
+
+  struct sdata_pair expected_sd_pairs_test_long_sd_id[] =
+  {
+    { ".SDATA.timeQuality.isSynced", "0"},
+    { ".SDATA.1234567890123456789012345678901234.i", "long_33"},
+    { NULL, NULL },
+  };
+
   struct msgparse_params params[] =
   {
     {
       "<5>1 2006-10-29T01:59:59.156+01:00 mymachine evntslog - - [timeQuality isSynced=\"0\"][1234567890123456789012345678901234 i=\"long_33\"] An application event log entry...",
       LP_SYSLOG_PROTOCOL, NULL,
-      43,        //pri
-      0, 0, 0,  // timestamp (sec/usec/zone)
-      "", //host
-      "syslog-ng", //app
-      "Error processing log message: <5>1 2006-10-29T01:59:59.156+01:00 mymachine evntslog - - [timeQuality isSynced=\"0\"][1234567890123456789012345678901>@<234 i=\"long_33\"] An application event log entry...", // msg
-      "", // sd str,
+      5,        //pri
+      1162083599, 156000, 3600,    // timestamp (sec/usec/zone)
+      "mymachine", //host
+      "evntslog", //app
+      "An application event log entry...", // msg
+      "[timeQuality isSynced=\"0\"][1234567890123456789012345678901234 i=\"long_33\"]", // sd str,
       0, // processid
       0, // msgid,
-      empty_sdata_pairs
+      expected_sd_pairs_test_long_sd_id
     },
     {NULL}
   };
