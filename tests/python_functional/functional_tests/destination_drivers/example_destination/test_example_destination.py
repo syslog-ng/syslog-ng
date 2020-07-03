b54f7dd158b5d4b3da6511387d3a7145bc04f48e
Antal Nemes <antal.nemes.hu@gmail.com>
2020-07-03 14:38:04 +0200

    light: add test for example destination
    Signed-off-by: Antal Nemes <antal.nemes.hu@gmail.com>

---
+++ tests/python_functional/functional_tests/destination_drivers/example_destination/test_example_destination.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#############################################################################
+# Copyright (c) 2020 Balabit
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+
+
+def test_example_destination_parser(config, syslog_ng):
+    generator_source = config.create_example_msg_generator_source(num=1)
+    example_destination = config.create_example_destination(filename="output.txt")
+    config.create_logpath(statements=[generator_source, example_destination])
+
+    syslog_ng.start(config)
+    assert syslog_ng.wait_for_message_in_console_log("Message sent using file name: output.txt")
---
+++ tests/python_functional/src/syslog_ng_config/statements/destinations/example_destination.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#############################################################################
+# Copyright (c) 2015-2018 Balabit
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+from src.syslog_ng_config.statements.destinations.destination_driver import DestinationDriver
+
+
+class ExampleDestination(DestinationDriver):
+    def __init__(self, **options):
+        self.driver_name = "example-destination"
+        super(ExampleDestination, self).__init__(None, options)
