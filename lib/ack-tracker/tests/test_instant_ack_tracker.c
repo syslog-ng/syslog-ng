ee5e25759d2bce66ddc33bdba2f5b5176522c37b
Laszlo Budai <laszlo.budai@outlook.com>
2020-08-07 19:31:38 +0200

    ack-tracker: add unit tests for ACK_INSTANT ack tracker
    Signed-off-by: Laszlo Budai <laszlo.budai@outlook.com>

--- lib/ack-tracker/tests/test_instant_ack_tracker.c
+++ lib/ack-tracker/tests/test_instant_ack_tracker.c
@@ -182,3 +182,49 @@ Test(instant_ack_tracker_bookmarkless, same_bookmark_for_all_messages)
   _deinit_log_source(src);
 }
 
+
+TestSuite(instant_ack_tracker, .init = _setup, .fini = _teardown);
+
+Test(instant_ack_tracker, request_bookmark_returns_same_bookmarks_until_pending_not_assigned)
+{
+  LogSource *src = _init_log_source(ACK_INSTANT);
+  cr_assert_not_null(src->ack_tracker);
+  AckTracker *ack_tracker = src->ack_tracker;
+  Bookmark *bm1 = ack_tracker_request_bookmark(ack_tracker);
+  Bookmark *bm2 = ack_tracker_request_bookmark(ack_tracker);
+  cr_expect_eq(bm1, bm2);
+  LogMessage *msg = log_msg_new_empty();
+  ack_tracker_track_msg(ack_tracker, msg);
+  log_msg_ref(msg);
+  bm2 = ack_tracker_request_bookmark(ack_tracker);
+  cr_expect_neq(bm1, bm2);
+  ack_tracker_manage_msg_ack(ack_tracker, msg, AT_PROCESSED);
+  log_msg_unref(msg);
+  _deinit_log_source(src);
+}
+
+Test(instant_ack_tracker, bookmark_saving)
+{
+  LogSource *src = _init_log_source(ACK_INSTANT);
+  TestLogPipeDst *dst = _init_test_logpipe_dst();
+  log_pipe_append(&src->super, &dst->super);
+  cr_assert_not_null(src->ack_tracker);
+  AckTracker *ack_tracker = src->ack_tracker;
+  Bookmark *bm = ack_tracker_request_bookmark(ack_tracker);
+  guint saved_ctr = 0;
+  _fill_bookmark(bm, &saved_ctr);
+  LogMessage *msg = log_msg_new_empty();
+  cr_expect_eq(window_size_counter_get(&src->window_size, NULL), 10);
+  log_source_post(src, msg);
+  // this is why we need a dummy 'destination' : keep back ACK
+  cr_expect_eq(window_size_counter_get(&src->window_size, NULL), 9);
+  cr_assert_eq(msg->ack_record->tracker, ack_tracker);
+  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
+  log_msg_ack(msg, &path_options, AT_PROCESSED);
+  // okay, ACK is done, save counter is 1
+  cr_expect_eq(saved_ctr, 1);
+  cr_expect_eq(window_size_counter_get(&src->window_size, NULL), 10);
+  log_msg_unref(msg);
+  _deinit_log_source(src);
+  _deinit_test_logpipe_dst(dst);
+}
