b654978834cb2cdc6c22052005211457c92c80ad
Attila Szakacs <attila.szakacs@oneidentity.com>
2020-10-16 13:35:24 +0200

    light: remove SourceWriter
    SourceWriter seems like an unnecessary layer.
    It is not general enough, as it forces to have `path` in write_log.
    
    SourceDrivers can simply use the necessary IO layer without the
    limitations of SourceWriter.
    
    DestinationReader should be removed as well, first, because of symmetry,
    second, the `path` argument and `wait_for_creation()` shows that it only
    works with FileIO, but it is a bigger refactor, which I did not want to
    include in this patchset.
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- tests/python_functional/src/syslog_ng_config/statements/sources/file_source.py
+++ tests/python_functional/src/syslog_ng_config/statements/sources/file_source.py
@@ -20,20 +20,23 @@
 # COPYING for details.
 #
 #############################################################################
+import logging
+
 from pathlib2 import Path
 
 import src.testcase_parameters.testcase_parameters as tc_parameters
 from src.driver_io.file.file_io import FileIO
 from src.syslog_ng_config.statements.sources.source_driver import SourceDriver
-from src.syslog_ng_config.statements.sources.source_writer import SourceWriter
+
+logger = logging.getLogger(__name__)
 
 
 class FileSource(SourceDriver):
     def __init__(self, file_name, **options):
         self.driver_name = "file"
-        self.path = Path(tc_parameters.WORKING_DIR, file_name)
+        self.set_path(file_name)
+        self.io = FileIO(self.get_path())
         super(FileSource, self).__init__([self.path], options)
-        self.source_writer = SourceWriter(FileIO)
 
     def get_path(self):
         return self.path
@@ -42,4 +45,10 @@ class FileSource(SourceDriver):
         self.path = Path(tc_parameters.WORKING_DIR, pathname)
 
     def write_log(self, formatted_log, counter=1):
-        self.source_writer.write_log(self.get_path(), formatted_log, counter=counter)
+        for _ in range(counter):
+            self.io.write(formatted_log)
+        logger.info(
+            "Content has been written to\nresource: {}\n"
+            "number of times: {}\n"
+            "content: {}\n".format(self.get_path(), counter, formatted_log),
+        )
