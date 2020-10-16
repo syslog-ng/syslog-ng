d66e11959f9dfbe3bbd53cad1028c36bf12235ec
Attila Szakacs <attila.szakacs@oneidentity.com>
2020-10-16 14:53:16 +0200

    light: use write_log() in pp tests
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- tests/python_functional/functional_tests/source_drivers/network_source/proxyprotocol/test_pp_acceptance.py
+++ tests/python_functional/functional_tests/source_drivers/network_source/proxyprotocol/test_pp_acceptance.py
@@ -20,8 +20,6 @@
 # COPYING for details.
 #
 #############################################################################
-import src.testcase_parameters.testcase_parameters as tc_parameters
-from src.common.operations import create_file
 
 TEMPLATE = r'"${PROXIED_SRCIP} ${PROXIED_DSTIP} ${PROXIED_SRCPORT} ${PROXIED_DSTPORT} ${PROXIED_IP_VERSION} ${MESSAGE}\n"'
 INPUT_MESSAGES = "PROXY TCP4 1.1.1.1 2.2.2.2 3333 4444\r\n" \
@@ -30,15 +28,12 @@ EXPECTED_MESSAGE0 = "1.1.1.1 2.2.2.2 3333 4444 4 message 0\n"
 
 
 def test_pp_acceptance(config, syslog_ng, loggen, port_allocator):
-    loggen_input_path = str(tc_parameters.WORKING_DIR) + "/loggen_input.txt"
-
     network_source = config.create_network_source(ip="localhost", port=port_allocator(), transport='"proxied-tcp"', flags="no-parse")
     file_destination = config.create_file_destination(file_name="output.log", template=TEMPLATE)
     config.create_logpath(statements=[network_source, file_destination])
 
     syslog_ng.start(config)
 
-    create_file(loggen_input_path, INPUT_MESSAGES)
-    loggen.start(network_source.options["ip"], network_source.options["port"], stream=True, permanent=True, read_file=loggen_input_path, dont_parse=True)
+    network_source.write_log(INPUT_MESSAGES)
 
     assert file_destination.read_log() == EXPECTED_MESSAGE0
