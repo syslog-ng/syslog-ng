b29bac20e4abda0e06d9554b91b58fcedb54ff90
Attila Szakacs <attila.szakacs@oneidentity.com>
2020-11-17 14:12:09 +0100

    github-actions: move gh_export to a common sh file
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

---
+++ .github/workflows/gh_export.sh
@@ -0,0 +1,9 @@
+gh_export()
+{
+    while [ "x$1" != "x" ]; do
+        echo "$1<<__EOF__" >> $GITHUB_ENV
+        eval echo \$$1     >> $GITHUB_ENV
+        echo "__EOF__"     >> $GITHUB_ENV
+        shift;
+    done
+}
