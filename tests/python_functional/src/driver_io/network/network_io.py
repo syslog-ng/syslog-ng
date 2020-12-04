7b263711dcba9b12fe4b2ad3525f90fc5826e508
Andras Mitzki <andras.mitzki@balabit.com>
2020-12-04 18:33:20 +0100

    LightNetworkIO: Start using File() class more precisely
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/python_functional/src/driver_io/network/network_io.py
+++ tests/python_functional/src/driver_io/network/network_io.py
@@ -38,9 +38,11 @@ class NetworkIO():
 
     def write(self, content, rate=None):
         loggen_input_file_path = Path(tc_parameters.WORKING_DIR, "loggen_input_{}.txt".format(get_unique_id()))
+
         loggen_input_file = File(loggen_input_file_path)
-        with loggen_input_file.open(mode="a+") as f:
-            f.write(content)
+        loggen_input_file.open(mode="w")
+        loggen_input_file.write(content)
+        loggen_input_file.close()
 
         Loggen().start(self.__ip, self.__port, read_file=str(loggen_input_file_path), dont_parse=True, permanent=True, rate=rate, **self.__transport.value)
 
