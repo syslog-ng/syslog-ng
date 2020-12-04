c32f67242b1ea8b92c900f3068fa02f568ad0036
Andras Mitzki <andras.mitzki@balabit.com>
2020-12-04 16:33:38 +0100

    LightCommonFile: Extend existing functionality of File class
    - File class will extended with generally needed functions
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>
    Signed-off-by: Andras Mitzki <andras.mitzki@balabit.com>

--- tests/python_functional/src/common/file.py
+++ tests/python_functional/src/common/file.py
@@ -22,7 +22,11 @@
 #############################################################################
 import logging
 
+from pathlib2 import Path
+
+from src.common.blocking import DEFAULT_TIMEOUT
 from src.common.blocking import wait_until_true
+from src.common.blocking import wait_until_true_custom
 from src.common.operations import open_file
 
 logger = logging.getLogger(__name__)
@@ -30,25 +34,72 @@ logger = logging.getLogger(__name__)
 
 class File(object):
     def __init__(self, file_path):
-        self.__file_path = file_path
+        self.path = Path(file_path)
         self.__opened_file = None
 
     def __del__(self):
-        if self.__opened_file:
-            self.__opened_file.close()
-            self.__opened_file = None
-
-    def __is_file_exist(self):
-        return self.__file_path.exists()
+        if self.is_opened():
+            self.close()
 
     def wait_for_creation(self):
-        file_created = wait_until_true(self.__is_file_exist)
+        file_created = wait_until_true(self.path.exists)
         if file_created:
-            logger.debug("File has been created:\n{}".format(self.__file_path))
+            logger.debug("File has been created: {}".format(self.path))
         else:
-            logger.debug("File not created:\n{}".format(self.__file_path))
+            raise Exception("File was not created in time: {}".format(self.path))
         return file_created
 
     def open(self, mode):
-        self.__opened_file = open_file(self.__file_path, mode)
+        self.__opened_file = open_file(self.path, mode)
         return self.__opened_file
+
+    def close(self):
+        self.__opened_file.close()
+        self.__opened_file = None
+
+    def is_opened(self):
+        return self.__opened_file is not None
+
+    def readline(self):
+        if not self.is_opened():
+            raise Exception("File was not opened before trying to read from it.")
+        return self.__opened_file.readline()
+
+    def read(self):
+        content = ""
+        while True:
+            buffer = self.__opened_file.read()
+            if buffer == "":
+                break
+            content += buffer
+        return content
+
+    def write(self, content):
+        self.__opened_file.write(content)
+        self.__opened_file.flush()
+
+    def wait_for_lines(self, lines, timeout=DEFAULT_TIMEOUT):
+        def find_lines_in_file(lines_to_find, lines_found, f):
+            line_read = f.readline()
+            if not line_read and lines_to_find:
+                return False
+            for line_to_find in lines_to_find:
+                if line_to_find in line_read:
+                    lines_found.append(line_read)
+                    lines_to_find.remove(line_to_find)
+                    break
+            everything_is_found = lines_to_find == []
+            return everything_is_found
+
+        lines_found = []
+        if not wait_until_true_custom(find_lines_in_file, (lines, lines_found, self), timeout=timeout, poll_freq=0):
+            raise Exception("Could not find all lines in {}. Remaining lines to find: {} Lines found: {}".format(self.path, lines, lines_found))
+
+        return lines_found
+
+    def wait_for_number_of_lines(self, number_of_lines, timeout=DEFAULT_TIMEOUT):
+        lines = ["\n"] * number_of_lines
+        try:
+            return self.wait_for_lines(lines, timeout)
+        except Exception:
+            raise Exception("Could not find {} number of lines in {}. Remaining number of lines: {}.".format(number_of_lines, self.path, len(lines)))
