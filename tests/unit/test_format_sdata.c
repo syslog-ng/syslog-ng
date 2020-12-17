980197d70424bd5d17b2e42bb5c369134b38b101
Kokan <kokaipeter@gmail.com>
2020-12-17 08:49:57 +0100

    test_format_sdata: new empty unit test
    Signed-off-by: Kokan <kokaipeter@gmail.com>

---
+++ tests/unit/test_format_sdata.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Balabit
+ * Copyright (c) 2020 Kokan <kokaipeter@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include <criterion/criterion.h>
+
+#include "syslog-ng.h"
+#include "logmsg/logmsg.h"
+#include "apphook.h"
+
+
+void
+setup(void)
+{
+  app_startup();
+}
+
+void
+teardown(void)
+{
+  app_shutdown();
+}
+
+TestSuite(sdata_format, .init = setup, .fini = teardown);
+
+
+Test(sdata_format, dummy)
+{
+  cr_assert(true);
+}
+
