3c3513b17dbc1969d5c0f3489512949286f3ab0f
Balazs Scheidler <bazsi77@gmail.com>
2021-02-06 08:42:07 +0100

    timeutils/wallclocktime: fix assertion errors in the unit test
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- lib/timeutils/tests/test_wallclocktime.c
+++ lib/timeutils/tests/test_wallclocktime.c
@@ -137,7 +137,7 @@ Test(wallclocktime, test_strptime_percent_z_parses_rfc822_timezone)
   /* local timezone */
   wct.wct_gmtoff = -1;
   end = wall_clock_time_strptime(&wct, "%b %d %Y %H:%M:%S %z", "Jan 16 2019 18:23:12 CET");
-  cr_expect(wct.wct_gmtoff == 1*3600, "Unexpected timezone offset: %ld, expected 0", wct.wct_gmtoff);
+  cr_expect(wct.wct_gmtoff == 1*3600, "Unexpected timezone offset: %ld, expected 1*3600", wct.wct_gmtoff);
 
   /* military zones */
   wct.wct_gmtoff = -1;
@@ -208,7 +208,7 @@ Test(wallclocktime, test_strptime_percent_Z_allows_timezone_to_be_optional)
 
   wct.wct_gmtoff = -1;
   end = wall_clock_time_strptime(&wct, "%b %d %Y %H:%M:%S %Z", "Jan 16 2019 18:23:12 Y");
-  cr_expect(wct.wct_gmtoff == 12*3600, "Unexpected timezone offset: %ld, expected -1", wct.wct_gmtoff);
+  cr_expect(wct.wct_gmtoff == 12*3600, "Unexpected timezone offset: %ld, expected 12*3600", wct.wct_gmtoff);
 
   /* invalid timezone offset, too short */
   wct.wct_gmtoff = -1;
