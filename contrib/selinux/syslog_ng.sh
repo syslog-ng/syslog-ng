e5b77e5e8cb00c2754d25e2a4cc5bc4771f709bc
Janos SZIGETVARI <jszigetvari@gmail.com>
2021-03-26 15:25:01 +0100

    contrib/selinux/syslog_ng.sh: add support for plugin architecture contrib/selinux/include/common.sh.inc: add code shared with plugins
        * add plugin related exit codes
        * fix shared code inclusion problems
        * move shared code to common.sh.inc
        * add plugin loading/choice/compatibilitiy check/installation/removal code
        * added installation options --wihout-plugins and --with-all-plugins
        * updated --help message
    
    Signed-off-by: Janos SZIGETVARI <jszigetvari@gmail.com>

--- contrib/selinux/syslog_ng.sh
+++ contrib/selinux/syslog_ng.sh
@@ -20,65 +20,37 @@
 # 246: syslog-ng not installed
 # 245: invalid plugin metadata
 # 244: tty not available
+# 243: invalid plugin encountered #1
+# 242: invalid plugin encountered #2
+# 241: invalid plugin encountered #3
+
+cd $( dirname $( readlink -f "${0}" ) )
+[ -f include/common.sh.inc ] && source include/common.sh.inc
 
 EL_FC=
 EL_TE=
 OS_VERSION=
+SYSLOG_VERSION=
+SYSLOG_BINARY=
+CONSOLE_TTY=
+CONFIRM_REPLY=
+WITH_PLUGINS="ask"
 INSTALL_PATH="/opt/syslog-ng"
-# ports 514/udp, 6514/udp and 6514/tcp are allowed by default
-# if you wish to add further ports, just add them to the end of the list
+PLUGIN_DB=
+# RHEL8 note: ports 10514/tcp, 10514/udp, 20514/tcp and 20514/udp have been
+#  allowed by default
+#
+# Post-RHEL6.5 note: ports 514/udp, 6514/udp and 6514/tcp are allowed by default
+#  if you wish to add further ports, just add them to the end of the list
 SYSLOG_NG_TCP_PORTS="601"
 SYSLOG_NG_UDP_PORTS="601"
 TASK_SELECTED="install_default"
 INPUT=
 
-query_install_path() {
-	echo -n "Please enter your installation path for Syslog-ng PE: [${INSTALL_PATH}] "
-	read INPUT
-}
-
-check_dir() {
-	if [ -d "${1}" ]; then
-		return 0
-	else
-		echo "The directory you specified does not exist!" >&2
-		return 1
-	fi
-}
-
-verify_input() {
-	INPUT="${INPUT:-${INSTALL_PATH}}"
-	echo -n "You have entered '${INPUT}'. Is this correct? [y/N] "
-	read ACCEPT
-	if [ "x${ACCEPT}x" != "xyx" ]; then return 0; fi
-	check_dir "${INPUT}" && return 1 || return 0
-}
-
-
-extract_version_string() {
-	sed -n 's:^[a-zA-Z ]\+\([0-9]\+\.[0-9]\+\)\(.[0-9]\+\)\?[a-zA-Z ()]\+$:\1:p'
-}
-
-
-detect_os_version() {
-	echo "Detecting RHEL/CentOS/Oracle Linux version..."
-	if [ -x "/usr/bin/lsb_release" ]; then
-		if lsb_release -d | grep -qE "Description:[[:space:]]+(CentOS|CentOS Linux|Red Hat Enterprise Linux Server|Oracle Linux Server|Enterprise Linux Enterprise Linux Server) release"; then
-			OS_VERSION=$( lsb_release -r | cut -f 2 )
-		else
-			echo "You don't seem to be running a supported Linux distribution!" >&2
-			exit 250
-		fi
-	else
-		# The package redhat-lsb-core is most likely not installed...
-		if [ -f "/etc/redhat-release" ]; then
-			OS_VERSION=$( extract_version_string < "/etc/redhat-release" )
-		else
-			echo "You don't seem to be running a supported OS!" >&2
-			exit 250
-		fi
-	fi
-}
+PLUGIN_NAME=
+PLUGIN_FEATURE=
+PE_SUPPORT_ADDED_VERSION=
+OSE_SUPPORT_ADDED_VERSION=
 
 
 omit_allowed_tcp_ports() {
@@ -91,13 +63,21 @@ omit_allowed_udp_ports() {
 }
 
 
-omit_allowed_ports() {
-	SYSLOG_NG_TCP_PORTS=$( omit_allowed_tcp_ports <<<"${SYSLOG_NG_TCP_PORTS}" )
-	SYSLOG_NG_UDP_PORTS=$( omit_allowed_udp_ports <<<"${SYSLOG_NG_UDP_PORTS}" )
+substitute_install_path() {
+	sed -e "s:^\\\$PATH\\\$:${INSTALL_PATH}:g" "core/root_unsafe/${EL_FC}"
+	sed -e "s:^\\\$PATH\\\$:${INSTALL_PATH}:g" "core/root_safe/${EL_FC}"
+}
+
+
+omit_install_path() {
+	sed -e "s:^\\\$PATH\\\$::g" "core/root_safe/${EL_FC}"
 }
 
 
 setup_vars() {
+	get_syslog_binary
+	get_syslog_ng_version
+
 	echo "Detected RHEL/CentOS/Oracle Linux ${OS_VERSION}."
 	case "${OS_VERSION}" in
 		5.*)
@@ -141,33 +121,325 @@ setup_vars() {
 }
 
 
-substitute_install_path() {
-	sed -e "s:^\\\$PATH\\\$:${INSTALL_PATH}:g" "src/root_unsafe/${EL_FC}"
-	sed -e "s:^\\\$PATH\\\$:${INSTALL_PATH}:g" "src/root_safe/${EL_FC}"
-}
-
-
-omit_install_path() {
-	sed -e "s:^\\\$PATH\\\$::g" "src/root_safe/${EL_FC}"
-}
-
-
 prepare_files() {
 	echo "Using '${INSTALL_PATH}'..." 
 	if [ "${INSTALL_PATH}" != "/" ]; then
-		
 		substitute_install_path > "syslog_ng.fc"
 	else
 		omit_install_path > "syslog_ng.fc"
 	fi
-	cat "src/syslog_ng.module.version" "src/${EL_TE}" > "syslog_ng.te"
+	cat "core/syslog_ng.module.version" "core/${EL_TE}" > "syslog_ng.te"
+}
+
+dump_PLUGIN_DB() {
+# workaround to ensure every newline gets where it belongs
+# note: fgrep caused havoc when PLUGIN_DB was presented to it as stdin
+	echo -e "${PLUGIN_DB}"
 }
 
 
-remove_trainling_slash() {
-	# the trailing slash in the install path (if present) breaks file context rules
-	# thus it needs to be removed (provided that the install path is not "/" itself)
-	sed -e 's:^\(.\+\)/$:\1:'
+fetch_plugin_dir() {
+# PLUGIN_DB format is "name:choice"
+	while read LINE; do [ -n "${LINE}" ] && echo "${LINE%%:*}"; done
+}
+
+fetch_plugin_choice() {
+# PLUGIN_DB format is "name:choice"
+	while read LINE; do [ -n "${LINE}" ] && echo -ne "${LINE##*:}"; done
+}
+
+get_plugin_list() {
+	while IFS= read -r -d $'\0' plugin_dir; do
+		#[ -n "${plugin_dir}" ] && 
+		echo "${plugin_dir}"
+	done < <( find plugins -maxdepth 1 -type d -a ! -name plugins -print0 | sort -zn )
+}
+
+
+init_plugin_db() {
+	PLUGIN_DB=$( get_plugin_list | sed 's/$/:/g' )
+}
+
+
+query_plugins() {
+# Metadata valiables for the current plugin
+# 
+# PLUGIN_NAME: name of the module that the plugin adds support for
+# PLUGIN_FEATURE: possible values: ALL, PE_ONLY, OSE_ONLY, NONE
+# PE_SUPPORT_ADDED_VERSION: string describing version of PE where the feature
+#   was added
+# OSE_SUPPORT_ADDED_VERSION: string describing version of OSE where the feature
+#   was added
+	if [ "${WITH_PLUGINS}" == "ask" ]; then
+		local P_DB_SCRATCH_BUF=
+		while read plugin_dir; do
+			PLUGIN_NAME=
+			PLUGIN_FEATURE=
+			PE_SUPPORT_ADDED_VERSION=
+			OSE_SUPPORT_ADDED_VERSION=
+
+			source "${plugin_dir}/include/include.sh"
+			validate_plugin_metadata "${plugin_dir}" "${PLUGIN_NAME}" "${PLUGIN_FEATURE}" "${PE_SUPPORT_ADDED_VERSION}" "${OSE_SUPPORT_ADDED_VERSION}"
+			print_plugin_info "${PLUGIN_NAME}" "${PLUGIN_FEATURE}" "${PE_SUPPORT_ADDED_VERSION}" "${OSE_SUPPORT_ADDED_VERSION}"
+
+			ask_plugin_install "${PLUGIN_NAME}"
+			read CONFIRM_REPLY <"${CONSOLE_TTY}"
+			while check_reply; do
+			ask_plugin_install "${PLUGIN_NAME}"
+				read CONFIRM_REPLY <"${CONSOLE_TTY}"
+			done
+
+			if [ "x${CONFIRM_REPLY}x" == "xyx" -o "x${CONFIRM_REPLY}x" == "xYx" ]; then
+				P_DB_SCRATCH_BUF="${plugin_dir}:Y\n${P_DB_SCRATCH_BUF}"
+			else
+				P_DB_SCRATCH_BUF="${plugin_dir}:N\n${P_DB_SCRATCH_BUF}"
+			fi
+		done < <( dump_PLUGIN_DB | fetch_plugin_dir )
+		PLUGIN_DB="${P_DB_SCRATCH_BUF}"
+	elif [ "${WITH_PLUGINS}" == "all" ]; then
+		PLUGIN_DB=$( dump_PLUGIN_DB | sed 's/:$/:Y/g' )
+	else
+		PLUGIN_DB=$( dump_PLUGIN_DB | sed 's/:$/:N/g' )
+	fi
+}
+
+
+ask_plugin_install() {
+	echo -n "You are about to install the SELinux policy module for '${1}'. Do you wish to proceed? [y/N] "
+}
+
+
+print_plugin_info() {
+	local NAME FEATURE 
+	NAME="${1}"
+	FEATURE="${2}"
+	[ -n "${3}" ] && PE_COMP_VER="${3} and up" || PE_COMP_VER="NONE" 
+	[ -n "${4}" ] && OSE_COMP_VER="${4} and up" || OSE_COMP_VER="NONE" 
+
+
+	echo -ne "Plugin summary:\nName:\t\t\t\t\t\t${NAME}\nFeature:\t\t\t\t\t"
+	case "${FEATURE}" in
+		"ALL")
+			echo "For all versions"
+			;;
+		"NONE")
+			echo "Disabled for all versions"
+			;;
+		"PE_ONLY")
+			echo "Premium Edition only"
+			;;
+		"OSE_ONLY")
+			echo "Open-Source Edition only"
+			;;
+		*)
+			echo "INVALID"
+			;;
+	esac
+	echo -ne "Premium Edition compatible versions:\t\t${PE_COMP_VER}\nOpen-Source Edition compltible versions:\t${OSE_COMP_VER}"
+
+	echo -ne "\n\nInstalled Syslog-ng instance:\nEdition:\t\t\t\t\t" 
+	if syslog_ng_is_pe; then
+		echo "Premium Edition"
+	elif syslog_ng_is_ose; then
+		echo "Open-Source Edition"
+	else
+		echo "Unknown"
+	fi
+	echo -e "Version:\t\t\t\t\t${SYSLOG_VERSION}\n" 
+}
+
+
+plugin_install_precheck() {
+	echo -ne "\nPlugin constraint evaluation result:\t\t"
+	if plugin_check_feature "${1}" && plugin_check_compatibility "${2}" "${3}"; then
+		echo "PASS"
+		return 0
+	else
+		echo "SKIP"
+		return 1
+	fi
+}
+
+
+plugin_check_feature() {
+	case "${1}" in
+		"ALL")
+			return 0
+			;;
+		"NONE")
+			return 1
+			;;
+		"PE_ONLY")
+			if syslog_ng_is_pe; then
+				return 0
+			else
+				return 1
+			fi
+			;;
+		"OSE_ONLY")
+			if syslog_ng_is_ose; then
+				return 0
+			else
+				return 1
+			fi
+			;;
+		*)
+			echo "Invalid plugin metadata PLUGIN_FEATURE: \'${1}\'!" >&2
+			exit 245
+			;;
+	esac
+}
+
+
+plugin_version_is_greater_than() {
+	local OUR_VER REF_VER
+	OUR_VER="${1}"
+	REF_VER="${2}"
+
+	IFS="." read O_MAJOR O_MINOR O_REVISION <<<"${OUR_VER}"
+	IFS="." read R_MAJOR R_MINOR R_REVISION <<<"${REF_VER}"
+
+	if (( $O_MAJOR > $R_MAJOR )); then
+		return 0
+	elif (( $O_MAJOR == $R_MAJOR )); then
+		if (( $O_MINOR > $R_MINOR )); then
+			return 0
+		elif (( $O_MINOR == $R_MINOR )); then
+			if [ -n "${O_MINOR}" -a -n "${R_MINOR}" ]; then
+				if (( $O_REVISION > $R_REVISION )); then
+					return 0
+				elif (( $O_REVISION == $R_REVISION )); then
+					return 0
+				else
+					return 1
+				fi
+			elif [ -z "${O_MINOR}" -a -z "${R_MINOR}" ]; then
+				return 0
+			elif [ -n "${O_MINOR}" -a -z "${R_MINOR}" ]; then
+				return 0
+			else
+				return 1
+			fi
+		else
+			return 1
+		fi
+	else
+		return 1
+	fi
+}
+
+
+plugin_check_compatibility() {
+	local PE_VER OSE_VER
+	PE_SUPPORTED_VER="${1}"
+	OSE_SUPPORTED_VER="${2}"
+
+	if syslog_ng_is_pe; then
+		if plugin_version_is_greater_than "${SYSLOG_VERSION}" "${PE_SUPPORTED_VER}" ; then
+			return 0
+		else
+			return 1
+		fi
+	elif syslog_ng_is_ose; then
+		if plugin_version_is_greater_than "${SYSLOG_VERSION}" "${OSE_SUPPORTED_VER}"; then
+			return 0
+		else
+			return 1
+		fi
+	else
+		return 1
+	fi
+}
+
+
+validate_plugin_metadata() {
+	local plugin_dir PLUGIN_NAME PLUGIN_FEATURE PE_SUPPORT_ADDED_VERSION OSE_SUPPORT_ADDED_VERSION
+
+	plugin_dir="${1}"
+	PLUGIN_NAME="${2}"
+	PLUGIN_FEATURE="${3}"
+	PE_SUPPORT_ADDED_VERSION="${4}"
+	OSE_SUPPORT_ADDED_VERSION="${5}"
+
+	if [[ ( -z "${PLUGIN_NAME}" || -z "${PLUGIN_FEATURE}" ) || ( -z "${PE_SUPPORT_ADDED_VERSION}" && -z "${OSE_SUPPORT_ADDED_VERSION}" ) ]]; then
+		echo "Invalid plugin was encountered in directory: \'${plugin_dir}\'!" >&2
+		exit 243 
+	elif [[ ( "${PLUGIN_FEATURE}" == "PE_ONLY" && -z "${PE_SUPPORT_ADDED_VERSION}" ) || ( "${PLUGIN_FEATURE}" == "OSE_ONLY" && -z "${OSE_SUPPORT_ADDED_VERSION}" ) ]]; then
+		echo "Invalid plugin was encountered in directory: \'${plugin_dir}\'!" >&2
+		exit 242
+	elif [[ ( "${PLUGIN_FEATURE}" == "ALL" ) && ( -z "${PE_SUPPORT_ADDED_VERSION}" || -z "${OSE_SUPPORT_ADDED_VERSION}" ) ]]; then
+		echo "Invalid plugin was encountered in directory: \'${plugin_dir}\'!" >&2
+		exit 241
+	fi
+}
+
+
+run_plugin_installers() {
+	local choice
+
+	while read plugin_dir; do
+		PLUGIN_NAME=
+		PLUGIN_FEATURE=
+		PE_SUPPORT_ADDED_VERSION=
+		OSE_SUPPORT_ADDED_VERSION=
+
+		source "${plugin_dir}/include/include.sh"
+		validate_plugin_metadata "${plugin_dir}" "${PLUGIN_NAME}" "${PLUGIN_FEATURE}" "${PE_SUPPORT_ADDED_VERSION}" "${OSE_SUPPORT_ADDED_VERSION}"
+
+		choice=$( echo -ne "${PLUGIN_DB}" | fgrep "${plugin_dir}:" | fetch_plugin_choice )
+		if [ "x${choice}x" == "xYx" ] ; then
+			if plugin_install_precheck "${PLUGIN_FEATURE}" "${PE_SUPPORT_ADDED_VERSION}" "${OSE_SUPPORT_ADDED_VERSION}"; then
+				if already_loaded; then
+					echo "The SELinux module for '${PLUGIN_NAME}' is already loaded! Skipping install..." >&2
+				else
+					"${plugin_dir}/setup.sh" --install-dir "${INSTALL_PATH}" --os "${OS_VERSION}"
+				fi
+			fi
+		fi
+	done < <( dump_PLUGIN_DB | fetch_plugin_dir )
+}
+
+
+run_plugin_callbacks() {
+	local choice buffer
+	while read plugin_dir; do
+		PLUGIN_NAME=
+		PLUGIN_FEATURE=
+		PE_SUPPORT_ADDED_VERSION=
+		OSE_SUPPORT_ADDED_VERSION=
+
+		source "${plugin_dir}/include/include.sh"
+		validate_plugin_metadata "${plugin_dir}" "${PLUGIN_NAME}" "${PLUGIN_FEATURE}" "${PE_SUPPORT_ADDED_VERSION}" "${OSE_SUPPORT_ADDED_VERSION}"
+
+		choice=$( dump_PLUGIN_DB | fgrep "${plugin_dir}:" | fetch_plugin_choice )
+		if [ "x${choice}x" == "xYx" ] ; then
+			for type in te if fc; do
+				buffer=$( return_files "${type}" )
+				[ -n "${buffer}" ] && cat >> "syslog_ng.${type}" <<<"${buffer}"
+			done
+		fi
+	done < <( dump_PLUGIN_DB | fetch_plugin_dir )
+}
+
+
+remove_plugin_modules() {
+	while read plugin_dir; do
+		PLUGIN_NAME=
+		PLUGIN_FEATURE=
+		PE_SUPPORT_ADDED_VERSION=
+		OSE_SUPPORT_ADDED_VERSION=
+
+		source "${plugin_dir}/include/include.sh"
+		validate_plugin_metadata "${plugin_dir}" "${PLUGIN_NAME}" "${PLUGIN_FEATURE}" "${PE_SUPPORT_ADDED_VERSION}" "${OSE_SUPPORT_ADDED_VERSION}"
+
+		if already_loaded; then
+			"${plugin_dir}/setup.sh" --remove --os "${OS_VERSION}"
+		else
+			echo "No installed ${PLUGIN_NAME} SELinux policy module was found. No removal is necessary. Skipping..."
+		fi
+	done < <( dump_PLUGIN_DB | fetch_plugin_dir )
+}
+
 
 filter_bogus_build_output() {
 	#filter misleading output caused by RHEL bug 1861968
@@ -191,6 +463,14 @@ add_ports() {
 		semanage port -a -t syslogd_port_t -p ${proto} ${port} 2>/dev/null || \
 		semanage port -m -t syslogd_port_t -p ${proto} ${port} 2>/dev/null
 	done
+}
+
+
+install_precheck() {
+	if syslog_ng_is_not_installed; then
+		echo "Syslog-ng does not seem to be installed!" >&2
+		exit 246
+	fi
 }
 
 
@@ -251,9 +531,9 @@ remove_module() {
 	fi
 }
 
-DIRNAME=$( dirname "${0}" )
-cd "${DIRNAME}"
-USAGE="Usage: $0\t[ --install-dir <DIRECTORY> | --remove | --help ]\n\n$0:\tA tool for building and managing the SELinux policy for the\n\t\tdefault syslog-ng installation."
+
+CMD=$( basename "${0}" )
+USAGE="Usage: ${CMD}\t[ --install-dir <DIRECTORY> | --remove | --help | --without-plugins | --with-all-plugins ]\n\n${CMD}:\tA tool for building and managing the SELinux policy for the\n\t\tdefault syslog-ng installation."
 
 
 while [ -n "${1}" ]; do
@@ -275,6 +555,14 @@ while [ -n "${1}" ]; do
 			TASK_SELECTED="remove"
 			shift
 			;;
+		--without-plugins)
+			WITH_PLUGINS="none"
+			shift
+			;;
+		--with-all-plugins)
+			WITH_PLUGINS="all"
+			shift
+			;;
 		*)
 			echo -e "ERROR: Invalid option: '${1}'\n${USAGE}" >&2
 			exit 255
@@ -292,6 +580,8 @@ case "${TASK_SELECTED}" in
 		detect_os_version
 		setup_vars
 		remove_module
+		init_plugin_db
+		remove_plugin_modules
 		exit 0
 		;;
 	install|install_default)
@@ -299,7 +589,9 @@ case "${TASK_SELECTED}" in
 			echo 'You must be root to run this script!' >&2
 			exit 254
 		fi
-		
+
+		get_console_tty
+
 		if [ -z "${INPUT}" ]; then 
 			query_install_path
 			while verify_input; do
@@ -310,8 +602,13 @@ case "${TASK_SELECTED}" in
 		INSTALL_PATH=$( remove_trailing_slash <<<"${INPUT}" )
 
 		detect_os_version
+		install_precheck
 		setup_vars
+		init_plugin_db
+		query_plugins
+		run_plugin_installers
 		prepare_files
+		run_plugin_callbacks
 		build_module
 		install_module
 		;;
