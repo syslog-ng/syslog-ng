c7bdc6526637ef24934addfeb44e806e6aa9df04
Andras Mitzki <andras.mitzki@quest.com>
2021-05-07 11:44:19 +0200

    LightNetworkIO: Extend NetworkIO() listener side API's with Netcat
    - add support for TCP and UDP listening
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>
    Signed-off-by: Andras Mitzki <andras.mitzki@quest.com>

--- tests/python_functional/src/driver_io/network/network_io.py
+++ tests/python_functional/src/driver_io/network/network_io.py
@@ -20,6 +20,8 @@
 # COPYING for details.
 #
 #############################################################################
+import atexit
+from enum import auto
 from enum import Enum
 
 from pathlib2 import Path
@@ -28,6 +30,7 @@ import src.testcase_parameters.testcase_parameters as tc_parameters
 from src.common.file import File
 from src.common.random_id import get_unique_id
 from src.helpers.loggen.loggen import Loggen
+from src.helpers.netcat.netcat import Netcat
 
 
 class NetworkIO():
@@ -35,6 +38,10 @@ class NetworkIO():
         self.__ip = ip
         self.__port = port
         self.__transport = transport
+        self.__listener = None
+        self.__listener_output_file = None
+
+        atexit.register(self.stop_listener)
 
     def write(self, content, rate=None):
         loggen_input_file_path = Path(tc_parameters.WORKING_DIR, "loggen_input_{}.txt".format(get_unique_id()))
@@ -44,11 +51,44 @@ class NetworkIO():
         loggen_input_file.write(content)
         loggen_input_file.close()
 
-        Loggen().start(self.__ip, self.__port, read_file=str(loggen_input_file_path), dont_parse=True, permanent=True, rate=rate, **self.__transport.value)
+        Loggen().start(self.__ip, self.__port, read_file=str(loggen_input_file_path), dont_parse=True, permanent=True, rate=rate, **self.__transport.to_loggen_params())
+
+    def start_listener(self):
+        self.__listener = Netcat()
+        self.__listener.start(self.__ip, self.__port, l=True, k=True, **self.__transport.to_netcat_params())  # noqa: E741
+        self.__listener_output_file = File(self.__listener.output_path)
+        self.__listener_output_file.open(mode="r")
+
+    def stop_listener(self):
+        if self.__listener is not None:
+            self.__listener.stop()
+
+    def read_number_of_lines(self, counter):
+        return self.__listener_output_file.wait_for_number_of_lines(counter)
+
+    def read_until_lines(self, lines):
+        return self.__listener_output_file.wait_for_lines(lines)
 
     class Transport(Enum):
-        TCP = {"inet": True, "stream": True}
-        UDP = {"dgram": True}
-        TLS = {"use_ssl": True}
-        PROXIED_TCP = {"inet": True, "stream": True}
-        PROXIED_TLS = {"use_ssl": True}
+        TCP = auto()
+        UDP = auto()
+        TLS = auto()
+        PROXIED_TCP = auto()
+        PROXIED_TLS = auto()
+
+        def to_loggen_params(self):
+            loggen_params_mapping = {
+                NetworkIO.Transport.TCP: {"inet": True, "stream": True},
+                NetworkIO.Transport.UDP: {"dgram": True},
+                NetworkIO.Transport.TLS: {"use_ssl": True},
+                NetworkIO.Transport.PROXIED_TCP: {"inet": True, "stream": True},
+                NetworkIO.Transport.PROXIED_TLS: {"use_ssl": True},
+            }
+            return loggen_params_mapping[self]
+
+        def to_netcat_params(self):
+            netcat_params_mapping = {
+                NetworkIO.Transport.TCP: {},
+                NetworkIO.Transport.UDP: {"u": True},
+            }
+            return netcat_params_mapping[self]
