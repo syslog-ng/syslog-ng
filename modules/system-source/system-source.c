c2fe89b975ba4b0642950411f66543364883d965
Peter Czanik <peter@czanik.hu>
2021-08-19 11:35:43 +0200

    system source: add NetBSD support
    Upstreaming patch from NetBSD pkgsrc
    
    Signed-off-by: Peter Czanik <peter@czanik.hu>

--- modules/system-source/system-source.c
+++ modules/system-source/system-source.c
@@ -301,6 +301,10 @@ system_generate_system_transports(GString *sysblock, CfgArgs *args)
       system_sysblock_add_unix_dgram(sysblock, "/var/run/log", NULL, NULL);
       system_sysblock_add_freebsd_klog(sysblock, u.release);
     }
+  else if (strcmp(u.sysname, "NetBSD") == 0)
+    {
+      system_sysblock_add_unix_dgram(sysblock, "/var/run/log", NULL, NULL);
+    }
   else if (strcmp(u.sysname, "HP-UX") == 0)
     {
       system_sysblock_add_pipe(sysblock, "/dev/log", 2048);
