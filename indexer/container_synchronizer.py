e6032e72c14989f97e62081f986a2419f11a70a7
Attila Szakacs <attila.szakacs@oneidentity.com>
2021-10-11 13:10:46 +0200

    container_synchronizer: add WorkingDir class
    This class helps us navigating and finding files.
    
    root_dir:
      - The directory, which will be made equal to the root directory of the
        container.
    
    working_dir:
      - This variable stores the container_dir plus the sub_dir.
      - This directory is where the ContainerSynchronizer will only make
        changes.
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- indexer/container_synchronizer.py
+++ indexer/container_synchronizer.py
@@ -1,11 +1,27 @@
 from pathlib import Path
 from typing import List
 
+DEFAULT_ROOT_DIR = Path("/tmp")
+
+
+class WorkingDir:
+    def __init__(self, root_dir: Path, sub_dir: Path) -> None:
+        self.__root_dir = root_dir
+        self.__working_dir = Path(root_dir, sub_dir)
+
+    @property
+    def root_dir(self) -> Path:
+        return self.__root_dir
+
+    @property
+    def working_dir(self) -> Path:
+        return self.__working_dir
+
 
 class ContainerSynchronizer:
     def __init__(self, connection_string: str, container_name: str, sub_dir: Path) -> None:
-        # TODO: implement
-        pass
+        self.remote_dir = WorkingDir(Path(""), sub_dir)
+        self.local_dir = WorkingDir(Path(DEFAULT_ROOT_DIR, container_name), sub_dir)
 
     @property
     def local_files(self) -> List[Path]:
