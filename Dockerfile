7f472439483eb9d4abc44f088900916123424f0c
Attila Szakacs <attila.szakacs@oneidentity.com>
2021-10-11 13:17:42 +0200

    container_synchronizer: introduce azure.storage.blob.ContainerClient
    This API helps us with operating on the container.
    https://pypi.org/project/azure-storage-blob/12.9.0/
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,6 @@ FROM ubuntu:focal
 
 RUN apt update && apt install -y \
   python3-pip
+
+RUN pip install \
+  azure-storage-blob
--- indexer/container_synchronizer.py
+++ indexer/container_synchronizer.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import List
 
+from azure.storage.blob import ContainerClient
+
 DEFAULT_ROOT_DIR = Path("/tmp")
 
 
@@ -22,6 +24,9 @@ class ContainerSynchronizer:
     def __init__(self, connection_string: str, container_name: str, sub_dir: Path) -> None:
         self.remote_dir = WorkingDir(Path(""), sub_dir)
         self.local_dir = WorkingDir(Path(DEFAULT_ROOT_DIR, container_name), sub_dir)
+        self.__client = ContainerClient.from_connection_string(
+            conn_str=connection_string, container_name=container_name
+        )
 
     @property
     def local_files(self) -> List[Path]:
