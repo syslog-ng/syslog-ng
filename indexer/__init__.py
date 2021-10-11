5c17ba416be61cac436d5c8a9402e95117e30e9a
Attila Szakacs <attila.szakacs@oneidentity.com>
2021-10-11 14:38:41 +0200

    deb_indexer: add skeleton of ReleaseDebIndexer and NightlyDebIndexer
    This commits includes some business logic, related to the dir structure:
      * Nightly:
        - incoming: nightly/
        - indexed: apt/dists/nightly/
      * Release:
        - incoming: release/<run_id>/
        - indexed: apt/dists/stable/
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

---
+++ indexer/__init__.py
@@ -0,0 +1,8 @@
+from .indexer import Indexer
+from .deb_indexer import ReleaseDebIndexer, NightlyDebIndexer
+
+__all__ = [
+    "Indexer",
+    "ReleaseDebIndexer",
+    "NightlyDebIndexer",
+]
---
+++ indexer/deb_indexer.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+
+from .indexer import Indexer, ClientSecretCredential
+
+
+class DebIndexer(Indexer):
+    def __init__(
+        self,
+        container_connection_string: str,
+        incoming_container_sub_dir: Path,
+        dist_dir: Path,
+        cdn_credential: ClientSecretCredential,
+    ) -> None:
+        super().__init__(
+            container_connection_string=container_connection_string,
+            incoming_container_sub_dir=incoming_container_sub_dir,
+            indexed_container_sub_dir=Path("apt", "dists", dist_dir),
+            cdn_credential=cdn_credential,
+        )
+
+    def __move_files_from_incoming_to_indexed(self, incoming_dir: Path, indexed_dir: Path) -> None:
+        #  TODO: implement
+        pass
+
+    def __create_packages_files(self, indexed_dir: Path) -> None:
+        #  TODO: implement
+        pass
+
+    def __create_release_file(self, indexed_dir: Path) -> None:
+        #  TODO: implement
+        pass
+
+    def _index_pkgs(self, incoming_dir: Path, indexed_dir: Path) -> None:
+        self.__move_files_from_incoming_to_indexed(incoming_dir, indexed_dir)
+        self.__create_packages_files(indexed_dir)
+        self.__create_release_file(indexed_dir)
+
+
+class ReleaseDebIndexer(DebIndexer):
+    def __init__(
+        self,
+        container_connection_string: str,
+        run_id: str,
+        cdn_credential: ClientSecretCredential,
+        gpg_key_path: Path,
+    ) -> None:
+        self.__gpg_key_path = gpg_key_path
+        super().__init__(
+            container_connection_string=container_connection_string,
+            incoming_container_sub_dir=Path("release", run_id),
+            dist_dir=Path("stable"),
+            cdn_credential=cdn_credential,
+        )
+
+    def _sign_pkgs(self, indexed_dir: Path) -> None:
+        #  TODO: implement
+        pass
+
+
+class NightlyDebIndexer(DebIndexer):
+    def __init__(
+        self,
+        container_connection_string: str,
+        cdn_credential: ClientSecretCredential,
+    ) -> None:
+        super().__init__(
+            container_connection_string=container_connection_string,
+            incoming_container_sub_dir=Path("nightly"),
+            dist_dir=Path("nightly"),
+            cdn_credential=cdn_credential,
+        )
+
+    def _sign_pkgs(self, indexed_dir: Path) -> None:
+        pass  # We do not sign the nightly package
