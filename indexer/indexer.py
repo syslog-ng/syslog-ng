16366f753f9662ab6acbff60719a91c36bc0591e
Attila Szakacs <attila.szakacs@oneidentity.com>
2021-10-22 13:42:42 +0200

    indexer: use ContainerSynchronizer
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,15 +1,33 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 
+from .container_synchronizer import ContainerSynchronizer
+
 
 class Indexer(ABC):
-    def __init__(self) -> None:
-        # TODO: implement
-        pass
+    INCOMING_CONTAINER_NAME = "secret"
+    INDEXED_CONTAINER_NAME = "secret"
+
+    def __init__(
+        self,
+        container_connection_string: str,
+        incoming_container_sub_dir: Path,
+        indexed_container_sub_dir: Path,
+    ) -> None:
+        self.__incoming_container = ContainerSynchronizer(
+            connection_string=container_connection_string,
+            container_name=Indexer.INCOMING_CONTAINER_NAME,
+            sub_dir=incoming_container_sub_dir,
+        )
+        self.__indexed_container = ContainerSynchronizer(
+            connection_string=container_connection_string,
+            container_name=Indexer.INDEXED_CONTAINER_NAME,
+            sub_dir=indexed_container_sub_dir,
+        )
 
     def __sync_from_remote(self) -> None:
-        # TODO: implement
-        pass
+        self.__incoming_container.sync_from_remote()
+        self.__indexed_container.sync_from_remote()
 
     @abstractmethod
     def _index_pkgs(self, incoming_dir: Path, indexed_dir: Path) -> None:
@@ -20,16 +38,16 @@ class Indexer(ABC):
         pass
 
     def __sync_to_remote(self) -> None:
-        # TODO: implement
-        pass
+        self.__incoming_container.sync_to_remote()
+        self.__indexed_container.sync_to_remote()
 
     def __refresh_cdn_cache(self) -> None:
         # TODO: implement
         pass
 
     def index(self) -> None:
-        incoming_dir = Path()
-        indexed_dir = Path()
+        incoming_dir = self.__incoming_container.local_dir.working_dir
+        indexed_dir = self.__indexed_container.local_dir.working_dir
 
         self.__sync_from_remote()
 
