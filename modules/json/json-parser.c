a2e076517966e6bfde8d8b1bc2b6d8ba007f8f7c
Balazs Scheidler <bazsi77@gmail.com>
2021-11-12 14:24:55 +0100

    json-parser: extract json_parser_extract_value
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- modules/json/json-parser.c
+++ modules/json/json-parser.c
@@ -72,6 +72,57 @@ json_parser_set_extract_prefix(LogParser *s, const gchar *extract_prefix)
   self->extract_prefix = g_strdup(extract_prefix);
 }
 
+static gboolean
+json_parser_extract_value(struct json_object *jso, GString *value, LogMessageValueType *type)
+{
+  switch (json_object_get_type(jso))
+    {
+    case json_type_boolean:
+      if (json_object_get_boolean(jso))
+        g_string_assign(value, "true");
+      else
+        g_string_assign(value, "false");
+      *type = LM_VT_BOOLEAN;
+      return TRUE;
+    case json_type_double:
+      g_string_printf(value, "%f",
+                      json_object_get_double(jso));
+      *type = LM_VT_DOUBLE;
+      return TRUE;
+    case json_type_int:
+      g_string_printf(value, "%"PRId64,
+                      json_object_get_int64(jso));
+      *type = LM_VT_INT64;
+      return TRUE;
+    case json_type_string:
+      g_string_assign(value,
+                      json_object_get_string(jso));
+      *type = LM_VT_STRING;
+      return TRUE;
+    case json_type_null:
+
+      /* null values are represented as an empty string (so when expands to
+       * "nothing" in type unaware situations, while type-aware consumers
+       * may reproduce it as a NULL).
+       *
+       * I was thinking about using a very specific string representation
+       * (e.g.  "NULL" as a value), however if we encounter a null at a
+       * place where we might also have a sub-object, and we explicitly
+       * reference a field within that sub-object, we would get an empty
+       * string in that case too (due to the key being unknown).  All in
+       * all, the base case is I think to use an empty string as value, and
+       * LM_VT_NULL as type.  Type aware consumers would ignore the string
+       * anyway.
+       */
+      g_string_truncate(value, 0);
+      *type = LM_VT_NULL;
+      return TRUE;
+    default:
+      break;
+    }
+  return FALSE;
+}
+
 static void
 json_parser_process_object(struct json_object *jso,
                            const gchar *prefix,
@@ -94,30 +145,11 @@ json_parser_process_single(struct json_object *jso,
   switch (json_object_get_type(jso))
     {
     case json_type_boolean:
-      parsed = TRUE;
-      if (json_object_get_boolean(jso))
-        g_string_assign(value, "true");
-      else
-        g_string_assign(value, "false");
-      type = LM_VT_BOOLEAN;
-      break;
     case json_type_double:
-      parsed = TRUE;
-      g_string_printf(value, "%f",
-                      json_object_get_double(jso));
-      type = LM_VT_DOUBLE;
-      break;
     case json_type_int:
-      parsed = TRUE;
-      g_string_printf(value, "%"PRId64,
-                      json_object_get_int64(jso));
-      type = LM_VT_INT64;
-      break;
     case json_type_string:
-      parsed = TRUE;
-      g_string_assign(value,
-                      json_object_get_string(jso));
-      type = LM_VT_STRING;
+    case json_type_null:
+      parsed = json_parser_extract_value(jso, value, &type);
       break;
     case json_type_object:
       if (prefix)
@@ -144,25 +176,7 @@ json_parser_process_single(struct json_object *jso,
         }
       break;
     }
-    case json_type_null:
-
-      /* null values are represented as an empty string (so when expands to
-       * "nothing" in type unaware situations, while type-aware consumers
-       * may reproduce it as a NULL).
-       *
-       * I was thinking about using a very specific string representation
-       * (e.g.  "NULL" as a value), however if we encounter a null at a
-       * place where we might also have a sub-object, and we explicitly
-       * reference a field within that sub-object, we would get an empty
-       * string in that case too (due to the key being unknown).  All in
-       * all, the base case is I think to use an empty string as value, and
-       * LM_VT_NULL as type.  Type aware consumers would ignore the string
-       * anyway.
-       */
-
       parsed = TRUE;
-      g_string_truncate(value, 0);
-      type = LM_VT_NULL;
       break;
     default:
       msg_debug("JSON parser encountered an unknown type, skipping",
