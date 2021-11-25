1d19912dad5cc220b5d7960f2a00ae9ac700bb71
Attila Szakacs <attila.szakacs@oneidentity.com>
2021-11-25 14:02:28 +0100

    cdn, remote_storage_synchronizer: add get_implementations() function
    This function will help us to create the needed instance set in
    the config.
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- cdn/__init__.py
+++ cdn/__init__.py
@@ -1,7 +1,22 @@
+from inspect import isclass
+from typing import Dict, Type
+
 from .cdn import CDN
 from .azure_cdn import AzureCDN
 
 __all__ = [
     "CDN",
     "AzureCDN",
+    "get_implementations",
 ]
+
+
+def get_implementations() -> Dict[str, Type[CDN]]:
+    implementations = {}
+
+    for cls_name in __all__:
+        cls = globals()[cls_name]
+        if isclass(cls) and cls != CDN and issubclass(cls, CDN):
+            implementations[cls.get_config_keyword()] = cls
+
+    return implementations
--- remote_storage_synchronizer/__init__.py
+++ remote_storage_synchronizer/__init__.py
@@ -1,7 +1,22 @@
+from inspect import isclass
+from typing import Dict, Type
+
 from .remote_storage_synchronizer import RemoteStorageSynchronizer
 from .azure_container_synchronizer import AzureContainerSynchronizer
 
 __all__ = [
     "RemoteStorageSynchronizer",
     "AzureContainerSynchronizer",
+    "get_implementations",
 ]
+
+
+def get_implementations() -> Dict[str, Type[RemoteStorageSynchronizer]]:
+    implementations = {}
+
+    for cls_name in __all__:
+        cls = globals()[cls_name]
+        if isclass(cls) and cls != RemoteStorageSynchronizer and issubclass(cls, RemoteStorageSynchronizer):
+            implementations[cls.get_config_keyword()] = cls
+
+    return implementations
