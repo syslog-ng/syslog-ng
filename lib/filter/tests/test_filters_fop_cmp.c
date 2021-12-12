2063b45de396eb061734334f51467e5b8c0e002e
Balazs Scheidler <bazsi77@gmail.com>
2021-12-12 15:07:04 +0100

    filter/test_filters_fop_cmp: refactor testcases for easier modification
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- lib/filter/tests/test_filters_fop_cmp.c
+++ lib/filter/tests/test_filters_fop_cmp.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2018 Balabit
  * Copyright (c) 2013 Gergely Nagy <algernon@balabit.hu>
+ * Copyright (c) 2021 Balazs Scheidler <bazsi77@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -21,72 +22,234 @@
  * COPYING for details.
  *
  */
-#include <criterion/criterion.h>
-#include <criterion/parameterized.h>
-#include "test_filters_common.h"
 
-#include "filter/filter-expr.h"
-#include "filter/filter-re.h"
-#include "filter/filter-pri.h"
-#include "filter/filter-op.h"
+#include <criterion/criterion.h>
+#include "libtest/cr_template.h"
+
+#include "logmsg/logmsg.h"
+#include "apphook.h"
 #include "filter/filter-cmp.h"
 #include "filter/filter-expr-grammar.h"
-#include "cfg.h"
 
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+LogMessage *
+_construct_filter_msg(void)
+{
+  LogMessage *msg = log_msg_new_empty();
+
+  msg->pri = 15;
+  log_msg_set_value(msg, LM_V_PROGRAM, "software", -1);
+  return msg;
+}
+
+gboolean
+evaluate_testcase(FilterExprNode *filter_node)
+{
+  LogMessage *msg = _construct_filter_msg();
+  gboolean result;
+
+  cr_assert_not_null(filter_node, "Constructing an in-list filter");
+  result = filter_expr_eval(filter_node, msg);
+
+  log_msg_unref(msg);
+  filter_expr_unref(filter_node);
+  return result;
+}
+
+gboolean
+evaluate(const gchar *left, gint operator, const gchar *right)
+{
+  return evaluate_testcase(fop_cmp_new(compile_template(left), compile_template(right), operator));
+}
+
+Test(filter, test_num_eq)
+{
+  cr_assert(evaluate("10", KW_NUM_EQ, "10"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_NUM_EQ, "7"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_NUM_EQ, "$SEVERITY_NUM"));
+
+  cr_assert_not(evaluate("10", KW_NUM_EQ, "11"));
+
+}
+Test(filter, test_num_ne)
+{
+  cr_assert(evaluate("10", KW_NUM_NE, "9"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_NUM_NE, "8"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_NUM_NE, "$FACILITY_NUM"));
+
+  cr_assert_not(evaluate("10", KW_NUM_NE, "10"));
+}
+
+Test(filter, test_num_lt)
+{
+  cr_assert(evaluate("10", KW_NUM_LT, "11"));
+  cr_assert(evaluate("7", KW_NUM_LT, "8"));
+  cr_assert(evaluate("7", KW_NUM_LT, "10"));
+  cr_assert(evaluate("$LEVEL_NUM", KW_NUM_LT, "8"));
+  cr_assert(evaluate("$LEVEL_NUM", KW_NUM_LT, "10"));
+
+  cr_assert_not(evaluate("11", KW_NUM_LT, "10"));
+  cr_assert_not(evaluate("11", KW_NUM_LT, "11"));
+}
+
+Test(filter, test_num_le)
+{
+  cr_assert(evaluate("11", KW_NUM_LE, "11"));
+  cr_assert(evaluate("10", KW_NUM_LE, "11"));
+  cr_assert(evaluate("7", KW_NUM_LE, "8"));
+  cr_assert(evaluate("7", KW_NUM_LE, "10"));
+  cr_assert(evaluate("$LEVEL_NUM", KW_NUM_LE, "8"));
+  cr_assert(evaluate("$LEVEL_NUM", KW_NUM_LE, "10"));
+
+  cr_assert_not(evaluate("11", KW_NUM_LE, "10"));
+}
+
+Test(filter, test_num_gt)
+{
+  cr_assert(evaluate("11", KW_NUM_GT, "10"));
+  cr_assert(evaluate("8", KW_NUM_GT, "7"));
+  cr_assert(evaluate("10", KW_NUM_GT, "7"));
+  cr_assert(evaluate("8", KW_NUM_GT, "$LEVEL_NUM"));
+  cr_assert(evaluate("10", KW_NUM_GT, "$LEVEL_NUM"));
+
+  cr_assert_not(evaluate("10", KW_NUM_GT, "11"));
+  cr_assert_not(evaluate("10", KW_NUM_GT, "10"));
+}
+
+Test(filter, test_num_ge)
+{
+  cr_assert(evaluate("10", KW_NUM_GE, "10"));
+  cr_assert(evaluate("11", KW_NUM_GE, "10"));
+  cr_assert(evaluate("8", KW_NUM_GE, "7"));
+  cr_assert(evaluate("10", KW_NUM_GE, "7"));
+  cr_assert(evaluate("8", KW_NUM_GE, "$LEVEL_NUM"));
+  cr_assert(evaluate("10", KW_NUM_GE, "$LEVEL_NUM"));
+
+  cr_assert_not(evaluate("10", KW_NUM_GE, "11"));
+}
+
+Test(filter, test_numeric_ordering)
+{
+  cr_assert_not(evaluate("10", KW_NUM_LT, "10"));
+  cr_assert(evaluate("10", KW_NUM_LE, "10"));
+  cr_assert(evaluate("10", KW_NUM_EQ, "10"));
+  cr_assert(evaluate("10", KW_NUM_GE, "10"));
+  cr_assert_not(evaluate("10", KW_NUM_GT, "10"));
+
+  cr_assert(evaluate("10", KW_NUM_LT, "11"));
+  cr_assert(evaluate("10", KW_NUM_LE, "11"));
+  cr_assert_not(evaluate("10", KW_NUM_EQ, "11"));
+  cr_assert_not(evaluate("10", KW_NUM_GE, "11"));
+  cr_assert_not(evaluate("10", KW_NUM_GT, "11"));
+
+  cr_assert_not(evaluate("11", KW_NUM_LT, "10"));
+  cr_assert_not(evaluate("11", KW_NUM_LE, "10"));
+  cr_assert_not(evaluate("11", KW_NUM_EQ, "10"));
+  cr_assert(evaluate("11", KW_NUM_GE, "10"));
+  cr_assert(evaluate("11", KW_NUM_GT, "10"));
+}
+
+/* string comparison */
+
+Test(filter, test_eq)
+{
+  cr_assert(evaluate("10", KW_EQ, "10"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_EQ, "7"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_EQ, "$SEVERITY_NUM"));
+
+  cr_assert_not(evaluate("10", KW_EQ, "11"));
+
+}
+
+Test(filter, test_ne)
+{
+  cr_assert(evaluate("10", KW_NE, "9"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_NE, "8"));
+  cr_assert(evaluate("$SEVERITY_NUM", KW_NE, "$FACILITY_NUM"));
+
+  cr_assert_not(evaluate("10", KW_NE, "10"));
+}
+
+Test(filter, test_lt)
+{
+  cr_assert(evaluate("10", KW_LT, "11"));
+  cr_assert(evaluate("7", KW_LT, "8"));
+  cr_assert(evaluate("$LEVEL_NUM", KW_LT, "8"));
+
+  cr_assert_not(evaluate("7", KW_LT, "10"));
+  cr_assert_not(evaluate("11", KW_LT, "10"));
+  cr_assert_not(evaluate("11", KW_LT, "11"));
+}
+
+Test(filter, test_le)
+{
+  cr_assert(evaluate("11", KW_LE, "11"));
+  cr_assert(evaluate("10", KW_LE, "11"));
+  cr_assert(evaluate("7", KW_LE, "8"));
+  cr_assert(evaluate("$LEVEL_NUM", KW_LE, "8"));
+
+  cr_assert_not(evaluate("7", KW_LE, "10"));
+  cr_assert_not(evaluate("11", KW_LE, "10"));
+  cr_assert_not(evaluate("$LEVEL_NUM", KW_LE, "10"));
+}
+
+Test(filter, test_gt)
+{
+  cr_assert(evaluate("11", KW_GT, "10"));
+  cr_assert(evaluate("8", KW_GT, "7"));
+  cr_assert(evaluate("8", KW_GT, "$LEVEL_NUM"));
+
+  cr_assert_not(evaluate("10", KW_GT, "7"));
+  cr_assert_not(evaluate("10", KW_GT, "11"));
+  cr_assert_not(evaluate("10", KW_GT, "10"));
+  cr_assert_not(evaluate("10", KW_GT, "$LEVEL_NUM"));
+}
+
+Test(filter, test_ge)
+{
+  cr_assert(evaluate("10", KW_GE, "10"));
+  cr_assert(evaluate("11", KW_GE, "10"));
+  cr_assert(evaluate("8", KW_GE, "7"));
+  cr_assert(evaluate("8", KW_GE, "$LEVEL_NUM"));
+
+  cr_assert_not(evaluate("10", KW_GE, "7"));
+  cr_assert_not(evaluate("10", KW_GE, "11"));
+  cr_assert_not(evaluate("10", KW_GE, "$LEVEL_NUM"));
+}
+
+Test(filter, test_string_ordering)
+{
+  cr_assert_not(evaluate("10", KW_LT, "10"));
+  cr_assert(evaluate("10", KW_LE, "10"));
+  cr_assert(evaluate("10", KW_EQ, "10"));
+  cr_assert(evaluate("10", KW_GE, "10"));
+  cr_assert_not(evaluate("10", KW_GT, "10"));
+
+  cr_assert(evaluate("10", KW_LT, "11"));
+  cr_assert(evaluate("10", KW_LE, "11"));
+  cr_assert_not(evaluate("10", KW_EQ, "11"));
+  cr_assert_not(evaluate("10", KW_GE, "11"));
+  cr_assert_not(evaluate("10", KW_GT, "11"));
+
+  cr_assert_not(evaluate("11", KW_LT, "10"));
+  cr_assert_not(evaluate("11", KW_LE, "10"));
+  cr_assert_not(evaluate("11", KW_EQ, "10"));
+  cr_assert(evaluate("11", KW_GE, "10"));
+  cr_assert(evaluate("11", KW_GT, "10"));
+}
+
+static void
+setup(void)
+{
+  app_startup();
+
+  configuration = cfg_new_snippet();
+}
+
+static void
+teardown(void)
+{
+  app_shutdown();
+  cfg_free(configuration);
+}
 
 TestSuite(filter, .init = setup, .fini = teardown);
-
-typedef struct _FilterParamFopCmp
-{
-  const gchar *msg;
-  const gchar *template1;
-  const gchar *template2;
-  gint op;
-  gboolean expected_result;
-} FilterParamFopCmp;
-
-
-ParameterizedTestParameters(filter, test_filter_fop_cmp)
-{
-  static FilterParamFopCmp test_data_list[] =
-  {
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "7", .op = KW_NUM_EQ, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "5", .op = KW_NUM_NE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "8", .op = KW_NUM_LT, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "10", .op = KW_NUM_LT, .expected_result = TRUE},
-    /* 7 lt 10 is FALSE as 10 orders lower when interpreted as a string */
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "10", .op = KW_LT, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "5", .op = KW_NUM_GT, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "7", .op = KW_NUM_GE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "$LEVEL_NUM", .template2 = "7", .op = KW_NUM_LE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "korte", .op = KW_LT, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "korte", .op = KW_LE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "korte", .op = KW_EQ, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "korte", .op = KW_NE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "korte", .op = KW_GE, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "korte", .op = KW_GT, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "korte", .template2 = "alma", .op = KW_LT, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "korte", .template2 = "alma", .op = KW_LE, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "korte", .template2 = "alma", .op = KW_EQ, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "korte", .template2 = "alma", .op = KW_NE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "korte", .template2 = "alma", .op = KW_GE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "korte", .template2 = "alma", .op = KW_GT, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "alma", .op = KW_LT, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "alma", .op = KW_LE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "alma", .op = KW_EQ, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "alma", .op = KW_NE, .expected_result = FALSE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "alma", .op = KW_GE, .expected_result = TRUE},
-    {.msg = "<15>Oct 15 16:17:01 host openvpn[2499]: PTHREAD support initialized", .template1 = "alma", .template2 = "alma", .op = KW_GT, .expected_result = FALSE},
-  };
-
-  return cr_make_param_array(FilterParamFopCmp, test_data_list, G_N_ELEMENTS(test_data_list));
-}
-
-ParameterizedTest(FilterParamFopCmp *param, filter, test_filter_fop_cmp)
-{
-  FilterExprNode *filter = fop_cmp_new(create_template(param->template1), create_template(param->template2), param->op);
-  testcase(param->msg, filter, param->expected_result);
-}
