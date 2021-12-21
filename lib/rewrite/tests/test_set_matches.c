808263e98dce0159d23e7ac93af2716b5e452f7b
Balazs Scheidler <bazsi77@gmail.com>
2021-12-21 19:11:00 +0100

    rewrite: implement set-matches() rewrite operation
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

---
+++ lib/rewrite/tests/test_set_matches.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2021 Balazs Scheidler <bazsi77@gmail.com>
+ * Copyright (c) 2019 Balabit
+ * Copyright (c) 2019 Kokan <kokaipeter@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include <criterion/criterion.h>
+#include "libtest/grab-logging.h"
+#include "libtest/msg_parse_lib.h"
+#include "libtest/cr_template.h"
+
+#include "apphook.h"
+#include "rewrite/rewrite-set-matches.h"
+#include "logmsg/logmsg.h"
+#include "scratch-buffers.h"
+
+GlobalConfig *configuration = NULL;
+LogMessage *msg;
+
+static void
+_perform_rewrite(LogRewrite *rewrite, LogMessage *msg_)
+{
+  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
+
+  log_pipe_init(&rewrite->super);
+  log_pipe_queue(&rewrite->super, log_msg_ref(msg_), &path_options);
+  log_pipe_deinit(&rewrite->super);
+  log_pipe_unref(&rewrite->super);
+}
+
+static void
+_perform_set_matches(LogTemplate *template, LogMessage *msg_)
+{
+  LogRewrite *rewrite = log_rewrite_set_matches_new(template, configuration);
+  log_template_unref(template);
+
+  _perform_rewrite(rewrite, msg_);
+}
+
+Test(set_matches, numeric)
+{
+  log_msg_set_match(msg, 0, "whatever", -1);
+  _perform_set_matches(compile_template("foo,bar"), msg);
+  assert_log_message_value_unset_by_name(msg, "0");
+  assert_log_message_match_value(msg, 1, "foo");
+  assert_log_message_match_value(msg, 2, "bar");
+}
+
+static void
+setup(void)
+{
+  app_startup();
+  configuration = cfg_new_snippet();
+  start_grabbing_messages();
+  msg = log_msg_new_empty();
+}
+
+static void
+teardown(void)
+{
+  scratch_buffers_explicit_gc();
+  log_msg_unref(msg);
+  stop_grabbing_messages();
+  cfg_free(configuration);
+  app_shutdown();
+}
+
+TestSuite(set_matches, .init = setup, .fini = teardown);
