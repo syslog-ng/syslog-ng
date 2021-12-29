abe99e683b65076649b63142b0e0e3dac5df902d
László Várady <laszlo.varady@protonmail.com>
2021-12-29 15:51:44 +0100

    modules: add skeleton for throttle() filter
    This is a preparation step for extracting throttle() into a separate
    module.
    
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>

---
+++ modules/throttle-filter/throttle-filter-parser.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2021 One Identity
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "driver.h"
+#include "cfg-parser.h"
+#include "throttle-filter-grammar.h"
+
+extern int throttle_filter_debug;
+
+int throttle_filter_parse(CfgLexer *lexer, LogDriver **instance, gpointer arg);
+
+static CfgLexerKeyword throttle_filter_keywords[] =
+{
+  { NULL }
+};
+
+CfgParser throttle_filter_parser =
+{
+#if SYSLOG_NG_ENABLE_DEBUG
+  .debug_flag = &throttle_filter_debug,
+#endif
+  .name = "throttle-filter",
+  .keywords = throttle_filter_keywords,
+  .parse = (gint (*)(CfgLexer *, gpointer *, gpointer)) throttle_filter_parse,
+  .cleanup = (void (*)(gpointer)) log_pipe_unref,
+};
+
+CFG_PARSER_IMPLEMENT_LEXER_BINDING(throttle_filter_, THROTTLE_FILTER_, LogDriver **)
