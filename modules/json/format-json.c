192c338d48d55a6c70f12859eecbc7c284460695
Balazs Scheidler <bazsi77@gmail.com>
2022-01-22 10:25:47 +0100

    format-json: add compatibility with 3.x
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- modules/json/format-json.c
+++ modules/json/format-json.c
@@ -83,6 +83,23 @@ tf_json_prepare(LogTemplateFunction *self, gpointer s, LogTemplate *parent,
       value_pairs_add_transforms(state->vp, vpts);
     }
 
+  if (cfg_is_config_version_older(parent->cfg, VERSION_VALUE_4_0) &&
+      !value_pairs_is_cast_to_strings_explicit(state->vp))
+    {
+      if (cfg_is_typing_feature_enabled(parent->cfg))
+        {
+          msg_warning("WARNING: $(format-json) starts using type information "
+                      "associated with name-value pairs in " VERSION_4_0
+                      ". This can possibly cause fields in the formatted JSON "
+                      "document to change types if no explicit type hint is "
+                      "specified. This change will cause the type in the output "
+                      "document match the original type that was parsed "
+                      "using json-parser(), add --no-cast argument "
+                      "to $(format-json) to keep the old behavior");
+        }
+      value_pairs_set_cast_to_strings(state->vp, TRUE);
+    }
+
   return TRUE;
 }
 
