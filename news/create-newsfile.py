f76ec8063e63d5845fae8ad9d62e0e1b456aef78
Attila Szakacs <attila.szakacs@oneidentity.com>
2022-03-01 14:47:22 +0100

    create-newsfile: do not indent empty lines
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- news/create-newsfile.py
+++ news/create-newsfile.py
@@ -80,6 +80,7 @@ def create_block(block_name, files):
 
         entry += '  * {}\n([#{}](https://github.com/syslog-ng/syslog-ng/pull/{}))'.format(f.read_text().rstrip(), pr_id, pr_id)
         entry = entry.replace('\n', '\n    ')
+        entry = entry.replace('\n    \n', '\n\n')
         block += entry + '\n'
     block += '\n'
     return block
