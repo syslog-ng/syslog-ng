45978e5500bbc04e7053170c7e8e221b1bd566b0
Andras Mitzki <andras.mitzki@quest.com>
2022-04-04 09:26:30 +0200

    Light: Add missing parameters for run_process_with_strace()
    - without this we can not start Light with '--run-under=strace'
    
    Signed-off-by: Andras Mitzki <andras.mitzki@quest.com>

--- tests/light/src/syslog_ng/syslog_ng_executor.py
+++ tests/light/src/syslog_ng/syslog_ng_executor.py
@@ -43,7 +43,7 @@ class SyslogNgExecutor(object):
         if external_tool == "valgrind":
             return self.run_process_with_valgrind(stderr, debug, trace, verbose, startup_debug, no_caps, config_path, persist_path, pid_path, control_socket_path)
         elif external_tool == "strace":
-            return self.run_process_with_strace()
+            return self.run_process_with_strace(stderr, debug, trace, verbose, startup_debug, no_caps, config_path, persist_path, pid_path, control_socket_path)
         else:
             raise Exception("Unknown external tool was selected: {}".format(external_tool))
 
@@ -68,7 +68,7 @@ class SyslogNgExecutor(object):
             stderr_path=self.__instance_paths.get_stderr_path(),
         )
 
-    def run_process_with_strace(self):
+    def run_process_with_strace(self, stderr, debug, trace, verbose, startup_debug, no_caps, config_path, persist_path, pid_path, control_socket_path):
         strace_command_args = [
             "strace",
             "-s",
@@ -79,7 +79,7 @@ class SyslogNgExecutor(object):
             "-o",
             self.__instance_paths.get_external_tool_output_path("strace"),
         ]
-        full_command_args = strace_command_args + self.__construct_syslog_ng_process()
+        full_command_args = strace_command_args + self.__construct_syslog_ng_process(stderr, debug, trace, verbose, startup_debug, no_caps, config_path, persist_path, pid_path, control_socket_path)
         return self.__process_executor.start(
             command=full_command_args,
             stdout_path=self.__instance_paths.get_stdout_path(),
