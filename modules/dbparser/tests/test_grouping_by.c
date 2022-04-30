2a9071cecd9a39969d5656c0a1e169fcc5144323
Balazs Scheidler <bazsi77@gmail.com>
2022-04-30 09:40:11 +0200

    test_grouping_by: use _compile_grouping_by() everywhere
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- modules/dbparser/tests/test_grouping_by.c
+++ modules/dbparser/tests/test_grouping_by.c
@@ -33,29 +33,6 @@
 #include "cfg.h"
 #include "scratch-buffers.h"
 
-static LogTemplate *
-_get_template(const gchar *template)
-{
-  LogTemplate *self = log_template_new(configuration, NULL);
-
-  cr_assert(log_template_compile(self, template, NULL));
-
-  return self;
-}
-
-static FilterExprNode *
-_compile_filter_expr(gchar *expr)
-{
-  FilterExprNode *expr_node;
-
-  CfgLexer *lexer = cfg_lexer_new_buffer(configuration, expr, strlen(expr));
-  cr_assert(lexer != NULL);
-
-  cr_assert(cfg_run_parser(configuration, lexer, &filter_expr_parser, (gpointer *) &expr_node, NULL));
-
-  return expr_node;
-}
-
 static LogParser *
 _compile_grouping_by(gchar *expr)
 {
@@ -99,48 +76,22 @@ test_capture_pipe_new(GlobalConfig *cfg)
   return self;
 }
 
-Test(grouping_by, create_grouping_by)
-{
-  LogParser *parser = grouping_by_new(configuration);
-
-  grouping_by_set_synthetic_message(parser, synthetic_message_new());
-
-  grouping_by_set_timeout(parser, 1);
-
-  LogTemplate *template = _get_template("$TEMPLATE");
-  grouping_by_set_key_template(parser, template);
-  log_template_unref(template);
-
-  cr_assert(log_pipe_init(&parser->super));
-
-  cr_assert(log_pipe_deinit(&parser->super));
-
-  log_pipe_unref(&parser->super);
-}
-
 Test(grouping_by, grouping_by_aggregates_messages)
 {
   TestCapturePipe *capture = test_capture_pipe_new(configuration);
-  LogParser *parser = grouping_by_new(configuration);
+  LogParser *parser = _compile_grouping_by(
+      "grouping-by(key(\"$key\")"
+      "    aggregate("
+      "        value(\"aggr\" \"$(list-slice :-1 $(context-values $PROGRAM))\")"
+      "    )"
+      "    timeout(1)"
+      "    trigger(\"$(context-length)\" == \"3\")"
+      ");");
   LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
-
-  LogTemplate *template = _get_template("$key");
-  grouping_by_set_key_template(parser, template);
-  log_template_unref(template);
-
-  SyntheticMessage *sm = synthetic_message_new();
-  cr_assert(synthetic_message_add_value_template_string(sm, configuration, "aggr",
-                                                        "$(list-slice :-1 $(context-values $PROGRAM))", NULL) == TRUE);
-
-  grouping_by_set_synthetic_message(parser, sm);
-  grouping_by_set_timeout(parser, 1);
 
   log_pipe_append(&parser->super, &capture->super);
   cr_assert(log_pipe_init(&capture->super) == TRUE);
   cr_assert(log_pipe_init(&parser->super) == TRUE);
-
-  FilterExprNode *trigger = _compile_filter_expr("\"$(context-length)\" == \"3\"");
-  grouping_by_set_trigger_condition(parser, trigger);
 
   LogMessage *msg = _create_input_msg("first");
   gboolean success = log_parser_process_message(parser, &msg, &path_options);
@@ -182,26 +133,18 @@ Test(grouping_by, cfg_persist_name_not_equal)
 
 Test(grouping_by, cfg_persist_name_equal)
 {
-  LogParser *parser = grouping_by_new(configuration);
-
-  LogTemplate *template = _get_template("$TEMPLATE1");
-  grouping_by_set_key_template(parser, template);
-  log_template_unref(template);
-
+  LogParser *parser = _compile_grouping_by("grouping-by(key(\"$TEMPLATE1\"));");
   gchar *persist_name1 = g_strdup(log_pipe_get_persist_name(&parser->super));
+  log_pipe_unref(&parser->super);
 
-  template = _get_template("$TEMPLATE1");
-  grouping_by_set_key_template(parser, template);
-  log_template_unref(template);
-
+  parser = _compile_grouping_by("grouping-by(key(\"$TEMPLATE1\"));");
   gchar *persist_name2 = g_strdup(log_pipe_get_persist_name(&parser->super));
+  log_pipe_unref(&parser->super);
 
   cr_assert_str_eq(persist_name1, persist_name2);
 
   g_free(persist_name1);
   g_free(persist_name2);
-
-  log_pipe_unref(&parser->super);
 }
 
 static void
