d08b6b6ec189ec5e150915521276cd6cebd2d73e
Attila Szakacs <attila.szakacs@oneidentity.com>
2022-05-25 13:46:35 +0200

    package-indexer: remove unused RPM indexer skeleton
    It is dead code, there is no need to drag it with us.
    We can add it back when we need it.
    
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- packaging/package-indexer/indexer/__init__.py
+++ packaging/package-indexer/indexer/__init__.py
@@ -22,12 +22,9 @@
 
 from .indexer import Indexer
 from .deb_indexer import ReleaseDebIndexer, NightlyDebIndexer
-from .rpm_indexer import ReleaseRpmIndexer, NightlyRpmIndexer
 
 __all__ = [
     "Indexer",
     "ReleaseDebIndexer",
     "NightlyDebIndexer",
-    "ReleaseRpmIndexer",
-    "NightlyRpmIndexer",
 ]
