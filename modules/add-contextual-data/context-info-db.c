3b7bad083602a5b309b6034e4d0fcfc0281132e0
Balazs Scheidler <bazsi77@gmail.com>
2022-09-29 10:46:21 +0200

    add-contextual-data: add "type" field into our debugging message
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- modules/add-contextual-data/context-info-db.c
+++ modules/add-contextual-data/context-info-db.c
@@ -340,6 +340,7 @@ context_info_db_import(ContextInfoDB *self, FILE *fp, const gchar *filename,
       msg_trace("add-contextual-data(): adding database entry",
                 evt_tag_str("selector", next_record->selector->str),
                 evt_tag_str("name", log_msg_get_value_name(next_record->value_handle, NULL)),
+                evt_tag_str("type", log_msg_value_type_to_str(next_record->value->type_hint)),
                 evt_tag_str("value", next_record->value->template));
       context_info_db_insert(self, next_record);
     }
