20f274d482ebe9f9574491cf9c3eeccf73fdcb49
Balazs Scheidler <bazsi77@gmail.com>
2022-11-13 10:12:35 +0100

    logproto-proxied-text-server: move struct to the implementation module
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- lib/logproto/logproto-proxied-text-server.c
+++ lib/logproto/logproto-proxied-text-server.c
@@ -30,6 +30,38 @@
 #include "transport/transport-factory-tls.h"
 #include "str-utils.h"
 
+#define IP_BUF_SIZE 64
+#define PROXY_PROTO_HDR_MAX_LEN_RFC 108
+#define PROXY_PROTO_HDR_MAX_LEN (PROXY_PROTO_HDR_MAX_LEN_RFC * 2)
+
+struct ProxyProtoInfo
+{
+  gboolean unknown;
+
+  gchar src_ip[IP_BUF_SIZE];
+  gchar dst_ip[IP_BUF_SIZE];
+
+  int ip_version;
+  int src_port;
+  int dst_port;
+};
+
+typedef struct _LogProtoProxiedTextServer
+{
+  LogProtoTextServer super;
+
+  // Info received from the proxied that should be added as LogTransportAuxData to
+  // any message received through this server instance.
+  struct ProxyProtoInfo *info;
+
+  // Flag to only run handshake() once
+  gboolean handshake_done;
+  gboolean has_to_switch_to_tls;
+
+  guchar proxy_header_buff[PROXY_PROTO_HDR_MAX_LEN + 1];
+  gsize proxy_header_buff_len;
+} LogProtoProxiedTextServer;
+
 #define PROXY_HDR_TCP4 "PROXY TCP4 "
 #define PROXY_HDR_TCP6 "PROXY TCP6 "
 #define PROXY_HDR_UNKNOWN "PROXY UNKNOWN"
--- lib/logproto/logproto-proxied-text-server.h
+++ lib/logproto/logproto-proxied-text-server.h
@@ -26,38 +26,6 @@
 
 #include "logproto-text-server.h"
 
-#define IP_BUF_SIZE 64
-#define PROXY_PROTO_HDR_MAX_LEN_RFC 108
-#define PROXY_PROTO_HDR_MAX_LEN (PROXY_PROTO_HDR_MAX_LEN_RFC * 2)
-
-struct ProxyProtoInfo
-{
-  gboolean unknown;
-
-  gchar src_ip[IP_BUF_SIZE];
-  gchar dst_ip[IP_BUF_SIZE];
-
-  int ip_version;
-  int src_port;
-  int dst_port;
-};
-
-typedef struct _LogProtoProxiedTextServer
-{
-  LogProtoTextServer super;
-
-  // Info received from the proxied that should be added as LogTransportAuxData to
-  // any message received through this server instance.
-  struct ProxyProtoInfo *info;
-
-  // Flag to only run handshake() once
-  gboolean handshake_done;
-  gboolean has_to_switch_to_tls;
-
-  guchar proxy_header_buff[PROXY_PROTO_HDR_MAX_LEN + 1];
-  gsize proxy_header_buff_len;
-} LogProtoProxiedTextServer;
-
 LogProtoServer *log_proto_proxied_text_server_new(LogTransport *transport, const LogProtoServerOptions *options);
 LogProtoServer *log_proto_proxied_text_tls_passthrough_server_new(LogTransport *transport,
     const LogProtoServerOptions *options);
