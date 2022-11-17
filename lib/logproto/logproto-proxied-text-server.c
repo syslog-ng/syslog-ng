5368135b2b4f41f2af5f63f3ad16e3cc3de1eb8c
Balazs Scheidler <bazsi77@gmail.com>
2022-11-17 14:17:33 +0100

    logproto-proxied-text-server: added copyright statement
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- lib/logproto/logproto-proxied-text-server.c
+++ lib/logproto/logproto-proxied-text-server.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 One Identity
+ * Copyright (c) 2022 Balazs Scheidler <bazsi77@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
