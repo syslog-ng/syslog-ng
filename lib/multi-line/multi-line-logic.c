bf435b07716fc773efb62c5395045194269c412e
Balazs Scheidler <bazsi77@gmail.com>
2022-11-18 17:20:07 +0100

    multi-line: added skeleton files for the multi-line interface
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

---
+++ lib/multi-line/multi-line-logic.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2013 Balabit
+ * Copyright (c) 2013 Balazs Scheidler <bazsi@balabit.hu>
+ * Copyright (c) 2022 Balazs Scheidler <bazsi77@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "multi-line/multi-line-logic.h"
+
+void
+multi_line_logic_free_method(MultiLineLogic *s)
+{
+  g_free(s);
+}
+
+void
+multi_line_logic_init_instance(MultiLineLogic *self)
+{
+  self->free_fn = multi_line_logic_free_method;
+}
---
+++ lib/multi-line/multi-line-logic.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2013 Balabit
+ * Copyright (c) 2013 Balazs Scheidler <bazsi@balabit.hu>
+ * Copyright (c) 2022 Balazs Scheidler <bazsi77@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef MULTI_LINE_LOGIC_H_INCLUDED
+#define MULTI_LINE_LOGIC_H_INCLUDED
+
+#include "syslog-ng.h"
+
+enum
+{
+  MLL_EXTRACTED    = 0x0001,
+  MLL_WAITING      = 0x0002,
+  MLL_CONSUME_LINE = 0x0010,
+  MLL_REWIND_LINE  = 0x0020,
+};
+
+#define MLL_CONSUME_PARTIAL_AMOUNT_SHIFT     8
+#define MLL_CONSUME_PARTIAL_AMOUNT_MASK      ~0xFF
+#define MLL_CONSUME_PARTIALLY(drop_length) (MLL_CONSUME_LINE | ((drop_length) << MLL_CONSUME_PARTIAL_AMOUNT_SHIFT))
+
+typedef struct _MultiLineLogic MultiLineLogic;
+struct _MultiLineLogic
+{
+  gint (*accumulate_line)(MultiLineLogic *self,
+                          const guchar *msg,
+                          gsize msg_len,
+                          gssize consumed_len);
+  void (*free_fn)(MultiLineLogic *s);
+};
+
+void multi_line_logic_init_instance(MultiLineLogic *self);
+void multi_line_logic_free_method(MultiLineLogic *s);
+
+static inline gint
+multi_line_logic_accumulate_line(MultiLineLogic *self,
+                                 const guchar *msg,
+                                 gsize msg_len,
+                                 gssize consumed_len)
+{
+  return self->accumulate_line(self, msg, msg_len, consumed_len);
+}
+
+static inline void
+multi_line_logic_free(MultiLineLogic *self)
+{
+  self->free_fn(self);
+}
+
+
+
+#endif
