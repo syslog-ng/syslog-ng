12c535fe668c4c1b7599c71b30bc9c940ad80604
Balazs Scheidler <bazsi77@gmail.com>
2022-12-28 22:04:15 +0100

    mainloop-call: extract main_loop_wait_for_pending_call_to_finish()
    As a preparation for fixing #4258, this patch extracts a few lines as
    a new function from main_loop_call().
    
    The goal of the new function is to wait for any pending executions of
    mainloop-calls, which can happen if a worker thread submits a new one while
    the old is still executing. At the moment, there can be only ONE outstanding
    mainloop-calls at the same time.
    
    Signed-off-by: Balazs Scheidler <bazsi77@gmail.com>

--- lib/mainloop-call.c
+++ lib/mainloop-call.c
@@ -56,12 +56,9 @@ static GMutex main_task_lock;
 static struct iv_list_head main_task_queue = IV_LIST_HEAD_INIT(main_task_queue);
 static struct iv_event main_task_posted;
 
-gpointer
-main_loop_call(MainLoopTaskFunc func, gpointer user_data, gboolean wait)
+static void
+main_loop_wait_for_pending_call_to_finish(void)
 {
-  if (main_loop_is_main_thread())
-    return func(user_data);
-
   g_mutex_lock(&main_task_lock);
 
   /* check if a previous call is being executed */
@@ -79,6 +76,16 @@ main_loop_call(MainLoopTaskFunc func, gpointer user_data, gboolean wait)
     {
       g_mutex_unlock(&call_info.lock);
     }
+  g_mutex_unlock(&main_task_lock);
+}
+
+gpointer
+main_loop_call(MainLoopTaskFunc func, gpointer user_data, gboolean wait)
+{
+  if (main_loop_is_main_thread())
+    return func(user_data);
+
+  main_loop_wait_for_pending_call_to_finish();
 
   /* call_info.lock is no longer needed, since we're the only ones using call_info now */
   INIT_IV_LIST_HEAD(&call_info.list);
@@ -86,6 +93,7 @@ main_loop_call(MainLoopTaskFunc func, gpointer user_data, gboolean wait)
   call_info.func = func;
   call_info.user_data = user_data;
   call_info.wait = wait;
+  g_mutex_lock(&main_task_lock);
   iv_list_add(&call_info.list, &main_task_queue);
   iv_event_post(&main_task_posted);
   if (wait)
