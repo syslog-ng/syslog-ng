e2228b2bb4d2dfa42f734778f29bc416155a023e
Attila Szakacs <attila.szakacs@axoflow.com>
2023-03-16 11:25:19 +0100

    logqueue-disk: add UT for non-reliable corrupted restart with qout
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- modules/diskq/tests/test_logqueue_disk.c
+++ modules/diskq/tests/test_logqueue_disk.c
@@ -248,6 +248,77 @@ Test(logqueue_disk, restart_corrupted_non_reliable)
 }
 
 static void
+_assert_log_queue_disk_non_reliable_has_messages_in_qout(LogQueue *q, guint num_of_messages)
+{
+  LogQueueDiskNonReliable *queue = (LogQueueDiskNonReliable *) q;
+  const guint item_number_per_message = 2;
+
+  LogMessage *msg = log_msg_new_empty();
+  const gssize log_msg_size = log_msg_get_size(msg);
+  log_msg_unref(msg);
+
+  cr_assert_eq(g_queue_get_length(queue->qout), num_of_messages * item_number_per_message);
+  cr_assert_eq(g_queue_get_length(queue->qoverflow), 0);
+  cr_assert_eq(g_queue_get_length(queue->qbacklog), 0);
+  cr_assert_eq(qdisk_get_length(queue->super.qdisk), 0);
+
+  cr_assert(q->memory_usage);
+  cr_assert(q->queued_messages);
+
+  cr_assert_eq(stats_counter_get(q->memory_usage), num_of_messages * log_msg_size);
+  cr_assert_eq(stats_counter_get(q->queued_messages), num_of_messages);
+  cr_assert_eq(atomic_gssize_get_unsigned(&q->stats_cache.memory_usage), num_of_messages * log_msg_size);
+  cr_assert_eq(atomic_gssize_get_unsigned(&q->stats_cache.queued_messages), num_of_messages);
+}
+
+Test(logqueue_disk, restart_corrupted_non_reliable_with_qout)
+{
+  start_grabbing_messages();
+
+  const gchar *filename = "restart_corrupted_non_reliable_with_qout.qf";
+  const gchar *corrupted_filename = "restart_corrupted_non_reliable_with_qout.qf.corrupted";
+
+  DiskQueueOptions options = {0};
+  disk_queue_options_set_default_options(&options);
+  disk_queue_options_reliable_set(&options, FALSE);
+  disk_queue_options_disk_buf_size_set(&options, MIN_DISK_BUF_SIZE);
+  disk_queue_options_mem_buf_size_set(&options, 4096);
+  disk_queue_options_qout_size_set(&options, 1);
+
+  LogQueue *queue = log_queue_disk_non_reliable_new(&options, "restart_corrupted_non_reliable_with_qout");
+  LogQueueDiskNonReliable *queue_disk_non_reliable = (LogQueueDiskNonReliable *) queue;
+  _register_stats_counters(queue);
+
+  cr_assert(log_queue_disk_start(queue, filename));
+  cr_assert_str_eq(log_queue_disk_get_filename(queue), filename);
+  _assert_file_exists(filename);
+  _assert_log_queue_disk_non_reliable_is_empty(queue);
+
+  LogPathOptions path_options = LOG_PATH_OPTIONS_INIT;
+  LogMessage *msg = log_msg_new_empty();
+  log_queue_push_tail(queue, msg, &path_options);
+
+  _assert_log_queue_disk_non_reliable_has_messages_in_qout(queue, 1);
+
+  log_queue_disk_restart_corrupted(&queue_disk_non_reliable->super);
+
+  cr_assert_str_eq(log_queue_disk_get_filename(queue), filename);
+  _assert_file_exists(corrupted_filename);
+  _assert_file_exists(filename);
+
+  _assert_log_queue_disk_non_reliable_has_messages_in_qout(queue, 1);
+
+  gboolean persistent;
+  log_queue_disk_stop(queue, &persistent);
+  log_queue_unref(queue);
+  disk_queue_options_destroy(&options);
+  unlink(filename);
+  unlink(corrupted_filename);
+
+  stop_grabbing_messages();
+}
+
+static void
 setup(void)
 {
   app_startup();
