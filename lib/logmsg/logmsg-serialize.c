d8f8445b7b2184c5ad80f456ae90c1ca4d3b9608
László Várady <laszlo.varady@anno.io>
2023-04-14 17:29:41 +0200

    logmgs-serialize: remove duplicated function
    Signed-off-by: László Várady <laszlo.varady@anno.io>

--- lib/logmsg/logmsg-serialize.c
+++ lib/logmsg/logmsg-serialize.c
@@ -368,41 +368,6 @@ error:
 }
 
 static gboolean
-log_msg_read_sd_param_first(SerializeArchive *sa, gchar *sd_element_name, LogMessage *self,
-                            gboolean *has_more)
-{
-  gchar *name = NULL, *value = NULL;
-  gsize name_len, value_len;
-  gchar sd_param_name[256]= {0};
-  gboolean success = FALSE;
-
-  if (!serialize_read_cstring(sa, &name, &name_len) ||
-      !serialize_read_cstring(sa, &value, &value_len))
-    goto error;
-
-  if (name_len != 0 && value_len != 0)
-    {
-      gsize sd_element_name_len = g_strlcpy(sd_param_name, sd_element_name, sizeof(sd_param_name));
-      if (sd_element_name_len >= sizeof(sd_param_name))
-        goto error;
-
-      g_strlcpy(sd_param_name + sd_element_name_len, name, sizeof(sd_param_name) - sd_element_name_len);
-      log_msg_set_value(self, log_msg_get_value_handle(sd_param_name), value, value_len);
-      *has_more = TRUE;
-    }
-  else
-    {
-      *has_more = FALSE;
-    }
-
-  success = TRUE;
-error:
-  g_free(name);
-  g_free(value);
-  return success;
-}
-
-static gboolean
 log_msg_read_sd_element(SerializeArchive *sa, LogMessage *self, gboolean *has_more)
 {
   gchar *sd_id = NULL;
@@ -429,7 +394,7 @@ log_msg_read_sd_element(SerializeArchive *sa, LogMessage *self, gboolean *has_mo
   sd_element_root[logmsg_sd_prefix_len + sd_id_len]='.';
 
 
-  if (!log_msg_read_sd_param_first(sa, sd_element_root, self, &has_more_param))
+  if (!log_msg_read_sd_param(sa, sd_element_root, self, &has_more_param))
     goto error;
 
   while (has_more_param)
