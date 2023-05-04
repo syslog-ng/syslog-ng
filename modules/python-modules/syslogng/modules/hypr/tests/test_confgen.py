bd46eab9952426b92d238a0ea19f6402a9b1d438
Attila Szakacs <attila.szakacs@axoflow.com>
2023-05-04 15:43:26 +0200

    python-modules/hypr: test more extensively in test_confgen.py
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- modules/python-modules/syslogng/modules/hypr/tests/test_confgen.py
+++ modules/python-modules/syslogng/modules/hypr/tests/test_confgen.py
@@ -1,5 +1,6 @@
 #############################################################################
 # Copyright (c) 2022 Balazs Scheidler <bazsi77@gmail.com>
+# Copyright (c) 2023 Attila Szakacs
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published
@@ -23,21 +24,93 @@ from syslogng.modules import hypr
 import base64
 
 
-def test_config_generator_generates_a_config_snippet_with_all_apps(mocker):
-    m = mocker.patch("requests.get", return_value=mocker.Mock(**{
+def test_config_generator_all_options(mocker):
+    mocker.patch("requests.get", return_value=mocker.Mock(**{
         'status_code': 200,
-        'json': mocker.Mock(return_value=[{"appID": "rp_foo"}, {"appID": "rp_bar"}])
+        'json': mocker.Mock(return_value=[
+            {"appID": "HYPRDefaultApplication"},
+            {"appID": "HYPRDefaultWorkstationApplication"},
+            {"appID": "rp_foo"},
+        ])
     }))
 
     bearer_token = b'bearer_token'
     base64_encoded_bearer_token = base64.b64encode(bearer_token).decode('utf8')
     hypr_args = {
-        'url': 'https://dummy.hypr.com/',
-        'bearer_token': base64_encoded_bearer_token,
+        'url': '"https://dummy.hypr.com/"',
+        'bearer_token': '"' + base64_encoded_bearer_token + '"',
     }
+
     snippet = hypr._hypr_config_generator(hypr_args)
-    assert 'syslogng.modules.hypr.HyprAuditSource' in snippet
-    assert '"url" => "https://dummy.hypr.com/"' in snippet
-    assert '"bearer_token" => "{}"'.format(base64_encoded_bearer_token) in snippet
-    assert '"rp_app_id" => "rp_foo"' in snippet
-    assert '"rp_app_id" => "rp_bar"' in snippet
+    expected_snippet = """
+        python-fetcher(
+            class("syslogng.modules.hypr.HyprAuditSource")
+            options(
+                "url" => "https://dummy.hypr.com/"
+                "rp_app_id" => "rp_foo"
+                "bearer_token" => "%s"
+                "page_size" => 100
+                "initial_hours" => 4
+            )
+            flags()
+            persist-name("hypr-https://dummy.hypr.com/-rp_foo")
+            fetch-no-data-delay(60)
+        );""" % base64_encoded_bearer_token
+
+    assert expected_snippet in snippet
+
+
+def test_config_generator_all_options(mocker):
+    mocker.patch("requests.get", return_value=mocker.Mock(**{
+        'status_code': 200,
+        'json': mocker.Mock(return_value=[{"appID": "rp_foo"}, {"appID": "rp_bar"}, {"appID": "rp_baz"}])
+    }))
+
+    bearer_token = b'bearer_token'
+    base64_encoded_bearer_token = base64.b64encode(bearer_token).decode('utf8')
+    hypr_args = {
+        'url': '"https://dummy.hypr.com/"',
+        'bearer_token': '"' + base64_encoded_bearer_token + '"',
+        'page_size': "123",
+        'initial_hours': "9",
+        'log_level': 'ERROR',
+        'application_skiplist': '"rp_baz", "rp_app"',
+        'persist_name': '"custom-persist-name"',
+        'fetch_no_data_delay': "42",
+        'flags': 'no-parse',
+    }
+
+    snippet = hypr._hypr_config_generator(hypr_args)
+    expected_snippets = [
+        """
+        python-fetcher(
+            class("syslogng.modules.hypr.HyprAuditSource")
+            options(
+                "url" => "https://dummy.hypr.com/"
+                "rp_app_id" => "rp_foo"
+                "bearer_token" => "%s"
+                "page_size" => 123
+                "initial_hours" => 9
+            )
+            flags(no-parse)
+            persist-name("custom-persist-name-rp_foo")
+            fetch-no-data-delay(42)
+        );""" % base64_encoded_bearer_token,
+        """
+        python-fetcher(
+            class("syslogng.modules.hypr.HyprAuditSource")
+            options(
+                "url" => "https://dummy.hypr.com/"
+                "rp_app_id" => "rp_bar"
+                "bearer_token" => "%s"
+                "page_size" => 123
+                "initial_hours" => 9
+            )
+            flags(no-parse)
+            persist-name("custom-persist-name-rp_bar")
+            fetch-no-data-delay(42)
+        );""" % base64_encoded_bearer_token,
+    ]
+
+    for expected_snippet in expected_snippets:
+        assert expected_snippet in snippet
