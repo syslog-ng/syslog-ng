533bedeebd667daeb154f273ccb22db4f0d95fbb
Hofi <hofione@gmail.com>
2023-05-12 13:57:25 +0200

    logthrdestdrv: Tiny logging enhancements
    Signed-off-by: Hofi <hofione@gmail.com>

--- lib/logthrdest/logthrdestdrv.c
+++ lib/logthrdest/logthrdestdrv.c
@@ -548,11 +548,14 @@ _perform_work(gpointer data)
        * everything.  We are awoken either by the
        * _message_became_available_callback() or if the next flush time has
        * arrived.  */
-      msg_trace("Queue empty, flushing previously buffered data",
+      gboolean should_flush = _should_flush_now(self);
+      msg_trace("Queue empty, flushing previously buffered data if needed",
+                evt_tag_str("should_flush", should_flush ? "YES" : "NO"),
                 evt_tag_str("driver", self->owner->super.super.id),
-                evt_tag_int("worker_index", self->worker_index));
+                evt_tag_int("worker_index", self->worker_index),
+                evt_tag_int("batch_size", self->batch_size));
 
-      if (_should_flush_now(self))
+      if (should_flush)
         _perform_flush(self);
       _schedule_restart_on_next_flush(self);
     }
