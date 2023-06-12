edccfe179fde4493dc5910e8d5117a71e572e930
Attila Szakacs <attila.szakacs@axoflow.com>
2023-06-12 09:12:37 +0200

    otel: add empty source files for otel-protobuf-formatter
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

---
+++ modules/grpc/otel/otel-protobuf-formatter.hpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 Attila Szakacs
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef OTEL_PROTOBUF_FORMATTER_HPP
+#define OTEL_PROTOBUF_FORMATTER_HPP
+
+#include "compat/cpp-start.h"
+#include "logmsg/logmsg.h"
+#include "compat/cpp-end.h"
+
+namespace syslogng {
+namespace grpc {
+namespace otel {
+
+}
+}
+}
+
+#endif
