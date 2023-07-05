61e19bbf1e3efc37b9c86921cb8e4191292e9b88
Alex Becker <beckeralex@protonmail.com>
2023-07-05 16:56:24 +0200

    transport: Add proxied socket logtransport base skeleton
    Signed-off-by: Alex Becker <beckeralex@protonmail.com>
    Signed-off-by: Hofi <hofione@gmail.com>

---
+++ lib/transport/transport-factory-proxied-socket.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2023 One Identity LLC.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef TRANSPORT_FACTORY_PROXIED_SOCKET_H_INCLUDED
+#define TRANSPORT_FACTORY_PROXIED_SOCKET_H_INCLUDED
+
+#include "transport/transport-factory.h"
+#include "transport/transport-proxied-socket.h"
+
+typedef struct _TransportFactoryProxiedSocket TransportFactoryProxiedSocket;
+
+struct _TransportFactoryProxiedSocket
+{
+  TransportFactory super;
+};
+
+TransportFactory *transport_factory_proxied_socket_new(gint sock_type);
+
+const TransportFactoryId *transport_factory_proxied_socket_id(void);
+
+#endif
