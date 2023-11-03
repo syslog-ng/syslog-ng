bfbf42d18c896f7816a1c0219ffe90b58217bf09
Balazs Scheidler <balazs.scheidler@axoflow.com>
2023-11-03 18:39:17 +0100

    filterx: initial skeleton for filterx expressions
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

---
+++ lib/filterx/filterx-globals.c
@@ -0,0 +1,11 @@
+#include "filterx/filterx-globals.h"
+
+void
+filterx_global_init(void)
+{
+}
+
+void
+filterx_global_deinit(void)
+{
+}
---
+++ lib/filterx/filterx-globals.h
@@ -0,0 +1,9 @@
+#ifndef FILTERX_GLOBALS_H_INCLUDED
+#define FILTERX_GLOBALS_H_INCLUDED
+
+#include "filterx-object.h"
+
+void filterx_global_init(void);
+void filterx_global_deinit(void);
+
+#endif
