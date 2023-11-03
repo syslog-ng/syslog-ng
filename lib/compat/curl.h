d49a6779a97d7286e5acfbc7a9d72272643b9bf0
Attila Szakacs <attila.szakacs@axoflow.com>
2023-11-03 15:03:30 +0100

    compat: add curl.h
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

---
+++ lib/compat/curl.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 Attila Szakacs
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef COMPAT_CURL_H_INCLUDED
+#define COMPAT_CURL_H_INCLUDED
+
+#include "compat/compat.h"
+#include <curl/curl.h>
+
+#ifndef CURL_VERSION_BITS
+#define CURL_VERSION_BITS(x,y,z) ((x)<<16|(y)<<8|(z))
+#endif
+
+#ifndef CURL_AT_LEAST_VERSION
+#define CURL_AT_LEAST_VERSION(x,y,z) \
+  (LIBCURL_VERSION_NUM >= CURL_VERSION_BITS(x, y, z))
+#endif
+
+#if !SYSLOG_NG_HAVE_DECL_CURLUE_LAST
+#define CURLUE_LAST (64)
+#endif
+
+#endif
