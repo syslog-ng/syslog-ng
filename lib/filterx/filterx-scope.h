58ae1e47d47a26f0c82363da77b2b325bb8e38fb
Balazs Scheidler <balazs.scheidler@axoflow.com>
2023-11-11 13:19:24 +0100

    filterx: add FilterXScope, the caching of unmarshalled values
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

---
+++ lib/filterx/filterx-scope.h
@@ -0,0 +1,15 @@
+#ifndef FILTERX_SCOPE_H_INCLUDED
+#define FILTERX_SCOPE_H_INCLUDED
+
+#include "filterx-object.h"
+#include "logmsg/logmsg.h"
+
+typedef struct _FilterXScope FilterXScope;
+
+void filterx_scope_sync_to_message(FilterXScope *self, LogMessage *msg);
+FilterXObject *filterx_scope_lookup_message_ref(FilterXScope *self, NVHandle handle);
+void filterx_scope_register_message_ref(FilterXScope *self, NVHandle handle, FilterXObject *value);
+FilterXScope *filterx_scope_new(void);
+void filterx_scope_free(FilterXScope *self);
+
+#endif
