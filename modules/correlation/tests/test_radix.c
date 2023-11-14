3543c6d5577ccef2cd6567f07dd4d3b4f7d67678
Romain Tartière <romain@blogreen.org>
2023-11-14 10:25:23 -1000

    correlation: Fix matching expression for NLSTRING
    `node_to_insert` seems to only contain a single patterndb expression in
    all other examples, so align with them.
    
    Signed-off-by: Romain Tartière <romain@blogreen.org>

--- modules/correlation/tests/test_radix.c
+++ modules/correlation/tests/test_radix.c
@@ -1046,22 +1046,22 @@ ParameterizedTestParameters(dbparser, test_radix_search_matches)
     },
     /* test_nlstring_matches */
     {
-      .node_to_insert = {"@NLSTRING:nlstring@\n", NULL},
+      .node_to_insert = {"@NLSTRING:nlstring@", NULL},
       .key = "foobar\r\nbaz",
       .expected_pattern = {"nlstring", "foobar", NULL},
     },
     {
-      .node_to_insert = {"@NLSTRING:nlstring@\n", NULL},
+      .node_to_insert = {"@NLSTRING:nlstring@", NULL},
       .key = "foobar\nbaz",
       .expected_pattern = {"nlstring", "foobar", NULL},
     },
     {
-      .node_to_insert = {"@NLSTRING:nlstring@\n", NULL},
+      .node_to_insert = {"@NLSTRING:nlstring@", NULL},
       .key = "\nbaz",
       .expected_pattern = {"nlstring", "", NULL},
     },
     {
-      .node_to_insert = {"@NLSTRING:nlstring@\n", NULL},
+      .node_to_insert = {"@NLSTRING:nlstring@", NULL},
       .key = "\r\nbaz",
       .expected_pattern = {"nlstring", "", NULL},
     }
