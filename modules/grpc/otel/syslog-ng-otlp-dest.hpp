2de381f119ab6efd2a27afc0757634fd805957b3
Attila Szakacs <attila.szakacs@axoflow.com>
2024-01-08 11:56:01 +0100

    syslog-ng-otlp: handle legacy persist names
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

--- modules/grpc/otel/syslog-ng-otlp-dest.hpp
+++ modules/grpc/otel/syslog-ng-otlp-dest.hpp
@@ -37,8 +37,13 @@ public:
 
   const char *format_stats_key(StatsClusterKeyBuilder *kb) override;
   const char *generate_persist_name() override;
+  bool init() override;
 
   LogThreadedDestWorker *construct_worker(int worker_index) override;
+
+private:
+  const char *generate_legacy_persist_name();
+  bool update_legacy_persist_name_if_exists();
 };
 
 }
