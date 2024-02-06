4208e259d49f09e126293e2bf3805b07e15182cf
shifter <shifter@axoflow.com>
2024-02-06 19:49:47 +0100

    filterx: introduce bytes getter
    Signed-off-by: shifter <shifter@axoflow.com>

--- lib/filterx/object-string.h
+++ lib/filterx/object-string.h
@@ -30,6 +30,7 @@ FILTERX_DECLARE_TYPE(bytes);
 FILTERX_DECLARE_TYPE(protobuf);
 
 const gchar *filterx_string_get_value(FilterXObject *s, gsize *length);
+const gchar *filterx_bytes_get_value(FilterXObject *s, gsize *length);
 
 FilterXObject *filterx_string_new(const gchar *str, gssize str_len);
 FilterXObject *filterx_bytes_new(const gchar *str, gssize str_len);
