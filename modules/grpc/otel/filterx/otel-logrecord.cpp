94e3067674fe9adb420126f2212688f6ddfaa91c
shifter <shifter@axoflow.com>
2024-02-22 16:37:25 +0100

    otel: filterx: update OtelLogRecordCPP and OtelConverter class to use otel-fields
    Signed-off-by: shifter <shifter@axoflow.com>

--- modules/grpc/otel/filterx/otel-logrecord.cpp
+++ modules/grpc/otel/filterx/otel-logrecord.cpp
@@ -22,6 +22,7 @@
 
 #include "otel-logrecord.hpp"
 #include "protobuf-field.hpp"
+#include "otel-field.hpp"
 
 #include "compat/cpp-start.h"
 
@@ -41,61 +42,6 @@
 using namespace syslogng::grpc::otel;
 
 /* C++ Implementations */
-class OtelConverter : public ProtobufField
-{
-public:
-  FilterXObject *FilterXObjectGetter(const google::protobuf::Message &message, ProtoReflectors reflectors)
-  {
-    const std::string &fieldName = reflectors.fieldDescriptor->name();
-    if (fieldName.compare("time_unix_nano") == 0 ||
-        fieldName.compare("observed_time_unix_nano") == 0)
-      {
-        uint64_t val = reflectors.reflection->GetUInt64(message, reflectors.fieldDescriptor);
-        UnixTime utime = unix_time_from_unix_epoch(val);
-        return filterx_datetime_new(&utime);
-      }
-
-    ProtobufField *converter = protobuf_converter_by_type(reflectors.fieldType);
-    if (converter)
-      {
-        return converter->Get(message, fieldName);
-      }
-    else
-      {
-        msg_error("field type not yet implemented", evt_tag_str("name", fieldName.c_str()), evt_tag_int("type",
-                  reflectors.fieldType));
-      }
-    return nullptr;
-  }
-  bool FilterXObjectSetter(google::protobuf::Message *message, ProtoReflectors reflectors, FilterXObject *object)
-  {
-    const std::string &fieldName = reflectors.fieldDescriptor->name();
-    if (fieldName.compare("time_unix_nano") == 0 ||
-        fieldName.compare("observed_time_unix_nano") == 0)
-      {
-        if (filterx_object_is_type(object, &FILTERX_TYPE_NAME(datetime)))
-          {
-            const UnixTime utime = filterx_datetime_get_value(object);
-            uint64_t unix_epoch = unix_time_to_unix_epoch(utime);
-            reflectors.reflection->SetUInt64(message, reflectors.fieldDescriptor, unix_epoch);
-            return true;
-          }
-      }
-    ProtobufField *converter = protobuf_converter_by_type(reflectors.fieldType);
-    if (converter)
-      {
-        return converter->Set(message, fieldName, object);
-      }
-    else
-      {
-        msg_error("field type not yet implemented", evt_tag_str("name", fieldName.c_str()), evt_tag_int("type",
-                  reflectors.fieldType));
-      }
-    return false;
-  }
-};
-
-static OtelConverter otel_converter;
 
 OtelLogRecordCpp::OtelLogRecordCpp(FilterXOtelLogRecord *folr) : super(folr)
 {
@@ -130,15 +76,38 @@ OtelLogRecordCpp::Marshal(void)
   return serializedString;
 }
 
-bool OtelLogRecordCpp::SetField(const gchar *attribute, FilterXObject *value)
+bool
+OtelLogRecordCpp::SetField(const gchar *attribute, FilterXObject *value)
 {
-  return otel_converter.Set(&this->logRecord, attribute, value);
+  try
+    {
+      ProtoReflectors reflectors(this->logRecord, attribute);
+      return otel_converter_by_field_descriptor(reflectors.fieldDescriptor)->Set(&this->logRecord, attribute, value);
+    }
+  catch(const std::exception &ex)
+    {
+      msg_error("protobuf-field: Failed to set field:",
+                evt_tag_str("message", ex.what()),
+                evt_tag_str("field_name", attribute));
+      return false;
+    }
 }
 
 FilterXObject *
 OtelLogRecordCpp::GetField(const gchar *attribute)
 {
-  return otel_converter.Get(this->logRecord, attribute);
+  try
+    {
+      ProtoReflectors reflectors(this->logRecord, attribute);
+      return otel_converter_by_field_descriptor(reflectors.fieldDescriptor)->Get(this->logRecord, attribute);
+    }
+  catch(const std::exception &ex)
+    {
+      msg_error("protobuf-field: Failed to get field:",
+                evt_tag_str("message", ex.what()),
+                evt_tag_str("field_name", attribute));
+      return nullptr;
+    }
 }
 
 const LogRecord &
@@ -247,9 +216,9 @@ FILTERX_DEFINE_TYPE(olr, FILTERX_TYPE_NAME(object),
                     .is_mutable = TRUE,
                     .marshal = _marshal,
                     .clone = _filterx_otel_logrecord_clone,
+                    // .map_to_json = _map_to_json,
                     .truthy = _truthy,
                     .getattr = _getattr,
                     .setattr = _setattr,
                     .free_fn = _free,
-                    // .map_to_json = _map_to_json,
                    );
