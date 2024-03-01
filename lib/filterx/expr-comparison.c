62ea0813616818d491537aa7b562b9d8d735af25
shifter <shifter@axoflow.com>
2024-03-01 13:04:52 +0100

    filterx: comparison ported from filter
    Signed-off-by: shifter <shifter@axoflow.com>

--- lib/filterx/expr-comparison.c
+++ lib/filterx/expr-comparison.c
@@ -20,14 +20,190 @@
  * COPYING for details.
  *
  */
+
 #include "filterx/expr-comparison.h"
+#include "filterx/object-datetime.h"
+#include "filterx/filterx-globals.h"
+#include "filterx/object-primitive.h"
+#include "filterx/object-null.h"
+#include "filterx/object-string.h"
+#include "filterx/object-json.h"
+#include "filterx/object-datetime.h"
+#include "filterx/object-message-value.h"
 #include "object-primitive.h"
+#include "generic-number.h"
+#include "parse-number.h"
+#include "scratch-buffers.h"
 
 typedef struct _FilterXComparison
 {
   FilterXBinaryOp super;
   gint operator;
 } FilterXComparison;
+
+static void
+convert_filterx_object_to_generic_number(FilterXObject *obj, GenericNumber *gn)
+{
+  if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(integer)) ||
+      filterx_object_is_type(obj, &FILTERX_TYPE_NAME(double)) ||
+      filterx_object_is_type(obj, &FILTERX_TYPE_NAME(boolean)))
+    *gn = filterx_primitive_get_value(obj);
+  else if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(string)))
+    {
+      if (!parse_generic_number(filterx_string_get_value(obj, NULL), gn))
+        gn_set_nan(gn);
+    }
+  else if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(null)))
+    gn_set_int64(gn, 0);
+  else if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(bytes)) ||
+           filterx_object_is_type(obj, &FILTERX_TYPE_NAME(protobuf)) ||
+           filterx_object_is_type(obj, &FILTERX_TYPE_NAME(message_value)) ||
+           filterx_object_is_type(obj, &FILTERX_TYPE_NAME(json)))
+    gn_set_nan(gn);
+  else if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(datetime)))
+    {
+      const UnixTime utime = filterx_datetime_get_value(obj);
+      uint64_t unix_epoch = unix_time_to_unix_epoch(utime);
+      gn_set_int64(gn, (uint64_t)unix_epoch);
+    }
+  else
+    {
+      gn_set_nan(gn);
+    }
+}
+
+static void
+convert_filterx_object_to_string(FilterXObject *obj, GString *dump)
+{
+  if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(string)))
+    {
+      gsize len;
+      const gchar *str = filterx_string_get_value(obj, &len);
+      g_string_assign(dump, str);
+    }
+  else if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(bytes)))
+    {
+      gsize len;
+      const gchar *str = filterx_bytes_get_value(obj, &len);
+      g_string_assign(dump, str);
+    }
+  else if (filterx_object_is_type(obj, &FILTERX_TYPE_NAME(protobuf)))
+    {
+      gsize len;
+      const gchar *str = filterx_protobuf_get_value(obj, &len);
+      g_string_assign(dump, str);
+    }
+  else
+    {
+      LogMessageValueType lmvt;
+      if (!filterx_object_marshal(obj, dump, &lmvt))
+        return;
+    }
+}
+
+static gboolean
+evaluate_comparison(gint cmp, gint operator)
+{
+  gboolean result = FALSE;
+  if (cmp == 0)
+    {
+      result = operator & FCMPX_EQ;
+    }
+  else if (cmp < 0)
+    {
+      result = !!(operator & FCMPX_LT);
+    }
+  else
+    {
+      result = !!(operator & FCMPX_GT);
+    }
+  return result;
+
+}
+
+static gboolean
+evaluate_comparison_gn(generic_number_cmp cmp, gint operator)
+{
+  if (cmp.is_nan)
+    return (operator & FCMPX_NE) == FCMPX_NE;
+  return evaluate_comparison(cmp.cmp, operator);
+}
+
+static gint
+cmp_as_string(FilterXObject *lhs, FilterXObject *rhs)
+{
+  gint result = 0;
+  GString *lhs_repr = scratch_buffers_alloc();
+  GString *rhs_repr = scratch_buffers_alloc();
+  convert_filterx_object_to_string(lhs, lhs_repr);
+  convert_filterx_object_to_string(rhs, rhs_repr);
+  if ((result = (lhs_repr->len - rhs_repr->len), result == 0))
+    result = memcmp(lhs_repr->str, rhs_repr->str, MIN(lhs_repr->len, rhs_repr->len));
+  return result;
+}
+
+static generic_number_cmp
+cmp_as_num(FilterXObject *lhs, FilterXObject *rhs)
+{
+  GenericNumber lhs_number, rhs_number;
+  convert_filterx_object_to_generic_number(lhs, &lhs_number);
+  convert_filterx_object_to_generic_number(rhs, &rhs_number);
+
+  if (gn_is_nan(&lhs_number) || gn_is_nan(&rhs_number))
+    {
+      return (generic_number_cmp)
+      {
+        .is_nan = TRUE
+      };
+    }
+  else
+    return (generic_number_cmp)
+    {
+      .cmp = gn_compare(&lhs_number, &rhs_number)
+    };
+}
+
+static gboolean
+evaluate_typed(FilterXObject *lhs, FilterXObject *rhs, gint operator)
+{
+  if (filterx_object_is_type(lhs, &FILTERX_TYPE_NAME(string)) ||
+      filterx_object_is_type(lhs, &FILTERX_TYPE_NAME(bytes)) ||
+      filterx_object_is_type(lhs, &FILTERX_TYPE_NAME(protobuf)) ||
+      filterx_object_is_type(lhs, &FILTERX_TYPE_NAME(message_value)) ||
+      filterx_object_is_type(lhs, &FILTERX_TYPE_NAME(json)))
+    return evaluate_comparison(cmp_as_string(lhs, rhs), operator);
+
+  if (filterx_object_is_type(lhs, &FILTERX_TYPE_NAME(null)) ||
+      filterx_object_is_type(rhs, &FILTERX_TYPE_NAME(null))
+     )
+    {
+      if ((operator & FCMPX_OP_MASK) == FCMPX_NE)
+        return lhs->type != rhs->type;
+      if ((operator & FCMPX_OP_MASK) == FCMPX_EQ)
+        return lhs->type == rhs->type;
+    }
+
+  return evaluate_comparison_gn(cmp_as_num(lhs, rhs), operator);
+}
+
+static gboolean
+evaluate_type_and_value(FilterXObject *lhs, FilterXObject *rhs, gint operator)
+{
+  if ((operator & FCMPX_OP_MASK) == FCMPX_EQ)
+    {
+      if (lhs->type != rhs->type)
+        return FALSE;
+    }
+  else if ((operator & FCMPX_OP_MASK) == FCMPX_NE)
+    {
+      if (lhs->type != rhs->type)
+        return TRUE;
+    }
+  else
+    g_assert_not_reached();
+  return evaluate_typed(lhs, rhs, operator);
+
+}
 
 static FilterXObject *
 _eval(FilterXExpr *s)
@@ -45,8 +221,21 @@ _eval(FilterXExpr *s)
       return NULL;
     }
 
-  /* FIXME: perform the comparison */
+  gint compare_mode = self->operator & FCMPX_MODE_MASK;
+  gint operator = self->operator & FCMPX_OP_MASK;
+
   gboolean result = TRUE;
+  if (compare_mode & FCMPX_TYPE_AWARE)
+    result = evaluate_typed(lhs_object, rhs_object, operator);
+  else if (compare_mode & FCMPX_STRING_BASED)
+    result = evaluate_comparison(cmp_as_string(lhs_object, rhs_object), operator);
+  else if (compare_mode & FCMPX_NUM_BASED)
+    result = evaluate_comparison_gn(cmp_as_num(lhs_object, rhs_object), operator);
+  else if (compare_mode & FCMPX_TYPE_AND_VALUE_BASED)
+    result = evaluate_type_and_value(lhs_object, rhs_object, operator);
+  else
+    g_assert_not_reached();
+
   filterx_object_unref(lhs_object);
   filterx_object_unref(rhs_object);
   return filterx_boolean_new(result);
--- lib/filterx/expr-comparison.h
+++ lib/filterx/expr-comparison.h
@@ -28,6 +28,7 @@
 #define FCMPX_EQ                   0x0001
 #define FCMPX_LT                   0x0002
 #define FCMPX_GT                   0x0004
+#define FCMPX_NE                   0x0006 // (FCMPX_LT + FCMPX_GT)
 #define FCMPX_TYPE_AWARE           0x0010
 #define FCMPX_STRING_BASED         0x0020
 #define FCMPX_NUM_BASED            0x0040
@@ -35,6 +36,12 @@
 
 #define FCMPX_OP_MASK      0x0007
 #define FCMPX_MODE_MASK    0x00F0
+
+typedef struct
+{
+  gint cmp;
+  gboolean is_nan;
+} generic_number_cmp;
 
 FilterXExpr *filterx_comparison_new(FilterXExpr *lhs, FilterXExpr *rhs, gint operator);
 
