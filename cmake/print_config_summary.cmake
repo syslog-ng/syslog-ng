080c8273b1c15e5376619350240bdd793d904f20
Hofi <hofione@gmail.com>
2024-04-03 22:15:57 +0200

    workflows: build env/parameters summary screens now show used compilers and their versions
    Signed-off-by: Hofi <hofione@gmail.com>

--- cmake/print_config_summary.cmake
+++ cmake/print_config_summary.cmake
@@ -21,29 +21,43 @@
 #
 # ############################################################################
 
-function(_print_summary_line _variableName _maxVarNameLen)
-  string(ASCII 27 Esc)
-  set(Red "${Esc}[31m")
-  set(Green "${Esc}[32m")
-  set(ResetFG "${Esc}[39m")
-  set(_variableValue "${${_variableName}}")
+string (ASCII 27 Esc)
+set (Red "${Esc}[31m")
+set (Green "${Esc}[32m")
+set (Yellow "${Esc}[33m")
+set (ResetFG "${Esc}[39m")
 
-  string(LENGTH "${_variableName}" _variableNameLen)
-  math(EXPR _variableNameLen "${_maxVarNameLen} - ${_variableNameLen}")
+function(_space_tabbed_string _variableName _maxVarNameLen _outputVariable)
+  set(_spaces "")
 
-  if(_variableNameLen LESS 0)
-    set(_variableNameLen 0)
+  if(_maxVarNameLen GREATER 0)
+    string(LENGTH "${_variableName}" _variableNameLen)
+    math(EXPR _variableNameLen "${_maxVarNameLen} - ${_variableNameLen}")
+
+    if(_variableNameLen LESS 0)
+      set(_variableNameLen 0)
+    endif()
+
+    string (REPEAT " " ${_variableNameLen} _spaces)
   endif()
 
-  string(REPEAT " " ${_variableNameLen} _spaces)
+  set(${_outputVariable} "${_variableName}${_spaces}" PARENT_SCOPE)
+  # return(PROPAGATE ${_outputVariable})
+endfunction()
+
+function(_print_summary_line _variableName _variableValue _maxVarNameLen)
+  set(_spaceTabbedVariableName "${_variableName}")
+
+  _space_tabbed_string("${_variableName}" ${_maxVarNameLen} _spaceTabbedVariableName)
+
   string(TOUPPER "${_variableValue}" _upperValue)
 
   if(_upperValue STREQUAL "ON" OR _upperValue STREQUAL "TRUE")
-    message("${_variableName}${_spaces}${Green}On${ResetFG}")
+    message("${_spaceTabbedVariableName}${Green}On${ResetFG}")
   elseif(_upperValue STREQUAL "OFF" OR _upperValue STREQUAL "FALSE")
-    message("${_variableName}${_spaces}${Red}Off${ResetFG}")
+    message("${_spaceTabbedVariableName}${Red}Off${ResetFG}")
   else()
-    message("${_variableName}${_spaces}${Green}${_variableValue}${ResetFG}")
+    message("${_spaceTabbedVariableName}${Green}${_variableValue}${ResetFG}")
   endif()
 endfunction()
 
@@ -58,7 +72,7 @@ function(_print_libraries _variableNames)
     string(REGEX MATCH "^(CMAKE_|_).*" _cmakeInternal "${_variableName}")
 
     if((_match_lib OR _match_incl) AND NOT _cmakeInternal)
-      _print_summary_line("${_variableName}" 40)
+      _print_summary_line("${_variableName}=" "${${_variableName}}" 0)
     endif()
   endforeach()
 endfunction()
@@ -69,7 +83,28 @@ function(_print_importants _variableNames _importantVariableNames)
       string(REGEX MATCH "^${_importantVariableName}$" _match "${_variableName}")
 
       if(_match)
-        _print_summary_line("${_importantVariableName}" 32)
+        string(REGEX MATCH ".*(_COMPILER)$" _isCompiler "${_variableName}")
+
+        if(_isCompiler)
+          set(_compilerVersion "")
+          get_filename_component (_executable_name "${${_variableName}}" NAME_WE)
+          execute_process (
+            COMMAND ${${_variableName}} --version
+            COMMAND grep -i ${_executable_name}
+            OUTPUT_VARIABLE _compilerVersion
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+          )
+          execute_process (
+            COMMAND which ${${_variableName}}
+            OUTPUT_VARIABLE _compilerPath
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+          )
+          _space_tabbed_string("${_variableName}" 32 _spaceTabbedVariableName)
+          _print_summary_line("${_spaceTabbedVariableName}${_executable_name}" "[${Yellow}${_compilerVersion}${Green}]${ResetFG} - ${_compilerPath}" 40)
+          unset(_compilerVersion)
+        else()
+          _print_summary_line("${_importantVariableName}" "${${_importantVariableName}}" 32)
+        endif()
       endif()
     endforeach()
   endforeach()
@@ -80,7 +115,7 @@ function(_print_options _variableNames)
     string(REGEX MATCH "^ENABLE_" _match "${_variableName}")
 
     if(_match)
-      _print_summary_line("${_variableName}" 32)
+      _print_summary_line("${_variableName}" "${${_variableName}}" 32)
     endif()
   endforeach()
 endfunction()
@@ -97,6 +132,7 @@ endfunction()
 function(print_config_summary)
   get_cmake_property(_variableNames VARIABLES)
   list(SORT _variableNames)
+  list (REMOVE_DUPLICATES _variableNames)
 
   _print_separator()
   message(NOTICE "syslog-ng Open Source Edition ${SYSLOG_NG_VERSION} configured")
@@ -110,7 +146,7 @@ function(print_config_summary)
       _print_separator()
     endif()
 
-    list(APPEND _importantVariableNames "IVYKIS_INTERNAL" "BUILD_TESTING")
+    list(APPEND _importantVariableNames "IVYKIS_INTERNAL" "BUILD_TESTING" "CMAKE_C_COMPILER" "CMAKE_CXX_COMPILER" "CMAKE_OBJC_COMPILER")
     if (APPLE)
       list(APPEND _importantVariableNames "FORCE_CLASSIC_LINKING")
     endif()
