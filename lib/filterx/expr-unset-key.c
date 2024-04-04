ca43d38c92cc4470a2c37764eb396f95a8558291
Attila Szakacs <attila.szakacs@axoflow.com>
2024-04-04 08:55:11 +0200

    filterx: add expr-unset-key
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>

---
+++ lib/filterx/expr-unset-key.c
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2024 Attila Szakacs
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "filterx/expr-unset-key.h"
+#include "filterx/object-primitive.h"
+
+typedef struct _FilterXUnsetKey
+{
+  FilterXExpr super;
+  FilterXExpr *operand;
+  FilterXExpr *key;
+} FilterXUnsetKey;
+
+static FilterXObject *
+_eval(FilterXExpr *s)
+{
+  FilterXUnsetKey *self = (FilterXUnsetKey *) s;
+
+  FilterXObject *variable = filterx_expr_eval_typed(self->operand);
+  if (!variable)
+    return NULL;
+
+  FilterXObject *key = filterx_expr_eval_typed(self->key);
+  if (!key)
+    {
+      filterx_object_unref(variable);
+      return NULL;
+    }
+
+  gboolean result = filterx_object_unset_key(variable, key);
+
+  filterx_object_unref(key);
+  filterx_object_unref(variable);
+  return filterx_boolean_new(result);
+}
+
+static void
+_free(FilterXExpr *s)
+{
+  FilterXUnsetKey *self = (FilterXUnsetKey *) s;
+  filterx_expr_unref(self->key);
+  filterx_expr_unref(self->operand);
+}
+
+/* NOTE: takes the object reference */
+FilterXExpr *
+filterx_unset_key_new(FilterXExpr *operand, FilterXExpr *key)
+{
+  FilterXUnsetKey *self = g_new0(FilterXUnsetKey, 1);
+
+  filterx_expr_init_instance(&self->super);
+  self->super.eval = _eval;
+  self->super.free_fn = _free;
+  self->operand = operand;
+  self->key = key;
+  return &self->super;
+}
