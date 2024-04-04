3500d4a7a391a81c7f315d5d149a652dd21cb3f0
shifter <shifter@axoflow.com>
2024-04-04 16:26:52 +0200

    filterx: protobuf typecast
    Signed-off-by: shifter <shifter@axoflow.com>

--- lib/filterx/filterx-globals.c
+++ lib/filterx/filterx-globals.c
@@ -72,6 +72,7 @@ filterx_builtin_functions_init(void)
   g_assert(filterx_builtin_function_register("isodate", filterx_typecast_datetime_isodate));
   g_assert(filterx_builtin_function_register("string", filterx_typecast_string));
   g_assert(filterx_builtin_function_register("bytes", filterx_typecast_bytes));
+  g_assert(filterx_builtin_function_register("protobuf", filterx_typecast_protobuf));
 }
 
 void
