49d121e00764828fc8092343fd4f840f429d52b6
Attila Szakacs <attila.szakacs@axoflow.com>
2024-09-03 15:43:59 +0200

    otel: derive DestDriver and DestWorker from grpc base classes
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/grpc/otel/otel-dest.cpp
+++ modules/grpc/otel/otel-dest.cpp
@@ -24,72 +24,9 @@
 #include "otel-dest.hpp"
 #include "otel-dest-worker.hpp"
 
-#define get_DestDriver(s) (((OtelDestDriver *) s)->cpp)
-
 using namespace syslogng::grpc::otel;
 
 /* C++ Implementations */
-
-DestDriver::DestDriver(OtelDestDriver *s)
-  : super(s), compression(false), batch_bytes(4 * 1000 * 1000)
-{
-  credentials_builder_wrapper.self = &credentials_builder;
-}
-
-void
-DestDriver::set_url(const char *url_)
-{
-  url.assign(url_);
-}
-
-const std::string &
-DestDriver::get_url() const
-{
-  return url;
-}
-
-void
-DestDriver::set_compression(bool compression_)
-{
-  compression = compression_;
-}
-
-bool
-DestDriver::get_compression() const
-{
-  return compression;
-}
-
-void
-DestDriver::set_batch_bytes(size_t batch_bytes_)
-{
-  batch_bytes = batch_bytes_;
-}
-
-size_t
-DestDriver::get_batch_bytes() const
-{
-  return batch_bytes;
-}
-
-void
-DestDriver::add_extra_channel_arg(std::string name, long value)
-{
-  int_extra_channel_args.push_back(std::pair<std::string, long> {name, value});
-}
-
-void
-DestDriver::add_extra_channel_arg(std::string name, std::string value)
-{
-  string_extra_channel_args.push_back(std::pair<std::string, std::string> {name, value});
-}
-
-void
-DestDriver::add_header(std::string name, std::string value)
-{
-  std::transform(name.begin(), name.end(), name.begin(), ::tolower);
-  headers.push_back(std::pair<std::string, std::string> {name, value});
-}
 
 const char *
 DestDriver::generate_persist_name()
@@ -118,153 +55,15 @@ DestDriver::format_stats_key(StatsClusterKeyBuilder *kb)
 LogThreadedDestWorker *
 DestDriver::construct_worker(int worker_index)
 {
-  return DestWorker::construct(&super->super, worker_index);
-}
-
-bool
-DestDriver::init()
-{
-  if (url.length() == 0)
-    {
-      msg_error("OpenTelemetry: url() option is mandatory",
-                log_pipe_location_tag(&super->super.super.super.super));
-      return false;
-    }
-
-  if (!credentials_builder.validate())
-    {
-      return false;
-    }
-
-  if (!log_threaded_dest_driver_init_method(&this->super->super.super.super.super))
-    return false;
-
-  log_threaded_dest_driver_register_aggregated_stats(&this->super->super);
-
-  StatsClusterKeyBuilder *kb = stats_cluster_key_builder_new();
-  format_stats_key(kb);
-  metrics.init(kb, log_pipe_is_internal(&super->super.super.super.super) ? STATS_LEVEL3 : STATS_LEVEL1);
-
-  return true;
-}
-
-bool
-DestDriver::deinit()
-{
-  metrics.deinit();
-  return log_threaded_dest_driver_deinit_method(&super->super.super.super.super);
-}
-
-GrpcClientCredentialsBuilderW *
-DestDriver::get_credentials_builder_wrapper()
-{
-  return &credentials_builder_wrapper;
-}
-
-/* C Wrappers */
-
-void
-otel_dd_set_url(LogDriver *s, const gchar *url)
-{
-  get_DestDriver(s)->set_url(url);
-}
-
-void
-otel_dd_set_compression(LogDriver *s, gboolean enable)
-{
-  get_DestDriver(s)->set_compression(enable);
-}
-
-void
-otel_dd_set_batch_bytes(LogDriver *s, glong b)
-{
-  get_DestDriver(s)->set_batch_bytes((size_t) b);
-}
-
-void
-otel_dd_add_int_channel_arg(LogDriver *s, const gchar *name, glong value)
-{
-  get_DestDriver(s)->add_extra_channel_arg(name, value);
-}
-
-void
-otel_dd_add_string_channel_arg(LogDriver *s, const gchar *name, const gchar *value)
-{
-  get_DestDriver(s)->add_extra_channel_arg(name, value);
-}
-
-void
-otel_dd_add_header(LogDriver *s, const gchar *name, const gchar *value)
-{
-  get_DestDriver(s)->add_header(name, value);
-}
-
-GrpcClientCredentialsBuilderW *
-otel_dd_get_credentials_builder(LogDriver *s)
-{
-  return get_DestDriver(s)->get_credentials_builder_wrapper();
-}
-
-static const gchar *
-_format_stats_key(LogThreadedDestDriver *s, StatsClusterKeyBuilder *kb)
-{
-  return get_DestDriver(s)->format_stats_key(kb);
-}
-
-static const gchar *
-_generate_persist_name(const LogPipe *s)
-{
-  return get_DestDriver(s)->generate_persist_name();
-}
-
-static gboolean
-_init(LogPipe *s)
-{
-  return get_DestDriver(s)->init();
-}
-
-static gboolean
-_deinit(LogPipe *s)
-{
-  return get_DestDriver(s)->deinit();
-}
-
-static LogThreadedDestWorker *
-_construct_worker(LogThreadedDestDriver *s, gint worker_index)
-{
-  return get_DestDriver(s)->construct_worker(worker_index);
-}
-
-static void
-_free(LogPipe *s)
-{
-  delete get_DestDriver(s);
-  log_threaded_dest_driver_free(s);
-}
-
-void
-otel_dd_init_super(LogThreadedDestDriver *s, GlobalConfig *cfg)
-{
-  log_threaded_dest_driver_init_instance(s, cfg);
-
-  s->super.super.super.init = _init;
-  s->super.super.super.deinit = _deinit;
-  s->super.super.super.free_fn = _free;
-  s->super.super.super.generate_persist_name = _generate_persist_name;
-
-  s->worker.construct = _construct_worker;
-  s->stats_source = stats_register_type("opentelemetry");
-  s->format_stats_key = _format_stats_key;
-  s->metrics.raw_bytes_enabled = TRUE;
+  GrpcDestWorker *worker = grpc_dw_new(this->super, worker_index);
+  worker->cpp = new DestWorker(worker);
+  return &worker->super;
 }
 
 LogDriver *
 otel_dd_new(GlobalConfig *cfg)
 {
-  OtelDestDriver *self = g_new0(OtelDestDriver, 1);
-
-  otel_dd_init_super(&self->super, cfg);
+  GrpcDestDriver *self = grpc_dd_new(cfg, "opentelemetry");
   self->cpp = new DestDriver(self);
-
   return &self->super.super.super;
 }
--- modules/grpc/otel/otel-dest.hpp
+++ modules/grpc/otel/otel-dest.hpp
@@ -30,8 +30,7 @@
 #include "logthrdest/logthrdestdrv.h"
 #include "compat/cpp-end.h"
 
-#include "credentials/grpc-credentials-builder.hpp"
-#include "metrics/grpc-metrics.hpp"
+#include "grpc-dest.hpp"
 
 #include <grpcpp/server.h>
 
@@ -41,60 +40,21 @@ namespace syslogng {
 namespace grpc {
 namespace otel {
 
-class DestDriver
+class DestDriver: public syslogng::grpc::DestDriver
 {
 public:
-  DestDriver(OtelDestDriver *s);
-  virtual ~DestDriver() {};
+  DestDriver(GrpcDestDriver *s) : syslogng::grpc::DestDriver(s) {};
 
-  void set_url(const char *url);
-  const std::string &get_url() const;
-
-  void set_compression(bool enable);
-  bool get_compression() const;
-
-  void set_batch_bytes(size_t bytes);
-  size_t get_batch_bytes() const;
-
-  void add_extra_channel_arg(std::string name, long value);
-  void add_extra_channel_arg(std::string name, std::string value);
-
-  void add_header(std::string name, std::string value);
-
-  virtual bool init();
-  virtual bool deinit();
-  virtual const char *format_stats_key(StatsClusterKeyBuilder *kb);
-  virtual const char *generate_persist_name();
-  virtual LogThreadedDestWorker *construct_worker(int worker_index);
-
-  GrpcClientCredentialsBuilderW *get_credentials_builder_wrapper();
-
-public:
-  syslogng::grpc::ClientCredentialsBuilder credentials_builder;
+  const char *format_stats_key(StatsClusterKeyBuilder *kb);
+  const char *generate_persist_name();
+  LogThreadedDestWorker *construct_worker(int worker_index);
 
 protected:
   friend class DestWorker;
-  OtelDestDriver *super;
-  std::string url;
-  bool compression;
-  size_t batch_bytes;
-  std::list<std::pair<std::string, long>> int_extra_channel_args;
-  std::list<std::pair<std::string, std::string>> string_extra_channel_args;
-  std::list<std::pair<std::string, std::string>> headers;
-  GrpcClientCredentialsBuilderW credentials_builder_wrapper;
-  DestDriverMetrics metrics;
 };
 
 }
 }
 }
-
-struct OtelDestDriver_
-{
-  LogThreadedDestDriver super;
-  syslogng::grpc::otel::DestDriver *cpp;
-};
-
-void otel_dd_init_super(LogThreadedDestDriver *s, GlobalConfig *cfg);
 
 #endif
