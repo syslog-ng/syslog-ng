a0f7674535e34b382648a82f090f7d8745a6ad6d
Attila Szakacs <attila.szakacs@axoflow.com>
2024-09-03 21:49:04 +0200

    loki: derive DestinationDriver and DestinationWorker from grpc base classes
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/grpc/loki/loki-dest.h
+++ modules/grpc/loki/loki-dest.h
@@ -33,26 +33,12 @@
 #include "template/templates.h"
 #include "credentials/grpc-credentials-builder.h"
 
-typedef struct _LokiDestDriver LokiDestDriver;
-
 LogDriver *loki_dd_new(GlobalConfig *cfg);
 
-void loki_dd_set_url(LogDriver *d, const gchar *url);
 void loki_dd_set_message_template_ref(LogDriver *d, LogTemplate *message);
 void loki_dd_add_label(LogDriver *d, const gchar *name, LogTemplate *value);
 gboolean loki_dd_set_timestamp(LogDriver *d, const gchar *t);
 void loki_dd_set_tenant_id(LogDriver *d, const gchar *tid);
-
-GrpcClientCredentialsBuilderW *loki_dd_get_credentials_builder(LogDriver *s);
-
-void loki_dd_set_keepalive_time(LogDriver *d, gint t);
-void loki_dd_set_keepalive_timeout(LogDriver *d, gint t);
-void loki_dd_set_keepalive_max_pings(LogDriver *d, gint p);
-
-void loki_dd_add_int_channel_arg(LogDriver *s, const gchar *name, glong value);
-void loki_dd_add_string_channel_arg(LogDriver *s, const gchar *name, const gchar *value);
-
-void loki_dd_add_header(LogDriver *s, const gchar *name, const gchar *value);
 
 LogTemplateOptions *loki_dd_get_template_options(LogDriver *d);
 
