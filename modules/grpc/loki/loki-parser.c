f758f6d3d8f48bb2ec34fcf97f85ed7dc7c2fe43
Attila Szakacs <attila.szakacs@axoflow.com>
2024-09-05 11:07:50 +0200

    loki: support batch_bytes() and add size related metrics
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/grpc/loki/loki-parser.c
+++ modules/grpc/loki/loki-parser.c
@@ -50,6 +50,7 @@ static CfgLexerKeyword loki_keywords[] =
   { "target_service_accounts", KW_TARGET_SERVICE_ACCOUNTS },
   { "adc", KW_ADC },
   { "tenant_id", KW_TENANT_ID },
+  { "batch_bytes", KW_BATCH_BYTES},
   { "channel_args", KW_CHANNEL_ARGS },
   { "headers", KW_HEADERS },
   { NULL }
