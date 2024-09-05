ffa8874ecf4a533aba690c6c22b537c2b47fefaf
Attila Szakacs <attila.szakacs@axoflow.com>
2024-09-05 11:18:47 +0200

    grpc: factor out common grpc keywords for parsers
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/grpc/otel/otel-parser.c
+++ modules/grpc/otel/otel-parser.c
@@ -24,6 +24,7 @@
 #include "driver.h"
 #include "cfg-parser.h"
 #include "otel-grammar.h"
+#include "grpc-parser.h"
 
 extern int otel_debug;
 
@@ -31,34 +32,10 @@ int otel_parse(CfgLexer *lexer, void **instance, gpointer arg);
 
 static CfgLexerKeyword otel_keywords[] =
 {
+  GRPC_KEYWORDS,
   { "opentelemetry",             KW_OPENTELEMETRY },
-  { "port",                      KW_PORT },
-  { "auth",                      KW_AUTH },
-  { "insecure",                  KW_INSECURE },
-  { "tls",                       KW_TLS },
-  { "key_file",                  KW_KEY_FILE },
-  { "cert_file",                 KW_CERT_FILE },
-  { "ca_file",                   KW_CA_FILE },
-  { "peer_verify",               KW_PEER_VERIFY },
-  { "optional_untrusted",        KW_OPTIONAL_UNTRUSTED },
-  { "optional_trusted",          KW_OPTIONAL_TRUSTED },
-  { "required_untrusted",        KW_REQUIRED_UNTRUSTED },
-  { "required_trusted",          KW_REQUIRED_TRUSTED },
-  { "alts",                      KW_ALTS },
-  { "url",                       KW_URL },
-  { "target_service_accounts",   KW_TARGET_SERVICE_ACCOUNTS },
-  { "adc",                       KW_ADC },
   { "syslog_ng_otlp",            KW_SYSLOG_NG_OTLP },
-  { "compression",               KW_COMPRESSION },
-  { "batch_bytes",               KW_BATCH_BYTES },
-  { "concurrent_requests",       KW_CONCURRENT_REQUESTS },
-  { "channel_args",              KW_CHANNEL_ARGS },
-  { "headers",                   KW_HEADERS },
   { "set_hostname",              KW_SET_HOSTNAME },
-  { "keep_alive",                KW_KEEP_ALIVE },
-  { "time",                      KW_TIME },
-  { "timeout",                   KW_TIMEOUT },
-  { "max_pings_without_data",    KW_MAX_PINGS_WITHOUT_DATA },
   { NULL }
 };
 
