28c0b47051c0eb3481043f607b430fc5bcee95b5
Attila Szakacs <attila.szakacs@axoflow.com>
2024-09-06 13:14:42 +0200

    otel: derive SourceDriver and SourceWorker from grpc base classes
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/grpc/otel/otel-source.hpp
+++ modules/grpc/otel/otel-source.hpp
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2023 Attila Szakacs
+ * Copyright (c) 2024 Axoflow
+ * Copyright (c) 2023-2024 Attila Szakacs <attila.szakacs@axoflow.com>
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 as published
@@ -25,70 +26,46 @@
 
 #include "otel-source.h"
 
-#include "compat/cpp-start.h"
-#include "logthrsource/logthrsourcedrv.h"
-#include "compat/cpp-end.h"
-
+#include "grpc-source.hpp"
+#include "grpc-source-worker.hpp"
 #include "otel-servicecall.hpp"
-#include "credentials/grpc-credentials-builder.hpp"
 
-#include <grpcpp/server.h>
-
-#include <list>
 
 namespace syslogng {
 namespace grpc {
 namespace otel {
 
-class SourceWorker;
-
-class SourceDriver
+class SourceDriver : public syslogng::grpc::SourceDriver
 {
 public:
-  SourceDriver(OtelSourceDriver *s);
+  SourceDriver(GrpcSourceDriver *s);
 
+  gboolean init() override;
+  gboolean deinit() override;
   void request_exit();
   void format_stats_key(StatsClusterKeyBuilder *kb);
   const char *generate_persist_name();
-  gboolean init();
-  gboolean deinit();
-
-  void add_extra_channel_arg(std::string name, long value);
-  void add_extra_channel_arg(std::string name, std::string value);
-  GrpcServerCredentialsBuilderW *get_credentials_builder_wrapper();
+  LogThreadedSourceWorker *construct_worker(int worker_index);
 
   std::unique_ptr<TraceService::AsyncService> trace_service;
   std::unique_ptr<LogsService::AsyncService> logs_service;
   std::unique_ptr<MetricsService::AsyncService> metrics_service;
 
 private:
-  friend SourceWorker;
-
-public:
-  guint64 port = 4317;
-  int fetch_limit = -1;
-  int concurrent_requests = 2;
-  syslogng::grpc::ServerCredentialsBuilder credentials_builder;
-  std::list<std::pair<std::string, long>> int_extra_channel_args;
-  std::list<std::pair<std::string, std::string>> string_extra_channel_args;
+  friend class SourceWorker;
 
 private:
-  OtelSourceDriver *super;
-  GrpcServerCredentialsBuilderW credentials_builder_wrapper;
   std::unique_ptr<::grpc::Server> server;
   std::list<std::unique_ptr<::grpc::ServerCompletionQueue>> cqs;
 };
 
-class SourceWorker
+class SourceWorker : public syslogng::grpc::SourceWorker
 {
 public:
-  SourceWorker(OtelSourceWorker *s, SourceDriver &d);
+  SourceWorker(GrpcSourceWorker *s, syslogng::grpc::SourceDriver &d);
 
   void run();
   void request_exit();
-
-private:
-  void post(LogMessage *msg);
 
 private:
   friend TraceServiceCall;
@@ -96,8 +73,6 @@ private:
   friend MetricsServiceCall;
 
 private:
-  OtelSourceWorker *super;
-  SourceDriver &driver;
   std::unique_ptr<::grpc::ServerCompletionQueue> cq;
 };
 
@@ -105,16 +80,6 @@ private:
 }
 }
 
-struct OtelSourceWorker_
-{
-  LogThreadedSourceWorker super;
-  syslogng::grpc::otel::SourceWorker *cpp;
-};
-
-struct OtelSourceDriver_
-{
-  LogThreadedSourceDriver super;
-  syslogng::grpc::otel::SourceDriver *cpp;
-};
+syslogng::grpc::otel::SourceDriver *otel_sd_get_cpp(GrpcSourceDriver *self);
 
 #endif
