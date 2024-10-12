9f992ec6bd329d7d271ecf748ae3b62e6ed82691
Attila Szakacs <attila.szakacs@axoflow.com>
2024-10-12 14:39:12 +0200

    grpc: add boilerplate for common Schema class
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

---
+++ modules/grpc/common/schema/grpc-schema.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024 Axoflow
+ * Copyright (c) 2024 Attila Szakacs <attila.szakacs@axoflow.com>
+ * Copyright (c) 2023 László Várady
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef GRPC_SCHEMA_HPP
+#define GRPC_SCHEMA_HPP
+
+#include "syslog-ng.h"
+
+namespace syslogng {
+namespace grpc {
+
+class Schema
+{
+};
+
+}
+}
+
+#endif
