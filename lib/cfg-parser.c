534fb9d1290eab47e77d08effbdb71198e5a743d
Balazs Scheidler <balazs.scheidler@axoflow.com>
2024-10-22 14:10:39 +0200

    cfg-grammar: make idle-timeout configurable for LogReader based sources
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- lib/cfg-parser.c
+++ lib/cfg-parser.c
@@ -150,6 +150,7 @@ static CfgLexerKeyword main_keywords[] =
   { "log_iw_size",        KW_LOG_IW_SIZE },
   { "log_msg_size",       KW_LOG_MSG_SIZE },
   { "trim_large_messages", KW_TRIM_LARGE_MESSAGES },
+  { "idle_timeout",       KW_IDLE_TIMEOUT },
   { "log_prefix",         KW_LOG_PREFIX, KWS_OBSOLETE, "program_override" },
   { "program_override",   KW_PROGRAM_OVERRIDE },
   { "host_override",      KW_HOST_OVERRIDE },
