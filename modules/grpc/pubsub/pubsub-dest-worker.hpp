4a501e0ddf738b80779f122203b20c599c369aa2
Attila Szakacs <attila.szakacs@axoflow.com>
2024-11-10 11:35:26 +0100

    grpc/pubsub: add boilerplates
    Signed-off-by: Attila Szakacs <attila.szakacs@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

---
+++ modules/grpc/pubsub/pubsub-dest-worker.hpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2024 Axoflow
+ * Copyright (c) 2024 Attila Szakacs <attila.szakacs@axoflow.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef PUBSUB_DEST_WORKER_HPP
+#define PUBSUB_DEST_WORKER_HPP
+
+#include "pubsub-dest.hpp"
+#include "grpc-dest-worker.hpp"
+
+namespace syslogng {
+namespace grpc {
+namespace pubsub {
+
+class DestWorker final : public syslogng::grpc::DestWorker
+{
+public:
+  DestWorker(GrpcDestWorker *s);
+
+  LogThreadedResult insert(LogMessage *msg);
+  LogThreadedResult flush(LogThreadedFlushMode mode);
+
+private:
+  DestDriver *get_owner();
+};
+
+}
+}
+}
+
+#endif
---
+++ modules/grpc/pubsub/pubsub-dest.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 Axoflow
+ * Copyright (c) 2024 Attila Szakacs <attila.szakacs@axoflow.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#ifndef PUBSUB_DEST_H
+#define PUBSUB_DEST_H
+
+#include "syslog-ng.h"
+
+#include "compat/cpp-start.h"
+#include "driver.h"
+#include "template/templates.h"
+
+LogDriver *pubsub_dd_new(GlobalConfig *cfg);
+
+#include "compat/cpp-end.h"
+
+#endif
