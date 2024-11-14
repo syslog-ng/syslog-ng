f9c3c5e13fbc96775fc1ab70a539d95474bff266
shifter <shifter@axoflow.com>
2024-11-14 11:56:23 +0100

    syslog-format: add implementation of check_program
    Signed-off-by: shifter <shifter@axoflow.com>

--- modules/syslogformat/syslog-format.c
+++ modules/syslogformat/syslog-format.c
@@ -329,6 +329,24 @@ _syslog_format_parse_version(LogMessage *msg, const guchar **data, gint *length)
   return TRUE;
 }
 
+static const gchar program_name_allowed_specials[] = ".-_()/";
+static gsize program_name_allowed_spacial_chars_len = G_N_ELEMENTS(program_name_allowed_specials) - 1;
+
+static inline gboolean
+_validate_program_char(const guchar ch, gboolean *has_alpha)
+{
+  if (isalpha(ch))
+    {
+      *has_alpha = TRUE;
+      return TRUE;
+    }
+  if (isdigit(ch))
+    return TRUE;
+  if (memchr(program_name_allowed_specials, ch, program_name_allowed_spacial_chars_len))
+    return TRUE;
+  return FALSE;
+}
+
 static void
 _syslog_format_parse_legacy_program_name(LogMessage *msg, const guchar **data, gint *length, guint flags)
 {
@@ -339,10 +357,24 @@ _syslog_format_parse_legacy_program_name(LogMessage *msg, const guchar **data, g
   src = *data;
   left = *length;
   prog_start = src;
+  gboolean has_alpha_char = FALSE;
+
   while (left && *src != ' ' && *src != '[' && *src != ':')
     {
+      if (G_UNLIKELY(flags & LP_CHECK_PROGRAM) && !_validate_program_char(*src, &has_alpha_char))
+        {
+          log_msg_set_tag_by_id(msg, LM_T_SYSLOG_RFC_3164_INVALID_PROGRAM);
+          return;
+        }
       _skip_char(&src, &left);
     }
+
+  if (G_UNLIKELY(flags & LP_CHECK_PROGRAM) && !has_alpha_char)
+    {
+      log_msg_set_tag_by_id(msg, LM_T_SYSLOG_RFC_3164_INVALID_PROGRAM);
+      return;
+    }
+
   log_msg_set_value(msg, LM_V_PROGRAM, (gchar *) prog_start, src - prog_start);
   if (left > 0 && *src == '[')
     {
