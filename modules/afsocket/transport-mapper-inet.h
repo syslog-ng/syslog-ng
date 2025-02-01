61f898983d062bbb84794838bf77d5e36244d76a
Balazs Scheidler <balazs.scheidler@axoflow.com>
2025-02-01 13:39:58 +0100

    afsocket: refactor TransportMapperInet transport setup logic
    This reworks the various boolean members in TransportMapperInet that
    control which logproto/transport we apply to a specific connection.
    
    With these renames, it's much easier to follow what happens and why.
    
    NOTE: there's a followup bugfix that fixes the same bug as #482.
    
    Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/afsocket/transport-mapper-inet.h
+++ modules/afsocket/transport-mapper-inet.h
@@ -32,10 +32,31 @@ typedef struct _TransportMapperInet
 
   gint server_port;
   const gchar *server_port_change_warning;
-  gboolean require_tls;
-  gboolean allow_tls;
-  gboolean require_tls_when_has_tls_context;
+  /* tls() options are required */
+  gboolean require_tls_configuration;
+  /* tls() options are optional, but are permitted */
+  gboolean allow_tls_configuration;
+
+  /* tls() options may be specified, but it is up to the transport/logproto
+   * plugin to start TLS.
+   *
+   * If this is TRUE, TLS encapsulation is started by either the
+   * LogTransport or the LogProtoServer instances with an explicit call to
+   * log_transport_stack_switch(LOG_TRANSPORT_TLS).  For example, this
+   * mechanism is used by LogProtoAutoServer (when TLS is detected), by
+   * LogTransportHAProxy or any potential LogProto implementations that have
+   * an explicit STARTTLS command (e.g. ALTP, RLTP).
+   *
+   * If this is FALSE, TLS will be started before LogProtoServer has any
+   * chance to read data,
+   */
+
+  gboolean delegate_tls_start_to_logproto;
+
+  /* HAProxy v1 or v2 protocol is to be used */
   gboolean proxied;
+  /* switch to TLS after plaintext haproxy negotiation */
+  gboolean proxied_passthrough;
   TLSContext *tls_context;
   TLSVerifier *tls_verifier;
   gpointer secret_store_cb_data;
