c4ebf1bee11402689165e245a8b12101677a23ab
Hofi <hofione@gmail.com>
2025-03-05 00:26:25 +0100

    multiline: add empty line separated (aka. double new-lined) multiline
    Signed-off-by: Hofi <hofione@gmail.com>

---
+++ lib/multi-line/empty-line-separated-multi-line.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 One Identity
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ */
+
+#ifndef MULTI_LINE_EMPTY_LINE_SEP_MULTI_LINE_H_INCLUDED
+#define MULTI_LINE_EMPTY_LINE_SEP_MULTI_LINE_H_INCLUDED
+
+#include "multi-line/multi-line-logic.h"
+
+MultiLineLogic *empty_line_separated_multi_line_new(void);
+
+#endif
