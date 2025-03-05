30c4bcc158e20be174fe205553fa1d493fe0900d
Hofi <hofione@gmail.com>
2025-03-05 18:42:21 +0100

    logproto_http_server: add transport mapper and handling of auto creation of http based transform
    Signed-off-by: Hofi <hofione@gmail.com>

--- lib/logproto/logproto-builtins.c
+++ lib/logproto/logproto-builtins.c
@@ -26,6 +26,7 @@
 #include "logproto-text-server.h"
 #include "logproto-framed-client.h"
 #include "logproto-framed-server.h"
+#include "logproto/logproto-http-server.h"
 #include "plugin.h"
 #include "plugin-types.h"
 
@@ -37,6 +38,7 @@ DEFINE_LOG_PROTO_SERVER(log_proto_dgram);
 DEFINE_LOG_PROTO_CLIENT(log_proto_text);
 DEFINE_LOG_PROTO_SERVER(log_proto_text);
 DEFINE_LOG_PROTO_SERVER(log_proto_text_with_nuls);
+DEFINE_LOG_PROTO_SERVER(log_proto_http);
 DEFINE_LOG_PROTO_CLIENT(log_proto_framed);
 DEFINE_LOG_PROTO_SERVER(log_proto_framed);
 
@@ -48,6 +50,7 @@ static Plugin framed_server_plugins[] =
   LOG_PROTO_CLIENT_PLUGIN(log_proto_text, "text"),
   LOG_PROTO_SERVER_PLUGIN(log_proto_text, "text"),
   LOG_PROTO_SERVER_PLUGIN(log_proto_text_with_nuls, "text-with-nuls"),
+  LOG_PROTO_SERVER_PLUGIN(log_proto_http, "http"),
   LOG_PROTO_CLIENT_PLUGIN(log_proto_framed, "framed"),
   LOG_PROTO_SERVER_PLUGIN(log_proto_framed, "framed"),
 };
