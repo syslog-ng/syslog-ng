d122bd2ff7a9fa8c23fa3a6110504a7159baa1ad
Hofi <hofione@gmail.com>
2025-03-11 16:00:14 +0100

    logproto-http-server: added close_after_send option
    Signed-off-by: Hofi <hofione@gmail.com>

--- lib/logproto/logproto-http-server.h
+++ lib/logproto/logproto-http-server.h
@@ -30,6 +30,7 @@ typedef struct _LogProtoHTTPServerOptions
 {
   LogProtoServerOptions super; // This must be the first !!!
   gboolean initialized;
+  gboolean close_after_send;
 
 } LogProtoHTTPServerOptions;
 
@@ -58,6 +59,9 @@ struct _LogProtoHTTPServer
 void log_proto_http_server_options_defaults(LogProtoHTTPServerOptionsStorage *options);
 void log_proto_http_server_options_init(LogProtoHTTPServerOptionsStorage *options, GlobalConfig *cfg);
 void log_proto_http_server_destroy(LogProtoHTTPServerOptionsStorage *options);
+void log_proto_http_server_options_set_close_after_send(
+  LogProtoHTTPServerOptionsStorage *options,
+  gboolean value);
 
 LogProtoServer *log_proto_http_server_new(LogTransport *transport,
                                           const LogProtoServerOptionsStorage *options);
