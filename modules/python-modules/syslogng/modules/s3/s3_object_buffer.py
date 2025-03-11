53e92c01c09ae15cf9b53932929f06227d6554f7
Bálint Horváth <bal.horv.98@gmail.com>
2025-03-11 17:13:18 +0100

    python/s3-dd: Introducing new helper classes
    These introduce simpler, easier to maintain replacements to the S3Object class. The new classes also help with separation of concern.
    
    Signed-off-by: Bálint Horváth <bal.horv.98@gmail.com>

---
+++ modules/python-modules/syslogng/modules/s3/s3_object_buffer.py
@@ -0,0 +1,174 @@
+#############################################################################
+# Copyright (c) 2025 One Identity LLC
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+import json
+from os import path, unlink
+from queue import SimpleQueue, Full, Empty
+from uuid import uuid4
+
+from .compressable_file_buffer import CompressableFileBuffer
+
+from pathlib import Path
+from threading import Lock
+from typing import Any, Dict, Callable
+from logging import getLogger
+
+
+class S3ObjectBuffer:
+
+    logger = getLogger("S3.ObjectBuffer.S3ObjectBuffer")
+
+    def __init__(self, working_dir: Path, object_key: str, target_index: int, target_timestamp: str, creator: str, object_settings: Dict[str, Any]) -> None:
+        self.__buffer_lock = Lock()
+        self.__meta = {
+            "creator": creator,
+            "object_key": object_key,
+            "suffix": object_settings.get("suffix", ""),
+            "target_index": target_index,
+            "target_timestamp": target_timestamp,
+            "storage_class": object_settings.get("storage_class", "STANDARD"),
+            "compression": object_settings.get("compression", False),
+            "compresslevel": object_settings.get("compresslevel", 0),
+            "max_size": object_settings.get("max_object_size", None),
+            "acl": object_settings.get("canned_acl", None)
+        }
+        self.__basename = f"{object_key}_{uuid4()}"
+        self.__path = Path(working_dir, self.__basename)
+        self.__meta_path = Path(working_dir, self.__basename + "_meta.json")
+        self.retries = 0
+        self.buffer = None
+        self.__buffer_source = "pending"
+        self.__finished = False
+
+    def create_file(self):
+        self.buffer = CompressableFileBuffer(self.__path, self.__meta["compression"], self.__meta["compresslevel"])
+        with open(self.__meta_path, "w") as meta_file:
+            meta_file.write(json.dumps(self.__meta))
+        self.__buffer_source = "created"
+
+    def load_metadata(self, meta_path: Path) -> None:
+        self.__meta_path = meta_path
+        self.__path = Path(str(meta_path).removesuffix("_meta.json"))
+        with open(self.__meta_path, "r") as meta_file:
+            self.__meta = json.loads(meta_file.read())
+        self.buffer = CompressableFileBuffer(self.__path, self.__meta["compression"], self.__meta["compresslevel"])
+        self.__buffer_source = "loaded"
+
+    def finish(self):
+        with self.__buffer_lock:
+            self.__finish()
+
+    def __finish(self):
+        if not self.__finished:
+            self.buffer.finish()
+            self.buffer.close()
+            self.__finished = True
+
+    def write(self, msg: bytes) -> bool:
+        if self.__finished:
+            return False
+        with self.__buffer_lock:
+            if self.buffer is None:
+                self.create_file()
+            if not self.buffer.write(msg):
+                return False
+            if self.buffer.tell() >= self.__meta["max_size"]:
+                self.__finish()
+        return True
+
+    @property
+    def creator(self) -> str:
+        return self.__meta["creator"]
+
+    @property
+    def path(self) -> Path:
+        return self.__path
+
+    @property
+    def object_key(self) -> str:
+        return self.__meta["object_key"]
+
+    @property
+    def full_target_key(self) -> str:
+        if self.__meta["target_index"] == 0:
+            full_key = self.__meta["object_key"] + self.__meta["suffix"]
+        else:
+            full_key = f"{self.__meta['object_key']}-{self.__meta['target_index']}{self.__meta['suffix']}"
+        return full_key
+
+    @property
+    def target_timestamp(self) -> str:
+        return self.__meta["target_timestamp"]
+
+    @property
+    def last_modified(self):
+        return int(path.getmtime(self.__path))
+
+    @property
+    def buffer_created(self):
+        return self.__buffer_source
+
+    @property
+    def finished(self):
+        if self.buffer is None:
+            return False
+        else:
+            return self.__finished
+
+    def deprecate(self):
+        with self.__buffer_lock:
+            self.buffer.unlink()
+            unlink(self.__meta_path)
+
+    @property
+    def target_index(self):
+        return self.__meta["target_index"]
+
+    @property
+    def storage_class(self):
+        return self.__meta["storage_class"]
+
+    @property
+    def acl(self):
+        return self.__meta["acl"]
+
+
+class S3ObjectQueue:
+
+    logger = getLogger("S3.ObjectBuffer.S3ObjectQueue")
+
+    def __init__(self):
+        self.queue: SimpleQueue[S3ObjectBuffer] = SimpleQueue()
+
+    def enqueue(self, s3_object: S3ObjectBuffer, enqueue_cb: Callable[[], None] | None):
+        try:
+            self.queue.put(s3_object)
+        except Full:
+            self.logger.error("Object queue is full, could not enqueue.")
+            return
+        if enqueue_cb:
+            enqueue_cb()
+
+    def dequeue(self) -> S3ObjectBuffer | None:
+        try:
+            return self.queue.get_nowait()
+        except Empty:
+            return None
---
+++ modules/python-modules/syslogng/modules/s3/s3_session_handler.py
@@ -0,0 +1,225 @@
+#############################################################################
+# Copyright (c) 2025 One Identity LLC
+# Copyright (c) 2023 Attila Szakacs
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# As an additional exemption you are allowed to compile & link against the
+# OpenSSL libraries as published by the OpenSSL project. See the file
+# COPYING for details.
+#
+#############################################################################
+from boto3.s3.transfer import TransferConfig
+from boto3.session import Session
+from boto3 import client
+from botocore.credentials import DeferredRefreshableCredentials, create_assume_role_refresher
+from botocore.exceptions import ClientError, EndpointConnectionError
+
+from .s3_object_buffer import S3ObjectQueue, S3ObjectBuffer
+
+from concurrent.futures import ThreadPoolExecutor, Future, CancelledError
+from logging import getLogger
+from typing import Any, Dict, Set
+from threading import Lock
+import re
+
+
+class S3SessionHandler:
+
+    logger = getLogger("S3.SessionHandler")
+    __max_retries = 10
+
+    def __init__(self, max_concurrent_uploads: int, upload_threads: int, object_queue: S3ObjectQueue, bucket: str, role: str | None, s3_session_config: Dict[str, Any], s3_client_config: Dict[str, Any], s3_sse_config: Dict[str, Any], s3_transfer_config: TransferConfig) -> None:
+        self.__bucket = bucket
+        self.__role = role
+        self.__client_config = s3_client_config
+        if s3_sse_config.get("ServerSideEncryption", None) is not None:
+            self.__client_config.update(s3_sse_config)
+        self.__session = Session(**s3_session_config)
+        self.__sts = None
+        self.__client = None
+
+        # NOTE: The Session.set_credentials always creates a new Credentials object from the given keys.
+        # NOTE: The DeferredRefreshableCredentials class is a child of RefreshableCredentials which is a
+        # NOTE: child of the Credentials class.
+        # ALSO NOTE: This solution is ugly as hell. We should find a more elegant one.
+        if role:
+            self.__sts = client("sts")
+            self.__session._session._credentials = DeferredRefreshableCredentials(
+                refresh_using=create_assume_role_refresher(
+                    self.__sts,
+                    {"RoleArn": self.__role, "RoleSessionName": "syslog-ng"},
+                ),
+                method="sts-assume-role",
+            )
+            self.logger.info(f"Using {self.__sts.get_caller_identity().get('Arn')} for the current session.")
+
+        self.__transfer_config = s3_transfer_config
+        self.__object_queue = object_queue
+        self.__max_concurrent_uploads = max_concurrent_uploads
+        self.__upload_threads = upload_threads
+        self.__thread_pool = ThreadPoolExecutor(max_workers=max_concurrent_uploads)
+        self.__futures: set[Future] = set()
+        self.__future_lock = Lock()
+        self.__connection_lock = Lock()
+        self.__upload_enabled = True
+
+    def open_connection(self):
+        with self.__connection_lock:
+            self.__client = self.__session.client("s3", **self.__client_config)
+
+    def close_connection(self):
+        with self.__connection_lock:
+            self.__client.close()
+            self.__client = None
+
+    def disable_upload(self):
+        with self.__connection_lock:
+            self.__upload_enabled = False
+
+    def enable_upload(self):
+        with self.__connection_lock:
+            self.__upload_enabled = True
+
+    @property
+    def connection_open(self):
+        with self.__connection_lock:
+            return self.__client is not None
+
+    def ready_bucket(self):
+        try:
+            self.__client.head_bucket(Bucket=self.__bucket)
+            return True
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                self.logger.info(f"Bucket ({self.__bucket}) does not exist, trying to create it")
+            try:
+                self.__client.create_bucket(Bucket=self.__bucket)
+            except (ClientError, EndpointConnectionError) as e:
+                self.logger.error(f"Failed to create bucket ({self.__bucket}): {e}")
+                return False
+            self.logger.info(f"Bucket ({self.__bucket}) successfully created")
+            return True
+
+    def upload_object(self, s3_object: S3ObjectBuffer) -> bool:
+        if not self.connection_open:
+            self.logger.error(f"Connection closed. Establishing client connection.")
+            self.open_connection()
+        rc = False
+        if s3_object is None:
+            return rc
+        if not self.__upload_enabled:
+            return rc
+        try:
+            self.__client.upload_file(Filename=s3_object.path, Bucket=self.__bucket, Key=s3_object.full_target_key, Config=self.__transfer_config, ExtraArgs={"StorageClass": s3_object.storage_class, "ACL": s3_object.acl})
+            rc = True
+            self.logger.debug(f"Object {s3_object.full_target_key} successfully uploaded.")
+            s3_object.deprecate()
+        except ClientError as e:
+            self.logger.error(f"Could not upload object {s3_object.path}, retries: {s3_object.retries}/{self.__max_retries}. Reason: {e}")
+            s3_object.retries += 1
+            if s3_object.retries < self.__max_retries:
+                self.__object_queue.enqueue(s3_object, None)
+                return rc
+            self.logger.warning(f"Maximum numbers of retries exceeded for object {s3_object.path}")
+
+        return rc
+
+    def __upload_complete_cb(self, future: Future) -> None:
+        try:
+            future.result()
+        except ClientError as e:
+            self.logger.error(f"Could not upload object: {e}")
+        self.__futures.remove(future)
+
+    def upload_loop(self) -> None:
+        while True:
+            with self.__future_lock:
+                if len(self.__futures) < self.__max_concurrent_uploads:
+                    s3_object = self.__object_queue.dequeue()
+                    if s3_object is None:
+                        return
+                    self.logger.debug(f"Available executor found for uploading object: {s3_object.full_target_key}")
+                    future = self.__thread_pool.submit(self.upload_object, s3_object)
+                    future.add_done_callback(self.__upload_complete_cb)
+                    self.__futures.add(future)
+                else:
+                    return
+
+    def trigger_upload(self) -> None:
+        self.upload_loop()
+
+    def wait_for_queue_empty(self, timeout) -> bool:
+        with self.__connection_lock and self.__future_lock:
+            if len(self.__futures) == 0:
+                return True
+            for future in set(self.__futures):
+                try:
+                    future.result(timeout)
+                except TimeoutError:
+                    self.logger.warning("Object upload timed out.")
+                    if not future.cancel():
+                        self.logger.error("Timed out object upload could not be cancelled.")
+                except CancelledError:
+                    self.logger.warning("Object upload already cancelled.")
+                except Exception as e:
+                    self.logger.error(f"Future could not complete. Reason: {e}")
+            if len(self.__futures) == 0:
+                return True
+            else:
+                self.logger.error("Could not finish some upload futures. Uploads will be retried on next syslog-ng startup.")
+                return False
+
+    def get_top_index_for_key(self, key: str, suffix: str) -> int | None:
+        top_index = -1
+        objects: Set[str] = set()
+        pagination_options: Dict[str, str] = {}
+
+        while True:
+            try:
+                response: Dict[str, Any] = self.__client.list_objects(
+                    Bucket=self.__bucket,
+                    Prefix=key,
+                    **pagination_options,
+                )
+            except (ClientError, EndpointConnectionError) as e:
+                self.logger.error(f"Failed to list objects matching prefix: {self.__bucket}/{key} => {e}")
+                return None
+
+            try:
+                for obj in response.get("Contents", []):
+                    objects.add(obj["Key"])
+                if not response["IsTruncated"]:
+                    break
+                pagination_options = {
+                    "Marker": response["Contents"][-1]["Key"]
+                }
+            except KeyError:
+                self.logger.error(f"Failed to list objects: {self.__bucket}/{key} => Unexpected response: {response}")
+                return None
+
+        for s3_object in objects:
+            if s3_object == key + suffix:
+                key_index = 0
+            else:
+                key_match = re.search(rf"\d+{suffix}$", s3_object)
+                if key_match:
+                    key_index = int(key_match.group().removesuffix(suffix))
+                else:
+                    key_index = -1
+            if key_index >= top_index:
+                top_index = key_index
+
+        # NOTE: python3 does not have a max integer size, thus this should not result in an overflow error
+        return top_index if top_index >= 0 else None
