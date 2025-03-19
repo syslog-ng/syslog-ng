axoflow/axosyslog

utf8utils: make unsafe_chars a bitmap for flags
Instead of allowing a list of characters, just allow a few flags, making
the checking for invalid characters faster.

Signed-off-by: Balazs Scheidler <balazs.scheidler@axoflow.com>

modules/json/filterx-format-json.c
@@ -138,7 +138,7 @@ _format_and_append_string(const gchar *value, gssize value_len, GString *result)
 {
   _append_comma_if_needed(result);
   g_string_append_c(result, '"');
-  append_unsafe_utf8_as_escaped(result, value, value_len, "\"", "\\u%04x", "\\\\x%02x");
+  append_unsafe_utf8_as_escaped(result, value, value_len, AUTF8_UNSAFE_QUOTE, "\\u%04x", "\\\\x%02x");
   g_string_append_c(result, '"');
   return TRUE;
 }
@@ -155,7 +155,7 @@ _format_and_append_dict_elem(FilterXObject *key, FilterXObject *value, gpointer 
 
   _append_comma_if_needed(result);
   g_string_append_c(result, '"');
-  append_unsafe_utf8_as_escaped(result, key_str, key_str_len, "\"", "\\u%04x", "\\\\x%02x");
+  append_unsafe_utf8_as_escaped(result, key_str, key_str_len, AUTF8_UNSAFE_QUOTE, "\\u%04x", "\\\\x%02x");
   g_string_append(result, "\":");
 
   return _format_and_append_value(value, result);
@@ -211,7 +211,7 @@ _repr_append(FilterXObject *value, GString *result)
 
   _append_comma_if_needed(result);
   g_string_append_c(result, '"');
-  append_unsafe_utf8_as_escaped(result, repr->str, repr->len, "\"", "\\u%04x", "\\\\x%02x");
+  append_unsafe_utf8_as_escaped(result, repr->str, repr->len, AUTF8_UNSAFE_QUOTE, "\\u%04x", "\\\\x%02x");
   g_string_append_c(result, '"');
 
 exit:

syslog-ng/syslog-ng

Signed-off-by: Hofi <hofione@gmail.com>

modules/json/filterx-format-json.c
@@ -175,7 +175,7 @@ _format_and_append_string(FilterXObject *value, GString *result)
 
   _append_comma_if_needed(result);
   g_string_append_c(result, '"');
-  append_unsafe_utf8_as_escaped(result, string_value, string_value_len, "\"", "\\u%04x", "\\\\x%02x");
+  append_unsafe_utf8_as_escaped(result, string_value, string_value_len, AUTF8_UNSAFE_QUOTE, "\\u%04x", "\\\\x%02x");
   g_string_append_c(result, '"');
   return TRUE;
 }
@@ -192,7 +192,7 @@ _format_and_append_dict_elem(FilterXObject *key, FilterXObject *value, gpointer 
 
   _append_comma_if_needed(result);
   g_string_append_c(result, '"');
-  append_unsafe_utf8_as_escaped(result, key_str, key_str_len, "\"", "\\u%04x", "\\\\x%02x");
+  append_unsafe_utf8_as_escaped(result, key_str, key_str_len, AUTF8_UNSAFE_QUOTE, "\\u%04x", "\\\\x%02x");
   g_string_append(result, "\":");
 
   return _format_and_append_value(value, result);
@@ -248,7 +248,7 @@ _repr_append(FilterXObject *value, GString *result)
 
   _append_comma_if_needed(result);
   g_string_append_c(result, '"');
-  append_unsafe_utf8_as_escaped(result, repr->str, repr->len, "\"", "\\u%04x", "\\\\x%02x");
+  append_unsafe_utf8_as_escaped(result, repr->str, repr->len, AUTF8_UNSAFE_QUOTE, "\\u%04x", "\\\\x%02x");
   g_string_append_c(result, '"');
 
 exit: