axoflow/axosyslog

grpc: extends pubsub destination with protovar option
The protovar option in the Pub/Sub destination allows users to directly access the Protobuf byte representation created by a FilterX function on the source side.

The Protobuf serialized variable is used to set the Pub/Sub message's content.
This enables users to fully control and manage the outgoing messages of the Pub/Sub destination from within FilterX.
By leveraging this option, users can customize the message payload dynamically, based on their specific requirements.
The data() and attributes() options cannot be used with protovar(). These options are mutually exclusive.

Signed-off-by: shifter <shifter@axoflow.com>

modules/grpc/pubsub/pubsub-dest.hpp
@@ -62,6 +62,12 @@ public:
     this->data = log_template_ref(d);
   }
 
+  void set_protovar(LogTemplate *d)
+  {
+    log_template_unref(this->data);
+    this->protovar = log_template_ref(d);
+  }
+
   void add_attribute(const std::string &name, LogTemplate *value)
   {
     this->attributes.push_back(NameValueTemplatePair{name, value});
@@ -74,6 +80,8 @@ private:
   LogTemplate *project = nullptr;
   LogTemplate *topic = nullptr;
   LogTemplate *data = nullptr;
+  LogTemplate *protovar = nullptr;
+  LogTemplate *default_data_template = nullptr;
   std::vector<NameValueTemplatePair> attributes;
 };
 

syslog-ng/syslog-ng

Signed-off-by: Hofi <hofione@gmail.com>

modules/grpc/pubsub/pubsub-dest.hpp
@@ -62,6 +62,12 @@ public:
     this->data = log_template_ref(d);
   }
 
+  void set_protovar(LogTemplate *p)
+  {
+    log_template_unref(this->protovar);
+    this->protovar = log_template_ref(p);
+  }
+
   void add_attribute(const std::string &name, LogTemplate *value)
   {
     this->attributes.push_back(NameValueTemplatePair{name, value});
@@ -74,6 +80,8 @@ private:
   LogTemplate *project = nullptr;
   LogTemplate *topic = nullptr;
   LogTemplate *data = nullptr;
+  LogTemplate *protovar = nullptr;
+  LogTemplate *default_data_template = nullptr;
   std::vector<NameValueTemplatePair> attributes;
 };
 