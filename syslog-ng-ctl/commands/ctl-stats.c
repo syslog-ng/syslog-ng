e052c461b8c067edf71b86b0a5b3d3297cd684c9
Hofi <hofione@gmail.com>
2025-07-23 21:22:13 +0200

    syslog-ng-ctl: added handling of `without-orphaned-metrics` option
    Signed-off-by: Hofi <hofione@gmail.com>

--- syslog-ng-ctl/commands/ctl-stats.c
+++ syslog-ng-ctl/commands/ctl-stats.c
@@ -27,18 +27,19 @@
 static gboolean stats_options_reset_is_set = FALSE;
 static gboolean stats_options_remove_orphans = FALSE;
 static gboolean stats_options_legacy_metrics = FALSE;
+static gboolean stats_options_without_orphaned = FALSE;
 static gboolean stats_options_without_header = FALSE;
 static gchar *stats_options_out_format = NULL;
 static gchar **stats_legacy_commands = NULL;
 
-static const gchar *
+static GString *
 _stats_command_builder(GOptionContext *ctx)
 {
   if (stats_options_reset_is_set)
-    return "RESET_STATS";
+    return g_string_new("RESET_STATS");
 
   if (stats_options_remove_orphans)
-    return "REMOVE_ORPHANED_STATS";
+    return g_string_new("REMOVE_ORPHANED_STATS");
 
   /* NOTE: Yes, this is a mess, but it's a legacy mess, so we have to keep it currently to be backward compatible.
    *       Unfortunately, there's no optional command construct in the GLib Commandline Option Parser
@@ -75,33 +76,37 @@ _stats_command_builder(GOptionContext *ctx)
       return NULL;
     }
 
+  GString *stats_format = g_string_sized_new(64);
+  g_string_append(stats_format, "STATS");
   if ((stats_command == NULL && stats_options_out_format == NULL) ||
       command_is_csv || option_is_csv)
     {
       if (option_is_prometheus || command_is_prometheus || option_is_kv || command_is_kv)
         goto on_collision;
+      g_string_append(stats_format, " CSV");
       if (stats_options_without_header)
-        return "STATS CSV WITHOUT_HEADER";
-      else
-        return "STATS CSV";
+        g_string_append(stats_format, " WITHOUT_HEADER");
     }
   else if (command_is_kv || option_is_kv)
     {
       if (option_is_csv || command_is_csv || option_is_prometheus || command_is_prometheus)
         goto on_collision;
-      return "STATS KV";
+      g_string_append(stats_format, " KV");
     }
   else if (command_is_prometheus || option_is_prometheus)
     {
       if (option_is_csv || command_is_csv || option_is_kv || command_is_kv)
         goto on_collision;
+      g_string_append(stats_format, " PROMETHEUS");
       if (stats_options_legacy_metrics)
-        return "STATS PROMETHEUS WITH_LEGACY";
-      else
-        return "STATS PROMETHEUS";
+        g_string_append(stats_format, " WITH_LEGACY");
     }
+  if (stats_options_without_orphaned)
+    g_string_append(stats_format, " WITHOUT_ORPHANED");
+  return stats_format;
 
 on_collision:
+  g_string_free(stats_format, TRUE);
   g_option_context_set_description(ctx, "stats");
   fprintf(stderr,
           "Error parsing command line arguments: Colliding stats format recieved: %s <-> %s\nUsing the `stats csv|kv|stats prometheus` format is deprecated, please use the new `--%s csv|--%s kv|--%s prometheus` options instead\n\n",
@@ -113,11 +118,13 @@ on_collision:
 gint
 slng_stats(int argc, char *argv[], const gchar *mode, GOptionContext *ctx)
 {
-  const gchar *command = _stats_command_builder(ctx);
+  GString *command = _stats_command_builder(ctx);
   if (command == NULL)
     return EINVAL;
 
-  return dispatch_command(command);
+  gint result = dispatch_command(command->str);
+  g_string_free(command, TRUE);
+  return result;
 }
 
 GOptionEntry stats_options[] =
@@ -125,6 +132,7 @@ GOptionEntry stats_options[] =
   { "reset", 'r', 0, G_OPTION_ARG_NONE, &stats_options_reset_is_set, "reset counters", NULL },
   { "remove-orphans", 'o', 0, G_OPTION_ARG_NONE, &stats_options_remove_orphans, "remove orphaned statistics", NULL},
   { "with-legacy-metrics", 'l', 0, G_OPTION_ARG_NONE, &stats_options_legacy_metrics, "show legacy metrics (only for prometheus format)", NULL},
+  { "without-orphaned-metrics", 'a', 0, G_OPTION_ARG_NONE, &stats_options_without_orphaned, "do not show orphaned metrics", NULL},
   { "no-header", 'n', 0, G_OPTION_ARG_NONE, &stats_options_without_header, "do not print stats header", NULL},
   { "format", 'm', 0, G_OPTION_ARG_STRING, &stats_options_out_format, "output format, default is <csv>", "<csv|kv|prometheus>" },
   { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &stats_legacy_commands, NULL, NULL },
