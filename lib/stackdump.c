87d7bfd9c048758bedf40c4aaa5d878d3f789055
Hofi <hofione@gmail.com>
2025-09-30 13:49:29 +0200

    stackdump: Add a unique prefix to the REG_ helpers as it could cause name collision with defines in ucontext.h
    Signed-off-by: Hofi <hofione@gmail.com>

--- lib/stackdump.c
+++ lib/stackdump.c
@@ -145,140 +145,140 @@ _stackdump_print_backtrace(void)
 #endif // OS variants
 
 /*
- * Reguster accessor hepers by OS / Architecture
+ * Register accessor helpers by OS / Architecture
  */
 
 #if defined(__linux__)
 /****************************************************************************
  * Linux x86_64
  ****************************************************************************/
-# define REG_RAX (p->gregs[REG_RAX])
-# define REG_RBX (p->gregs[REG_RBX])
-# define REG_RCX (p->gregs[REG_RCX])
-# define REG_RDX (p->gregs[REG_RDX])
-# define REG_RSI (p->gregs[REG_RSI])
-# define REG_RDI (p->gregs[REG_RDI])
-# define REG_RBP (p->gregs[REG_RBP])
-# define REG_RSP (p->gregs[REG_RSP])
-# define REG_R8  (p->gregs[REG_R8])
-# define REG_R9  (p->gregs[REG_R9])
-# define REG_R10 (p->gregs[REG_R10])
-# define REG_R11 (p->gregs[REG_R11])
-# define REG_R12 (p->gregs[REG_R12])
-# define REG_R13 (p->gregs[REG_R13])
-# define REG_R14 (p->gregs[REG_R14])
-# define REG_R15 (p->gregs[REG_R15])
-# define REG_RIP (p->gregs[REG_RIP])
+# define SDUMP_REG_RAX (p->gregs[REG_RAX])
+# define SDUMP_REG_RBX (p->gregs[REG_RBX])
+# define SDUMP_REG_RCX (p->gregs[REG_RCX])
+# define SDUMP_REG_RDX (p->gregs[REG_RDX])
+# define SDUMP_REG_RSI (p->gregs[REG_RSI])
+# define SDUMP_REG_RDI (p->gregs[REG_RDI])
+# define SDUMP_REG_RBP (p->gregs[REG_RBP])
+# define SDUMP_REG_RSP (p->gregs[REG_RSP])
+# define SDUMP_REG_R8  (p->gregs[REG_R8])
+# define SDUMP_REG_R9  (p->gregs[REG_R9])
+# define SDUMP_REG_R10 (p->gregs[REG_R10])
+# define SDUMP_REG_R11 (p->gregs[REG_R11])
+# define SDUMP_REG_R12 (p->gregs[REG_R12])
+# define SDUMP_REG_R13 (p->gregs[REG_R13])
+# define SDUMP_REG_R14 (p->gregs[REG_R14])
+# define SDUMP_REG_R15 (p->gregs[REG_R15])
+# define SDUMP_REG_RIP (p->gregs[REG_RIP])
 /****************************************************************************
  * Linux arm64
  ****************************************************************************/
-# define REG_X(n)   (p->regs[n])
-# define REG_FP     (p->regs[29])
-# define REG_LR     (p->regs[30])
-# define REG_SP     (p->sp)
-# define REG_PC     (p->pc)
-# define REG_PSTATE (p->pstate)
-# define FMT_PSTATE "%016llx"
+# define SDUMP_REG_X(n)   (p->regs[n])
+# define SDUMP_REG_FP     (p->regs[29])
+# define SDUMP_REG_LR     (p->regs[30])
+# define SDUMP_REG_SP     (p->sp)
+# define SDUMP_REG_PC     (p->pc)
+# define SDUMP_REG_PSTATE (p->pstate)
+# define SDUMP_FMT_PSTATE "%016llx"
 /****************************************************************************
  * Linux i386
  ****************************************************************************/
-# define REG_EAX (p->gregs[REG_EAX])
-# define REG_EBX (p->gregs[REG_EBX])
-# define REG_ECX (p->gregs[REG_ECX])
-# define REG_EDX (p->gregs[REG_EDX])
-# define REG_ESI (p->gregs[REG_ESI])
-# define REG_EDI (p->gregs[REG_EDI])
-# define REG_EBP (p->gregs[REG_EBP])
-# define REG_ESP (p->gregs[REG_ESP])
-# define REG_EIP (p->gregs[REG_EIP])
+# define SDUMP_REG_EAX (p->gregs[REG_EAX])
+# define SDUMP_REG_EBX (p->gregs[REG_EBX])
+# define SDUMP_REG_ECX (p->gregs[REG_ECX])
+# define SDUMP_REG_EDX (p->gregs[REG_EDX])
+# define SDUMP_REG_ESI (p->gregs[REG_ESI])
+# define SDUMP_REG_EDI (p->gregs[REG_EDI])
+# define SDUMP_REG_EBP (p->gregs[REG_EBP])
+# define SDUMP_REG_ESP (p->gregs[REG_ESP])
+# define SDUMP_REG_EIP (p->gregs[REG_EIP])
 
 #elif defined(__APPLE__)
 /****************************************************************************
  * macOS x86_64
  ****************************************************************************/
-# define REG_RAX (p->__ss.__rax)
-# define REG_RBX (p->__ss.__rbx)
-# define REG_RCX (p->__ss.__rcx)
-# define REG_RDX (p->__ss.__rdx)
-# define REG_RSI (p->__ss.__rsi)
-# define REG_RDI (p->__ss.__rdi)
-# define REG_RBP (p->__ss.__rbp)
-# define REG_RSP (p->__ss.__rsp)
-# define REG_R8  (p->__ss.__r8)
-# define REG_R9  (p->__ss.__r9)
-# define REG_R10 (p->__ss.__r10)
-# define REG_R11 (p->__ss.__r11)
-# define REG_R12 (p->__ss.__r12)
-# define REG_R13 (p->__ss.__r13)
-# define REG_R14 (p->__ss.__r14)
-# define REG_R15 (p->__ss.__r15)
-# define REG_RIP (p->__ss.__rip)
+# define SDUMP_REG_RAX (p->__ss.__rax)
+# define SDUMP_REG_RBX (p->__ss.__rbx)
+# define SDUMP_REG_RCX (p->__ss.__rcx)
+# define SDUMP_REG_RDX (p->__ss.__rdx)
+# define SDUMP_REG_RSI (p->__ss.__rsi)
+# define SDUMP_REG_RDI (p->__ss.__rdi)
+# define SDUMP_REG_RBP (p->__ss.__rbp)
+# define SDUMP_REG_RSP (p->__ss.__rsp)
+# define SDUMP_REG_R8  (p->__ss.__r8)
+# define SDUMP_REG_R9  (p->__ss.__r9)
+# define SDUMP_REG_R10 (p->__ss.__r10)
+# define SDUMP_REG_R11 (p->__ss.__r11)
+# define SDUMP_REG_R12 (p->__ss.__r12)
+# define SDUMP_REG_R13 (p->__ss.__r13)
+# define SDUMP_REG_R14 (p->__ss.__r14)
+# define SDUMP_REG_R15 (p->__ss.__r15)
+# define SDUMP_REG_RIP (p->__ss.__rip)
 /****************************************************************************
  * macOS arm64
  ****************************************************************************/
-# define REG_X(n)   (p->__ss.__x[n])
-# define REG_FP     (p->__ss.__fp)
-# define REG_LR     (p->__ss.__lr)
-# define REG_SP     (p->__ss.__sp)
-# define REG_PC     (p->__ss.__pc)
-# define REG_PSTATE (p->__ss.__cpsr)
-# define FMT_PSTATE "%08x"
+# define SDUMP_REG_X(n)   (p->__ss.__x[n])
+# define SDUMP_REG_FP     (p->__ss.__fp)
+# define SDUMP_REG_LR     (p->__ss.__lr)
+# define SDUMP_REG_SP     (p->__ss.__sp)
+# define SDUMP_REG_PC     (p->__ss.__pc)
+# define SDUMP_REG_PSTATE (p->__ss.__cpsr)
+# define SDUMP_FMT_PSTATE "%08x"
 /****************************************************************************
  * macOS i386
  ****************************************************************************/
-# define REG_EAX (p->__ss.__eax)
-# define REG_EBX (p->__ss.__ebx)
-# define REG_ECX (p->__ss.__ecx)
-# define REG_EDX (p->__ss.__edx)
-# define REG_ESI (p->__ss.__esi)
-# define REG_EDI (p->__ss.__edi)
-# define REG_EBP (p->__ss.__ebp)
-# define REG_ESP (p->__ss.__esp)
-# define REG_EIP (p->__ss.__eip)
+# define SDUMP_REG_EAX (p->__ss.__eax)
+# define SDUMP_REG_EBX (p->__ss.__ebx)
+# define SDUMP_REG_ECX (p->__ss.__ecx)
+# define SDUMP_REG_EDX (p->__ss.__edx)
+# define SDUMP_REG_ESI (p->__ss.__esi)
+# define SDUMP_REG_EDI (p->__ss.__edi)
+# define SDUMP_REG_EBP (p->__ss.__ebp)
+# define SDUMP_REG_ESP (p->__ss.__esp)
+# define SDUMP_REG_EIP (p->__ss.__eip)
 
 #elif defined(__FreeBSD__)
 /****************************************************************************
  * FreeBSD x86_64
  ****************************************************************************/
-# define REG_RAX (p->mc_rax)
-# define REG_RBX (p->mc_rbx)
-# define REG_RCX (p->mc_rcx)
-# define REG_RDX (p->mc_rdx)
-# define REG_RSI (p->mc_rsi)
-# define REG_RDI (p->mc_rdi)
-# define REG_RBP (p->mc_rbp)
-# define REG_RSP (p->mc_rsp)
-# define REG_R8  (p->mc_r8)
-# define REG_R9  (p->mc_r9)
-# define REG_R10 (p->mc_r10)
-# define REG_R11 (p->mc_r11)
-# define REG_R12 (p->mc_r12)
-# define REG_R13 (p->mc_r13)
-# define REG_R14 (p->mc_r14)
-# define REG_R15 (p->mc_r15)
-# define REG_RIP (p->mc_rip)
+# define SDUMP_REG_RAX (p->mc_rax)
+# define SDUMP_REG_RBX (p->mc_rbx)
+# define SDUMP_REG_RCX (p->mc_rcx)
+# define SDUMP_REG_RDX (p->mc_rdx)
+# define SDUMP_REG_RSI (p->mc_rsi)
+# define SDUMP_REG_RDI (p->mc_rdi)
+# define SDUMP_REG_RBP (p->mc_rbp)
+# define SDUMP_REG_RSP (p->mc_rsp)
+# define SDUMP_REG_R8  (p->mc_r8)
+# define SDUMP_REG_R9  (p->mc_r9)
+# define SDUMP_REG_R10 (p->mc_r10)
+# define SDUMP_REG_R11 (p->mc_r11)
+# define SDUMP_REG_R12 (p->mc_r12)
+# define SDUMP_REG_R13 (p->mc_r13)
+# define SDUMP_REG_R14 (p->mc_r14)
+# define SDUMP_REG_R15 (p->mc_r15)
+# define SDUMP_REG_RIP (p->mc_rip)
 /****************************************************************************
  * FreeBSD arm64
  ****************************************************************************/
-# define REG_X(n)   (p->mc_gpregs.gp_x[n])
-# define REG_FP     (p->mc_gpregs.gp_x[29])
-# define REG_LR     (p->mc_gpregs.gp_lr)
-# define REG_SP     (p->mc_gpregs.gp_sp)
-# define REG_PC     (p->mc_gpregs.gp_elr)
-# define REG_PSTATE (p->mc_gpregs.gp_spsr)
-# define FMT_PSTATE "%08" PRIx32
+# define SDUMP_REG_X(n)   (p->mc_gpregs.gp_x[n])
+# define SDUMP_REG_FP     (p->mc_gpregs.gp_x[29])
+# define SDUMP_REG_LR     (p->mc_gpregs.gp_lr)
+# define SDUMP_REG_SP     (p->mc_gpregs.gp_sp)
+# define SDUMP_REG_PC     (p->mc_gpregs.gp_elr)
+# define SDUMP_REG_PSTATE (p->mc_gpregs.gp_spsr)
+# define SDUMP_FMT_PSTATE "%08" PRIx32
 /****************************************************************************
  * FreeBSD i386
  ****************************************************************************/
-# define REG_EAX (p->mc_eax)
-# define REG_EBX (p->mc_ebx)
-# define REG_ECX (p->mc_ecx)
-# define REG_EDX (p->mc_edx)
-# define REG_ESI (p->mc_esi)
-# define REG_EDI (p->mc_edi)
-# define REG_EBP (p->mc_ebp)
-# define REG_ESP (p->mc_esp)
-# define REG_EIP (p->mc_eip)
+# define SDUMP_REG_EAX (p->mc_eax)
+# define SDUMP_REG_EBX (p->mc_ebx)
+# define SDUMP_REG_ECX (p->mc_ecx)
+# define SDUMP_REG_EDX (p->mc_edx)
+# define SDUMP_REG_ESI (p->mc_esi)
+# define SDUMP_REG_EDI (p->mc_edi)
+# define SDUMP_REG_EBP (p->mc_ebp)
+# define SDUMP_REG_ESP (p->mc_esp)
+# define SDUMP_REG_EIP (p->mc_eip)
 
 #endif // #elif defined(__FreeBSD__)
 
@@ -292,17 +292,17 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
     "rsi=%016llx rdi=%016llx rbp=%016llx rsp=%016llx "
     "r8=%016llx r9=%016llx r10=%016llx r11=%016llx "
     "r12=%016llx r13=%016llx r14=%016llx r15=%016llx rip=%016llx",
-    (unsigned long long) REG_RAX, (unsigned long long) REG_RBX,
-    (unsigned long long) REG_RCX, (unsigned long long) REG_RDX,
-    (unsigned long long) REG_RSI, (unsigned long long) REG_RDI,
-    (unsigned long long) REG_RBP, (unsigned long long) REG_RSP,
-    (unsigned long long) REG_R8,  (unsigned long long) REG_R9,
-    (unsigned long long) REG_R10, (unsigned long long) REG_R11,
-    (unsigned long long) REG_R12, (unsigned long long) REG_R13,
-    (unsigned long long) REG_R14, (unsigned long long) REG_R15,
-    (unsigned long long) REG_RIP);
+    (unsigned long long) SDUMP_REG_RAX, (unsigned long long) SDUMP_REG_RBX,
+    (unsigned long long) SDUMP_REG_RCX, (unsigned long long) SDUMP_REG_RDX,
+    (unsigned long long) SDUMP_REG_RSI, (unsigned long long) SDUMP_REG_RDI,
+    (unsigned long long) SDUMP_REG_RBP, (unsigned long long) SDUMP_REG_RSP,
+    (unsigned long long) SDUMP_REG_R8,  (unsigned long long) SDUMP_REG_R9,
+    (unsigned long long) SDUMP_REG_R10, (unsigned long long) SDUMP_REG_R11,
+    (unsigned long long) SDUMP_REG_R12, (unsigned long long) SDUMP_REG_R13,
+    (unsigned long long) SDUMP_REG_R14, (unsigned long long) SDUMP_REG_R15,
+    (unsigned long long) SDUMP_REG_RIP);
 
-  _stackdump_print_stack((gpointer) REG_RSP);
+  _stackdump_print_stack((gpointer) SDUMP_REG_RSP);
 }
 
 #elif defined(__aarch64__) || defined(__arm64__) || defined(__arm64)
@@ -317,15 +317,15 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
     "x12=%016llx x13=%016llx x14=%016llx x15=%016llx x16=%016llx x17=%016llx "
     "x18=%016llx x19=%016llx x20=%016llx x21=%016llx x22=%016llx x23=%016llx "
     "x24=%016llx x25=%016llx x26=%016llx x27=%016llx x28=%016llx fp=%016llx "
-    "lr=%016llx sp=%016llx pc=%016llx pstate=" FMT_PSTATE,
-    REG_X(0), REG_X(1), REG_X(2), REG_X(3), REG_X(4), REG_X(5),
-    REG_X(6), REG_X(7), REG_X(8), REG_X(9), REG_X(10), REG_X(11),
-    REG_X(12), REG_X(13), REG_X(14), REG_X(15), REG_X(16), REG_X(17),
-    REG_X(18), REG_X(19), REG_X(20), REG_X(21), REG_X(22), REG_X(23),
-    REG_X(24), REG_X(25), REG_X(26), REG_X(27), REG_X(28), REG_FP,
-    REG_LR, REG_SP, REG_PC, REG_PSTATE);
+    "lr=%016llx sp=%016llx pc=%016llx pstate=" SDUMP_FMT_PSTATE,
+    SDUMP_REG_X(0), SDUMP_REG_X(1), SDUMP_REG_X(2), SDUMP_REG_X(3), SDUMP_REG_X(4), SDUMP_REG_X(5),
+    SDUMP_REG_X(6), SDUMP_REG_X(7), SDUMP_REG_X(8), SDUMP_REG_X(9), SDUMP_REG_X(10), SDUMP_REG_X(11),
+    SDUMP_REG_X(12), SDUMP_REG_X(13), SDUMP_REG_X(14), SDUMP_REG_X(15), SDUMP_REG_X(16), SDUMP_REG_X(17),
+    SDUMP_REG_X(18), SDUMP_REG_X(19), SDUMP_REG_X(20), SDUMP_REG_X(21), SDUMP_REG_X(22), SDUMP_REG_X(23),
+    SDUMP_REG_X(24), SDUMP_REG_X(25), SDUMP_REG_X(26), SDUMP_REG_X(27), SDUMP_REG_X(28), SDUMP_REG_FP,
+    SDUMP_REG_LR, SDUMP_REG_SP, SDUMP_REG_PC, SDUMP_REG_PSTATE);
 
-  _stackdump_print_stack((gpointer) REG_SP);
+  _stackdump_print_stack((gpointer) SDUMP_REG_SP);
 }
 
 #elif defined(__i386__) || defined(__i386) || defined(i386) || defined(__i686__)
@@ -336,13 +336,13 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
   console_printf(
     "Registers: eax=%08x ebx=%08x ecx=%08x edx=%08x "
     "esi=%08x edi=%08x ebp=%08x esp=%08x eip=%08x",
-    (unsigned int) REG_EAX, (unsigned int) REG_EBX,
-    (unsigned int) REG_ECX, (unsigned int) REG_EDX,
-    (unsigned int) REG_ESI, (unsigned int) REG_EDI,
-    (unsigned int) REG_EBP, (unsigned int) REG_ESP,
-    (unsigned int) REG_EIP);
+    (unsigned int) SDUMP_REG_EAX, (unsigned int) SDUMP_REG_EBX,
+    (unsigned int) SDUMP_REG_ECX, (unsigned int) SDUMP_REG_EDX,
+    (unsigned int) SDUMP_REG_ESI, (unsigned int) SDUMP_REG_EDI,
+    (unsigned int) SDUMP_REG_EBP, (unsigned int) SDUMP_REG_ESP,
+    (unsigned int) SDUMP_REG_EIP);
 
-  _stackdump_print_stack((gpointer) REG_ESP);
+  _stackdump_print_stack((gpointer) SDUMP_REG_ESP);
 }
 
 #else
