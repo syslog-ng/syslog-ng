3d83df8628c98f111cb46d4ae5207b73698dcc41
Hofi <hofione@gmail.com>
2025-09-30 11:23:49 +0200

    stackdump: use register accessor macros instead of code repetitions
    Signed-off-by: Hofi <hofione@gmail.com>

--- lib/stackdump.c
+++ lib/stackdump.c
@@ -100,72 +100,6 @@ _stackdump_print_backtrace(void)
     }
 }
 
-#ifdef __x86_64__
-/****************************************************************************
- * x86_64 support
- ****************************************************************************/
-void
-_stackdump_print_registers(_STRUCT_MCONTEXT *p)
-{
-  console_printf(
-    "Registers: "
-    "rax=%016lx rbx=%016lx rcx=%016lx rdx=%016lx rsi=%016lx rdi=%016lx "
-    "rbp=%016lx rsp=%016lx r8=%016lx r9=%016lx r10=%016lx r11=%016lx "
-    "r12=%016lx r13=%016lx r14=%016lx r15=%016lx rip=%016lx",
-    p->rax, p->rbx, p->rcx, p->rdx, p->rsi, p->rdi, p->rbp, p->rsp, p->r8, p->r9, p->r10, p->r11, p->r12, p->r13, p->r14,
-    p->r15, p->rip);
-
-  _stackdump_print_stack((gpointer) p->rsp);
-}
-
-#elif defined(__aarch64__)
-/****************************************************************************
- * arm_64 support
- ****************************************************************************/
-
-void
-_stackdump_print_registers(_STRUCT_MCONTEXT *p)
-{
-  console_printf(
-    "Registers: "
-    "x0=%016llx x1=%016llx x2=%016llx x3=%016llx x4=%016llx x5=%016llx "
-    "x6=%016llx x7=%016llx x8=%016llx x9=%016llx x10=%016llx x11=%016llx "
-    "x12=%016llx x13=%016llx x14=%016llx x15=%016llx x16=%016llx x17=%016llx "
-    "x18=%016llx x19=%016llx x20=%016llx x21=%016llx x22=%016llx x23=%016llx "
-    "x24=%016llx x25=%016llx x26=%016llx x27=%016llx x28=%016llx fp=%016llx "
-    "lr=%016llx sp=%016llx pc=%016llx pstate=%016llx",
-    p->regs[0], p->regs[1], p->regs[2], p->regs[3], p->regs[4], p->regs[5],
-    p->regs[6], p->regs[7], p->regs[8], p->regs[9], p->regs[10], p->regs[11],
-    p->regs[12], p->regs[13], p->regs[14], p->regs[15], p->regs[16], p->regs[17],
-    p->regs[18], p->regs[19], p->regs[20], p->regs[21], p->regs[22], p->regs[23],
-    p->regs[24], p->regs[25], p->regs[26], p->regs[27], p->regs[28], p->regs[29],
-    p->regs[30], p->sp, p->pc, p->pstate);
-
-  _stackdump_print_stack((gpointer) p->sp);
-}
-
-#elif defined(__x86__)
-/****************************************************************************
- * i386 support
- ****************************************************************************/
-
-void
-_stackdump_print_registers(_STRUCT_MCONTEXT *p)
-{
-  console_printf(
-    "Registers: eax=%08lx ebx=%08lx ecx=%08lx edx=%08lx esi=%08lx edi=%08lx ebp=%08lx esp=%08lx eip=%08lx",
-    p->eax, p->ebx, p->ecx, p->edx, p->esi, p->edi, p->ebp, p->esp, p->eip);
-
-  _stackdump_print_stack((gpointer) p->esp);
-}
-
-#else
-/****************************************************************************
- * unsupported platform
- ****************************************************************************/
-#error "This is an unsupported architecture for stackdump currenty :("
-#endif
-
 #elif defined(__APPLE__) || defined(__FreeBSD__)
 /****************************************************************************
  * macOS / FreeBSD
@@ -203,15 +137,152 @@ _stackdump_print_backtrace(void)
     }
 }
 
-#if defined(__APPLE__)
+#else // #elif defined(__APPLE__) || defined(__FreeBSD__)
 /****************************************************************************
- * macOS
+ * unsupported OS
  ****************************************************************************/
+#error "This is an unsupported OS for stackdump currenty :("
+#endif // OS variants
 
-#ifdef __x86_64__
+/*
+ * Reguster accessor hepers by OS / Architecture
+ */
+
+#if defined(__linux__)
 /****************************************************************************
- * x86_64 support
+ * Linux x86_64
  ****************************************************************************/
+# define REG_RAX (p->gregs[REG_RAX])
+# define REG_RBX (p->gregs[REG_RBX])
+# define REG_RCX (p->gregs[REG_RCX])
+# define REG_RDX (p->gregs[REG_RDX])
+# define REG_RSI (p->gregs[REG_RSI])
+# define REG_RDI (p->gregs[REG_RDI])
+# define REG_RBP (p->gregs[REG_RBP])
+# define REG_RSP (p->gregs[REG_RSP])
+# define REG_R8  (p->gregs[REG_R8])
+# define REG_R9  (p->gregs[REG_R9])
+# define REG_R10 (p->gregs[REG_R10])
+# define REG_R11 (p->gregs[REG_R11])
+# define REG_R12 (p->gregs[REG_R12])
+# define REG_R13 (p->gregs[REG_R13])
+# define REG_R14 (p->gregs[REG_R14])
+# define REG_R15 (p->gregs[REG_R15])
+# define REG_RIP (p->gregs[REG_RIP])
+/****************************************************************************
+ * Linux arm64
+ ****************************************************************************/
+# define REG_X(n)   (p->regs[n])
+# define REG_FP     (p->regs[29])
+# define REG_LR     (p->regs[30])
+# define REG_SP     (p->sp)
+# define REG_PC     (p->pc)
+# define REG_PSTATE (p->pstate)
+# define FMT_PSTATE "%016llx"
+/****************************************************************************
+ * Linux i386
+ ****************************************************************************/
+# define REG_EAX (p->gregs[REG_EAX])
+# define REG_EBX (p->gregs[REG_EBX])
+# define REG_ECX (p->gregs[REG_ECX])
+# define REG_EDX (p->gregs[REG_EDX])
+# define REG_ESI (p->gregs[REG_ESI])
+# define REG_EDI (p->gregs[REG_EDI])
+# define REG_EBP (p->gregs[REG_EBP])
+# define REG_ESP (p->gregs[REG_ESP])
+# define REG_EIP (p->gregs[REG_EIP])
+
+#elif defined(__APPLE__)
+/****************************************************************************
+ * macOS x86_64
+ ****************************************************************************/
+# define REG_RAX (p->__ss.__rax)
+# define REG_RBX (p->__ss.__rbx)
+# define REG_RCX (p->__ss.__rcx)
+# define REG_RDX (p->__ss.__rdx)
+# define REG_RSI (p->__ss.__rsi)
+# define REG_RDI (p->__ss.__rdi)
+# define REG_RBP (p->__ss.__rbp)
+# define REG_RSP (p->__ss.__rsp)
+# define REG_R8  (p->__ss.__r8)
+# define REG_R9  (p->__ss.__r9)
+# define REG_R10 (p->__ss.__r10)
+# define REG_R11 (p->__ss.__r11)
+# define REG_R12 (p->__ss.__r12)
+# define REG_R13 (p->__ss.__r13)
+# define REG_R14 (p->__ss.__r14)
+# define REG_R15 (p->__ss.__r15)
+# define REG_RIP (p->__ss.__rip)
+/****************************************************************************
+ * macOS arm64
+ ****************************************************************************/
+# define REG_X(n)   (p->__ss.__x[n])
+# define REG_FP     (p->__ss.__fp)
+# define REG_LR     (p->__ss.__lr)
+# define REG_SP     (p->__ss.__sp)
+# define REG_PC     (p->__ss.__pc)
+# define REG_PSTATE (p->__ss.__cpsr)
+# define FMT_PSTATE "%08x"
+/****************************************************************************
+ * macOS i386
+ ****************************************************************************/
+# define REG_EAX (p->__ss.__eax)
+# define REG_EBX (p->__ss.__ebx)
+# define REG_ECX (p->__ss.__ecx)
+# define REG_EDX (p->__ss.__edx)
+# define REG_ESI (p->__ss.__esi)
+# define REG_EDI (p->__ss.__edi)
+# define REG_EBP (p->__ss.__ebp)
+# define REG_ESP (p->__ss.__esp)
+# define REG_EIP (p->__ss.__eip)
+
+#elif defined(__FreeBSD__)
+/****************************************************************************
+ * FreeBSD x86_64
+ ****************************************************************************/
+# define REG_RAX (p->mc_rax)
+# define REG_RBX (p->mc_rbx)
+# define REG_RCX (p->mc_rcx)
+# define REG_RDX (p->mc_rdx)
+# define REG_RSI (p->mc_rsi)
+# define REG_RDI (p->mc_rdi)
+# define REG_RBP (p->mc_rbp)
+# define REG_RSP (p->mc_rsp)
+# define REG_R8  (p->mc_r8)
+# define REG_R9  (p->mc_r9)
+# define REG_R10 (p->mc_r10)
+# define REG_R11 (p->mc_r11)
+# define REG_R12 (p->mc_r12)
+# define REG_R13 (p->mc_r13)
+# define REG_R14 (p->mc_r14)
+# define REG_R15 (p->mc_r15)
+# define REG_RIP (p->mc_rip)
+/****************************************************************************
+ * FreeBSD arm64
+ ****************************************************************************/
+# define REG_X(n)   (p->mc_gpregs.gp_x[n])
+# define REG_FP     (p->mc_gpregs.gp_x[29])
+# define REG_LR     (p->mc_gpregs.gp_lr)
+# define REG_SP     (p->mc_gpregs.gp_sp)
+# define REG_PC     (p->mc_gpregs.gp_elr)
+# define REG_PSTATE (p->mc_gpregs.gp_spsr)
+# define FMT_PSTATE "%08" PRIx32
+/****************************************************************************
+ * FreeBSD i386
+ ****************************************************************************/
+# define REG_EAX (p->mc_eax)
+# define REG_EBX (p->mc_ebx)
+# define REG_ECX (p->mc_ecx)
+# define REG_EDX (p->mc_edx)
+# define REG_ESI (p->mc_esi)
+# define REG_EDI (p->mc_edi)
+# define REG_EBP (p->mc_ebp)
+# define REG_ESP (p->mc_esp)
+# define REG_EIP (p->mc_eip)
+
+#endif // #elif defined(__FreeBSD__)
+
+#if defined(__x86_64__) || defined(__x86_64) || defined(__amd64__) || defined(__amd64)
 
 void
 _stackdump_print_registers(_STRUCT_MCONTEXT *p)
@@ -221,19 +292,20 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
     "rsi=%016llx rdi=%016llx rbp=%016llx rsp=%016llx "
     "r8=%016llx r9=%016llx r10=%016llx r11=%016llx "
     "r12=%016llx r13=%016llx r14=%016llx r15=%016llx rip=%016llx",
-    p->__ss.__rax, p->__ss.__rbx, p->__ss.__rcx, p->__ss.__rdx,
-    p->__ss.__rsi, p->__ss.__rdi, p->__ss.__rbp, p->__ss.__rsp,
-    p->__ss.__r8, p->__ss.__r9, p->__ss.__r10, p->__ss.__r11,
-    p->__ss.__r12, p->__ss.__r13, p->__ss.__r14, p->__ss.__r15,
-    p->__ss.__rip);
+    (unsigned long long) REG_RAX, (unsigned long long) REG_RBX,
+    (unsigned long long) REG_RCX, (unsigned long long) REG_RDX,
+    (unsigned long long) REG_RSI, (unsigned long long) REG_RDI,
+    (unsigned long long) REG_RBP, (unsigned long long) REG_RSP,
+    (unsigned long long) REG_R8,  (unsigned long long) REG_R9,
+    (unsigned long long) REG_R10, (unsigned long long) REG_R11,
+    (unsigned long long) REG_R12, (unsigned long long) REG_R13,
+    (unsigned long long) REG_R14, (unsigned long long) REG_R15,
+    (unsigned long long) REG_RIP);
 
-  _stackdump_print_stack((gpointer)p->__ss.__rsp);
+  _stackdump_print_stack((gpointer) REG_RSP);
 }
 
-#elif defined(__aarch64__)
-/****************************************************************************
- * arm_64 support
- ****************************************************************************/
+#elif defined(__aarch64__) || defined(__arm64__) || defined(__arm64)
 
 void
 _stackdump_print_registers(_STRUCT_MCONTEXT *p)
@@ -245,24 +317,18 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
     "x12=%016llx x13=%016llx x14=%016llx x15=%016llx x16=%016llx x17=%016llx "
     "x18=%016llx x19=%016llx x20=%016llx x21=%016llx x22=%016llx x23=%016llx "
     "x24=%016llx x25=%016llx x26=%016llx x27=%016llx x28=%016llx fp=%016llx "
-    "lr=%016llx sp=%016llx pc=%016llx cpsr=%08x",
-    p->__ss.__x[0], p->__ss.__x[1], p->__ss.__x[2], p->__ss.__x[3],
-    p->__ss.__x[4], p->__ss.__x[5], p->__ss.__x[6], p->__ss.__x[7],
-    p->__ss.__x[8], p->__ss.__x[9], p->__ss.__x[10], p->__ss.__x[11],
-    p->__ss.__x[12], p->__ss.__x[13], p->__ss.__x[14], p->__ss.__x[15],
-    p->__ss.__x[16], p->__ss.__x[17], p->__ss.__x[18], p->__ss.__x[19],
-    p->__ss.__x[20], p->__ss.__x[21], p->__ss.__x[22], p->__ss.__x[23],
-    p->__ss.__x[24], p->__ss.__x[25], p->__ss.__x[26], p->__ss.__x[27],
-    p->__ss.__x[28], p->__ss.__fp, p->__ss.__lr, p->__ss.__sp,
-    p->__ss.__pc, p->__ss.__cpsr);
+    "lr=%016llx sp=%016llx pc=%016llx pstate=" FMT_PSTATE,
+    REG_X(0), REG_X(1), REG_X(2), REG_X(3), REG_X(4), REG_X(5),
+    REG_X(6), REG_X(7), REG_X(8), REG_X(9), REG_X(10), REG_X(11),
+    REG_X(12), REG_X(13), REG_X(14), REG_X(15), REG_X(16), REG_X(17),
+    REG_X(18), REG_X(19), REG_X(20), REG_X(21), REG_X(22), REG_X(23),
+    REG_X(24), REG_X(25), REG_X(26), REG_X(27), REG_X(28), REG_FP,
+    REG_LR, REG_SP, REG_PC, REG_PSTATE);
 
-  _stackdump_print_stack((gpointer)p->__ss.__sp);
+  _stackdump_print_stack((gpointer) REG_SP);
 }
 
-#elif defined(__x86__)
-/****************************************************************************
- * i386 support
- ****************************************************************************/
+#elif defined(__i386__) || defined(__i386) || defined(i386) || defined(__i686__)
 
 void
 _stackdump_print_registers(_STRUCT_MCONTEXT *p)
@@ -270,10 +336,13 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
   console_printf(
     "Registers: eax=%08x ebx=%08x ecx=%08x edx=%08x "
     "esi=%08x edi=%08x ebp=%08x esp=%08x eip=%08x",
-    p->__ss.__eax, p->__ss.__ebx, p->__ss.__ecx, p->__ss.__edx,
-    p->__ss.__esi, p->__ss.__edi, p->__ss.__ebp, p->__ss.__esp, p->__ss.__eip);
+    (unsigned int) REG_EAX, (unsigned int) REG_EBX,
+    (unsigned int) REG_ECX, (unsigned int) REG_EDX,
+    (unsigned int) REG_ESI, (unsigned int) REG_EDI,
+    (unsigned int) REG_EBP, (unsigned int) REG_ESP,
+    (unsigned int) REG_EIP);
 
-  _stackdump_print_stack((gpointer)p->__ss.__esp);
+  _stackdump_print_stack((gpointer) REG_ESP);
 }
 
 #else
@@ -282,98 +351,6 @@ _stackdump_print_registers(_STRUCT_MCONTEXT *p)
  ****************************************************************************/
 #error "This is an unsupported architecture for stackdump currenty :("
 #endif
-
-#elif defined(__FreeBSD__)
-/****************************************************************************
- * FreeBSD
- ****************************************************************************/
-
-#ifdef __x86_64__
-/****************************************************************************
- * x86_64 support
- ****************************************************************************/
-
-void
-_stackdump_print_registers(_STRUCT_MCONTEXT *p)
-{
-  console_printf(
-    "Registers: rax=%016llx rbx=%016llx rcx=%016llx rdx=%016llx "
-    "rsi=%016llx rdi=%016llx rbp=%016llx rsp=%016llx "
-    "r8=%016llx r9=%016llx r10=%016llx r11=%016llx "
-    "r12=%016llx r13=%016llx r14=%016llx r15=%016llx rip=%016llx",
-    p->mc_rax, p->mc_rbx, p->mc_rcx, p->mc_rdx,
-    p->mc_rsi, p->mc_rdi, p->mc_rbp, p->mc_rsp,
-    p->mc_r8, p->mc_r9, p->mc_r10, p->mc_r11,
-    p->mc_r12, p->mc_r13, p->mc_r14, p->mc_r15,
-    p->mc_rip);
-
-  _stackdump_print_stack((gpointer)p->mc_rsp);
-}
-
-#elif __aarch64__
-/****************************************************************************
- * arm_64 support
- ****************************************************************************/
-
-void
-_stackdump_print_registers(_STRUCT_MCONTEXT *p)
-{
-  console_printf(
-    "Registers: "
-    "x0=%016" PRIx64 " x1=%016" PRIx64 " x2=%016" PRIx64 " x3=%016" PRIx64
-    " x4=%016" PRIx64 " x5=%016" PRIx64 " x6=%016" PRIx64 " x7=%016" PRIx64
-    " x8=%016" PRIx64 " x9=%016" PRIx64 " x10=%016" PRIx64 " x11=%016" PRIx64
-    " x12=%016" PRIx64 " x13=%016" PRIx64 " x14=%016" PRIx64 " x15=%016" PRIx64
-    " x16=%016" PRIx64 " x17=%016" PRIx64 " x18=%016" PRIx64 " x19=%016" PRIx64
-    " x20=%016" PRIx64 " x21=%016" PRIx64 " x22=%016" PRIx64 " x23=%016" PRIx64
-    " x24=%016" PRIx64 " x25=%016" PRIx64 " x26=%016" PRIx64 " x27=%016" PRIx64
-    " x28=%016" PRIx64 " x29=%016" PRIx64 " fp=%016" PRIx64 " lr=%016" PRIx64
-    " sp=%016" PRIx64 " pc=%016" PRIx64 " cpsr=%08" PRIx32,
-    p->mc_gpregs.gp_x[0], p->mc_gpregs.gp_x[1], p->mc_gpregs.gp_x[2], p->mc_gpregs.gp_x[3],
-    p->mc_gpregs.gp_x[4], p->mc_gpregs.gp_x[5], p->mc_gpregs.gp_x[6], p->mc_gpregs.gp_x[7],
-    p->mc_gpregs.gp_x[8], p->mc_gpregs.gp_x[9], p->mc_gpregs.gp_x[10], p->mc_gpregs.gp_x[11],
-    p->mc_gpregs.gp_x[12], p->mc_gpregs.gp_x[13], p->mc_gpregs.gp_x[14], p->mc_gpregs.gp_x[15],
-    p->mc_gpregs.gp_x[16], p->mc_gpregs.gp_x[17], p->mc_gpregs.gp_x[18], p->mc_gpregs.gp_x[19],
-    p->mc_gpregs.gp_x[20], p->mc_gpregs.gp_x[21], p->mc_gpregs.gp_x[22], p->mc_gpregs.gp_x[23],
-    p->mc_gpregs.gp_x[24], p->mc_gpregs.gp_x[25], p->mc_gpregs.gp_x[26], p->mc_gpregs.gp_x[27],
-    p->mc_gpregs.gp_x[28], p->mc_gpregs.gp_x[29], p->mc_gpregs.gp_lr,
-    p->mc_gpregs.gp_sp, p->mc_gpregs.gp_elr, p->mc_gpregs.gp_spsr);
-
-  _stackdump_print_stack((gpointer)p->mc_gpregs.gp_sp);
-}
-
-#elif defined(__x86__)
-/****************************************************************************
- * i386 support
- ****************************************************************************/
-
-void
-_stackdump_print_registers(_STRUCT_MCONTEXT *p)
-{
-  console_printf(
-    "Registers: eax=%08x ebx=%08x ecx=%08x edx=%08x "
-    "esi=%08x edi=%08x ebp=%08x esp=%08x eip=%08x",
-    p->mc_eax, p->mc_ebx, p->mc_ecx, p->mc_edx,
-    p->mc_esi, p->mc_edi, p->mc_ebp, p->mc_esp, p->mc_eip);
-
-  _stackdump_print_stack((gpointer)p->mc_esp);
-}
-
-#else
-/****************************************************************************
- * unsupported platform
- ****************************************************************************/
-#error "This is an unsupported architecture for stackdump currenty :("
-#endif
-
-#endif // #elif defined(__FreeBSD__)
-
-#else // #elif defined(__APPLE__) || defined(__FreeBSD__)
-/****************************************************************************
- * unsupported OS
- ****************************************************************************/
-#error "This is an unsupported OS for stackdump currenty :("
-#endif // OS variants
 
 static inline void
 _stackdump_log(_STRUCT_MCONTEXT *p)
