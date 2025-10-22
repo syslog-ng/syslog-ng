0f200c18aa7158b01959a58a1d8815599b6e172d
balint.zsiros@quest.com <balint.zsiros@quest.com>
2025-10-22 13:29:15 +0200

    lib/host-resolve: Win patch
    - Headers for windows way of dns handling added.
    - On Linux, res_init() re-reads /etc/resolv.conf. On Windows, getaddrinfo() uses the DNS Client service, which already tracks system DNS changes.
    
    Note: In case we must pick up DNS changes immediately after a reload signal, you may call DnsFlushResolverCache(), but that’s optional and slightly disruptive.
    
    Signed-off-by: Balint Gyorgy Zsiros <balint.zsiros@oneidentity.com>

--- lib/host-resolve.c
+++ lib/host-resolve.c
@@ -33,7 +33,13 @@
 #include <iv.h>
 
 #include <string.h>
+
+#ifndef _WIN32
 #include <resolv.h>
+#else
+#include <windns.h>     /* DnsQuery_*, DNS_RECORD, etc. (if you need raw DNS) */
+/* ws2tcpip is already reachable via compat/socket.h for getaddrinfo/getnameinfo */
+#endif
 
 #ifndef AI_V4MAPPED
 #define AI_V4MAPPED 0
@@ -475,7 +481,10 @@ host_resolve_options_destroy(HostResolveOptions *options)
 static void
 _reinit_resolver(gint type, gpointer user_data)
 {
+  /* Windows: no-op; DNS Client service updates automatically. DnsFlushResolverCache() for an immediate refresh can be used.*/
+#ifndef _WIN32
   res_init();
+#endif
 }
 
 void
