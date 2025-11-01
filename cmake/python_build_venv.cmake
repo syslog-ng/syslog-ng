587c362fcb299c73f2bd0ebfbaa26c5b9d351af0
Hofi <hofione@gmail.com>
2025-11-01 19:19:26 +0100

    cmake: add support for the missing PYTHON_PACKAGES_INSTALL option (autotools with-python-packages alternative)
    Signed-off-by: Hofi <hofione@gmail.com>

--- cmake/python_build_venv.cmake
+++ cmake/python_build_venv.cmake
@@ -1,17 +1,35 @@
+# --- Python package handling mode (venv / system / none) ---
+set(PYTHON_PACKAGES_INSTALL "venv" CACHE STRING "Python package installation mode (venv, system, none)")
 
-set(PYTHON_VENV_DIR "${PROJECT_BINARY_DIR}/venv")
+# Configure venv/system/none behaviour
+if(PYTHON_PACKAGES_INSTALL STREQUAL "none")
+    set(PYTHON_VENV_EXECUTABLE "${PYTHON_EXECUTABLE}")
+elseif(PYTHON_PACKAGES_INSTALL STREQUAL "system")
+    set(PYTHON_VENV_EXECUTABLE "${PYTHON_EXECUTABLE}")
+elseif(PYTHON_PACKAGES_INSTALL STREQUAL "venv")
+    set(PYTHON_VENV_DIR "${CMAKE_CURRENT_BINARY_DIR}/venv")
+    set(PYTHON_VENV_EXECUTABLE "${PYTHON_VENV_DIR}/bin/python")
+else()
+    message(FATAL_ERROR "Unknown PYTHON_PACKAGES_INSTALL='${PYTHON_PACKAGES_INSTALL}', valid values are: venv, system, none")
+endif()
+
 set(PYTHON_VENV_TOUCHFILE "${PROJECT_BINARY_DIR}/.python-venv-built")
-set(PYTHON_VENV_EXECUTABLE "${PYTHON_VENV_DIR}/bin/python")
 
 add_custom_command(
     OUTPUT ${PYTHON_VENV_TOUCHFILE}
-    COMMAND ${PROJECT_SOURCE_DIR}/scripts/build-python-venv.sh "${PYTHON_EXECUTABLE}" "${PROJECT_BINARY_DIR}/venv" "${PROJECT_SOURCE_DIR}" && touch "${PYTHON_VENV_TOUCHFILE}"
+    COMMAND /bin/sh -c "\
+            if [ \"${PYTHON_PACKAGES_INSTALL}\" = \"venv\" ]; then \
+                '${PROJECT_SOURCE_DIR}/scripts/build-python-venv.sh' '${PYTHON_EXECUTABLE}' '${PROJECT_BINARY_DIR}/venv' '${PROJECT_SOURCE_DIR}'; \
+            fi; \
+            touch '${PYTHON_VENV_TOUCHFILE}'"
     VERBATIM
 )
 
-# the touchfile is cleaned up by cmake automatically, we only need to remove
-# the venv directory
-set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${PYTHON_VENV_DIR}")
+if(PYTHON_PACKAGES_INSTALL STREQUAL "venv")
+    # the touchfile is cleaned up by cmake automatically, we only need to remove
+    # the venv directory
+    set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${PYTHON_VENV_DIR}")
+endif()
 
 add_custom_target(BuildPyVirtualEnv
-                  DEPENDS ${PYTHON_VENV_TOUCHFILE})
+    DEPENDS ${PYTHON_VENV_TOUCHFILE})
