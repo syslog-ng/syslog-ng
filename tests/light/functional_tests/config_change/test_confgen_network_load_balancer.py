d311416d0f9f2a9a206af0952dc634c32bc8c5d4
Daniele Ferla <danieleferla1@gmail.com>
2025-11-09 16:34:57 +0100

    network-load-balancer: fix copyright and styling issues
    Signed-off-by: Daniele Ferla <14147983+egglessness@users.noreply.github.com>

--- tests/light/functional_tests/config_change/test_confgen_network_load_balancer.py
+++ tests/light/functional_tests/config_change/test_confgen_network_load_balancer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #############################################################################
-# Copyright (c) 2025 Daniele Ferla
+# Copyright (c) 2025 Balabit, Daniele Ferla
 #
 # This program is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 as published
