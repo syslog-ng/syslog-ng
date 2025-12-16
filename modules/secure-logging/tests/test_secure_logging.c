0e5d8ccb682c1367e2b3ed96576bd42134798981
Airbus Commercial Aircraft <secure-logging@airbus.com>
2025-12-16 10:51:22 +0100

    slog: cli test script activation for test of CI tool chain
    Set preprocessor RUN_CLI_SLOG_SMOKETEST set to 1
    
    Signed-off-by: Airbus Commercial Aircraft <secure-logging@airbus.com>

--- modules/secure-logging/tests/test_secure_logging.c
+++ modules/secure-logging/tests/test_secure_logging.c
@@ -784,19 +784,19 @@ void test_slog_performance(void)
 }
 
 
-#define RUN_CLI_SLOG_SMOKETEST 0
+#define RUN_CLI_SLOG_SMOKETEST 1
 
 /* Notes in regard RUN_CLI_SLOG_SMOKETEST
    *
    * - For the time being, RUN_CLI_SLOG_SMOKETEST is set to 0 to disable
    *   the test of cli tools in context of unit tests.
    *   Developers might set it to 1 manually here at least on GNU Linux systems.
+   *   When RUN_CLI_SLOG_SMOKETEST is set to 1, unit test will last longer.
    * - Currently the CI test do fail when set to 1. Reasons:
    *   -- chmod is used to make test scripts and helper scripts executable
    *      this causes in CI system a 'high severity security vulnerability'
    *   -- Expected 'sed' is not available for CI macOS version, but used
-   *      by update_conf_path.sh
-   *
+   *      by update_conf_path.sh.
    */
 
 
