6fe8bed108ff083e782afb473a01762a8eb3551a
Hofi <hofione@gmail.com>
2026-06-30 14:37:49 +0200

    http: only require worker-partition-key() for message-dependent URLs
    The init-time guard previously rejected any url() that contained a $
    sign when batching was enabled. That fired on SCL-wrapped destinations
    that only use $ via constant-arg template functions like
    `$(url-encode literal)`, even though the resulting URL is the same
    for every message and no partitioning would ever be needed.
    
    Split the guard into two independent checks:
    
    * flush_on_worker_key_change() is still enabled for any templated URL,
      preserving the existing flush semantics
    * worker-partition-key() is now only required when the URL is
      message-dependent (i.e. its rendering can vary between messages)
    
    Error message updated to refer to "message-dependent templates" so
    users searching for the new wording find the relaxed rule.
    
    Signed-off-by: Hofi <hofione@gmail.com>

--- modules/http/http.c
+++ modules/http/http.c
@@ -450,19 +450,20 @@ http_dd_init(LogPipe *s)
   if (!log_threaded_dest_driver_init_method(s))
     return FALSE;
 
-  if ((self->super.batch_lines || self->batch_bytes) && http_load_balancer_is_url_templated(self->load_balancer))
-    {
-      log_threaded_dest_driver_set_flush_on_worker_key_change(&self->super.super.super, TRUE);
+  gboolean batching = self->super.batch_lines || self->batch_bytes;
 
-      if (!self->super.worker_partition_key)
-        {
-          msg_error("http: worker-partition-key() must be set if using templates in the url() option "
-                    "while batching is enabled. "
-                    "Make sure to set worker-partition-key() with a template that contains all the templates "
-                    "used in the url() option",
-                    log_pipe_location_tag(&self->super.super.super.super));
-          return FALSE;
-        }
+  if (batching && http_load_balancer_is_url_templated(self->load_balancer))
+    log_threaded_dest_driver_set_flush_on_worker_key_change(&self->super.super.super, TRUE);
+
+  if (batching && http_load_balancer_is_url_message_dependent(self->load_balancer)
+      && !self->super.worker_partition_key)
+    {
+      msg_error("http: worker-partition-key() must be set if using message-dependent templates "
+                "in the url() option while batching is enabled. "
+                "Make sure to set worker-partition-key() with a template that contains all the "
+                "message-dependent templates used in the url() option",
+                log_pipe_location_tag(&self->super.super.super.super));
+      return FALSE;
     }
   if (self->batch_bytes > 0 && self->super.batch_lines == 0)
     self->super.batch_lines = G_MAXINT;
